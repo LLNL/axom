cdc9ed6b6c4ce0a57df99b8e78dcd47f787eaf83
Cyrus Harrison <cyrush@llnl.gov>
2015-03-17 20:33:18 -0700

    added cmake helper script

---
+++ host-configs/Darwin.cmake
@@ -0,0 +1,9 @@
+###############################################################################
+#
+# CMake Cache Seed file for naples (Cyrus' laptop)
+#
+###############################################################################
+
+# use clang compilers
+set(CMAKE_C_COMPILER "clang" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "clang++" CACHE PATH "")
---
+++ scripts/config-build.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+###############################################################################
+# file: config-build.sh
+#
+# description:
+#  Configures an out-of-source debug build of the toolkit, 
+#  optionally includes a initial cmake settings file.
+#
+###############################################################################
+
+#------------------------------------------------------------------------------
+#clean up existing build + install directories
+#------------------------------------------------------------------------------
+rm -rf build-debug install-debug
+
+#------------------------------------------------------------------------------
+# create a new build directory
+#------------------------------------------------------------------------------
+mkdir build-debug
+mkdir install-debug
+cd build-debug
+
+#------------------------------------------------------------------------------
+# setup desired basic cmake options
+#------------------------------------------------------------------------------
+export CMAKE_OPTS=" -DCMAKE_BUILD_TYPE=Debug"
+export CMAKE_OPTS="$CMAKE_OPTS -DCMAKE_INSTALL_PREFIX=../install-debug"
+
+#------------------------------------------------------------------------------
+# include an initial cmake settings file if appropriate
+#------------------------------------------------------------------------------
+
+# first look for a specific config for this machine
+export HOST_CONFIG=../host-configs/`hostname`.cmake
+echo "Looking for host-config file: $HOST_CONFIG"
+if [[ -e  "$HOST_CONFIG" ]]; then
+    echo "FOUND: $HOST_CONFIG"
+    export CMAKE_OPTS="$CMAKE_OPTS -C $HOST_CONFIG"
+# then check for a sys-type based config
+elif [[ "$SYS_TYPE" != "" ]]; then
+    export HOST_CONFIG=../host-configs/$SYS_TYPE.cmake
+    echo "Looking for SYS_TYPE based host-config file: $HOST_CONFIG"
+    if [[ -e  "$HOST_CONFIG" ]]; then
+        echo "FOUND: $HOST_CONFIG"
+        export CMAKE_OPTS="$CMAKE_OPTS -C $HOST_CONFIG"
+    fi
+else 
+    # fallback to simple a uname based config (Linux / Darwin / etc)
+    export HOST_CONFIG=../host-configs/`uname`.cmake
+    echo "Looking for uname based host-config file: $HOST_CONFIG"
+    if [[ -e  "$HOST_CONFIG" ]]; then
+        echo "FOUND: $HOST_CONFIG"
+        export CMAKE_OPTS="$CMAKE_OPTS -C $HOST_CONFIG"
+    fi
+fi
+
+#------------------------------------------------------------------------------
+# run cmake to configure
+#------------------------------------------------------------------------------
+echo "cmake $CMAKE_OPTS ../src"
+cmake  $CMAKE_OPTS \
+       ../src 
+# return to the starting dir
+cd ../
