14a90d8b067ae41943b36de7c510c9526d480c0d
Cyrus Harrison <cyrush@llnl.gov>
2015-03-19 15:16:51 -0700

    make example gtest and catch smoke uint tests pass

--- src/components/datastore/tests/testCatchExample.cxx
+++ src/components/datastore/tests/testCatchExample.cxx
@@ -2,16 +2,9 @@
 #include "catch.hpp"
 
 
-TEST_CASE( "This should fail") {
-
-  int one = 1;
-  REQUIRE( one == 2 );
-
-}
-
 //------------------------------------------------------------------------------
 
-TEST_CASE( "This should pass") {
+TEST_CASE( "This basic catch test should pass") {
 
   int one = 1;
   REQUIRE_FALSE( one == 2 );
