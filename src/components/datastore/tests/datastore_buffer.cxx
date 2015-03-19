67d04d351578a003df17933c4fe52dcd74101fe6
Cyrus Harrison <cyrush@llnl.gov>
2015-03-19 15:42:02 -0700

    simple data store buffer id stack reuse test

--- src/components/datastore/tests/datastore_buffer.cxx
+++ src/components/datastore/tests/datastore_buffer.cxx
@@ -9,12 +9,17 @@ using DataStoreNS::DataBuffer;
 
 //------------------------------------------------------------------------------
 
-TEST(datastore_smoke,create_datastore)
+TEST(datastore_buffer,create_buffers)
 {
     DataStore *ds = new DataStore();
-    DataBuffer *dbuff = ds->CreateDataBuffer();
-    EXPECT_EQ(dbuff->GetUID(),1);
+    DataBuffer *dbuff_0 = ds->CreateDataBuffer();
+    DataBuffer *dbuff_1 = ds->CreateDataBuffer();
     
-
-//    delete ds;
+    EXPECT_EQ(dbuff_0->GetUID(),0);
+    EXPECT_EQ(dbuff_1->GetUID(),1);
+    ds->DeleteDataBuffer(0);
+    
+    DataBuffer *dbuff_3 = ds->CreateDataBuffer();
+    EXPECT_EQ(dbuff_3->GetUID(),0);
+    delete ds;
 }
