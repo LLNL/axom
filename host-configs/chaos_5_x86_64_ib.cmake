72b36ae64949775daa998965cb2a9ea5db06742b
Cyrus Harrison <cyrush@llnl.gov>
2015-04-03 11:04:58 -0700

    add config for lc machines

---
+++ host-configs/chaos_5_x86_64_ib.cmake
@@ -0,0 +1,8 @@
+###############################################################################
+#
+# CMake Cache Seed file for chaos_5_x86_64_ib machines
+#
+###############################################################################
+
+set(CMAKE_C_COMPILER "/usr/apps/gnu/4.7.1/bin/gcc" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/usr/apps/gnu/4.7.1/bin/g++" CACHE PATH "")
