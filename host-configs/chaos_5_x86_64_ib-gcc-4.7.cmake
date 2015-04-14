a450071068b12951c53bb5708632a4f85b89a7f8
Cyrus Harrison <cyrush@llnl.gov>
2015-04-13 20:38:41 -0700

    made SYS_TYPE chaos_5_x86_64_ib.cmake, point to more specific chaos_5_x86_64_ib-gcc-4.7.cmake

---
+++ host-configs/chaos_5_x86_64_ib-gcc-4.7.cmake
@@ -0,0 +1,8 @@
+###############################################################################
+#
+# CMake Cache Seed file for chaos_5_x86_64_ib machines using gcc 4.7
+#
+###############################################################################
+
+set(CMAKE_C_COMPILER "/usr/apps/gnu/4.7.1/bin/gcc" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/usr/apps/gnu/4.7.1/bin/g++" CACHE PATH "")
