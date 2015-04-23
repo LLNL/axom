d5900f9f7e527a939dd5e6f9ca6e4beb3f9ab6d1
Kenneth Weiss <kweiss@llnl.gov>
2015-04-22 18:49:48 -0700

    Got meshapi to build under reorganized file structure. Added simple class (Hello) and test to ensure that build system is properly setup. Added component's name to the custom cmake target -- copy_headers_target, and updated meshapi and datastore cmake files.
    Conflicts:
    	src/components/meshapi/CMakeLists.txt

---
+++ src/components/meshapi/tests/testHello.cxx
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+
+#include "meshapi/hello.hpp"
+//------------------------------------------------------------------------------
+
+TEST(gtest_meshapi_hello,basic_assert_example)
+{
+    EXPECT_TRUE( true );
+}
+
+TEST(gtest_meshapi_hello,test_calling_hello)
+{
+    asctoolkit::meshapi::Hello h;
+    h.print();
+    EXPECT_TRUE( true );
+}
