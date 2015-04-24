10676016932af998d5214af09e6b6813d60bb42f
Kenneth Weiss <kweiss@llnl.gov>
2015-04-23 20:16:25 -0700

    Added some functions to API definition of Set with documentation

--- src/components/meshapi/src/Set.cpp
+++ src/components/meshapi/src/Set.cpp
@@ -5,16 +5,39 @@
  *      Author: weiss27
  */
 
+#include <stdexcept>
+#include <sstream>
 #include "Set.hpp"
 
-namespace asctoolkit
-{
-namespace meshapi
-{
+namespace asctoolkit {
+namespace meshapi {
 
+Set::Index& Set::at( size_type idx )
+{
+  if(idx > m_entities.size())
+  {
+    std::stringstream sstr;
+    sstr<< "MeshAPI::Set -- requested out of range element at position "
+        << idx << ", but set only has " << m_entities.size() << " elements.";
+    throw std::out_of_range(sstr.str());
+  }
+  return m_entities[idx];
+}
 
+Set::Index const& Set::at( size_type idx ) const
+{
+  if(idx > m_entities.size())
+  {
+    std::stringstream sstr;
+    sstr<< "MeshAPI::Set -- requested out of range element at position "
+        << idx << ", but set only has " << m_entities.size() << " elements.";
+    throw std::out_of_range(sstr.str());
+  }
+  return m_entities[idx];
+}
 
 
 
 } /* namespace meshapi */
 } /* namespace asctoolkit */
+
---
+++ src/components/meshapi/src/Utilities.hpp
@@ -0,0 +1,18 @@
+#ifndef MESHAPI_UTILITIES_H_
+#define MESHAPI_UTILITIES_H_
+
+
+namespace asctoolkit{
+namespace meshapi{
+
+
+  class NotImplementedException{};
+
+
+
+
+
+} // end namespace meshapi
+} // end namespace asctoolkit
+
+#endif //  MESHAPI_UTILITIES_H_
