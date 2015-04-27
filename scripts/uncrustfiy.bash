fde0dc10197817c2548231c9401c7c7cfed679a3
Randolph Settgast <settgast1@llnl.gov>
2015-04-27 12:20:32 -0700

    added uncrustify to TPL

--- scripts/uncrustfiy.bash
+++ scripts/uncrustfiy.bash
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-for file in $(find -E . -type f -regex ".*\.(hpp|cpp)" -follow -print0 | xargs -0); do
-    ~/Codes/uncrustify/src/uncrustify -c uncrustify.cfg --no-backup $file
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+UNCRUSTIFY_EXEC=$DIR/../src/TPL/uncrustify/src/uncrustify
+
+for file in $(find . -type f -regex ".*\.\(hpp\|cpp\)" -follow -print0 | xargs -0); do
+    $UNCRUSTIFY_EXEC -c uncrustify.cfg --no-backup $file
     echo $file
 done
 
