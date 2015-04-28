0a1a5e4d75e0839c12470c14da95b37e2f5f84a5
Randolph Settgast <settgast1@llnl.gov>
2015-04-28 11:43:41 -0700

    modified uncrustify source, and bash script

--- scripts/uncrustfiy.bash
+++ scripts/uncrustfiy.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-UNCRUSTIFY_EXEC=$DIR/../src/TPL/uncrustify/src/uncrustify
+#DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+#UNCRUSTIFY_EXEC=$DIR/../src/TPL/uncrustify/src/uncrustify
 
 for file in $(find . -type f -regex ".*\.\(hpp\|cpp\)" -follow -print0 | xargs -0); do
-    $UNCRUSTIFY_EXEC -c uncrustify.cfg --no-backup $file
+#    $UNCRUSTIFY_EXEC -c uncrustify.cfg --no-backup $file
+     $1 -c $2 --no-backup $file
     echo $file
 done
 
