14f9398fc0958a9e4eb214509b626ed892cab479
Cyrus Harrison <cyrush@llnl.gov>
2015-05-15 11:19:23 -0700

    update host-configs

--- host-configs/chaos_5_x86_64_ib-gcc-4.7.cmake
+++ host-configs/chaos_5_x86_64_ib-gcc-4.7.cmake
@@ -9,7 +9,22 @@
 ###############################################################################
 set(CMAKE_C_COMPILER "/usr/apps/gnu/4.7.1/bin/gcc" CACHE PATH "")
 set(CMAKE_CXX_COMPILER "/usr/apps/gnu/4.7.1/bin/g++" CACHE PATH "")
-set(SPHINX_EXECUTABLE "/usr/gapps/asctoolkit/tools/spack/opt/chaos_5_x86_64_ib/gcc\@4.4.7/python\@2.7.8-703c7a96/bin/sphinx-build" CACHE PATH "")
+
+#######
+# uberenv host-config for asctoolkit
+#######
+# cmake from uberenv
+# cmake exectuable path: /usr/gapps/asctoolkit/thirdparty_libs/spack/opt/chaos_5_x86_64_ib/gcc@4.4.7/cmake@3.2.2/bin/cmake
+
+# sphinx from uberenv
+set(SPHINX_EXECUTABLE "/usr/gapps/asctoolkit/thirdparty_libs/spack/opt/chaos_5_x86_64_ib/gcc@4.4.7/python@2.7.8/bin/sphinx-build" CACHE PATH "")
+
+# uncrustify from uberenv
+set(UNCRUSTIFY_EXECUTABLE "/usr/gapps/asctoolkit/thirdparty_libs/spack/opt/chaos_5_x86_64_ib/gcc@4.4.7/uncrustify@0.61/bin/uncrustify" CACHE PATH "")
+
+# boost headers from uberenv
+set(BOOST_ROOT "/usr/gapps/asctoolkit/thirdparty_libs/spack/opt/chaos_5_x86_64_ib/gcc@4.4.7/boost-headers@1.55.0" CACHE PATH "")
+
 
 ###############################################################################
 # Additional Compiler Flags
