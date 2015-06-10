71793559b5e68e50d787076998e601c6b2c2a2da
Cyrus Harrison <cyrush@llnl.gov>
2015-06-09 20:39:36 -0700

    update host-config for cyrus' laptop (naples.cmake)

--- host-configs/naples.cmake
+++ host-configs/naples.cmake
@@ -10,4 +10,24 @@
 set(CMAKE_C_COMPILER "clang" CACHE PATH "")
 set(CMAKE_CXX_COMPILER "clang++" CACHE PATH "")
 
-set(BOOST_ROOT "/Users/harrison37/Work/masonry/build-mb-2.9.1-darwin-10.9-x86_64/thirdparty_shared/visit/boost/1_57_0/darwin-x86_64/" CACHE PATH "")
+#######
+# uberenv host-config for asctoolkit
+#######
+# cmake from uberenv
+# cmake exectuable path: /Users/harrison37/Work/asctoolkit/uberenv_libs/spack/opt/macosx_10.9_x86_64/gcc@4.2.1/cmake@3.2.2/bin/cmake
+
+# python from uberenv
+set(PYTHON_EXECUTABLE "/Users/harrison37/Work/asctoolkit/uberenv_libs/spack/opt/macosx_10.9_x86_64/gcc@4.2.1/python@2.7.8/bin/python" CACHE PATH "")
+
+# sphinx from uberenv
+set(SPHINX_EXECUTABLE "/Users/harrison37/Work/asctoolkit/uberenv_libs/spack/opt/macosx_10.9_x86_64/gcc@4.2.1/python@2.7.8/bin/sphinx-build" CACHE PATH "")
+
+# uncrustify from uberenv
+set(UNCRUSTIFY_EXECUTABLE "/Users/harrison37/Work/asctoolkit/uberenv_libs/spack/opt/macosx_10.9_x86_64/gcc@4.2.1/uncrustify@0.61/bin/uncrustify" CACHE PATH "")
+
+# boost headers from uberenv
+set(ENABLE_BOOST ON CACHE PATH "")
+set(BOOST_ROOT "/Users/harrison37/Work/asctoolkit/uberenv_libs/spack/opt/macosx_10.9_x86_64/gcc@4.2.1/boost-headers@1.58.0" CACHE PATH "")
+
+
+
