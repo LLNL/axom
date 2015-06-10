b1176b2995d109a8a8ab3456b44b00860f3df0ad
Aaron Black <black27@llnl.gov>
2015-06-10 15:35:52 -0700

    Update BG/Q xlc host file, comment out python, etc.  Not there.

--- host-configs/bgqos_0-xlc.cmake
+++ host-configs/bgqos_0-xlc.cmake
@@ -17,16 +17,16 @@ set(CMAKE_CXX_COMPILER "bgxlc++_r" CACHE PATH "")
 # cmake exectuable path: /usr/gapps/asctoolkit/thirdparty_libs/spack/opt/bgqos_0/gcc@4.4.7/cmake@3.2.2/bin/cmake
 
 # python from uberenv
-set(PYTHON_EXECUTABLE "/usr/gapps/asctoolkit/thirdparty_libs/spack/opt/bgqos_0/gcc@4.4.7/python@2.7.8/bin/python" CACHE PATH "")
+#set(PYTHON_EXECUTABLE "/usr/gapps/asctoolkit/thirdparty_libs/spack/opt/bgqos_0/gcc@4.4.7/python@2.7.8/bin/python" CACHE PATH "")
 
 # sphinx from uberenv
-set(SPHINX_EXECUTABLE "/usr/gapps/asctoolkit/thirdparty_libs/spack/opt/bgqos_0/gcc@4.4.7/python@2.7.8/bin/sphinx-build" CACHE PATH "")
+#set(SPHINX_EXECUTABLE "/usr/gapps/asctoolkit/thirdparty_libs/spack/opt/bgqos_0/gcc@4.4.7/python@2.7.8/bin/sphinx-build" CACHE PATH "")
 
 # uncrustify from uberenv
-set(UNCRUSTIFY_EXECUTABLE "/usr/gapps/asctoolkit/thirdparty_libs/spack/opt/bgqos_0/gcc@4.4.7/uncrustify@0.61/bin/uncrustify" CACHE PATH "")
+#set(UNCRUSTIFY_EXECUTABLE "/usr/gapps/asctoolkit/thirdparty_libs/spack/opt/bgqos_0/gcc@4.4.7/uncrustify@0.61/bin/uncrustify" CACHE PATH "")
 
 # boost headers from uberenv
-set(BOOST_ROOT "/usr/gapps/asctoolkit/thirdparty_libs/spack/opt/bgqos_0/gcc@4.4.7/boost-headers@1.55.0" CACHE PATH "")
+#set(BOOST_ROOT "/usr/gapps/asctoolkit/thirdparty_libs/spack/opt/bgqos_0/gcc@4.4.7/boost-headers@1.55.0" CACHE PATH "")
 
 
 ###############################################################################
