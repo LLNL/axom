fbc38c39b4408f8f074081b1b56fc8bce979c044
Lee Taylor <ltaylor@llnl.gov>
2015-06-10 18:39:27 -0700

    Add classes to help cleanup use of dictionaries

--- src/components/common/shroud/src/util.py
+++ src/components/common/shroud/src/util.py
@@ -1,7 +1,10 @@
-#!/bin/env python3
+#!/bin/env python
+from __future__ import print_function
+
 
 import collections
 import string
+import json
 
 fmt = string.Formatter()
 
@@ -56,6 +59,77 @@ def update(d, u):
     return d
 
 
+class Argument(object):
+    """ Collect fields for an argument.
+    This used to be a dict but a class has better access semantics: i.attr vs d['attr']
+    It also initializes default values to avoid  d.get('attr', default)
+    """
+    # valid fields
+    fields = ['name', 'type', 'other']
+    defaults = dict(
+        type='dtype',
+        other='dother'
+        )
+
+    def __init__(self, name, **kw):
+        self.name = name
+#        for key, defvalue in self.defaults.items():
+#            setattr(self, key, defvalue)
+        self.__dict__.update(self.defaults) # set all default values
+        for key in kw:
+            if key in self.defaults:
+                setattr(self, key, kw[key])
+            else:
+                raise RuntimeError("Unknown key for Argument %s", key)
+
+    def _to_dict(self):
+        """Convert instance to a dictionary for json.
+        """
+        a = {}
+        for key in self.fields:
+            a[key] = getattr(self, key)
+        return a
+
+
+class ExpandedEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if hasattr(obj, '_to_dict'):
+            return obj._to_dict()
+         # Let the base class default method raise the TypeError
+        return json.JSONEncoder.default(self, obj)
+
+
+class Option(object):
+    """
+    If attribute is not found, look in parent's.
+    A replacement for a dictionary to allow obj.name syntax.
+    It will automatically look in __parent to attribute if not found to allow
+    A nesting of options.
+    Use __attr to avoid xporting to json
+    """
+    def __init__(self, parent, **kw):
+        self.__parent = parent
+        self.__hidden = 43
+        for key, value in kw.items():
+            setattr(self, key, value)
+
+    def __getattr__(self, name):
+        # we get here if the attribute does not exist in current instance
+        if self.__parent:
+            return getattr(self.__parent, name)
+        else:
+            raise AttributeError("%r object has no attribute %r" %
+                                 (self.__class__.__name__, name))
+
+    def _to_dict(self):
+        d = {}
+        skip = '_' + self.__class__.__name__ + '__'
+        for key, value in self.__dict__.items():
+            if not key.startswith(skip):
+                d[key] = value
+        return d
+
+
 
 if __name__ == '__main__':
     print(un_camel('incrementCount'))
@@ -71,3 +145,31 @@ if __name__ == '__main__':
 #    b.update(a)
     update(b, a)
     print("CCCC", b)
+
+
+    # Argument
+    print("Test Argument")
+    a = Argument('abc', type='def') #, bird='abcd')
+    print(json.dumps(a, cls=ExpandedEncoder, sort_keys=True))
+
+    # Option
+    print("Test Option")
+    lev0 = Option(None, a=1, b=2, c=3)
+    lev1 = Option(lev0, x=100, y=01, z=102)
+    lev0.c2 = 32
+    lev1.z2 = 103
+
+    print(lev0.a)
+    print(lev0.c2)
+    #print(lev0.z)
+    print(lev1.a)
+    print(lev1.z)
+    print(lev1.c2)
+    print(lev1.z2)
+    try:
+        print(lev1.nosuch)
+    except AttributeError:
+        print("Passed nosuch attribute")
+
+    print(json.dumps(lev0, cls=ExpandedEncoder, sort_keys=True))
+    print(json.dumps(lev1, cls=ExpandedEncoder, sort_keys=True))
