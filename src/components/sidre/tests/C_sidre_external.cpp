01c894d3cc5e930c653c1804244d18163f36de12
Lee Taylor <ltaylor@llnl.gov>
2015-06-15 14:50:37 -0700

    Convert to C

--- src/components/sidre/tests/C_sidre_external.cpp
+++ src/components/sidre/tests/C_sidre_external.cpp
@@ -8,30 +8,20 @@
  * further review from Lawrence Livermore National Laboratory.
  */
 
-#include "gtest/gtest.h"
-
-#include <vector>
-
-#include "sidre/sidre.hpp"
-
-using asctoolkit::sidre::DataBuffer;
-using asctoolkit::sidre::DataGroup;
-using asctoolkit::sidre::DataStore;
-using asctoolkit::sidre::DataView;
-
-using namespace conduit;
+#include <stdlib.h>
 
 //------------------------------------------------------------------------------
 // Test DataBuffer::declareExternal()
 //------------------------------------------------------------------------------
-TEST(sidre_external, declare_external_buffer)
+
+TEST(C_sidre_external, declare_external_buffer)
 {
-  DataStore * ds   = new DataStore();
+  ATK_datastore * ds = ATK_datastore_new();
 
   const int len = 11;
 
-  int * idata = new int[len];
-  double * ddata = new double[len];
+  int * idata = (int *) malloc(sizeof(int) * len);
+  double * ddata = (double *) malloc(sizeof(double) * len);
 
   for (int ii = 0 ; ii < len ; ++ii)
   {
@@ -39,44 +29,46 @@ TEST(sidre_external, declare_external_buffer)
     ddata[ii] = idata[ii] * 2.0;
   }
 
-  DataBuffer * dbuff_0 = ds->createBuffer();
-  DataBuffer * dbuff_1 = ds->createBuffer();
-  DataBuffer * dbuff_2 = ds->createBuffer();
+  ATK_databuffer * dbuff_0 ATK_datastore_create_buffer(ds);
+  ATK_databuffer * dbuff_1 ATK_datastore_create_buffer(ds);
+  ATK_databuffer * dbuff_2 ATK_datastore_create_buffer(ds);
 
-  dbuff_0->allocate(DataType::c_double(len));
-  dbuff_1->declareExternal(idata, DataType::c_int(len));
-  dbuff_2->declareExternal(ddata, DataType::c_double(len));
+  ATK_databuffer_allocate(dbuff_0, ATK_C_INT_T, len);
+  ATK_databuffer_declare_external(dbuff_1, idata, ATK_C_INT_T, len);
+  ATK_databuffer_declare_external(dbuff_2, ddata, ATK_C_DOUBLE_T, len);
 
-  EXPECT_EQ(dbuff_0->isExternal(), false);
-  EXPECT_EQ(dbuff_1->isExternal(), true);
-  EXPECT_EQ(dbuff_2->isExternal(), true);
+  EXPECT_EQ(ATK_databuffer_is_external(dbuff_0), false);
+  EXPECT_EQ(ATK_databuffer_is_external(dbuff_1), true);
+  EXPECT_EQ(ATK_databuffer_is_external(dbuff_2), true);
 
+#if 0
   EXPECT_EQ(dbuff_0->getNode().schema().total_bytes(),
             sizeof(CONDUIT_NATIVE_DOUBLE)*len);
   EXPECT_EQ(dbuff_1->getNode().schema().total_bytes(),
             sizeof(CONDUIT_NATIVE_INT)*len);
   EXPECT_EQ(dbuff_2->getNode().schema().total_bytes(),
             sizeof(CONDUIT_NATIVE_DOUBLE)*len);
+#endif
 
-  ds->print();
+  ATK_databuffer_print(ds);
 
-  delete ds;
-  delete [] idata;
-  delete [] ddata;
+  ATK_datastore_delete(ds);
+  free(idata);
+  free(ddata);
 }
 
 //------------------------------------------------------------------------------
-// Test DataGroup::createExternalView()
+// Test DataGroup::create_external_view()
 //------------------------------------------------------------------------------
-TEST(sidre_external, create_external_view)
+TEST(C_sidre_external, create_external_view)
 {
-  DataStore * ds   = new DataStore();
-  DataGroup * root = ds->getRoot();
+  ATK_datastore * ds = ATK_datastore_new();
+  ATK_datagroup * root = ATK_datastore_get_root(ds);
 
   const int len = 11;
 
-  int * idata = new int[len];
-  double * ddata = new double[len];
+  int * idata = (int *) malloc(sizeof(int) * len);
+  double * ddata = (double *) malloc(sizeof(double) * len);
 
   for (int ii = 0 ; ii < len ; ++ii)
   {
@@ -84,44 +76,44 @@ TEST(sidre_external, create_external_view)
     ddata[ii] = idata[ii] * 2.0;
   }
 
-  (void) root->createExternalView("idata", idata,
-                                  DataType::c_int(len));
-  (void) root->createExternalView("ddata", ddata,
-                                  DataType::c_double(len));
-  EXPECT_EQ(root->getNumViews(), 2u);
+  ATK_dataview *iview = ATK_datagroup_create_external_view(root, "idata", idata, ATK_C_INT_T, len);
+  ATK_dataview *dview = ATK_datagroup_create_external_view(root, "ddata", ddata, ATK_C_DOUBLE_T, len);
+  EXPECT_EQ(ATK_datagroup_get_num_views(root), 2u);
 
+#ifdef XXX
   root->getView("idata")->getNode().print_detailed();
   root->getView("ddata")->getNode().print_detailed();
+#endif
 
-  int * idata_chk = root->getView("idata")->getValue();
+  int * idata_chk = (int *) ATK_dataview_get_data_buffer(iview);
   for (int ii = 0 ; ii < len ; ++ii)
   {
     EXPECT_EQ(idata_chk[ii], idata[ii]);
   }
 
-  double * ddata_chk = root->getView("ddata")->getValue();
+  double * idata_chk = (double *) ATK_dataview_get_data_buffer(dview);
   for (int ii = 0 ; ii < len ; ++ii)
   {
     EXPECT_EQ(ddata_chk[ii], ddata[ii]);
   }
 
-  delete ds;
-  delete [] idata;
-  delete [] ddata;
+  ATK_datastore_delete(ds);
+  free(idata);
+  free(ddata);
 }
 
 //------------------------------------------------------------------------------
 // Test DataGroup::save(), DataGroup::load() with external buffers
 //------------------------------------------------------------------------------
-TEST(sidre_external, save_load_external_view)
+TEST(C_sidre_external, save_load_external_view)
 {
-  DataStore * ds   = new DataStore();
-  DataGroup * root = ds->getRoot();
+  ATK_datastore * ds = ATK_datastore_new();
+  ATK_datagroup * root = ATK_datastore_get_root(ds);
 
   const int len = 11;
 
-  int * idata = new int[len];
-  double * ddata = new double[len];
+  int * idata = (int *) malloc(sizeof(int) * len);
+  double * ddata = (double *) malloc(sizeof(double) * len);
 
   for (int ii = 0 ; ii < len ; ++ii)
   {
@@ -129,50 +121,60 @@ TEST(sidre_external, save_load_external_view)
     ddata[ii] = idata[ii] * 2.0;
   }
 
-  (void) root->createExternalView("idata", idata,
-                                  DataType::c_int(len));
-  (void) root->createExternalView("ddata", ddata,
-                                  DataType::c_double(len));
-  EXPECT_EQ(root->getNumViews(), 2u);
-  EXPECT_EQ(root->getView("idata")->getBuffer()->isExternal(), true);
-  EXPECT_EQ(root->getView("ddata")->getBuffer()->isExternal(), true);
+  ATK_dataview *iview = ATK_datagroup_create_external_view(root, "idata", idata, ATK_C_INT_T, len);
+  ATK_dataview *dview = ATK_datagroup_create_external_view(root, "ddata", ddata, ATK_C_DOUBLE_T, len);
+  EXPECT_EQ(ATK_datagroup_get_num_views(root), 2u);
+  ATK_databuffer *tmpbuf;
+  tmpbuf = ATK_dataview_get_buffer(iview)
+  EXPECT_EQ(ATK_databuffer_is_external(), true);
+  tmpbuf = ATK_dataview_get_buffer(dview)
+  EXPECT_EQ(ATK_databuffer_is_external(), true);
 
+#ifdef XXX
+  ATK_dataview *iview = ATK_datagroup_get_view(root, "idata");
+  ATK_dataview *dview = ATK_datagroup_get_view(root, "idata");
   root->getView("idata")->getNode().print_detailed();
   root->getView("ddata")->getNode().print_detailed();
+#endif
 
-  ds->getRoot()->save("out_sidre_external_save_restore_external_view", "conduit");
+  ATK_datagroup_save(root, "out_sidre_external_save_restore_external_view", "conduit");
 
-  ds->print();
+  ATK_databuffer_print(ds);
 
 
-  DataStore * ds2 = new DataStore();
+  ATK_datastore * ds2 = ATK_datastore_new();
+  ATK_datagroup * root2 = ATK_datastore_get_root(ds);
 
-  ds2->getRoot()->load("out_sidre_external_save_restore_external_view","conduit");
+  ATK_datagroup_load(root, "out_sidre_external_save_restore_external_view","conduit");
 
-  ds2->print();
+  ATK_datagroup_print(ds2);
 
-  DataGroup * root2 = ds2->getRoot();
+  ATK_dataview iview2 = ATK_datagroup_getView(root2, "idata");
+  ATK_dataview dview2 = ATK_datagroup_getView(root2, "ddata");
 
-  EXPECT_EQ(root2->getNumViews(), 2u);
-  EXPECT_EQ(root2->getView("idata")->getBuffer()->isExternal(), false);
-  EXPECT_EQ(root2->getView("ddata")->getBuffer()->isExternal(), false);
+  EXPECT_EQ(ATK_datagroup_get_num_views(root2), 2u);
+  ATK_databuffer tmpbuf;
+  tmpbuf = ATK_dataview_get_buffer(iview2);
+  EXPECT_EQ(ATK_databuffer_is_external(tmpbuf), false);
+  tmpbuf = ATK_dataview_get_buffer(dview2);
+  EXPECT_EQ(ATK_databuffer_is_external(tmpbuf), false);
 
-  int * idata_chk = root2->getView("idata")->getValue();
+  int * idata_chk = (int *) ATK_dataview_get_data_buffer(iview2);
   for (int ii = 0 ; ii < len ; ++ii)
   {
     EXPECT_EQ(idata_chk[ii], idata[ii]);
   }
 
-  double * ddata_chk = root2->getView("ddata")->getValue();
+  double * idata_chk = (double *) ATK_dataview_get_data_buffer(dview2);
   for (int ii = 0 ; ii < len ; ++ii)
   {
     EXPECT_EQ(ddata_chk[ii], ddata[ii]);
   }
 
-  delete ds;
-  delete ds2;
-  delete [] idata;
-  delete [] ddata;
+  ATK_datastore_delete(ds);
+  ATK_datastore_delete(ds2);
+  free(idata);
+  free(ddata);
 }
 
 //----------------------------------------------------------------------
