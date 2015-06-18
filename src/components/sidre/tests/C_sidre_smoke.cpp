bc5ec81520231b657c95cd9839c8116d04eac623
Rich Hornung <hornung1@llnl.gov>
2015-06-18 14:04:28 -0700

    Attempted to expose same tests that were turned of to check potential error conditions that were exposed in the C++ tests in the last commit.
    Not all tests can be run until some holes in the C API are filled.

--- src/components/sidre/tests/C_sidre_smoke.cpp
+++ src/components/sidre/tests/C_sidre_smoke.cpp
@@ -21,6 +21,32 @@ TEST(C_sidre_smoke,create_datastore)
   EXPECT_TRUE( true );
 }
 
+//------------------------------------------------------------------------------
+
+TEST(sidre_smoke,valid_invalid)
+{
+  ATK_datastore * ds = ATK_datastore_new();
+
+  ATK_IndexType idx = 3;
+  EXPECT_TRUE(idx != ATK_InvalidID);
+
+  const char name[] = "foo";
+#if 0 // C API needs some additions to make this work...
+  EXPECT_TRUE(ATK_isNameValid(name) == 0); 
+#endif
+
+  ATK_datagroup * root = ATK_datastore_get_root(ds);
+
+#if 0 // C API needs some additions to make this work...
+  const char * gp_name = ATK_datagroup_get_group_name(root, idx);
+  EXPECT_TRUE(gp_name == NULL);
+  EXPECT_TRUE(ATK_isNameValid(name) == 0);  
+#endif
+  EXPECT_TRUE(ATK_datagroup_get_group_index(root, name) == ATK_InvalidID);
+
+  ATK_datastore_delete(ds);
+}
+
 //----------------------------------------------------------------------
 //----------------------------------------------------------------------
 #include "slic/UnitTestLogger.hpp"
