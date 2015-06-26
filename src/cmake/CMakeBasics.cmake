b138430bfcd716ece91652b4811a7e6efa3cf687
Aaron Black <black27@llnl.gov>
2015-06-26 10:19:36 -0700

    Update compiler flags for code coverage support.

--- src/cmake/CMakeBasics.cmake
+++ src/cmake/CMakeBasics.cmake
@@ -106,29 +106,23 @@ endif()
 # Enable code coverage via gcov
 # Note: Only supported for gnu.
 ################################
-option(ENABLE_CODECOV "Enable/disable code coverage via gcov." ON)
+option(ENABLE_CODECOV "Enable/disable code coverage via gcov." OFF)
 
 # These should be set in some separate macro later that sets coverage flags for each compiler.
-SET(GCC_COVERAGE_COMPILE_FLAGS "-fprofile-arcs -ftest-coverage")
-SET(GCC_COVERAGE_LINK_FLAGS    "-lgcov")
-#SET(GCC_COVERAGE_COMPILE_FLAGS "--coverage")
-#SET(GCC_COVERAGE_LINK_FLAGS    "--coverage")
+#SET(GCC_COVERAGE_COMPILE_FLAGS "-fprofile-arcs -ftest-coverage")
+#SET(GCC_COVERAGE_LINK_FLAGS    "-lgcov")
+SET(GCC_COVERAGE_COMPILE_FLAGS "--coverage")
+SET(GCC_COVERAGE_LINK_FLAGS    "--coverage")
 
-if(ENABLE_CODECOV)
-    if(CMAKE_BUILD_TYPE MATCHES Debug)
-        if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-            MESSAGE(STATUS "Enabling code coverage using gcov.")
-            SET( CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} ${GCC_COVERAGE_COMPILE_FLAGS}" )
-            SET( CMAKE_EXE_LINKER_FLAGS  "${CMAKE_EXE_LINKER_FLAGS} ${GCC_COVERAGE_LINK_FLAGS}" )
-        else()
-            MESSAGE(FATAL_ERROR "Code coverage requires build type Debug.")
-        endif()
-
+if (CMAKE_BUILD_TYPE MATCHES Debug)
+    if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+        MESSAGE(STATUS "Debug build is using gnu, code coverage via gcov enabled.")
+        SET( CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} ${GCC_COVERAGE_COMPILE_FLAGS}" )
+        SET( CMAKE_EXE_LINKER_FLAGS  "${CMAKE_EXE_LINKER_FLAGS} ${GCC_COVERAGE_LINK_FLAGS}" )
     else()
-        MESSAGE(FATAL_ERROR "Code coverage is only supported with the gnu compiler.")
+        MESSAGE(STATUS "Debug build is not using gnu, code coverage is disabled.")
+        SET(ENABLE_CODECOV OFF)
     endif()
-else()
-    MESSAGE(FATAL_ERROR "Code coverage is only supported with the gnu compiler.")
 endif()
 
 
