905a4b72251bf1f0115367d53b57a5ddc3f529f3
George Zagaris <zagaris2@llnl.gov>
2015-07-28 15:27:46 -0700

    ENH: Add ATK_NOT_USED macro in common/ATKMacros.hpp

---
+++ src/components/common/src/ATKMacros.hpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2015, Lawrence Livermore National Security, LLC.
+ * Produced at the Lawrence Livermore National Laboratory.
+ *
+ * All rights reserved.
+ *
+ * This source code cannot be distributed without permission and
+ * further review from Lawrence Livermore National Laboratory.
+ */
+
+/*!
+ *******************************************************************************
+ * \file ATKMacros.hpp
+ *
+ * \date Jul 28, 2015
+ * \author George Zagaris (zagaris2@llnl.gov)
+ *
+ *******************************************************************************
+ */
+
+#ifndef ATKMACROS_HPP_
+#define ATKMACROS_HPP_
+
+/*!
+ *******************************************************************************
+ * \def ATK_NOT_USED(x)
+ * \brief Macro used to silence compiler warnings in methods with unused
+ *  arguments.
+ *******************************************************************************
+ */
+#define ATK_NOT_USED(x)
+
+
+
+#endif /* ATKMACROS_HPP_ */
