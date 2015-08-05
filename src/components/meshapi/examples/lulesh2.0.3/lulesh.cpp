fa523fcf3fb1ba18062e2ab352e5521b801a8537
Kenneth Weiss <kweiss@llnl.gov>
2015-08-04 17:01:32 -0700

    Added copyright statement to meshapi files. Resolves ATK-262.
    I made these changes with the new python script: scripts/addLLNLCopyright.py

--- src/components/meshapi/examples/lulesh2.0.3/lulesh.cpp
+++ src/components/meshapi/examples/lulesh2.0.3/lulesh.cpp
@@ -1,4 +1,15 @@
 /*
+ * Copyright (c) 2015, Lawrence Livermore National Security, LLC.
+ * Produced at the Lawrence Livermore National Laboratory.
+ *
+ * All rights reserved.
+ *
+ * This source code cannot be distributed without permission and further
+ * review from Lawrence Livermore National Laboratory.
+ */
+
+
+/*
    This is a Version 2.0 MPI + OpenMP implementation of LULESH
 
                  Copyright (c) 2010-2013.
--- src/components/meshapi/src/FileUtilities.hpp
+++ src/components/meshapi/src/FileUtilities.hpp
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2015, Lawrence Livermore National Security, LLC.
+ * Produced at the Lawrence Livermore National Laboratory.
+ *
+ * All rights reserved.
+ *
+ * This source code cannot be distributed without permission and further
+ * review from Lawrence Livermore National Laboratory.
+ */
+
+
 #ifndef MESHAPI_FILE_UTILITIES_H_
 #define MESHAPI_FILE_UTILITIES_H_
 
