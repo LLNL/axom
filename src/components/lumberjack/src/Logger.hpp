4230fe52e70c8796350c7581df2693a84bf7280b
Chris White <white238@llnl.gov>
2015-08-12 16:23:11 -0700

    Add copyright

--- src/components/lumberjack/src/Logger.hpp
+++ src/components/lumberjack/src/Logger.hpp
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2015, Lawrence Livermore National Security, LLC.
+ * Produced at the Lawrence Livermore National Laboratory.
+ *
+ * All rights reserved.
+ *
+ * This source code cannot be distributed without permission and
+ * further review from Lawrence Livermore National Laboratory.
+ */
+
 #ifndef LOGGER_HPP
 #define LOGGER_HPP
 
