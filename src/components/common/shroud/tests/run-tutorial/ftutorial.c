303ab2ca07934979b18806b9334032915eaf44d4
Lee Taylor <ltaylor@llnl.gov>
2015-09-16 14:16:34 -0700

    chasm: Add file chasm_Fortran_binding.h It replaces the struct of function pointers used in the original chasm.  Instead some macros are created to map a generic name to the specific implementation.  Only one implementation is compiled at a time, the assumption is that only one Fortran compiler will be used within a code.

--- src/components/common/shroud/tests/run-tutorial/ftutorial.c
+++ src/components/common/shroud/tests/run-tutorial/ftutorial.c
@@ -2,9 +2,7 @@
  *  ftutorial.c - C routines called from Fortran.
  */
 
-//#include "compilers/GNU.h"
-// F90_SetCompilerCharacteristics()
-int printArrayDesc_GNU(const void* desc, int rank);
+#include "chasm/chasm_Fortran_binding.h"
 
 
 void *global_allocatable;  // silence 'unused parameter' warnings
@@ -13,5 +11,5 @@ void *global_allocatable;  // silence 'unused parameter' warnings
 void all_test1_(void *array)
 {
   global_allocatable = array;
-  printArrayDesc_GNU(array, 1);
+  CHASM_printArrayDesc(array, 1);
 }
