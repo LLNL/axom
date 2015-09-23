fe761121faacdac9b7635406d258b7c78349f52d
Lee Taylor <ltaylor@llnl.gov>
2015-09-23 16:15:44 -0700

    Change SidreAllocatable.hpp to use cog for generating code. This makes it easier to only generate part of a file.

--- src/components/sidre/src/capi/SidreAllocatable.hpp
+++ src/components/sidre/src/capi/SidreAllocatable.hpp
@@ -10,6 +10,11 @@ extern "C" {
 namespace asctoolkit {
 namespace sidre {
 
+//[[[cog
+//import cog
+//import genfsidresplicer as gen
+//gen.print_lines(cog.outl, gen.print_atk_size_allocatable_header)
+//]]]
 size_t atk_size_allocatable_int_scalar_ptr_(void *array);
 size_t atk_size_allocatable_int_1d_ptr_(void *array);
 size_t atk_size_allocatable_long_scalar_ptr_(void *array);
@@ -18,6 +23,7 @@ size_t atk_size_allocatable_float_scalar_ptr_(void *array);
 size_t atk_size_allocatable_float_1d_ptr_(void *array);
 size_t atk_size_allocatable_double_scalar_ptr_(void *array);
 size_t atk_size_allocatable_double_1d_ptr_(void *array);
+//[[[end]]]
 
 }  // namespace asctoolkit
 }  // namespace sidre
