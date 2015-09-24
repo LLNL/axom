cdbfdfe6bb79ef56b6951bfde8a38fc2228e5ec9
Lee Taylor <ltaylor@llnl.gov>
2015-09-23 21:13:08 -0700

    No need to generate SidreAllocatable.hpp now

--- src/components/sidre/src/capi/SidreAllocatable.hpp
+++ src/components/sidre/src/capi/SidreAllocatable.hpp
@@ -9,20 +9,7 @@ extern "C" {
 namespace asctoolkit {
 namespace sidre {
 
-//[[[cog
-//import cog
-//import genfsidresplicer as gen
-//gen.print_lines(cog.outl, gen.print_atk_size_allocatable_header)
-//]]]
-size_t atk_size_allocatable_int_scalar_ptr_(void *array);
-size_t atk_size_allocatable_int_1d_ptr_(void *array);
-size_t atk_size_allocatable_long_scalar_ptr_(void *array);
-size_t atk_size_allocatable_long_1d_ptr_(void *array);
-size_t atk_size_allocatable_float_scalar_ptr_(void *array);
-size_t atk_size_allocatable_float_1d_ptr_(void *array);
-size_t atk_size_allocatable_double_scalar_ptr_(void *array);
-size_t atk_size_allocatable_double_1d_ptr_(void *array);
-//[[[end]]]
+void RegisterFortranAllocatableMetaBuffers(void);
 
 }  // namespace asctoolkit
 }  // namespace sidre
