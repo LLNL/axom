fd74762923e0ff3e5c113440ae382b298607f501
Lee Taylor <ltaylor@llnl.gov>
2015-09-23 17:55:40 -0700

    Revert previous commit

--- src/components/sidre/src/capi/SidreAllocatable.cpp
+++ src/components/sidre/src/capi/SidreAllocatable.cpp
@@ -1,31 +1,112 @@
 //
-// SidreAllocatable.hpp - Routines used by Fortran interface
+// SidreAllocatable.cpp - Routines used by Fortran interface
 // Uses cog to insert some generated code into this file.
 //
-#ifndef SIDREALLOCATABLE_HPP_
-#define SIDREALLOCATABLE_HPP_
+#include <cstddef>
+#include "common/CommonTypes.hpp"
+#include "SidreWrapperHelpers.hpp"
 
 extern "C" {
 namespace asctoolkit {
 namespace sidre {
 
+
 //[[[cog
 //import cog
 //import genfsidresplicer as gen
-//gen.print_lines(cog.outl, gen.print_atk_size_allocatable_header)
+//gen.print_lines(cog.outl, gen.print_atk_register_allocatable)
 //]]]
-size_t atk_size_allocatable_int_scalar_ptr_(void *array);
-size_t atk_size_allocatable_int_1d_ptr_(void *array);
-size_t atk_size_allocatable_long_scalar_ptr_(void *array);
-size_t atk_size_allocatable_long_1d_ptr_(void *array);
-size_t atk_size_allocatable_float_scalar_ptr_(void *array);
-size_t atk_size_allocatable_float_1d_ptr_(void *array);
-size_t atk_size_allocatable_double_scalar_ptr_(void *array);
-size_t atk_size_allocatable_double_1d_ptr_(void *array);
+
+// Fortran callable routine.
+// Needed for each type-kind-rank to get address of allocatable array.
+void *atk_register_allocatable_int_scalar_ptr_(
+    DataGroup *group,
+    char *name, int lname,
+    void *array, int atk_type, int rank)
+{
+    return register_allocatable(group, std::string(name, lname), array, atk_type, rank); 
+}
+
+
+// Fortran callable routine.
+// Needed for each type-kind-rank to get address of allocatable array.
+void *atk_register_allocatable_int_1d_ptr_(
+    DataGroup *group,
+    char *name, int lname,
+    void *array, int atk_type, int rank)
+{
+    return register_allocatable(group, std::string(name, lname), array, atk_type, rank); 
+}
+
+
+// Fortran callable routine.
+// Needed for each type-kind-rank to get address of allocatable array.
+void *atk_register_allocatable_long_scalar_ptr_(
+    DataGroup *group,
+    char *name, int lname,
+    void *array, int atk_type, int rank)
+{
+    return register_allocatable(group, std::string(name, lname), array, atk_type, rank); 
+}
+
+
+// Fortran callable routine.
+// Needed for each type-kind-rank to get address of allocatable array.
+void *atk_register_allocatable_long_1d_ptr_(
+    DataGroup *group,
+    char *name, int lname,
+    void *array, int atk_type, int rank)
+{
+    return register_allocatable(group, std::string(name, lname), array, atk_type, rank); 
+}
+
+
+// Fortran callable routine.
+// Needed for each type-kind-rank to get address of allocatable array.
+void *atk_register_allocatable_float_scalar_ptr_(
+    DataGroup *group,
+    char *name, int lname,
+    void *array, int atk_type, int rank)
+{
+    return register_allocatable(group, std::string(name, lname), array, atk_type, rank); 
+}
+
+
+// Fortran callable routine.
+// Needed for each type-kind-rank to get address of allocatable array.
+void *atk_register_allocatable_float_1d_ptr_(
+    DataGroup *group,
+    char *name, int lname,
+    void *array, int atk_type, int rank)
+{
+    return register_allocatable(group, std::string(name, lname), array, atk_type, rank); 
+}
+
+
+// Fortran callable routine.
+// Needed for each type-kind-rank to get address of allocatable array.
+void *atk_register_allocatable_double_scalar_ptr_(
+    DataGroup *group,
+    char *name, int lname,
+    void *array, int atk_type, int rank)
+{
+    return register_allocatable(group, std::string(name, lname), array, atk_type, rank); 
+}
+
+
+// Fortran callable routine.
+// Needed for each type-kind-rank to get address of allocatable array.
+void *atk_register_allocatable_double_1d_ptr_(
+    DataGroup *group,
+    char *name, int lname,
+    void *array, int atk_type, int rank)
+{
+    return register_allocatable(group, std::string(name, lname), array, atk_type, rank); 
+}
 //[[[end]]]
+
 
 }  // namespace asctoolkit
 }  // namespace sidre
 }  // extern "C"
-#endif /* SIDREALLOCATABLE_HPP_ */
 
