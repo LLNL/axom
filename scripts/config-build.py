b81403062c1740bf6b69bfd9b459d03967a411f7
Cyrus Harrison <cyrush@llnl.gov>
2015-11-03 11:08:09 -0800

    update scripts/config-build.py with an option for an explicit host-config file

--- scripts/config-build.py
+++ scripts/config-build.py
@@ -3,73 +3,152 @@
 #
 # Please keep parser option names as close to possible as the names of the cmake options they are wrapping.
 
+import sys
+import os
+import subprocess
 import argparse
 
 parser = argparse.ArgumentParser(description="Configure cmake build.")
 
-parser.add_argument("-bp", "--buildpath", type=str, default="", help="specify path for build directory.  If not specified, will create one under current directory.")
-parser.add_argument("-ip", "--installpath", type=str, default="", help="specify path for installation directory.  If not specified, will create one under current directory.")
-parser.add_argument("-e", "--eclipse", action='store_true', help="create an eclipse project file.")
-parser.add_argument("-c", "--compiler", type=str, choices=["intel","intel_16_0_109","gnu","gnu_4_9_3","clang","clang_3_5_0","xl"], default="gnu", help="compiler to use.")
-parser.add_argument("-bt", "--buildtype", type=str, choices=["Release", "Debug", "RelWithDebInfo", "MinSizeRel"], default="Debug", help="build type.")
-parser.add_argument("-ecc", "--exportcompilercommands", action='store_true',
-	 help="generate a compilation database.  Can be used by the clang tools such as clang-modernize.  Will create a file called 'compile_commands.json' in your build directory.")
-parser.add_argument("-co", "--cmakeoption", type=str, help="specify additional cmake options to add to cmake line.  Use with caution, if you are doing something non-trivial, use ccmake or cmake-gui.")
+parser.add_argument("-bp",
+                    "--buildpath",
+                    type=str,
+                    default="",
+                    help="specify path for build directory.  If not specified, will create one under current directory.")
+
+parser.add_argument("-ip",
+                    "--installpath", 
+                    type=str, default="",
+                    help="specify path for installation directory.  If not specified, will create one under current directory.")
+
+parser.add_argument("-c",
+                    "--compiler",
+                    type=str,
+                    choices=["intel","intel_16_0_109","gnu","gnu_4_9_3","clang","clang_3_5_0","xl"],
+                    default="gnu",
+                    help="compiler to use.")
+
+parser.add_argument("-bt",
+                    "--buildtype",
+                    type=str,
+                    choices=["Release", "Debug", "RelWithDebInfo", "MinSizeRel"],
+                    default="Debug",
+                    help="build type.")
+
+parser.add_argument("-e",
+                    "--eclipse",
+                    action='store_true',
+                    help="create an eclipse project file.")
+
+parser.add_argument("-x",
+                    "--xcode",
+                    action='store_true',
+                    help="create an xcode project.")
+
+parser.add_argument("-ecc",
+                    "--exportcompilercommands",
+                    action='store_true',
+	                help="generate a compilation database.  Can be used by the clang tools such as clang-modernize.  Will create a file called 'compile_commands.json' in your build directory.")
+
+parser.add_argument("-co",
+                    "--cmakeoption",
+                    type=str,
+                    help="specify additional cmake options to add to cmake line.  Use with caution, if you are doing something non-trivial, use ccmake or cmake-gui.")
+
+parser.add_argument("-hc",
+                    "--hostconfig",
+                    default="",
+                    type=str,
+                    help="select a specific host-config file to initalize CMake's cache")
 
 args = parser.parse_args()
 
-import sys
-import os
-scriptsdir = os.path.dirname( os.path.join( os.getcwd(), sys.argv[0] ) )
 
-cmakeline = "cmake"
 
-# Add build type (opt or debug)
-cmakeline += " -DCMAKE_BUILD_TYPE=" + args.buildtype
 
-# Check if 'SYS_TYPE' exists, and look for cache file there.
-cachefile = scriptsdir.replace("scripts","host-configs")
-
+########################
+# Find CMake Cache File
+########################
 shortsystype = ""
-if "SYS_TYPE" in os.environ:
-    systype = os.environ["SYS_TYPE"]
-    shortsystype = systype.split("_")[0]
-    cachefile = os.path.join( cachefile, shortsystype, "%s.cmake" % args.compiler ) 
+scriptsdir = os.path.dirname( os.path.abspath(sys.argv[0]) )
+
+if args.hostconfig != "":
+    cachefile = os.path.abspath(args.hostconfig)
+    shortsystype = os.path.split(cachefile)[1]
+    if shortsystype.endswith(".cmake"):
+        shortsystype = shortsystype[:-6]
 else:
-    import platform
-    shortsystype = platform.node()
-    cachefile = os.path.join(cachefile, "other", "%s.cmake" % shortsystype )
+    # Check if 'SYS_TYPE' exists, and look for cache file there.
+    cachefile = scriptsdir.replace("scripts","host-configs")
+    if "SYS_TYPE" in os.environ:
+        systype = os.environ["SYS_TYPE"]
+        shortsystype = systype.split("_")[0]
+        cachefile = os.path.join( cachefile, shortsystype, "%s.cmake" % args.compiler ) 
+    else:
+        import platform
+        shortsystype = platform.node()
+        cachefile = os.path.join(cachefile, "other", "%s.cmake" % shortsystype )
 
 assert os.path.exists( cachefile ), "Could not find cmake cache file '%s'." % cachefile
-cmakeline += " -C %s" % cachefile
 
-if args.buildpath == "":
+
+#####################
+# Setup Build Dir
+#####################
+if args.buildpath != "":
+    # use explicit build path
+    buildpath = args.buildpath
+elif args.hostconfig != "":
+    # use host config name (via shortsystype) as build dir base
+    buildpath = "-".join(["build",shortsystype,args.buildtype])
+elif args.buildpath == "":
     # Generate build directory name based on platform, buildtype, compiler
     buildpath = "-".join([shortsystype, args.compiler, args.buildtype])
-else:
-    buildpath = args.buildpath
+
+buildpath = os.path.abspath(buildpath)
 
 if os.path.exists(buildpath):
     print "Build directory '%s' already exists.  Deleting..." % buildpath
     import shutil
     shutil.rmtree(buildpath)
+
 print "Creating build directory '%s'..." % buildpath
 os.makedirs(buildpath)
 
+#####################
+# Setup Install Dir
+#####################
 # For install directory, we will clean up old ones, but we don't need to create it, cmake will do that.
-if args.installpath == "":
-    # Generate install directory name based on platform, buildtype, compiler
-    installpath = os.path.abspath("-".join([shortsystype, args.compiler, args.buildtype,"install"]))
-else:
+if args.installpath != "":
     installpath = os.path.abspath(args.installpath)
+elif args.hostconfig != "":
+    # use host config name (via shortsystype) as install dir base
+    installpath = "-".join(["install",shortsystype,args.buildtype])
+else:
+    # Generate install directory name based on platform, buildtype, compiler
+    installpath = "-".join([shortsystype, args.compiler, args.buildtype,"install"])
+
+installpath = os.path.abspath(installpath)
 
 if os.path.exists(installpath):
     print "Install directory '%s' already exists, deleting..." % installpath
     import shutil
     shutil.rmtree(installpath)
-cmakeline += " -DCMAKE_INSTALL_PREFIX=%s" % installpath
+
 print "Creating install path '%s'..." % installpath
 os.makedirs(installpath)
+
+############################
+# Build CMake command line
+############################
+
+cmakeline = "cmake"
+# Add cache file option
+cmakeline += " -C %s" % cachefile
+# Add build type (opt or debug)
+cmakeline += " -DCMAKE_BUILD_TYPE=" + args.buildtype
+# Set install dir
+cmakeline += " -DCMAKE_INSTALL_PREFIX=%s" % installpath
 
 if args.exportcompilercommands:
     cmakeline += " -DCMAKE_EXPORT_COMPILE_COMMANDS=on"
@@ -77,13 +156,20 @@ if args.exportcompilercommands:
 if args.eclipse:
     cmakeline += ' -G "Eclipse CDT4 - Unix Makefiles"'
 
+if args.xcode:
+    cmakeline += ' -G "XCode"'
+
 if args.cmakeoption:
     cmakeline += " " + args.cmakeoption
 
 cmakeline += " %s/../src " % scriptsdir
 
+############################
+# Run CMake
+############################
 print "Changing to build directory..."
 os.chdir(buildpath)
 print "Executing cmake line: '%s'..." % cmakeline
 print 
-os.system(cmakeline)
+subprocess.call(cmakeline,shell=True)
+
