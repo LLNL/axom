25b4b302452a0a9c527a03f67cc0f4923731e0f0
Lee Taylor <taylor16@llnl.gov>
2015-11-21 16:47:36 -0800

    Test F_name_generic

--- src/components/shroud/src/main.py
+++ src/components/shroud/src/main.py
@@ -812,7 +812,7 @@ class Namify(object):
             util.eval_template2(node, 'F_name_impl', '_function')
 
         util.eval_template(options, fmt_func, 'F_name_method')
-        util.eval_template(options, fmt_func, 'F_name_generic')
+        util.eval_template2(node, 'F_name_generic')
 
         if 'F_this' in options:
             fmt_func.F_this = options.F_this
