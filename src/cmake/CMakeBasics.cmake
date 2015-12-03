466b03da7f4d5187e7ea665409e4a71eb5bc94b7
Chris White <white238@llnl.gov>
2015-12-03 14:01:49 -0800

    Remove compiler flags for mpi that were causing old versions of the headers to be included

--- src/cmake/CMakeBasics.cmake
+++ src/cmake/CMakeBasics.cmake
@@ -292,23 +292,8 @@ macro(make_library)
    endif()
 
    if ( ${ENABLE_MPI} )
-
       add_target_definitions( TO ${arg_LIBRARY_NAME}
                               TARGET_DEFINITIONS USE_MPI )
-
-      target_include_directories( ${arg_LIBRARY_NAME} PUBLIC
-                                  ${MPI_C_INCLUDE_PATH} )
-
-      if(NOT "${MPI_CXX_COMPILE_FLAGS}" STREQUAL "")
-            set_target_properties( ${arg_LIBRARY_NAME} PROPERTIES COMPILE_FLAGS
-                                   ${MPI_CXX_COMPILE_FLAGS} )
-      endif()
-
-      if(NOT "${MPI_CXX_LINK_FLAGS}" STREQUAL "")
-            set_target_properties( ${arg_LIBRARY_NAME} PROPERTIES LINK_FLAGS
-                                   ${MPI_CXX_LINK_FLAGS} )
-      endif()
-
    endif()
 
    if ( ${arg_WITH_OPENMP} )
@@ -410,21 +395,6 @@ macro(make_executable)
    if ( ${ENABLE_MPI} )
       add_target_definitions( TO ${exe_name}
                               TARGET_DEFINITIONS USE_MPI )
-
-      target_include_directories( ${exe_name} PUBLIC
-                                  ${MPI_C_INCLUDE_PATH} )
-
-      if(NOT "${MPI_CXX_COMPILE_FLAGS}" STREQUAL "")
-            set_target_properties( ${exe_name} PROPERTIES COMPILE_FLAGS
-                                   ${MPI_CXX_COMPILE_FLAGS} )
-      endif()
-
-      if(NOT "${MPI_CXX_LINK_FLAGS}" STREQUAL "")
-            set_target_properties( ${exe_name} PROPERTIES LINK_FLAGS
-                                   ${MPI_CXX_LINK_FLAGS} )
-      endif()
-
-      target_link_libraries( ${exe_name} ${MPI_C_LIBRARIES})
    endif()
 
    if ( ${arg_WITH_OPENMP} )
