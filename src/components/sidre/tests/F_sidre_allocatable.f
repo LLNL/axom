b2301b8a62215dfc0b612c5065e23a1136531412
Lee Taylor <taylor16@llnl.gov>
2015-12-12 16:45:21 -0800

    Change genfsidresplicer.py to maxdims = 3. Add a test for a 3d array in F_sidre_allocatable.f

--- src/components/sidre/tests/F_sidre_allocatable.f
+++ src/components/sidre/tests/F_sidre_allocatable.f
@@ -50,7 +50,7 @@ contains
 
     rank = view%get_shape(7, extents)
     call assert_equals(rank, 1)
-    call assert_true(extents(1) == 10)
+    call assert_true(extents(1) == size(iarray, 1))
 
     ! get array via a pointer
     call view%get_value(ipointer)
@@ -61,6 +61,61 @@ contains
     deallocate(iarray)
 
   end subroutine local_allocatable_int
+
+!----------------------------------------------------------------------
+
+  subroutine local_allocatable_int_3d
+    integer, allocatable :: iarray(:,:,:)
+    integer, pointer :: ipointer(:,:,:)
+
+    type(datastore) ds
+    type(datagroup) root
+    type(dataview)  view
+    integer type
+    integer num_elements
+    integer i, j, k
+    integer rank
+    integer(SIDRE_LENGTH) extents(7)
+
+    ds = datastore_new()
+    root = ds%get_root()
+
+    allocate(iarray(2,3,4))
+
+    do i=1,2
+       do j=1,3
+          do k=1,4
+             iarray(i,j,k) = i*100 + j*1-0 + k
+          enddo
+       enddo
+    enddo
+
+    view = root%create_array_view("iarray", iarray)
+
+    type = view%get_type_id()
+    call assert_equals(type, SIDRE_INT_ID)
+
+    num_elements = view%get_num_elements()
+    call assert_equals(num_elements, size(iarray))
+
+    rank = view%get_num_dimensions()
+    call assert_equals(rank, 3)
+
+    rank = view%get_shape(7, extents)
+    call assert_equals(rank, 3)
+    call assert_true(extents(1) == size(iarray, 1))
+    call assert_true(extents(2) == size(iarray, 2))
+    call assert_true(extents(3) == size(iarray, 3))
+
+    ! get array via a pointer
+    call view%get_value(ipointer)
+    call assert_true(all(iarray.eq.ipointer))
+
+    call ds%delete()
+
+    deallocate(iarray)
+
+  end subroutine local_allocatable_int_3d
 
 !----------------------------------------------------------------------
 !
@@ -155,6 +210,7 @@ program fortran_test
   call init_fruit
 
   call local_allocatable_int
+  call local_allocatable_int_3d
   call local_static_int
   call local_allocatable_double
 
