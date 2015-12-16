90cbbc80fd0c562bd6af3e20a084f895838b5fd1
Aaron Black <black27@llnl.gov>
2015-12-16 15:34:37 -0800

    Revert "Revert "Merge branch 'sprint/10' of ssh://cz-stash.llnl.gov:7999/atk/asctoolkit into sprint/10""
    This reverts commit ae0c15317e0753c07c2e0e9642414cb96e3dabdb.  I hate my
    life.

--- src/components/shroud/tests/example/wrapfExClass2.f
+++ src/components/shroud/tests/example/wrapfExClass2.f
@@ -66,7 +66,8 @@ module exclass2_mod
     
     interface
         
-        function aa_exclass2_ex_class2(name) result(rv) &
+        function aa_exclass2_ex_class2(name) &
+                result(rv) &
                 bind(C, name="AA_exclass2_ex_class2")
             use iso_c_binding
             implicit none
@@ -74,7 +75,8 @@ module exclass2_mod
             type(C_PTR) :: rv
         end function aa_exclass2_ex_class2
         
-        function aa_exclass2_ex_class2_bufferify(name, Lname) result(rv) &
+        function aa_exclass2_ex_class2_bufferify(name, Lname) &
+                result(rv) &
                 bind(C, name="AA_exclass2_ex_class2_bufferify")
             use iso_c_binding
             implicit none
@@ -90,7 +92,8 @@ module exclass2_mod
             type(C_PTR), value, intent(IN) :: self
         end subroutine aa_exclass2_delete
         
-        pure function aa_exclass2_get_name(self) result(rv) &
+        pure function aa_exclass2_get_name(self) &
+                result(rv) &
                 bind(C, name="AA_exclass2_get_name")
             use iso_c_binding
             implicit none
@@ -98,7 +101,8 @@ module exclass2_mod
             type(C_PTR) rv
         end function aa_exclass2_get_name
         
-        function aa_exclass2_get_name_length(self) result(rv) &
+        function aa_exclass2_get_name_length(self) &
+                result(rv) &
                 bind(C, name="AA_exclass2_get_name_length")
             use iso_c_binding
             implicit none
@@ -106,7 +110,8 @@ module exclass2_mod
             integer(C_INT) :: rv
         end function aa_exclass2_get_name_length
         
-        function aa_exclass2_get_class1(self, in) result(rv) &
+        function aa_exclass2_get_class1(self, in) &
+                result(rv) &
                 bind(C, name="AA_exclass2_get_class1")
             use iso_c_binding
             implicit none
@@ -139,7 +144,8 @@ module exclass2_mod
             type(C_PTR), value, intent(IN) :: self
         end subroutine aa_exclass2_destroyall
         
-        pure function aa_exclass2_get_type_id(self) result(rv) &
+        pure function aa_exclass2_get_type_id(self) &
+                result(rv) &
                 bind(C, name="AA_exclass2_get_type_id")
             use iso_c_binding
             implicit none
@@ -179,7 +185,8 @@ module exclass2_mod
             real(C_DOUBLE), value, intent(IN) :: value
         end subroutine aa_exclass2_set_value_double
         
-        function aa_exclass2_get_value_int(self) result(rv) &
+        function aa_exclass2_get_value_int(self) &
+                result(rv) &
                 bind(C, name="AA_exclass2_get_value_int")
             use iso_c_binding
             implicit none
@@ -187,7 +194,8 @@ module exclass2_mod
             integer(C_INT) :: rv
         end function aa_exclass2_get_value_int
         
-        function aa_exclass2_get_value_double(self) result(rv) &
+        function aa_exclass2_get_value_double(self) &
+                result(rv) &
                 bind(C, name="AA_exclass2_get_value_double")
             use iso_c_binding
             implicit none
