7ad35d01c91b44a2a1903d86e03cf8e251f268f5
Aaron Black <black27@llnl.gov>
2016-01-06 08:59:19 -0800

    Update code coverage logic - should halt if codecov enabled on unsupported build type.

--- src/cmake/SetupCodeMetrics.cmake
+++ src/cmake/SetupCodeMetrics.cmake
@@ -2,20 +2,23 @@
 # Setup code metrics - coverage, profiling, etc
 #################################################
 
-################################
+########################################
 # Enable code coverage via gcov
-# Note: Only supported for gnu.
-################################
-if ( (CMAKE_BUILD_TYPE STREQUAL "Debug") AND (ENABLE_CODECOV) )
-
-   if ( (CMAKE_COMPILER_IS_GNUCXX) OR ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang") )
-      include(CodeCoverage)
-      add_code_coverage_target(coverage make test)
-      SET( CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_COVERAGE}" )
-      SET( CMAKE_EXE_LINKER_FLAGS  "${CMAKE_EXE_LINKER_FLAGS} ${CMAKE_EXE_LINKER_FLAGS_COVERAGE}" )
- 	   MESSAGE(STATUS "Code coverage: Detected clang or gnu, compiler flags added.")
-   else()
- 	   MESSAGE(STATUS "Code coverage: Did not detect clang or gnu, compiler flags not added.")
+# Note: Only supported for gnu or clang.
+########################################
+if (ENABLE_CODECOV)
+   if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+	   if ( (CMAKE_COMPILER_IS_GNUCXX) OR ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang") ) 
+      	include(CodeCoverage)
+	      add_code_coverage_target(coverage make test)
+	      SET( CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_COVERAGE}" )
+	      SET( CMAKE_EXE_LINKER_FLAGS  "${CMAKE_EXE_LINKER_FLAGS} ${CMAKE_EXE_LINKER_FLAGS_COVERAGE}" )
+	 	   MESSAGE(STATUS "Code coverage enabled via gcov.")
+	   else()
+	 	   MESSAGE(FATAL_ERROR "Code coverage not enabled: Requires clang or gnu compiler.")
+		endif()
+	else()
+      MESSAGE(FATAL_ERROR "Code coverage not enabled: Requires debug build type.")
    endif()
 
 endif()
