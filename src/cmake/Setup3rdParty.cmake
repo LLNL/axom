dbd1f475756ccfc6a87ce4dce1a0f6a243b455d2
Aaron Black <black27@llnl.gov>
2016-01-08 12:21:20 -0800

    fix boost checks when finding it.

--- src/cmake/Setup3rdParty.cmake
+++ src/cmake/Setup3rdParty.cmake
@@ -27,20 +27,15 @@ include(cmake/FindUncrustify.cmake)
 
 ################################
 # Find boost headers
-#
-# This should only be enabled if we aren't building with C++11 support.  The
-# policy is to use boost only to replace C++11 assets for older (non-c++11)
-# compilers.
-# For developers wanting to evaluate or explore code additions and want to
-# use boost, they override the below logic by modifying this file, but should
-# do so in a private branch only.
 ################################
-if (NOT ENABLE_CXX11)
-  find_package(Boost
-                1.55
-                REQUIRED)
-  MESSAGE(STATUS "Boost include dir: " ${Boost_INCLUDE_DIR})
-  MESSAGE(STATUS "Boost version: " ${Boost_VERSION} )
+if (ENABLE_BOOST)
+  if defined(BOOST_ROOT)
+    find_package(Boost
+                 1.55
+                 REQUIRED)
+    MESSAGE(STATUS "Boost include dir: " ${Boost_INCLUDE_DIR})
+    MESSAGE(STATUS "Boost version: " ${Boost_VERSION} )
+  else()
+    MESSAGE(FATAL_ERROR "ENABLE_BOOST is true, but BOOST_ROOT was not set.  Check your host-config file.")
+  endif()
 endif()
-
-
