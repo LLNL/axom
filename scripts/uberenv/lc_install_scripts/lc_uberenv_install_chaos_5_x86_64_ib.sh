c840240cb3ef804e7f8182eb959a77ee367d033d
Aaron Black <atk@cab687.llnl.gov>
2016-03-11 13:52:44 -0800

    Add convenience script to install all chaos compilers with uberenv.

---
+++ scripts/uberenv/lc_install_scripts/lc_uberenv_install_chaos_5_x86_64_ib.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+###############################################################################
+# use uberenv to build all third party libs on chaos 5
+###############################################################################
+./lc_uberenv_install_chaos_5_x86_64_ib_clang_3_5_0.sh
+./lc_uberenv_install_chaos_5_x86_64_ib_gcc_4_7_1.sh
+./lc_uberenv_install_chaos_5_x86_64_ib_gcc_4_9_3.sh
+./lc_uberenv_install_chaos_5_x86_64_ib_intel_15_0_187.sh
+./lc_uberenv_install_chaos_5_x86_64_ib_intel_16_0_109.sh
