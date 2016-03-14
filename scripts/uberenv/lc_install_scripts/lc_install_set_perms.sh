7b28cda83f8fcb8cdb414198c5845a931c2b7b28
Aaron Black <black27@llnl.gov>
2016-03-14 11:30:54 -0700

    Fix message in lc_install_set_perms.

--- scripts/uberenv/lc_install_scripts/lc_install_set_perms.sh
+++ scripts/uberenv/lc_install_scripts/lc_install_set_perms.sh
@@ -4,7 +4,7 @@
 # third party libs
 ###############################################################################
 # set group to toolkitd
-echo "Changing group to toolkit for $LC_TPL_PATH/spack, $LC_TPL_PATH/*.cmake"
+echo "Changing group to toolkitd for $LC_TPL_PATH/spack, $LC_TPL_PATH/*.cmake"
 chgrp -f -R toolkitd  ${LC_TPL_PATH}/spack ${LC_TPL_PATH}/*.cmake
 # allow group members to read, write, and exec
 echo "Allowing group read, write, execute on $LC_TPL_PATH/spack, $LC_TPL_PATH/*.cmake"
