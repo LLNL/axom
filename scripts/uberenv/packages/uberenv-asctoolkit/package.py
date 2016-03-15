dfb0199d6ac00c51a39e3f1f065b2672024065e2
Aaron Black <black27@llnl.gov>
2016-03-15 09:37:25 -0700

    Update uberenv asctoolkit package to dump host config with numbers stripped from node names.  ie - cab not cab670.

--- scripts/uberenv/packages/uberenv-asctoolkit/package.py
+++ scripts/uberenv/packages/uberenv-asctoolkit/package.py
@@ -11,14 +11,10 @@ class UberenvAsctoolkit(Package):
     # hash for dummy tarfile
     version('0.1', '8d378ef62dedc2df5db447b029b71200')
     
-    homepage = "http://lc.llnl.gov/toolkit"
-    # standard spack packages
+    # all of theses are custom
+    depends_on("cmake")
     depends_on("doxygen")
     depends_on("python")
-    depends_on("uncrustify")
-    
-    # custom spack packages
-    depends_on("cmake~ncurses~openssl@3.3.1")
     depends_on("conduit")
     depends_on("py-sphinx")
     depends_on("py-breathe")
@@ -33,9 +29,12 @@ class UberenvAsctoolkit(Package):
     depends_on("boost-headers")
     depends_on("sparsehash-headers")
 
+    # this was pushed to develop, but not yet in the diy branch
+    depends_on("uncrustify")
+
     # use dummy tarfile to avoid downloads
     def url_for_version(self, version):
-        dummy_tar_path =  os.path.abspath(pjoin(os.path.dirname(__file__)))
+        dummy_tar_path =  os.path.abspath(pjoin(os.path.split(__file__)[0]))
         dummy_tar_path = pjoin(dummy_tar_path,"uberenv-asctoolkit.tar.gz")
         url      = "file://" + dummy_tar_path
         return url
@@ -59,7 +58,7 @@ class UberenvAsctoolkit(Package):
         sphinx_build_exe = pjoin(spec['python'].prefix.bin,"sphinx-build")
         uncrustify_exe   = pjoin(spec['uncrustify'].prefix.bin,"uncrustify")
 
-        host_cfg_fname = "%s-%s-%s.cmake" % (socket.gethostname(),sys_type,spec.compiler)
+        host_cfg_fname = "%s-%s-%s.cmake" % (socket.gethostname().rstrip('1234567890'),sys_type,spec.compiler)
         host_cfg_fname = pjoin(dest_dir,host_cfg_fname)
         cfg = open(host_cfg_fname,"w")
         cfg.write("##################################\n")
@@ -69,7 +68,7 @@ class UberenvAsctoolkit(Package):
         cfg.write("##################################\n\n")
         # show path to cmake for reference
         cfg.write("# cmake from uberenv\n")
-        cfg.write("# cmake executable path: %s\n\n" % cmake_exe)
+        cfg.write("# cmake exectuable path: %s\n\n" % cmake_exe)
 
         # compiler settings
         cfg.write("#######\n")
@@ -91,7 +90,7 @@ class UberenvAsctoolkit(Package):
         cfg.write('set(CONDUIT_DIR "%s" CACHE PATH "")\n\n' % conduit_dir)
 
         cfg.write("# doxygen from uberenv\n")
-        cfg.write('set(DOXYGEN_EXECUTABLE "%s" CACHE PATH "")\n\n' % doxygen_exe)
+        cfg.write("# doxygen exectuable path: %s\n\n" % doxygen_exe)
 
         cfg.write("# python from uberenv\n")
         cfg.write('set(PYTHON_EXECUTABLE "%s" CACHE PATH "")\n\n' % python_exe)
@@ -114,7 +113,7 @@ class UberenvAsctoolkit(Package):
             cfg.write('set(LCOV_PATH "%s/usr/bin/lcov" CACHE PATH "")\n\n' % spec['lcov'].prefix)
             cfg.write('set(GENHTML_PATH "%s/usr/bin/genhtml" CACHE PATH "")\n\n' % spec['lcov'].prefix)
         else:
-            cfg.write("# lcov and genhtml not built by uberenv\n\n")
+            cfg.write("# lcov and genhtml not build by uberenv\n\n")
 
         cfg.write("##################################\n")
         cfg.write("# end uberenv host-config\n")
@@ -124,7 +123,6 @@ class UberenvAsctoolkit(Package):
         mkdirp(prefix)
         install(host_cfg_fname,prefix)
         print "[result host-config file: %s]" % host_cfg_fname
-
 
 
 
