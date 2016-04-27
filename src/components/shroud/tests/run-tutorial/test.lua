b8ed35bed15d0adf08eaae4d0a4164ba3ef3ee7f
Lee Taylor <taylor16@llnl.gov>
2016-04-26 23:32:01 -0700

    Add additional tests

--- src/components/shroud/tests/run-tutorial/test.lua
+++ src/components/shroud/tests/run-tutorial/test.lua
@@ -9,6 +9,13 @@ print(tutorial.LastFunctionCalled())
 rv_double = tutorial.Function2(1.0, 4)
 print(tutorial.LastFunctionCalled(), rv_double)
 
+
+tutorial.Function10()
+print(tutorial.LastFunctionCalled())
+tutorial.Function10("foo", 1.0)
+print(tutorial.LastFunctionCalled())
+
+
 rv_int = tutorial.overload1(10)
 print(tutorial.LastFunctionCalled(), rv_int)
 -- This should call overload (double type, int num)
@@ -21,6 +28,8 @@ rv_int = tutorial.overload1(10, 11, 12)
 print(tutorial.LastFunctionCalled(), rv_int)
 rv_int = tutorial.overload1(1.0, 10, 11, 12)
 print(tutorial.LastFunctionCalled(), rv_int)
+
+-- rv_int = tutorial.overload1("no such overload")
 
 
 -- call a class
