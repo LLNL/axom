40ea5afea908e9e24a9ecd1c0d70bf6661cf737a
Lee Taylor <taylor16@llnl.gov>
2016-04-27 22:50:03 -0700

    Add more tests

--- src/components/shroud/tests/run-tutorial/test.lua
+++ src/components/shroud/tests/run-tutorial/test.lua
@@ -1,13 +1,45 @@
 -- test tutorial module
 
 local tutorial = require "tutorial"
-local rv_int, rv_double
+local rv_int, rv_double, rv_logical, rv_char
 
 tutorial.Function1()
 print(tutorial.LastFunctionCalled())
 
 rv_double = tutorial.Function2(1.0, 4)
 print(tutorial.LastFunctionCalled(), rv_double)
+
+
+rv_logical = tutorial.Function3(false)
+print(tutorial.LastFunctionCalled(), rv_logical)
+
+rv_char = tutorial.Function4a("dog", "cat")
+print(tutorial.LastFunctionCalled(), rv_char)
+
+rv_char = tutorial.Function4b("dog", "cat")
+print(tutorial.LastFunctionCalled(), rv_char)
+
+rv_double = tutorial.Function5()
+-- 13.1415
+print(tutorial.LastFunctionCalled(), rv_double)
+rv_double = tutorial.Function5(11.0)
+print(tutorial.LastFunctionCalled(), rv_double)
+-- 11.0
+rv_double = tutorial.Function5(11.0, false)
+print(tutorial.LastFunctionCalled(), rv_double)
+-- 1.0
+
+tutorial.Function6("name")
+print(tutorial.LastFunctionCalled())
+tutorial.Function6(1)
+print(tutorial.LastFunctionCalled())
+
+--[[
+tutorial.Function7(1)
+print(tutorial.LastFunctionCalled())
+tutorial.Function7(10.0)
+print(tutorial.LastFunctionCalled())
+--]]
 
 
 tutorial.Function10()
