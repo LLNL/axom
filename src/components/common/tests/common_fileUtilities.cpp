9024476578a34ec3243e065629c531023f454674
Cyrus Harrison <cyrush@llnl.gov>
2016-05-11 15:31:44 -0700

    remove common test's deps on slic

--- src/components/common/tests/common_fileUtilities.cpp
+++ src/components/common/tests/common_fileUtilities.cpp
@@ -90,22 +90,3 @@ TEST(gtest_common_fileUtilities,common_fileUtil_pathExists)
   EXPECT_FALSE( pathExists(missingFile) );
 }
 
-
-//----------------------------------------------------------------------
-#include "slic/UnitTestLogger.hpp"
-using asctoolkit::slic::UnitTestLogger;
-
-int main(int argc, char * argv[])
-{
-  int result = 0;
-
-  ::testing::InitGoogleTest(&argc, argv);
-
-  UnitTestLogger logger;  // create & initialize test logger,
-
-  // finalized when exiting main scope
-
-  result = RUN_ALL_TESTS();
-
-  return result;
-}
