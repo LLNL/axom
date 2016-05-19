aedfa5a1c0b69763acdafff3cba43f562c9cd313
Chris White <white238@llnl.gov>
2016-05-19 12:50:14 -0700

    Add some error checks to the bamboo scripts

--- scripts/bamboo/chaos-gcc@4.9.3-code-tools.sh
+++ scripts/bamboo/chaos-gcc@4.9.3-code-tools.sh
@@ -1,36 +1,57 @@
-#!/bin/sh
+#!/bin/bash
+
 echo "Configuring..."
 echo "-----------------------------------------------------------------------"
 ./scripts/config-build.py -c gcc@4.9.3 --buildtype Debug -DENABLE_CODECOV=TRUE 
+if [ $? -ne 0 ]; then
+    echo "Error: config-build.py failed"
+fi
+echo "-----------------------------------------------------------------------"
+
 cd build-chaos-gcc@4.9.3-debug
-echo "-----------------------------------------------------------------------"
+    echo "Generating C/Fortran binding..."
+    make generate
+    if [ $? -ne 0 ]; then
+        echo "Error: 'make generate' failed"
+    fi
+    echo "-----------------------------------------------------------------------"
 
-echo "-----------------------------------------------------------------------"
-echo "Generating C/Fortran binding..."
-make generate
-echo "-----------------------------------------------------------------------"
+    echo "Building..."
+    echo "-----------------------------------------------------------------------"
+    make -j16
+    if [ $? -ne 0 ]; then
+        echo "Error: 'make' failed"
+    fi
+    echo "-----------------------------------------------------------------------"
 
-echo "Building..."
-echo "-----------------------------------------------------------------------"
-make -j16
-echo "-----------------------------------------------------------------------"
+    echo "Run tests and collect code coverage output."
+    echo "-----------------------------------------------------------------------"
+    make coverage ARGS="-T Test -E mpi"
+    if [ $? -ne 0 ]; then
+        echo "Error: 'make coverage' failed"
+    fi
+    echo "-----------------------------------------------------------------------"
 
-echo "Run tests and collect code coverage output."
-echo "-----------------------------------------------------------------------"
-make coverage ARGS="-T Test -E mpi"
-echo "-----------------------------------------------------------------------"
+    echo "Install code coverage report to web space..."
+    echo "-----------------------------------------------------------------------"
+    pushd /usr/global/web-pages/lc/www/toolkit
+        rm -rf ./coverage
+        if [ $? -ne 0 ]; then
+            echo "Error: 'rm' failed"
+        fi
+    popd
+    cp -R ./coverage /usr/global/web-pages/lc/www/toolkit
+    if [ $? -ne 0 ]; then
+        echo "Error: 'cp' failed"
+    fi
+    echo "-----------------------------------------------------------------------"
 
-echo "Install code coverage report to web space..."
-echo "-----------------------------------------------------------------------"
-pushd /usr/global/web-pages/lc/www/toolkit
-rm -rf ./coverage
-popd
-cp -R ./coverage /usr/global/web-pages/lc/www/toolkit
-echo "-----------------------------------------------------------------------"
-
-echo "Running valgrind..."
-echo "-----------------------------------------------------------------------"
-make test ARGS=" -E mpi -D ExperimentalMemCheck --output-on-failure"
-echo "-----------------------------------------------------------------------"
-
+    echo "Running valgrind..."
+    echo "-----------------------------------------------------------------------"
+    make test ARGS=" -E mpi -D ExperimentalMemCheck --output-on-failure"
+    if [ $? -ne 0 ]; then
+        echo "Error: 'make test' failed"
+    fi
+    echo "-----------------------------------------------------------------------"
+cd ..
 # TODO - Should we copy valgrind output to web area so team can access???
--- scripts/bamboo/chaos-gcc@4.9.3-release.sh
+++ scripts/bamboo/chaos-gcc@4.9.3-release.sh
@@ -1,39 +1,76 @@
-#!/bin/sh
+#!/bin/bash
+
 echo "Configuring..."
 echo "-----------------------------------------------------------------------"
 ./scripts/config-build.py -c gcc@4.9.3 --buildtype Release --buildpath build-chaos-release --installpath install-chaos-release
+if [ $? -ne 0 ]; then
+    echo "Error: config-build.py failed"
+fi
+echo "-----------------------------------------------------------------------"
+
 cd build-chaos-release
-echo "-----------------------------------------------------------------------"
+    echo "Building..."
+    echo "-----------------------------------------------------------------------"
+    make -j16
+    if [ $? -ne 0 ]; then
+        echo "Error: 'make' failed"
+    fi
+    echo "-----------------------------------------------------------------------"
 
-echo "Building..."
-echo "-----------------------------------------------------------------------"
-make -j16
-echo "-----------------------------------------------------------------------"
+    echo "Running tests..."
+    echo "-----------------------------------------------------------------------"
+    make test ARGS="-T Test"
+    if [ $? -ne 0 ]; then
+        echo "Error: 'make test' failed"
+    fi
+    echo "-----------------------------------------------------------------------"
 
-echo "Running tests..."
-echo "-----------------------------------------------------------------------"
-make test ARGS="-T Test"
-echo "-----------------------------------------------------------------------"
+    echo "Making docs..."
+    echo "-----------------------------------------------------------------------"
+    make docs
+    if [ $? -ne 0 ]; then
+        echo "Error: 'make docs' failed"
+    fi
+    echo "-----------------------------------------------------------------------"
 
-echo "Making docs..."
-echo "-----------------------------------------------------------------------"
-make docs
-echo "-----------------------------------------------------------------------"
-
-echo "Installing files..."
-echo "-----------------------------------------------------------------------"
-make install
-echo "-----------------------------------------------------------------------"
+    echo "Installing files..."
+    echo "-----------------------------------------------------------------------"
+    make install
+    if [ $? -ne 0 ]; then
+        echo "Error: 'make install' failed"
+    fi
+    echo "-----------------------------------------------------------------------"
+cd ..
 
 echo "Installing docs to web space..."
 echo "-----------------------------------------------------------------------"
-# Is there a way to terminate if the make docs above failed?  So we don't try to copy over the
-# files if that failed.
-cd ..
 rm -rf /usr/global/web-pages/lc/www/toolkit/docs_old
+if [ $? -ne 0 ]; then
+    echo "Error: 'rm' failed"
+fi
+
 mv /usr/global/web-pages/lc/www/toolkit/docs /usr/global/web-pages/lc/www/toolkit/docs_old
+if [ $? -ne 0 ]; then
+    echo "Error: 'mv' failed"
+fi
+
 cp -R ./install-chaos-release/docs /usr/global/web-pages/lc/www/toolkit/
+if [ $? -ne 0 ]; then
+    echo "Error: 'cp' failed"
+fi
+
 chgrp -R toolkit /usr/global/web-pages/lc/www/toolkit/docs/
+if [ $? -ne 0 ]; then
+    echo "Error: 'chgrp' failed"
+fi
+
 chmod -R g+r+w+X /usr/global/web-pages/lc/www/toolkit/docs/
+if [ $? -ne 0 ]; then
+    echo "Error: 'chmod' failed"
+fi
+
 chmod -R o+r+X /usr/global/web-pages/lc/www/toolkit/docs/
+if [ $? -ne 0 ]; then
+    echo "Error: 'chmod' failed"
+fi
 echo "-----------------------------------------------------------------------"
