f1c4280e0f4510b748691da00929256076aeda43
Kenneth Weiss <weiss27@llnl.gov>
2016-05-19 11:01:11 -0700

    Guard fortran MPI header vs. module logic behind an if(ENABLE_MPI).

--- src/cmake/SetupCompilerOptions.cmake
+++ src/cmake/SetupCompilerOptions.cmake
@@ -201,19 +201,21 @@ if(ENABLE_FORTRAN)
     include(FortranCInterface)
     FortranCInterface_HEADER(${HEADER_INCLUDES_DIRECTORY}/common/FC.h MACRO_NAMESPACE "FC_")
 
-    # Determine if we should use fortran mpif.h header or fortran mpi module
-    find_path(mpif_path
-        NAMES "mpif.h"
-        PATHS ${MPI_Fortran_INCLUDE_PATH}
-        NO_DEFAULT_PATH
-        )
-    
-    if(mpif_path)
-        set(MPI_Fortran_USE_MPIF ON CACHE PATH "")
-        message(STATUS "Using MPI Fortran header: mpif.h")
-    else()
-        set(MPI_Fortran_USE_MPIF OFF CACHE PATH "")
-        message(STATUS "Using MPI Fortran module: mpi.mod")
+    if (ENABLE_MPI)
+        # Determine if we should use fortran mpif.h header or fortran mpi module
+        find_path(mpif_path
+            NAMES "mpif.h"
+            PATHS ${MPI_Fortran_INCLUDE_PATH}
+            NO_DEFAULT_PATH
+            )
+        
+        if(mpif_path)
+            set(MPI_Fortran_USE_MPIF ON CACHE PATH "")
+            message(STATUS "Using MPI Fortran header: mpif.h")
+        else()
+            set(MPI_Fortran_USE_MPIF OFF CACHE PATH "")
+            message(STATUS "Using MPI Fortran module: mpi.mod")
+        endif()
     endif()
     
 else()
