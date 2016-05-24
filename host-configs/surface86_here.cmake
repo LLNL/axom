bfdb2b7875032f929a689d1d3f13f82d5dd3bb49
Cyrus Harrison <cyrush@llnl.gov>
2016-05-24 16:27:49 -0700

    tpls: add cz host configs

---
+++ host-configs/surface86_here.cmake
@@ -0,0 +1,13 @@
+#######
+# uberenv host-config for asctoolkit
+#######
+# sphinx from uberenv
+set(SPHINX_EXECUTABLE "/usr/gapps/asctoolkit/thirdparty_libs/spack/opt/chaos_5_x86_64_ib/gcc@4.4.7/python@2.7.8/bin/sphinx-build" CACHE PATH "")
+
+# uncrustify from uberenv
+set(UNCRUSTIFY_EXECUTABLE "/usr/gapps/asctoolkit/thirdparty_libs/spack/opt/chaos_5_x86_64_ib/gcc@4.4.7/uncrustify@0.61/bin/uncrustify" CACHE PATH "")
+
+# boost headers from uberenv
+set(BOOST_ROOT "/usr/gapps/asctoolkit/thirdparty_libs/spack/opt/chaos_5_x86_64_ib/gcc@4.4.7/boost-headers@1.55.0" CACHE PATH "")
+
+
