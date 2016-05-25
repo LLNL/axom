b2b33b4d112f637fb761423e7f031e34d4801742
Cyrus Harrison <cyrush@llnl.gov>
2016-05-25 13:31:46 -0700

    uberenv: sbang fix sphinx post breathe install
    for some reason breathe's setuptools install reinstalls sphinx's
    scripts (even though it finds the existing sphinx install).
    This breaks the sbang patch we already had done, so we need to
    reapply it.

--- scripts/uberenv/packages/py-breathe/package.py
+++ scripts/uberenv/packages/py-breathe/package.py
@@ -15,3 +15,16 @@ class PyBreathe(Package):
         # breathe + sphinx doesn't play well with --prefix installs, for now simply 
         # install to the spack python
         python('setup.py', 'install') #, '--prefix=%s' % prefix)
+        # sphinx_build lives in python's bin dir and for some reason breathe's
+        # setuptools install reinstalls the sphinx build scripts (even though
+        # it finds the existing sphinx install).
+        # To keep this from undermining us, we need to patch the new copies
+        # of these scripts.
+        sphinx_scripts = ["sphinx-apidoc",
+                          "sphinx-autogen",
+                          "sphinx-build",
+                          "sphinx-quickstart"]
+        for script in sphinx_scripts:
+            script_path = join_path(spec["python"].prefix,"bin",script)
+            # use spack sbang to fix issues with shebang that is too long
+            filter_shebang(script_path)
