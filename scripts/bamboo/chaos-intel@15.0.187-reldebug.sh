d02bfe2c68089b655c47b38981a880d1c6c358f6
Kenneth Weiss <weiss27@llnl.gov>
2016-06-02 20:40:07 -0700

    Updated bamboo scripts to use host-config files. ATK-762.
    Also separated variables for easier readability.

--- scripts/bamboo/chaos-intel@15.0.187-reldebug.sh
+++ scripts/bamboo/chaos-intel@15.0.187-reldebug.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
+
+COMPILER="intel@15.0.187"
+
+if [[ $HOSTNAME == rz* ]]; then
+    HC="host-configs/rzmerl-chaos_5_x86_64_ib-${COMPILER}.cmake"
+else
+    HC="host-configs/surface-chaos_5_x86_64_ib-${COMPILER}.cmake"
+fi
+
+BT="RelWithDebInfo"
+BP="build-chaos-${COMPILER}-${BT,,}"
+IP="install-chaos-${COMPILER}-${BT,,}"
+COMP_OPT=""
+BUILD_OPT="-DENABLE_CXX11=FALSE"
+
+
+
 echo "Configuring..."
 echo "-----------------------------------------------------------------------"
-./scripts/config-build.py -c intel@15.0.187 --buildtype RelWithDebInfo -DENABLE_CXX11=FALSE
+./scripts/config-build.py -ecc -hc $HC -bt $BT -bp $BP -ip $IP $COMP_OPT $BUILD_OPT    
 if [ $? -ne 0 ]; then
     echo "Error: config-build.py failed"
     exit 1
 fi
 echo "-----------------------------------------------------------------------"
 
-cd build-chaos-intel@15.0.187-relwithdebinfo
+cd $BP
     echo "Generating C/Fortran binding..."
     make VERBOSE=1 generate
     if [ $? -ne 0 ]; then
