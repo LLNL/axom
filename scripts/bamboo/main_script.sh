0e671ba5d52af6e4a0368adc34b59657b2f8fe9c
Michael H. Chang <chang28@llnl.gov>
2016-09-19 14:13:17 -0700

    remove all environment variables setting from main_script.sh

--- scripts/bamboo/main_script.sh
+++ scripts/bamboo/main_script.sh
@@ -3,21 +3,6 @@
 # 09-16-2016 chang28, build-and-test.sh "clang@3.5.0" "Debug" ""
 # 09-19-2016 chang28, the decider has decided to have a configuration file call a main_script file, this is the main_script file, all environment variables are set up in the configuration file. 
 
-#COMPILER="clang@3.5.0"
-#COMPILER=$1
-
-#if [[ $HOSTNAME == rz* ]]; then
-#    HC="host-configs/rzmerl-chaos_5_x86_64_ib-${COMPILER}.cmake"
-#else
-#    HC="host-configs/surface-chaos_5_x86_64_ib-${COMPILER}.cmake"
-#fi
-#BT="Debug"
-BT=$2
-BP="atk_build"
-IP="atk_install"
-COMP_OPT=""
-BUILD_OPT=$3
-OPTIONS="-ecc -hc $HC -bt $BT -bp $BP -ip $IP $COMP_OPT $BUILD_OPT"
 
 echo "Configuring..."
 echo "-----------------------------------------------------------------------"
