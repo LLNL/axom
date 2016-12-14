da0a4cac90e1dafadc1adc294de44315c23205ef
Kenneth Weiss <weiss27@llnl.gov>
2016-12-13 17:50:50 -0800

    Adds definitions to config file for enabled components and libraries (built-in and third party).

--- src/cmake/CMakeConfigureFile.cmake
+++ src/cmake/CMakeConfigureFile.cmake
@@ -4,7 +4,38 @@
 
 if( (CMAKE_CXX_STANDARD EQUAL 11) OR (CMAKE_CXX_STANDARD EQUAL 14) )
     set(USE_CXX11 TRUE)
+    set(ATK_USE_CXX11 TRUE)
 endif()
+
+
+## Add a configuration define for each library dependency 
+## that we might need to know about in the code
+
+set(DEPS_OPTIONS BOOST MPI OPENMP)              # vars of the form ENABLE_DEP
+foreach(dep in ${DEPS_OPTIONS})
+    if( ENABLE_${dep} )
+        set(ATK_USE_${dep} TRUE)
+    endif()
+endforeach()
+
+set(DEPS_BUILTIN CONDUIT HDF5 SPARSEHASH FMT)  # vars of the form DEP_FOUND
+foreach(dep in ${DEPS_BUILTIN})
+    if( ${dep}_FOUND )
+        set(ATK_USE_${dep} TRUE)
+    endif()
+endforeach()
+
+
+## Add a configuration define for each enabled component
+set(COMPS COMMON LUMBERJACK SLIC SLAM SIDRE MINT QUEST SPIO)
+foreach(comp in ${COMPS})
+    if( ENABLE_${comp} )
+        set(ATK_USE_${comp} TRUE)
+    endif()
+endforeach()
+
+
+
 
 configure_file(
     components/common/src/config.hpp.in
