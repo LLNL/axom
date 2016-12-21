6b2bf23d0f42dcdd526478a8e8ac73f1d498b108
Cyrus Harrison <cyrush@llnl.gov>
2016-12-21 12:25:34 -0800

    enh: update conduit to resolve bgq issues
    use newer version of conduit that compiles on bgq using xl and gcc
    
    add variants to the toolkit uberenv package that allow us to
    build subsets of our dependencies. These variants allow us to build
    using a system cmake, or skip building dev tools such as uncrustify.

--- scripts/uberenv/packages/uberenv-asctoolkit/package.py
+++ scripts/uberenv/packages/uberenv-asctoolkit/package.py
@@ -5,6 +5,9 @@ import os
 import platform
 from os.path import join as pjoin
 
+def cmake_cache_entry(name,value):
+    return 'set("%s" "%s" CACHE PATH "")\n\n' % (name,value)
+
 class UberenvAsctoolkit(Package):
     """Spack Based Uberenv Build for Toolkit TPLs """
 
@@ -12,31 +15,46 @@ class UberenvAsctoolkit(Package):
     version('0.1', '8d378ef62dedc2df5db447b029b71200')
  
     homepage = "http://lc.llnl.gov/toolkit"
-    # standard spack packages
-    depends_on("doxygen")
-    depends_on("python")
-    depends_on("hdf5~shared~zlib~fortran")
-    depends_on("uncrustify")
-    depends_on("lua@5.1.5")
 
-    # custom spack packages
-    depends_on("cmake@3.3.1")
-    depends_on("conduit~shared")
-    depends_on("py-sphinx")
-    depends_on("py-breathe")
-    depends_on("py-pyyaml")
-    depends_on("py-parsley")
-    depends_on("py-cogapp")
+    # variants that allow us to winnow what TPLS we build
+    # use ~cmake to skip cmake build and use whatever cmake is in the
+    # users path 
+    # (given the pain of building cmake on BGQ, this is really on for BGQ)
+    variant('cmake',   default=True, description="Build cmake.")
+    variant('devtools',default=True, description="Build development tools (such as sphinx, uncrustify, etc)")
+    variant("python",  default=True, description="Build python")
+    variant("lua",     default=True, description="Build lua")
+
+
+    depends_on("conduit~shared+cmake",when="+cmake")
+    depends_on("conduit~shared~cmake",when="~cmake")
+    
+    depends_on("hdf5~shared~zlib~fortran")
+    # boost, header only
+    depends_on("boost-headers")
+    depends_on("sparsehash-headers")
+
+    # optional tpl builds
+    depends_on("cmake@3.3.1",when="+cmake")
+
+    depends_on("python",    when="+devtools")
+    depends_on("doxygen",   when="+devtools")
+    depends_on("uncrustify",when="+devtools")
+
+    depends_on("python",   when="+python")
+    depends_on("lua@5.1.5",when="+lua")
+
+    depends_on("py-sphinx", when="+devtools")
+    depends_on("py-breathe",when="+devtools")
+    depends_on("py-pyyaml", when="+devtools")
+    depends_on("py-parsley",when="+devtools")
+    depends_on("py-cogapp", when="+devtools")
 
     if "darwin" in platform.system().lower():
         depends_on("mpich")
 
     if not "darwin" in platform.system().lower():
-        depends_on("lcov")
-
-    # boost, header only
-    depends_on("boost-headers")
-    depends_on("sparsehash-headers")
+        depends_on("lcov",when="+devtools")
 
     # use dummy tarfile to avoid downloads
     def url_for_version(self, version):
@@ -65,14 +83,15 @@ class UberenvAsctoolkit(Package):
             sys_type = env["SYS_TYPE"]
 
         # conduit
-        conduit_dir      = spec['conduit'].prefix
-        cmake_exe        = pjoin(spec['cmake'].prefix.bin,"cmake")
-        doxygen_exe      = pjoin(spec['doxygen'].prefix.bin,"doxygen")
-        python_exe       = pjoin(spec['python'].prefix.bin,"python")
-        lua_dir          = spec['lua'].prefix
-        sphinx_build_exe = pjoin(spec['python'].prefix.bin,"sphinx-build")
-        uncrustify_exe   = pjoin(spec['uncrustify'].prefix.bin,"uncrustify")
-
+        if "+cmake" in spec:
+            cmake_exe = pjoin(spec['cmake'].prefix.bin,"cmake")
+        else:
+            cmake_exe = which("cmake")
+            if cmake_exe is None:
+                #error could not find cmake!
+                crash()
+            cmake_exe = cmake_exe.command
+        
         host_cfg_fname = "%s-%s-%s.cmake" % (socket.gethostname().rstrip('1234567890'),sys_type,spec.compiler)
         host_cfg_fname = pjoin(dest_dir,host_cfg_fname)
         cfg = open(host_cfg_fname,"w")
@@ -91,50 +110,76 @@ class UberenvAsctoolkit(Package):
         cfg.write("#######\n")
         cfg.write("# using %s compiler spec\n" % spec.compiler)
         cfg.write("#######\n\n")
+
         cfg.write("# c compiler used by spack\n")
-        cfg.write('set(CMAKE_C_COMPILER "%s" CACHE PATH "")\n\n' % c_compiler)
+        cfg.write(cmake_cache_entry("CMAKE_C_COMPILER",c_compiler))
+
         cfg.write("# cpp compiler used by spack\n")
-        cfg.write('set(CMAKE_CXX_COMPILER "%s" CACHE PATH "")\n\n' % cpp_compiler)
+        cfg.write(cmake_cache_entry("CMAKE_CXX_COMPILER",cpp_compiler))
+        
         cfg.write("# fortran compiler used by spack\n")
         if not f_compiler is None:
-            cfg.write('set(ENABLE_FORTRAN ON CACHE PATH "")\n\n')
-            cfg.write('set(CMAKE_Fortran_COMPILER  "%s" CACHE PATH "")\n\n' % f_compiler)
+            cfg.write(cmake_cache_entry("ENABLE_FORTRAN","ON"))
+            cfg.write(cmake_cache_entry("CMAKE_Fortran_COMPILER",f_compiler))
         else:
             cfg.write("# no fortran compiler\n\n")
-            cfg.write('set(ENABLE_FORTRAN OFF CACHE PATH "")\n\n')
+            cfg.write(cmake_cache_entry("ENABLE_FORTRAN","OFF"))
 
         cfg.write("# hdf5 from uberenv\n")
-        cfg.write('set(HDF5_DIR "%s" CACHE PATH "")\n\n' % spec['hdf5'].prefix)
+        cfg.write(cmake_cache_entry("HDF5_DIR",spec['hdf5'].prefix))
 
         cfg.write("# conduit from uberenv\n")
-        cfg.write('set(CONDUIT_DIR "%s" CACHE PATH "")\n\n' % conduit_dir)
-
-        cfg.write("# doxygen from uberenv\n")
-        cfg.write('set(DOXYGEN_EXECUTABLE "%s" CACHE PATH "")\n\n' % doxygen_exe)
-
-        cfg.write("# python from uberenv\n")
-        cfg.write('set(PYTHON_EXECUTABLE "%s" CACHE PATH "")\n\n' % python_exe)
-        
-        cfg.write("# lua from uberenv\n")
-        cfg.write('set(LUA_DIR "%s" CACHE PATH "")\n\n' % lua_dir)
-        
-        cfg.write("# sphinx from uberenv\n")
-        cfg.write('set(SPHINX_EXECUTABLE "%s" CACHE PATH "")\n\n' % sphinx_build_exe)
-
-        cfg.write("# uncrustify from uberenv\n")
-        cfg.write('set(UNCRUSTIFY_EXECUTABLE "%s" CACHE PATH "")\n\n' % uncrustify_exe)
+        cfg.write(cmake_cache_entry("CONDUIT_DIR",spec['conduit'].prefix))
 
         cfg.write("# sparsehash headers from uberenv\n")
-        cfg.write('set(SPARSEHASH_DIR "%s" CACHE PATH "")\n\n' % spec['sparsehash-headers'].prefix)
+        cfg.write(cmake_cache_entry("SPARSEHASH_DIR",spec['sparsehash-headers'].prefix))
 
         cfg.write("# boost headers from uberenv\n")
-        cfg.write('set(ENABLE_BOOST ON CACHE PATH "")\n')
-        cfg.write('set(BOOST_ROOT "%s" CACHE PATH "")\n\n' % spec['boost-headers'].prefix)
+        cfg.write(cmake_cache_entry("ENABLE_BOOST","ON"))
+        cfg.write(cmake_cache_entry("BOOST_ROOT",spec['boost-headers'].prefix))
+
+        # optional tpls
+
+        if "python" in spec or "devtools" in spec:
+            cfg.write("# python from uberenv\n")
+            python_exe = pjoin(spec['python'].prefix.bin,"python")
+            cfg.write(cmake_cache_entry("PYTHON_EXECUTABLE",python_exe))
+        else:
+            cfg.write("# python not build by uberenv\n\n")
+
+        if "lua" in spec:
+            cfg.write("# lua from uberenv\n")
+            cfg.write(cmake_cache_entry("LUA_DIR",spec['lua'].prefix))
+        else:
+            cfg.write("# lua not build by uberenv\n\n")
+
+        # optional tpls (dev tools)
+
+        if "doxygen" in spec:
+            cfg.write("# doxygen from uberenv\n")
+            doxygen_exe = pjoin(spec['doxygen'].prefix.bin,"doxygen")
+            cfg.write(cmake_cache_entry("DOXYGEN_EXECUTABLE", doxygen_exe))
+        else:
+            cfg.write("# doxygen not built by uberenv\n\n")
+
+        if "sphinx" in spec:
+            cfg.write("# sphinx from uberenv\n")
+            sphinx_build_exe = pjoin(spec['python'].prefix.bin,"sphinx-build")
+            cfg.write(cmake_cache_entry("SPHINX_EXECUTABLE", sphinx_build_exe))
+        else:
+            cfg.write("# sphinx not built by uberenv\n\n")
+
+        if "uncrustify" in spec:
+            cfg.write("# uncrustify from uberenv\n")
+            uncrustify_exe   = pjoin(spec['uncrustify'].prefix.bin,"uncrustify")
+            cfg.write(cmake_cache_entry("UNCRUSTIFY_EXECUTABLE", uncrustify_exe))
+        else:
+            cfg.write("# uncrustify not built by uberenv\n\n")
 
         cfg.write("# lcov and genhtml from uberenv\n")
         if "lcov" in spec:
-            cfg.write('set(LCOV_PATH "%s/usr/bin/lcov" CACHE PATH "")\n\n' % spec['lcov'].prefix)
-            cfg.write('set(GENHTML_PATH "%s/usr/bin/genhtml" CACHE PATH "")\n\n' % spec['lcov'].prefix)
+            cfg.write(cmake_cache_entry("LCOV_PATH", pjoin(spec['lcov'].prefix,"usr","bin","lcov")))
+            cfg.write(cmake_cache_entry("GENHTML_PATH",pjoin(spec['lcov'].prefix,"usr","bin","genhtml")))
         else:
             cfg.write("# lcov and genhtml not built by uberenv\n\n")
 
@@ -144,20 +189,20 @@ class UberenvAsctoolkit(Package):
             mpicc    = pjoin(spec['mpich'].prefix.bin,"mpicc")
             mpif90   = pjoin(spec['mpich'].prefix.bin,"mpif90")
             cfg.write("# MPI Support\n")
-            cfg.write('set(ENABLE_MPI ON CACHE PATH "")\n\n')
-            cfg.write('set(MPI_C_COMPILER  "%s" CACHE PATH "")\n\n' % mpicc)
+            cfg.write(cmake_cache_entry("ENABLE_MPI","ON"))
+            cfg.write(cmake_cache_entry("MPI_C_COMPILER",mpicc))
             # we use `mpicc` as `MPI_CXX_COMPILER` b/c we don't want to introduce 
             # linking deps to the MPI C++ libs (we aren't using C++ features of MPI)
-            cfg.write('set(MPI_CXX_COMPILER "%s" CACHE PATH "")\n\n' % mpicc)
-            cfg.write('set(MPI_Fortran_COMPILER "%s" CACHE PATH "")\n\n' % mpif90)
-            cfg.write('set(MPIEXEC "%s" CACHE PATH "")\n\n' % mpiexec)
+            cfg.write(cmake_cache_entry("MPI_CXX_COMPILER",mpicc))
+            cfg.write(cmake_cache_entry("MPI_Fortran_COMPILER",mpif90))
+            cfg.write(cmake_cache_entry("MPIEXEC",mpiexec))
 
 
         # Note (KW 3/2016) -- per ATK-659, we are temporarily disabling CXX11 for default configurations on intel builds 
         if "intel" in spec.compiler.name:
             cfg.write("# Temporarily disable CXX11 on intel builds until we resolve issue ATK-619\n")
-            cfg.write('set(BLT_CXX_STD "c++98" CACHE PATH "")\n')
-            
+            cfg.write(cmake_cache_entry("BLT_CXX_STD","c++98"))
+
         cfg.write("##################################\n")
         cfg.write("# end uberenv host-config\n")
         cfg.write("##################################\n")
