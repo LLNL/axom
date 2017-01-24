91b90678154647ff11eb7122cc48c3797c19e2e2
Michael H. Chang <chang28@llnl.gov>
2017-01-24 11:22:09 -0800

    add more message

--- scripts/bamboo/bgq_test.sh
+++ scripts/bamboo/bgq_test.sh
@@ -1,1 +1,4 @@
+# chang28 01-23-2017, this is the script that kicks of test
+echo "running bgq_test.sh"
+echo "srun -N1 -n1 -ppsmall ../../atk_build/tests/slic_fmt"
 srun -N1 -n1 -ppsmall ../../atk_build/tests/slic_fmt
