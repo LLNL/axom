43e513435ec7dc26e11cb9116cf3f822f1585604
Michael H. Chang <chang28@llnl.gov>
2017-01-25 10:21:25 -0800

    add test case slic_asserts

--- scripts/bamboo/bgq_test.sh
+++ scripts/bamboo/bgq_test.sh
@@ -1,6 +1,10 @@
-# chang28 01-23-2017, this is the script that kicks of test
-echo "running bgq_test.sh"
+# chang28 01-23-2017, this is the script that kicks off test
+# chang28 01-25-2017, 
+
+echo "running bgq_test.sh version 0.9"
 echo "cd ../../atk_build/tests"
 cd ../../atk_build/tests
+echo "srun -N1 -n1 -ppsmall ./slic_asserts "
+srun -N1 -n1 -ppsmall ./slic_asserts
 echo "srun -N1 -n1 -ppsmall ./slic_fmt "
 srun -N1 -n1 -ppsmall ./slic_fmt
