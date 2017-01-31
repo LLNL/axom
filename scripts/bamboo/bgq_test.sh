cce9a16f23c7374a05694a359a6b4f737a7a9ba7
Michael H. Chang <chang28@llnl.gov>
2017-01-31 15:59:23 -0800

    make sure the name is right

--- scripts/bamboo/bgq_test.sh
+++ scripts/bamboo/bgq_test.sh
@@ -1,7 +1,7 @@
 # chang28 01-23-2017, this is the script that kicks off test
 # chang28 01-25-2017, 
 # chang28 01-26-2017, 
-# chang28, 01-31-2017, change the script to generate bgq_autogen_test.sh on the fly to run this script please cd asctoolkit and ./scripts/bamboo/bgq-test.sh
+# chang28, 01-31-2017, change the script to generate bgq_autogen_test.sh on the fly to run this script please cd asctoolkit and ./scripts/bamboo/bgq_test.sh
 
 echo "running bgq_test.sh version 1.00"
 echo "cd atk_build"
