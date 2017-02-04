6cfcfd8e66de4d4e0122b7498583d60957211b30
Lee Taylor <ltaylor@llnl.gov>
2017-02-04 12:02:58 -0800

    Rename src directory to shroud
    The convention is to name the source directory after the Python
    module.

--- src/components/shroud/setup.py
+++ src/components/shroud/setup.py
@@ -72,12 +72,6 @@ setup(
     # simple. Or you can use find_packages().
 #    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     packages=['shroud'],
-    package_dir = {'shroud':'src'},
-#    package_dir={ '': '${CMAKE_CURRENT_SOURCE_DIR}' },
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
