4a22549337c5eba51df07fd732a50c8c81065745
Cyrus <cyrush@llnl.gov>
2017-02-09 08:05:38 -0800

    update rz chaos host configs

--- host-configs/rzmerl-chaos_5_x86_64_ib-clang@3.5.0.cmake
+++ host-configs/rzmerl-chaos_5_x86_64_ib-clang@3.5.0.cmake
@@ -7,7 +7,7 @@
 ##################################
 
 # cmake from uberenv
-# cmake executable path: /usr/workspace/wsrzc/toolkit/thirdparty_libs/builds/2017_02_07_16_21_30/spack/opt/spack/chaos_5_x86_64_ib/clang-3.5.0/cmake-3.3.1-yonvpqi2syehljvwq2uotcjmb2xydcoi/bin/cmake
+# cmake executable path: /usr/workspace/wsrzc/toolkit/thirdparty_libs/builds/2017_02_09_00_20_03/spack/opt/spack/chaos_5_x86_64_ib/clang-3.5.0/cmake-3.3.1-yonvpqi2syehljvwq2uotcjmb2xydcoi/bin/cmake
 
 #######
 # using clang@3.5.0 compiler spec
@@ -25,36 +25,36 @@ set("CMAKE_CXX_COMPILER" "/usr/global/tools/clang/chaos_5_x86_64_ib/clang-omp-3.
 set("ENABLE_FORTRAN" "OFF" CACHE PATH "")
 
 # hdf5 from uberenv
-set("HDF5_DIR" "/usr/workspace/wsrzc/toolkit/thirdparty_libs/builds/2017_02_07_16_21_30/spack/opt/spack/chaos_5_x86_64_ib/clang-3.5.0/hdf5-1.8.16-vnqt5g36quxilv4oa3ceqhgjhaelxd56" CACHE PATH "")
+set("HDF5_DIR" "/usr/workspace/wsrzc/toolkit/thirdparty_libs/builds/2017_02_09_00_20_03/spack/opt/spack/chaos_5_x86_64_ib/clang-3.5.0/hdf5-1.8.16-ffqxeq6flvcqk2fo6gscvkqvuhvouovx" CACHE PATH "")
 
 # conduit from uberenv
-set("CONDUIT_DIR" "/usr/workspace/wsrzc/toolkit/thirdparty_libs/builds/2017_02_07_16_21_30/spack/opt/spack/chaos_5_x86_64_ib/clang-3.5.0/conduit-0.2.1-yftov5c2i272fsknq4by6nyhejr7rt3x" CACHE PATH "")
+set("CONDUIT_DIR" "/usr/workspace/wsrzc/toolkit/thirdparty_libs/builds/2017_02_09_00_20_03/spack/opt/spack/chaos_5_x86_64_ib/clang-3.5.0/conduit-0.2.1-2zxz34weo5uxrn756dxq5dj2n2abzpvs" CACHE PATH "")
 
 # sparsehash headers from uberenv
-set("SPARSEHASH_DIR" "/usr/workspace/wsrzc/toolkit/thirdparty_libs/builds/2017_02_07_16_21_30/spack/opt/spack/chaos_5_x86_64_ib/clang-3.5.0/sparsehash-headers-2.0.2-afi2mzxg35xtfnrdfsci5toyjcwdolpq" CACHE PATH "")
+set("SPARSEHASH_DIR" "/usr/workspace/wsrzc/toolkit/thirdparty_libs/builds/2017_02_09_00_20_03/spack/opt/spack/chaos_5_x86_64_ib/clang-3.5.0/sparsehash-headers-2.0.2-afi2mzxg35xtfnrdfsci5toyjcwdolpq" CACHE PATH "")
 
 # boost headers from uberenv
-set("BOOST_DIR" "/usr/workspace/wsrzc/toolkit/thirdparty_libs/builds/2017_02_07_16_21_30/spack/opt/spack/chaos_5_x86_64_ib/clang-3.5.0/boost-headers-1.58.0-a6n4lbbqdnhhkivpxazq5e4zrdhaspei" CACHE PATH "")
+set("BOOST_DIR" "/usr/workspace/wsrzc/toolkit/thirdparty_libs/builds/2017_02_09_00_20_03/spack/opt/spack/chaos_5_x86_64_ib/clang-3.5.0/boost-headers-1.58.0-a6n4lbbqdnhhkivpxazq5e4zrdhaspei" CACHE PATH "")
 
 # python from uberenv
-set("PYTHON_EXECUTABLE" "/usr/workspace/wsrzc/toolkit/thirdparty_libs/builds/2017_02_07_16_21_30/spack/opt/spack/chaos_5_x86_64_ib/clang-3.5.0/python-2.7.11-kmpji7fw4s22cfziy4byyfl22wjrmc7n/bin/python" CACHE PATH "")
+set("PYTHON_EXECUTABLE" "/usr/workspace/wsrzc/toolkit/thirdparty_libs/builds/2017_02_09_00_20_03/spack/opt/spack/chaos_5_x86_64_ib/clang-3.5.0/python-2.7.11-kmpji7fw4s22cfziy4byyfl22wjrmc7n/bin/python" CACHE PATH "")
 
 # lua from uberenv
-set("LUA_DIR" "/usr/workspace/wsrzc/toolkit/thirdparty_libs/builds/2017_02_07_16_21_30/spack/opt/spack/chaos_5_x86_64_ib/clang-3.5.0/lua-5.1.5-mkz7ex36zg7oqdon6vxocr2tfk2i6in5" CACHE PATH "")
+set("LUA_DIR" "/usr/workspace/wsrzc/toolkit/thirdparty_libs/builds/2017_02_09_00_20_03/spack/opt/spack/chaos_5_x86_64_ib/clang-3.5.0/lua-5.1.5-mkz7ex36zg7oqdon6vxocr2tfk2i6in5" CACHE PATH "")
 
 # doxygen from uberenv
-set("DOXYGEN_EXECUTABLE" "/usr/workspace/wsrzc/toolkit/thirdparty_libs/builds/2017_02_07_16_21_30/spack/opt/spack/chaos_5_x86_64_ib/clang-3.5.0/doxygen-1.8.11-ef4b7mhn6rksthrqz47zxtndjhg2e6wq/bin/doxygen" CACHE PATH "")
+set("DOXYGEN_EXECUTABLE" "/usr/workspace/wsrzc/toolkit/thirdparty_libs/builds/2017_02_09_00_20_03/spack/opt/spack/chaos_5_x86_64_ib/clang-3.5.0/doxygen-1.8.11-ef4b7mhn6rksthrqz47zxtndjhg2e6wq/bin/doxygen" CACHE PATH "")
 
 # sphinx from uberenv
-set("SPHINX_EXECUTABLE" "/usr/workspace/wsrzc/toolkit/thirdparty_libs/builds/2017_02_07_16_21_30/spack/opt/spack/chaos_5_x86_64_ib/clang-3.5.0/python-2.7.11-kmpji7fw4s22cfziy4byyfl22wjrmc7n/bin/sphinx-build" CACHE PATH "")
+set("SPHINX_EXECUTABLE" "/usr/workspace/wsrzc/toolkit/thirdparty_libs/builds/2017_02_09_00_20_03/spack/opt/spack/chaos_5_x86_64_ib/clang-3.5.0/python-2.7.11-kmpji7fw4s22cfziy4byyfl22wjrmc7n/bin/sphinx-build" CACHE PATH "")
 
 # uncrustify from uberenv
-set("UNCRUSTIFY_EXECUTABLE" "/usr/workspace/wsrzc/toolkit/thirdparty_libs/builds/2017_02_07_16_21_30/spack/opt/spack/chaos_5_x86_64_ib/clang-3.5.0/uncrustify-0.61-hfvykjq7rsj5v7kb3blntogdcuewppl2/bin/uncrustify" CACHE PATH "")
+set("UNCRUSTIFY_EXECUTABLE" "/usr/workspace/wsrzc/toolkit/thirdparty_libs/builds/2017_02_09_00_20_03/spack/opt/spack/chaos_5_x86_64_ib/clang-3.5.0/uncrustify-0.61-hfvykjq7rsj5v7kb3blntogdcuewppl2/bin/uncrustify" CACHE PATH "")
 
 # lcov and genhtml from uberenv
-set("LCOV_PATH" "/usr/workspace/wsrzc/toolkit/thirdparty_libs/builds/2017_02_07_16_21_30/spack/opt/spack/chaos_5_x86_64_ib/clang-3.5.0/lcov-1.11-otvm7uutci77rwj7cygg4d5vgecdorif/usr/bin/lcov" CACHE PATH "")
+set("LCOV_PATH" "/usr/workspace/wsrzc/toolkit/thirdparty_libs/builds/2017_02_09_00_20_03/spack/opt/spack/chaos_5_x86_64_ib/clang-3.5.0/lcov-1.11-otvm7uutci77rwj7cygg4d5vgecdorif/usr/bin/lcov" CACHE PATH "")
 
-set("GENHTML_PATH" "/usr/workspace/wsrzc/toolkit/thirdparty_libs/builds/2017_02_07_16_21_30/spack/opt/spack/chaos_5_x86_64_ib/clang-3.5.0/lcov-1.11-otvm7uutci77rwj7cygg4d5vgecdorif/usr/bin/genhtml" CACHE PATH "")
+set("GENHTML_PATH" "/usr/workspace/wsrzc/toolkit/thirdparty_libs/builds/2017_02_09_00_20_03/spack/opt/spack/chaos_5_x86_64_ib/clang-3.5.0/lcov-1.11-otvm7uutci77rwj7cygg4d5vgecdorif/usr/bin/genhtml" CACHE PATH "")
 
 ##################################
 # end uberenv host-config
