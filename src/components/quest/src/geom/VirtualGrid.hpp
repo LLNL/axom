f59e5a2a963b19ceedd3464bd37da18799ade532
Arlie Capps <capps2@llnl.gov>
2017-02-24 10:22:19 -0800

    Addressed many reviewer comments as well as minor style and spacing issues.

--- src/components/quest/src/geom/VirtualGrid.hpp
+++ src/components/quest/src/geom/VirtualGrid.hpp
@@ -14,8 +14,8 @@
 #include "common/CommonTypes.hpp"
 #include "slic/slic.hpp"
 
-
-// #include "slam/Utilities.hpp"
+#include "quest/BoundingBox.hpp"
+#include "quest/Point.hpp"
 
 
 // C/C++ includes
@@ -27,14 +27,10 @@
 #include <stdio.h>
 
 
-#include "quest/BoundingBox.hpp"
-#include "quest/Point.hpp"
-
-
 namespace quest {
 
 //NDIMS IS EITHER 2 or 3
-template< typename T,int NDIMS >
+template< typename T, int NDIMS >
 class VirtualGrid
 {
 public:
@@ -44,29 +40,38 @@ public:
 
 public:
 
-  VirtualGrid();
-  VirtualGrid(const PointType& pt,const double * spacing,const int * res);
+  VirtualGrid(const PointType& origin, const double * spacing, const int * res);
+  VirtualGrid(const double * origin, const double * spacing, const int * res);
     
-  int getIndex(const PointType & pt);
+  int getBinIndex(const PointType & pt);
   int getNumBins();
   bool binEmpty(int index);
   std::vector<T>& getBinContents(int index);
   const std::vector<T>& getBinContents(int index) const;
   void clear(int index);
-  void addObj(const T& obj,int index);
   void insert(const BoxType& BB, const T& obj);
+
+protected:
+
+  VirtualGrid();
     
 private:
 
   PointType m_origin;
-  double m_step[NDIMS];
+  double m_spacing[NDIMS];
   int m_resolution[NDIMS];
+
+  void addObj(const T& obj, int index);
+  bool isValidIndex(int index);
 
   struct Bin {
     std::vector<T> ObjectArray;
     //other stuff
   };
   std::vector<Bin> m_bins;
+
+  DISABLE_COPY_AND_ASSIGNMENT(VirtualGrid);
+  DISABLE_MOVE_AND_ASSIGNMENT(VirtualGrid);
 
 };//end class
 
@@ -75,15 +80,15 @@ private:
 namespace quest
 {
 
-template< typename T,int NDIMS >
-VirtualGrid< T,NDIMS >::VirtualGrid()
+template< typename T, int NDIMS >
+VirtualGrid< T, NDIMS >::VirtualGrid()
 {
 
   SLIC_ASSERT((NDIMS == 3) || (NDIMS == 2));
     
   for (int i=0; i<NDIMS; ++i) {
     m_origin[i] = 0;
-    m_step[i] = 1.0;
+    m_spacing[i] = 1.0;
     m_resolution[i] = 100;
   }
 
@@ -96,8 +101,8 @@ VirtualGrid< T,NDIMS >::VirtualGrid()
 
 }
 
-template< typename T,int NDIMS >
-VirtualGrid< T, NDIMS >::VirtualGrid(const PointType& pt,
+template< typename T, int NDIMS >
+VirtualGrid< T, NDIMS >::VirtualGrid(const PointType& origin,
                                      const double * step,
                                      const int * res)
 {
@@ -106,13 +111,13 @@ VirtualGrid< T, NDIMS >::VirtualGrid(const PointType& pt,
   SLIC_ASSERT((NDIMS == 3) || (NDIMS == 2));
 
   for (int i=0; i<NDIMS;++i) {
-    m_origin[i] = pt[i];
-    
+    m_origin[i] = origin[i];
+
     SLIC_ASSERT(step[i] !=0 );
-    // Quick fix to reduce the chance of rounding errors that were causing 
+    // Quick fix to reduce the chance of rounding errors that were causing
     // off by one indexing during divisions.
     // A more robust fix will be needed in the future.
-    m_step[i] = step[i];
+    m_spacing[i] = step[i];
     m_resolution[i] = res[i];
   }
 
@@ -124,24 +129,53 @@ VirtualGrid< T, NDIMS >::VirtualGrid(const PointType& pt,
   }
 }
 
-template< typename T,int NDIMS >
-int VirtualGrid<T,NDIMS>::getIndex(const PointType & pt)
+template< typename T, int NDIMS >
+VirtualGrid< T, NDIMS >::VirtualGrid(const double * origin,
+                                     const double * step,
+                                     const int * res)
+{
+  SLIC_ASSERT(origin != ATK_NULLPTR);
+  SLIC_ASSERT(step != ATK_NULLPTR);
+  SLIC_ASSERT(res != ATK_NULLPTR);
+  SLIC_ASSERT((NDIMS == 3) || (NDIMS == 2));
+
+  for (int i=0; i<NDIMS;++i) {
+    m_origin[i] = origin[i];
+    
+    SLIC_ASSERT(step[i] !=0 );
+    // Quick fix to reduce the chance of rounding errors that were causing 
+    // off by one indexing during divisions.
+    // A more robust fix will be needed in the future.
+    m_spacing[i] = step[i];
+    m_resolution[i] = res[i];
+  }
+
+  //assumes either 2 or 3 dimensional
+  if (NDIMS == 2) {
+    m_bins.resize(m_resolution[0] * m_resolution[1]);
+  } else {
+    m_bins.resize(m_resolution[0] * m_resolution[1] * m_resolution[2]);
+  }
+}
+
+template< typename T, int NDIMS >
+int VirtualGrid<T, NDIMS>::getBinIndex(const PointType & pt)
 {
   SLIC_ASSERT((NDIMS == 3) || (NDIMS == 2));
 
   //The below causes off by one due to rounding errors.  
   // I have done a quick fix above (in the non-default ctor).
-  int i = (pt[0] - m_origin[0])/m_step[0];
+  int i = (pt[0] - m_origin[0])/m_spacing[0];
 
 
   SLIC_ASSERT(i>=0 && i<m_resolution[0]);
    
-  int j = (pt[1] - m_origin[1])/m_step[1];
+  int j = (pt[1] - m_origin[1])/m_spacing[1];
 
   SLIC_ASSERT(j>=0 && j<m_resolution[1]);
 
   if (NDIMS==3) {
-    int k = (pt[2] - m_origin[2])/m_step[2];
+    int k = (pt[2] - m_origin[2])/m_spacing[2];
 
     SLIC_ASSERT(k>=0 && k<m_resolution[2]);
 
@@ -151,16 +185,22 @@ int VirtualGrid<T,NDIMS>::getIndex(const PointType & pt)
   }
 }
 
-template< typename T,int NDIMS >
-int VirtualGrid<T,NDIMS>::getNumBins()
+template< typename T, int NDIMS >
+bool VirtualGrid<T, NDIMS>::isValidIndex(int index)
+{
+  return index >=0 && index < static_cast<int>(m_bins.size());
+}
+
+template< typename T, int NDIMS >
+int VirtualGrid<T, NDIMS>::getNumBins()
 {
   return m_bins.size();
 }
 
-template< typename T,int NDIMS >
-bool VirtualGrid<T,NDIMS>::binEmpty(int index)
+template< typename T, int NDIMS >
+bool VirtualGrid<T, NDIMS>::binEmpty(int index)
 {
-  SLIC_ASSERT(index >=0 && index < static_cast<int>(m_bins.size()));
+  SLIC_ASSERT(isValidIndex(index));
 
   if (m_bins[index].ObjectArray.size() > 0) {
     return false;
@@ -169,80 +209,80 @@ bool VirtualGrid<T,NDIMS>::binEmpty(int index)
   }
 }
 
-template< typename T,int NDIMS >
+template< typename T, int NDIMS >
 //rchange these to pointers with a length?
-std::vector<T>& VirtualGrid<T,NDIMS>::getBinContents(int index)
+std::vector<T>& VirtualGrid<T, NDIMS>::getBinContents(int index)
 {
-  SLIC_ASSERT(index >=0 && index < static_cast<int>(m_bins.size()));
+  SLIC_ASSERT(isValidIndex(index));
   return  m_bins[index].ObjectArray;
 }
 
-template< typename T,int NDIMS >
+template< typename T, int NDIMS >
 //see comment on above function
-const std::vector<T>& VirtualGrid<T,NDIMS>::getBinContents(int index) const
+const std::vector<T>& VirtualGrid<T, NDIMS>::getBinContents(int index) const
 {
-  SLIC_ASSERT(index >=0 && index < static_cast<int>(m_bins.size()));
+  SLIC_ASSERT(isValidIndex(index));
   return  m_bins[index].ObjectArray;
 }
 
-template< typename T,int NDIMS >
-void VirtualGrid<T,NDIMS>::clear(int index)
+template< typename T, int NDIMS >
+void VirtualGrid<T, NDIMS>::clear(int index)
 {
-  SLIC_ASSERT(index >=0 && index < static_cast<int>(m_bins.size()));
+  SLIC_ASSERT(isValidIndex(index));
   m_bins[index].ObjectArray.resize(0);
 }
 
-template< typename T,int NDIMS >
-void VirtualGrid<T,NDIMS>::addObj(const T& obj,int index)
+template< typename T, int NDIMS >
+void VirtualGrid<T, NDIMS>::addObj(const T& obj, int index)
 {
-  SLIC_ASSERT(index >=0 && index < static_cast<int>(m_bins.size()));
+  SLIC_ASSERT(isValidIndex(index));
   m_bins[index].ObjectArray.push_back(obj);
 }
 
 
 //WIP For NDIMS == 3, NDIMS == 2 is same but needs to be added
 // by removing the k case and z_length
-template< typename T,int NDIMS>
-void VirtualGrid<T,NDIMS>::insert(const BoxType& BB, 
+template< typename T, int NDIMS>
+void VirtualGrid<T, NDIMS>::insert(const BoxType& BB,
                                   const T& obj)
 {
   SLIC_ASSERT((NDIMS == 3) || (NDIMS == 2));
-  PointType min,max;
+  PointType min, max;
    
   min = BB.getMin();
   max = BB.getMax();   
     
-  PointType tempx = PointType::make_point(max[0],min[1],min[2]);
-  PointType tempy = PointType::make_point(min[0],max[1],min[2]);
-  PointType tempz = PointType::make_point(max[0],min[1],max[2]);
+  PointType tempx = PointType::make_point(max[0], min[1], min[2]);
+  PointType tempy = PointType::make_point(min[0], max[1], min[2]);
+  PointType tempz = PointType::make_point(max[0], min[1], max[2]);
    
-  int start = getIndex(min);
+  int start = getBinIndex(min);
   //  SLIC_INFO("Minimum is "<< start);
   //  SLIC_INFO("Getting xlength with tempx"<<tempx);
 
-  int xlength = (getIndex(tempx)- start);
-  //    SLIC_INFO("Getting end index "<<getIndex(tempx));
+  int xlength = (getBinIndex(tempx)- start);
+  //    SLIC_INFO("Getting end index "<<getBinIndex(tempx));
   //SLIC_INFO("Getting x and y res");
 
   int x_res = m_resolution[0];
   int y_res = m_resolution[1];
    
   //    SLIC_INFO("Getting x and y res"<<x_res<<"and "<<y_res);
-  int ylength = (getIndex(tempy) / x_res) - ( start / x_res);
-  int zlength = (getIndex(tempz) / (x_res * y_res)) - (start / (x_res*y_res));
+  int ylength = (getBinIndex(tempy) / x_res) - ( start / x_res);
+  int zlength = (getBinIndex(tempz) / (x_res * y_res)) - (start / (x_res*y_res));
 
 
   //   SLIC_INFO("Lengths"<<xlength<<"and "<<ylength<<"and "<<zlength);
 
 
-  //int end = getIndex(max);
+  //int end = getBinIndex(max);
     
   for (int k=0; k<= zlength; ++k) {
     for (int j=0; j<=ylength; ++j) {
       for (int i=0; i<=xlength; ++i) {
 	// SLIC_INFO("Adding object at index " <<
         //           start + i + (j * x_res) + (k * x_res * y_res)) ;
-        addObj(obj,start + i + (j * x_res) + (k * x_res * y_res)); 
+        addObj(obj, start + i + (j * x_res) + (k * x_res * y_res));
       }
     }
   }
--- src/components/quest/tests/quest_virtual_grid.cpp
+++ src/components/quest/tests/quest_virtual_grid.cpp
@@ -17,47 +17,61 @@
 #include "quest/VirtualGrid.hpp"
 
 //-----------------------------------------------------------------------------
-TEST( quest_virtual_grid, default_constructor)
-{   static const int DIM = 3;
-    typedef double CoordType;
-    typedef quest::Point<CoordType, DIM> QPoint;
-    
-    quest::VirtualGrid<QPoint,DIM> valid;
-
-    EXPECT_TRUE(valid.getNumBins() == (100 * 100 * 100));//bins = 100^NDIMS
-    EXPECT_TRUE(valid.binEmpty(0));
-
-}
-
-TEST( quest_virtual_grid, argument_constructor)
+TEST( quest_virtual_grid, point_constructor)
 {
     static const int DIM = 3;
     typedef double CoordType;
     typedef quest::Point<CoordType, DIM> QPoint;
     
-    QPoint pt1 =QPoint::make_point(0,0,0);
-    double step[DIM] = {10,10,10};
-    int resolution[DIM] = {10, 10, 10};
+    QPoint origin = QPoint::make_point(0,0,0);
+    const int spacing = 10;
+    double step[DIM] = {spacing, spacing, spacing};
+    const int resolution = 10;
+    int res[DIM] = {resolution, resolution, resolution};
 
-    quest::VirtualGrid<QPoint,DIM> valid(pt1,step,resolution);
-    EXPECT_TRUE(valid.getNumBins() == (10 * 10 * 10));
+    quest::VirtualGrid<QPoint,DIM> valid(origin, step, res);
+    EXPECT_TRUE(valid.getNumBins() == (spacing * spacing * spacing));
     EXPECT_TRUE(valid.binEmpty(0));
 
 }
-TEST( quest_virtual_grid, indexing)
+
+TEST( quest_virtual_grid, double_array_constructor)
 {
-     static const int DIM = 3;
+    static const int DIM = 3;
     typedef double CoordType;
     typedef quest::Point<CoordType, DIM> QPoint;
     
-    quest::VirtualGrid<QPoint,DIM> valid;
+    double origin[DIM] = {0, 0, 0};
+    const int spacing = 10;
+    double step[DIM] = {spacing, spacing, spacing};
+    const int resolution = 10;
+    int res[DIM] = {resolution, resolution, resolution};
+
+    quest::VirtualGrid<QPoint,DIM> valid(origin, step, res);
+    EXPECT_TRUE(valid.getNumBins() == (spacing * spacing * spacing));
+    EXPECT_TRUE(valid.binEmpty(0));
+
+}
+
+TEST( quest_virtual_grid, indexing)
+{
+    static const int DIM = 3;
+    typedef double CoordType;
+    typedef quest::Point<CoordType, DIM> QPoint;
+    
+    double origin[DIM] = {0, 0, 0};
+    const int spacing = 1;
+    double step[DIM] = {spacing, spacing, spacing};
+    const int resolution = 100;
+    int res[DIM] = {resolution, resolution, resolution};
+    quest::VirtualGrid<QPoint,DIM> valid(origin, step, res);
 
     
     QPoint pt1 = QPoint::make_point(1.5,0,0);
-    EXPECT_TRUE(valid.getIndex(pt1) == 1);
+    EXPECT_TRUE(valid.getBinIndex(pt1) == 1);
     
     QPoint pt2 = QPoint::make_point(0,1.5,0);
-    EXPECT_TRUE(valid.getIndex(pt2) == 100);
+    EXPECT_TRUE(valid.getBinIndex(pt2) == 100);
 
 }
 
@@ -66,13 +80,19 @@ TEST(quest_virtual_grid, add_stuff){
     typedef double CoordType;
     typedef quest::Point<CoordType, DIM> QPoint;
     typedef quest::BoundingBox<CoordType, DIM> QBBox;
-    quest::VirtualGrid<QPoint,DIM> valid;
+
+    double origin[DIM] = {0, 0, 0};
+    const int spacing = 1;
+    double step[DIM] = {spacing, spacing, spacing};
+    const int resolution = 100;
+    int res[DIM] = {resolution, resolution, resolution};
+    quest::VirtualGrid<QPoint,DIM> valid(origin, step, res);
 
     QPoint pt1 = QPoint::make_point(1.1,1.1,1.1);
     QBBox bbox1(pt1);
 
     valid.insert(bbox1,pt1);
-    int index = valid.getIndex(pt1);
+    int index = valid.getBinIndex(pt1);
     EXPECT_TRUE(valid.getBinContents(index).size() == 1);
 
 
@@ -83,13 +103,19 @@ TEST(quest_virtual_grid, delete_stuff){
     typedef double CoordType;
     typedef quest::Point<CoordType, DIM> QPoint;
     typedef quest::BoundingBox<CoordType, DIM> QBBox;
-    quest::VirtualGrid<QPoint,DIM> valid;
+
+    double origin[DIM] = {0, 0, 0};
+    const int spacing = 1;
+    double step[DIM] = {spacing, spacing, spacing};
+    const int resolution = 100;
+    int res[DIM] = {resolution, resolution, resolution};
+    quest::VirtualGrid<QPoint,DIM> valid(origin, step, res);
 
     QPoint pt1 = QPoint::make_point(1.1,1.1,1.1);
     QBBox bbox1(pt1);
 
     valid.insert(bbox1,pt1);
-    int index = valid.getIndex(pt1);
+    int index = valid.getBinIndex(pt1);
     valid.clear(index);
     EXPECT_TRUE(valid.binEmpty(index));   
 }
