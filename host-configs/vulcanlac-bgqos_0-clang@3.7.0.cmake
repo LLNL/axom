066f68b38e11d0e596627f6a2bbadba965994263
Michael H. Chang <chang28@llnl.gov>
2017-03-01 14:05:27 -0800

    New host-configs files with the manual edit

--- host-configs/vulcanlac-bgqos_0-clang@3.7.0.cmake
+++ host-configs/vulcanlac-bgqos_0-clang@3.7.0.cmake
@@ -25,16 +25,16 @@ set("CMAKE_CXX_COMPILER" "/usr/local/bin/bgclang++" CACHE PATH "")
 set("ENABLE_FORTRAN" "OFF" CACHE PATH "")
 
 # hdf5 from uberenv
-set("HDF5_DIR" "/usr/workspace/wsa/toolkit/thirdparty_libs/builds/2017_02_09_09_39_16/spack/opt/spack/bgqos_0/clang-3.7.0/hdf5-1.8.16-bosaqxj3xd5fhyovqnda3rgj2kjsj4ah" CACHE PATH "")
+set("HDF5_DIR" "/usr/workspace/wsa/toolkit/thirdparty_libs/builds/2017_03_01_13_40_58/spack/opt/spack/bgqos_0/clang-3.7.0/hdf5-1.8.16-bosaqxj3xd5fhyovqnda3rgj2kjsj4ah" CACHE PATH "")
 
 # conduit from uberenv
-set("CONDUIT_DIR" "/usr/workspace/wsa/toolkit/thirdparty_libs/builds/2017_02_09_09_39_16/spack/opt/spack/bgqos_0/clang-3.7.0/conduit-0.2.1-oiiieme5mlcpao7pqwrk2mdquxnaguqm" CACHE PATH "")
+set("CONDUIT_DIR" "/usr/workspace/wsa/toolkit/thirdparty_libs/builds/2017_03_01_13_40_58/spack/opt/spack/bgqos_0/clang-3.7.0/conduit-0.2.1-oiiieme5mlcpao7pqwrk2mdquxnaguqm" CACHE PATH "")
 
 # sparsehash headers from uberenv
-set("SPARSEHASH_DIR" "/usr/workspace/wsa/toolkit/thirdparty_libs/builds/2017_02_09_09_39_16/spack/opt/spack/bgqos_0/clang-3.7.0/sparsehash-headers-2.0.2-jnxnoo3nsy2l6vutmvlvwri4tzgbqmro" CACHE PATH "")
+set("SPARSEHASH_DIR" "/usr/workspace/wsa/toolkit/thirdparty_libs/builds/2017_03_01_13_40_58/spack/opt/spack/bgqos_0/clang-3.7.0/sparsehash-headers-2.0.2-jnxnoo3nsy2l6vutmvlvwri4tzgbqmro" CACHE PATH "")
 
 # boost headers from uberenv
-set("BOOST_DIR" "/usr/workspace/wsa/toolkit/thirdparty_libs/builds/2017_02_09_09_39_16/spack/opt/spack/bgqos_0/clang-3.7.0/boost-headers-1.58.0-qddl3bajxtossmhy4mazvjpah4zgx5aj" CACHE PATH "")
+set("BOOST_DIR" "/usr/workspace/wsa/toolkit/thirdparty_libs/builds/2017_03_01_13_40_58/spack/opt/spack/bgqos_0/clang-3.7.0/boost-headers-1.58.0-qddl3bajxtossmhy4mazvjpah4zgx5aj" CACHE PATH "")
 
 # python not build by uberenv
 
@@ -52,4 +52,26 @@ set("BOOST_DIR" "/usr/workspace/wsa/toolkit/thirdparty_libs/builds/2017_02_09_09
 ##################################
 # end uberenv host-config
 ##################################
+
+##############################################################################
+# !---------------------------------------------------------------------------
+##############################################################################
+# Options added manually to 
+# lc bgq clang@3.7.0 host configs
+##############################################################################
+
+##############################################################################
+# MPI - manually added for now
+##############################################################################
+set(ENABLE_MPI ON CACHE PATH "")
+set(MPI_C_COMPILER "/usr/apps/gnu/clang/llnl/bin/mpiclang" CACHE PATH "")
+set(MPI_CXX_COMPILER "/usr/apps/gnu/clang/llnl/bin/mpiclang++11-fastmpi" CACHE PATH "")
+set(MPI_Fortran_COMPILER  "/usr/local/tools/compilers/ibm/mpif90-4.8.4-fastmpi" CACHE PATH "")
+
+set(MPIEXEC "/usr/bin/srun" CACHE PATH "")
+set(MPIEXEC_NUMPROC_FLAG "-n" CACHE PATH "")
+
+##############################################################################
+# !---------------------------------------------------------------------------
+##############################################################################
 
--- host-configs/vulcanlac-bgqos_0-gcc@4.7.2.cmake
+++ host-configs/vulcanlac-bgqos_0-gcc@4.7.2.cmake
@@ -25,16 +25,16 @@ set("ENABLE_FORTRAN" "OFF" CACHE PATH "")
 set("CMAKE_Fortran_COMPILER" "/usr/local/tools/toolchain-4.7.2/scripts/bggfortran" CACHE PATH "")
 
 # hdf5 from uberenv
-set("HDF5_DIR" "/usr/workspace/wsa/toolkit/thirdparty_libs/builds/2017_02_09_09_39_16/spack/opt/spack/bgqos_0/gcc-4.7.2/hdf5-1.8.16-gsqekrbdiryaddbxrb7lkv5ivwyyylcc" CACHE PATH "")
+set("HDF5_DIR" "/usr/workspace/wsa/toolkit/thirdparty_libs/builds/2017_03_01_13_40_58/spack/opt/spack/bgqos_0/gcc-4.7.2/hdf5-1.8.16-gsqekrbdiryaddbxrb7lkv5ivwyyylcc" CACHE PATH "")
 
 # conduit from uberenv
-set("CONDUIT_DIR" "/usr/workspace/wsa/toolkit/thirdparty_libs/builds/2017_02_09_09_39_16/spack/opt/spack/bgqos_0/gcc-4.7.2/conduit-0.2.1-y7kksgazxhxo4qkgbz54vdjfkpmkqu52" CACHE PATH "")
+set("CONDUIT_DIR" "/usr/workspace/wsa/toolkit/thirdparty_libs/builds/2017_03_01_13_40_58/spack/opt/spack/bgqos_0/gcc-4.7.2/conduit-0.2.1-y7kksgazxhxo4qkgbz54vdjfkpmkqu52" CACHE PATH "")
 
 # sparsehash headers from uberenv
-set("SPARSEHASH_DIR" "/usr/workspace/wsa/toolkit/thirdparty_libs/builds/2017_02_09_09_39_16/spack/opt/spack/bgqos_0/gcc-4.7.2/sparsehash-headers-2.0.2-24icetuqyud64fxt3dfx3vxlucy5apk6" CACHE PATH "")
+set("SPARSEHASH_DIR" "/usr/workspace/wsa/toolkit/thirdparty_libs/builds/2017_03_01_13_40_58/spack/opt/spack/bgqos_0/gcc-4.7.2/sparsehash-headers-2.0.2-24icetuqyud64fxt3dfx3vxlucy5apk6" CACHE PATH "")
 
 # boost headers from uberenv
-set("BOOST_DIR" "/usr/workspace/wsa/toolkit/thirdparty_libs/builds/2017_02_09_09_39_16/spack/opt/spack/bgqos_0/gcc-4.7.2/boost-headers-1.58.0-bgbbwvnccff6bzg32vt2dk5343c346jn" CACHE PATH "")
+set("BOOST_DIR" "/usr/workspace/wsa/toolkit/thirdparty_libs/builds/2017_03_01_13_40_58/spack/opt/spack/bgqos_0/gcc-4.7.2/boost-headers-1.58.0-bgbbwvnccff6bzg32vt2dk5343c346jn" CACHE PATH "")
 
 # python not build by uberenv
 
@@ -52,4 +52,26 @@ set("BOOST_DIR" "/usr/workspace/wsa/toolkit/thirdparty_libs/builds/2017_02_09_09
 ##################################
 # end uberenv host-config
 ##################################
+
+##############################################################################
+# !---------------------------------------------------------------------------
+##############################################################################
+# Options added manually to 
+# lc bgq gcc@4.7.2 host configs
+##############################################################################
+
+##############################################################################
+# MPI - manually added for now
+##############################################################################
+set(ENABLE_MPI ON CACHE PATH "")
+set(MPI_C_COMPILER "/usr/local/tools/compilers/ibm/mpicc-4.7.2-fastmpi" CACHE PATH "")
+set(MPI_CXX_COMPILER "/usr/local/tools/compilers/ibm/mpicxx-4.7.2-fastmpi" CACHE PATH "")
+set(MPI_Fortran_COMPILER  "/usr/local/tools/compilers/ibm/mpif90-4.7.2-fastmpi" CACHE PATH "")
+
+set(MPIEXEC "/usr/bin/srun" CACHE PATH "")
+set(MPIEXEC_NUMPROC_FLAG "-n" CACHE PATH "")
+
+##############################################################################
+# !---------------------------------------------------------------------------
+##############################################################################
 
