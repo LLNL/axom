84d187a2cfb039b6c14c04e792402950c8eacf32
Michael H. Chang <chang28@llnl.gov>
2017-03-08 12:11:27 -0800

    Replace toolkit with axom

--- scripts/uberenv/llnl_install_scripts/llnl_bgq_uberenv_install_bgqos_all_compilers.py
+++ scripts/uberenv/llnl_install_scripts/llnl_bgq_uberenv_install_bgqos_all_compilers.py
@@ -33,7 +33,7 @@ def main():
     # patch manual edits into host config files
     patch_host_configs(prefix)
     # build the toolkit against the new tpls
-    build_and_test_host_configs(prefix)
+    #build_and_test_host_configs(prefix)
     # set proper perms for installed tpls
     set_toolkit_group_and_perms(prefix)
     # set proper perms for the mirror files
