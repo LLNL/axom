fc03caa48275f0e22b3d7cd287aacec93d754197
Michael H. Chang <chang28@llnl.gov>
2017-03-20 11:16:26 -0700

    Change the source where the test lists comes from becuase projects_test_command.txt is not available

--- scripts/bamboo/bgq_test.sh
+++ scripts/bamboo/bgq_test.sh
@@ -7,5 +7,7 @@
 echo "running bgq_test.sh version 1.00"
 echo "cd atk_build"
 cd atk_build
-grep -v ^# project_test_commands.txt | awk '{$1 = ""; printf "echo "; print; printf "srun -N1 -n1 -ppsmall " ;print}' > bgq_autogen_test.sh
+#grep -v ^# project_test_commands.txt | awk '{$1 = ""; printf "echo "; print; printf "srun -N1 -n1 -ppsmall " ;print}' > bgq_autogen_test.sh
+find ./tests -print > bgq_test_lists.txt
+grep -v ^# bgq_test_lists.txt | awk '{printf "echo "; print; printf "srun -N1 -n1 -ppsmall " ;print}' > bgq_autogen_test.sh
 chmod a+x bgq_autogen_test.sh
