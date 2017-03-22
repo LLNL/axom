7d32fda9807247d51e7f4d43e070e9a02878f1fb
Kenneth Weiss <weiss27@llnl.gov>
2017-03-22 13:31:52 -0700

    Run the script on a login node, tests are run in separate allocation.
    Misc:
    * Added usage note for new BLT option flags
    * Fixed a string concatenation error that added a '+' to the configure options
    * Changed allocation to use 4 nodes of BG/Q

--- scripts/bamboo/bgq_build_test_uno_compiler.sh
+++ scripts/bamboo/bgq_build_test_uno_compiler.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # 10-20-2016 chang28, 
-# 10-20-2016 chang28,  bgq_build_test_uno_compiler.sh "Debug" "" "gcc@4.7.2"
-# 01-13-2017, chang28, turn off testing
+# 10-20-2016 chang28, bgq_build_test_uno_compiler.sh "Debug" "" "gcc@4.7.2"
+# 01-13-2017 chang28, turn off testing
+# 03-22-2017 weiss27, Need to run with two configuration options on BG/Q and compile on login nodes
+#                     bgq_build_test_uno_compiler.sh "Debug" "-DENABLE_GTEST_DEATH_TESTS=OFF -DENABLE_WRAP_TESTS_WITH_MPIEXEC=ON" gcc@4.7.2
 
 echo bgq version 1.0.0
 #BT="Debug"
@@ -30,8 +32,10 @@ DOCS_DIR="${TOOLKIT_WEB_ROOT}/docs"
 COMPILER=$3
 if [[ $HOSTNAME == rz* ]]; then
     HOST_CONFIGURATION="host-configs/rzuseqlac-bgqos_0-${COMPILER}.cmake"
+    PARTITION="pdebug"
 else
     HOST_CONFIGURATION="host-configs/vulcanlac-bgqos_0-${COMPILER}.cmake"
+    PARTITION="psmall"
 fi
 
 OPTIONS="-ecc -hc $HOST_CONFIGURATION -bt $BUILD_TYPE -bp $BUILD_PATH -ip $INSTALL_PATH $COMP_OPT $BUILD_OPT"
@@ -49,7 +53,7 @@ if [ "$RUN_BGQ_TEST" = true ]; then
     echo "Running tests env CTEST_OUTPUT_ON_FAILURE=1 ..."
     export CTEST_OUTPUT_ON_FAILURE=1
     echo "-----------------------------------------------------------------------"
-    $CTEST_EXE -T Test -j$JOBS
+    salloc -N4 -p$PARTITION $CTEST_EXE -T Test -j$JOBS
     if [ $? -ne 0 ]; then
         echo "Error: 'make test' failed"
         exit 1
