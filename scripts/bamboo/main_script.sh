94e795d94f3a0450f7a3619739c8eed43af01fec
Michael H. Chang <chang28@llnl.gov>
2017-03-23 12:58:53 -0700

    There is a typo in main_script.sh, an extra + was introduced, fixed it

--- scripts/bamboo/main_script.sh
+++ scripts/bamboo/main_script.sh
@@ -8,7 +8,7 @@ echo main_script version 0.9.9
 echo "Configuring..."
 echo "-----------------------------------------------------------------------"
 if [ "$DOC" = false ]; then
-   OPTIONS=$OPTIONS+" -DENABLE_DOCS=false";
+   OPTIONS=$OPTIONS " -DENABLE_DOCS=false";
 fi
 echo "Options: $OPTIONS"
 ./scripts/config-build.py $OPTIONS
