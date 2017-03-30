06d35d571e074d35de1480a715845e4a098038b7
Michael H. Chang <chang28@llnl.gov>
2017-03-30 12:54:36 -0700

    New Toss3 RZ host-configs files with MPI enabled

--- host-configs/rzgenie-toss_3_x86_64_ib-clang@3.9.0.cmake
+++ host-configs/rzgenie-toss_3_x86_64_ib-clang@3.9.0.cmake
@@ -7,7 +7,7 @@
 ##################################
 
 # cmake from uberenv
-# cmake executable path: /usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_13_15_14_37/spack/opt/spack/toss_3_x86_64_ib/clang-3.9.0/cmake-3.3.1-usuntzhfbmwz5ci5fryejg52swgb5ruo/bin/cmake
+# cmake executable path: /usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_30_09_55_10/spack/opt/spack/toss_3_x86_64_ib/clang-3.9.0/cmake-3.3.1-usuntzhfbmwz5ci5fryejg52swgb5ruo/bin/cmake
 
 #######
 # using clang@3.9.0 compiler spec
@@ -25,36 +25,36 @@ set("CMAKE_CXX_COMPILER" "/usr/tce/packages/clang/clang-3.9.0/bin/clang++" CACHE
 set("ENABLE_FORTRAN" "OFF" CACHE PATH "")
 
 # hdf5 from uberenv
-set("HDF5_DIR" "/usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_13_15_14_37/spack/opt/spack/toss_3_x86_64_ib/clang-3.9.0/hdf5-1.8.16-qy646zjgaj4crjrgwrlqj2slnvjel5re" CACHE PATH "")
+set("HDF5_DIR" "/usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_30_09_55_10/spack/opt/spack/toss_3_x86_64_ib/clang-3.9.0/hdf5-1.8.16-qy646zjgaj4crjrgwrlqj2slnvjel5re" CACHE PATH "")
 
 # conduit from uberenv
-set("CONDUIT_DIR" "/usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_13_15_14_37/spack/opt/spack/toss_3_x86_64_ib/clang-3.9.0/conduit-0.2.1-spqnui5wsqcmq46b3uyaimo5gxz3bttj" CACHE PATH "")
+set("CONDUIT_DIR" "/usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_30_09_55_10/spack/opt/spack/toss_3_x86_64_ib/clang-3.9.0/conduit-0.2.1-spqnui5wsqcmq46b3uyaimo5gxz3bttj" CACHE PATH "")
 
 # sparsehash headers from uberenv
-set("SPARSEHASH_DIR" "/usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_13_15_14_37/spack/opt/spack/toss_3_x86_64_ib/clang-3.9.0/sparsehash-headers-2.0.2-6dfc4uip2i4pyxqbxjk4tefubkme3ydl" CACHE PATH "")
+set("SPARSEHASH_DIR" "/usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_30_09_55_10/spack/opt/spack/toss_3_x86_64_ib/clang-3.9.0/sparsehash-headers-2.0.2-6dfc4uip2i4pyxqbxjk4tefubkme3ydl" CACHE PATH "")
 
 # boost headers from uberenv
-set("BOOST_DIR" "/usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_13_15_14_37/spack/opt/spack/toss_3_x86_64_ib/clang-3.9.0/boost-headers-1.58.0-cjtvgl2bbxo727ky5bfaiihg4uublmbp" CACHE PATH "")
+set("BOOST_DIR" "/usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_30_09_55_10/spack/opt/spack/toss_3_x86_64_ib/clang-3.9.0/boost-headers-1.58.0-cjtvgl2bbxo727ky5bfaiihg4uublmbp" CACHE PATH "")
 
 # python from uberenv
-set("PYTHON_EXECUTABLE" "/usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_13_15_14_37/spack/opt/spack/toss_3_x86_64_ib/clang-3.9.0/python-2.7.11-dozo3tjzgctvcgazuvlp4gcnduc43ery/bin/python" CACHE PATH "")
+set("PYTHON_EXECUTABLE" "/usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_30_09_55_10/spack/opt/spack/toss_3_x86_64_ib/clang-3.9.0/python-2.7.11-dozo3tjzgctvcgazuvlp4gcnduc43ery/bin/python" CACHE PATH "")
 
 # lua from uberenv
-set("LUA_DIR" "/usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_13_15_14_37/spack/opt/spack/toss_3_x86_64_ib/clang-3.9.0/lua-5.1.5-izxbfxhlava24dron2y7i2pruwx3cuh3" CACHE PATH "")
+set("LUA_DIR" "/usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_30_09_55_10/spack/opt/spack/toss_3_x86_64_ib/clang-3.9.0/lua-5.1.5-izxbfxhlava24dron2y7i2pruwx3cuh3" CACHE PATH "")
 
 # doxygen from uberenv
-set("DOXYGEN_EXECUTABLE" "/usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_13_15_14_37/spack/opt/spack/toss_3_x86_64_ib/clang-3.9.0/doxygen-1.8.11-bprlm2fmh7jvshvtty2a2c6utnyw6u7s/bin/doxygen" CACHE PATH "")
+set("DOXYGEN_EXECUTABLE" "/usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_30_09_55_10/spack/opt/spack/toss_3_x86_64_ib/clang-3.9.0/doxygen-1.8.11-bprlm2fmh7jvshvtty2a2c6utnyw6u7s/bin/doxygen" CACHE PATH "")
 
 # sphinx from uberenv
-set("SPHINX_EXECUTABLE" "/usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_13_15_14_37/spack/opt/spack/toss_3_x86_64_ib/clang-3.9.0/python-2.7.11-dozo3tjzgctvcgazuvlp4gcnduc43ery/bin/sphinx-build" CACHE PATH "")
+set("SPHINX_EXECUTABLE" "/usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_30_09_55_10/spack/opt/spack/toss_3_x86_64_ib/clang-3.9.0/python-2.7.11-dozo3tjzgctvcgazuvlp4gcnduc43ery/bin/sphinx-build" CACHE PATH "")
 
 # uncrustify from uberenv
-set("UNCRUSTIFY_EXECUTABLE" "/usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_13_15_14_37/spack/opt/spack/toss_3_x86_64_ib/clang-3.9.0/uncrustify-0.61-vgt2p36odqlrkexkus4xzzy53uyiopug/bin/uncrustify" CACHE PATH "")
+set("UNCRUSTIFY_EXECUTABLE" "/usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_30_09_55_10/spack/opt/spack/toss_3_x86_64_ib/clang-3.9.0/uncrustify-0.61-vgt2p36odqlrkexkus4xzzy53uyiopug/bin/uncrustify" CACHE PATH "")
 
 # lcov and genhtml from uberenv
-set("LCOV_PATH" "/usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_13_15_14_37/spack/opt/spack/toss_3_x86_64_ib/clang-3.9.0/lcov-1.11-hucmk5dnudjpfjy2vhnid46g3fmi3r7n/usr/bin/lcov" CACHE PATH "")
+set("LCOV_PATH" "/usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_30_09_55_10/spack/opt/spack/toss_3_x86_64_ib/clang-3.9.0/lcov-1.11-hucmk5dnudjpfjy2vhnid46g3fmi3r7n/usr/bin/lcov" CACHE PATH "")
 
-set("GENHTML_PATH" "/usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_13_15_14_37/spack/opt/spack/toss_3_x86_64_ib/clang-3.9.0/lcov-1.11-hucmk5dnudjpfjy2vhnid46g3fmi3r7n/usr/bin/genhtml" CACHE PATH "")
+set("GENHTML_PATH" "/usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_30_09_55_10/spack/opt/spack/toss_3_x86_64_ib/clang-3.9.0/lcov-1.11-hucmk5dnudjpfjy2vhnid46g3fmi3r7n/usr/bin/genhtml" CACHE PATH "")
 
 ##################################
 # end uberenv host-config
@@ -70,17 +70,18 @@ set("GENHTML_PATH" "/usr/workspace/wsrzc/axom/thirdparty_libs/builds/2017_03_13_
 ##############################################################################
 # MPI - manually added for now
 ##############################################################################
-set("ENABLE_MPI" ON CACHE PATH "")
-set("MPI_C_COMPILER" "/usr/tce/packages/mvapich2/mvapich2-2.2-clang-3.9.0/bin/mpicc" CACHE PATH "")
-set("MPI_CXX_COMPILER" "/usr/tce/packages/mvapich2/mvapich2-2.2-clang-3.9.0/bin/mpiccxx" CACHE PATH "")
-set("MPI_Fortran_COMPILER" "/usr/tce/packages/mvapich2/mvapich2-2.2-clang-3.9.0/bin/mpif90" CACHE PATH "")
+set(ENABLE_MPI ON CACHE PATH "")
+set(MPI_C_COMPILER "/usr/tce/packages/mvapich2/mvapich2-2.2-clang-3.9.0/bin/mpicc" CACHE PATH "")
+set(MPI_CXX_COMPILER "/usr/tce/packages/mvapich2/mvapich2-2.2-clang-3.9.0/bin/mpiccxx" CACHE PATH "")
+set(MPI_Fortran_COMPILER "/usr/tce/packages/mvapich2/mvapich2-2.2-clang-3.9.0/bin/mpif90" CACHE PATH "")
 
-set("MPI_C_LIBRARIES"   "/usr/tce/packages/mvapich2/mvapich2-2.2-clang-3.9.0/lib/libmpi.a" CACHE PATH "")
-set("MPI_CXX_LIBRARIES" "/usr/tce/packages/mvapich2/mvapich2-2.2-clang-3.9.0/lib/libmpicxx.a" CACHE PATH "")
-set("MPI_CXX_INCLUDE_PATH" "/usr/tce/packages/mvapich2/mvapich2-2.2-clang-3.9.0/include" CACHE PATH "")
+set(MPI_C_LIBRARIES   "/usr/tce/packages/mvapich2/mvapich2-2.2-clang-3.9.0/lib/libmpi.a" CACHE PATH "")
+set(MPI_CXX_LIBRARIES "/usr/tce/packages/mvapich2/mvapich2-2.2-clang-3.9.0/lib/libmpicxx.a" CACHE PATH "")
+set(MPI_CXX_INCLUDE_PATH "/usr/tce/packages/mvapich2/mvapich2-2.2-clang-3.9.0/include" CACHE PATH "")
+set(MPI_Fortran_LIBRARIES   "/usr/tce/packages/mvapich2/mvapich2-2.2-clang-3.9.0/lib/libmpifort.a" CACHE PATH "")
 
-set("MPIEXEC" "/usr/bin/srun" CACHE PATH "")
-set("MPIEXEC_NUMPROC_FLAG" "-n" CACHE PATH "")
+set(PIEXEC "/usr/bin/srun" CACHE PATH "")
+set(MPIEXEC_NUMPROC_FLAG "-n" CACHE PATH "")
 
 
 ##############################################################################
