c915af4af8ad97ed541daac462c4c2ed89fb9828
Lee Taylor <ltaylor@llnl.gov>
2017-04-11 00:02:08 -0700

    Regenerate spio interface

--- src/components/spio/src/interface/c_fortran/wrapIOManager.h
+++ src/components/spio/src/interface/c_fortran/wrapIOManager.h
@@ -36,25 +36,25 @@ SPIO_iomanager * SPIO_iomanager_new(MPI_Fint com);
 
 void SPIO_iomanager_delete(SPIO_iomanager * self);
 
-void SPIO_iomanager_write(SPIO_iomanager * self, SIDRE_group * grp, int num_files, const char * file_string, const char * protocol);
+void SPIO_iomanager_write(SPIO_iomanager * self, SIDRE_group * group, int num_files, const char * file_string, const char * protocol);
 
-void SPIO_iomanager_write_bufferify(SPIO_iomanager * self, SIDRE_group * grp, int num_files, const char * file_string, int Lfile_string, const char * protocol, int Lprotocol);
+void SPIO_iomanager_write_bufferify(SPIO_iomanager * self, SIDRE_group * group, int num_files, const char * file_string, int Lfile_string, const char * protocol, int Lprotocol);
 
-void SPIO_iomanager_write_group_to_root_file(SPIO_iomanager * self, SIDRE_group * grp, const char * file_name);
+void SPIO_iomanager_write_group_to_root_file(SPIO_iomanager * self, SIDRE_group * group, const char * file_name);
 
-void SPIO_iomanager_write_group_to_root_file_bufferify(SPIO_iomanager * self, SIDRE_group * grp, const char * file_name, int Lfile_name);
+void SPIO_iomanager_write_group_to_root_file_bufferify(SPIO_iomanager * self, SIDRE_group * group, const char * file_name, int Lfile_name);
 
-void SPIO_iomanager_read_0(SPIO_iomanager * self, SIDRE_group * grp, const char * file_string, const char * protocol);
+void SPIO_iomanager_read_0(SPIO_iomanager * self, SIDRE_group * group, const char * file_string, const char * protocol);
 
-void SPIO_iomanager_read_0_bufferify(SPIO_iomanager * self, SIDRE_group * grp, const char * file_string, int Lfile_string, const char * protocol, int Lprotocol);
+void SPIO_iomanager_read_0_bufferify(SPIO_iomanager * self, SIDRE_group * group, const char * file_string, int Lfile_string, const char * protocol, int Lprotocol);
 
-void SPIO_iomanager_read_1(SPIO_iomanager * self, SIDRE_group * grp, const char * root_file);
+void SPIO_iomanager_read_1(SPIO_iomanager * self, SIDRE_group * group, const char * root_file);
 
-void SPIO_iomanager_read_1_bufferify(SPIO_iomanager * self, SIDRE_group * grp, const char * root_file, int Lroot_file);
+void SPIO_iomanager_read_1_bufferify(SPIO_iomanager * self, SIDRE_group * group, const char * root_file, int Lroot_file);
 
-void SPIO_iomanager_load_external_data(SPIO_iomanager * self, SIDRE_group * grp, const char * root_file);
+void SPIO_iomanager_load_external_data(SPIO_iomanager * self, SIDRE_group * group, const char * root_file);
 
-void SPIO_iomanager_load_external_data_bufferify(SPIO_iomanager * self, SIDRE_group * grp, const char * root_file, int Lroot_file);
+void SPIO_iomanager_load_external_data_bufferify(SPIO_iomanager * self, SIDRE_group * group, const char * root_file, int Lroot_file);
 
 #ifdef __cplusplus
 }
