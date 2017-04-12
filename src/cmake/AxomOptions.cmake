2f9a62917f4dd7aeba807704033c11070ab9b3c8
Lee Taylor <ltaylor@llnl.gov>
2017-04-11 17:09:41 -0700

    Remove Shroud reference

--- src/cmake/AxomOptions.cmake
+++ src/cmake/AxomOptions.cmake
@@ -1,5 +1,4 @@
 
 option(ENABLE_PYTHON "Enables python use." ON)
-#option(ENABLE_SHROUD "Enables Shroud for generating language bindings" OFF)
 option(ENABLE_CFORTRAN_API "Enables Fortran interface for components." ON)
 option(ENABLE_ALL_COMPONENTS "Enables all components by default" ON)
