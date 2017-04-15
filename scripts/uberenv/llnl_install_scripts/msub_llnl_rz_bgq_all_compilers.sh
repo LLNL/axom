e77c08b734523da465f6a67873ede774d9555b5c
Michael H. Chang <chang28@llnl.gov>
2017-04-14 21:29:00 -0700

    Add msub file for RZ bgq

---
+++ scripts/uberenv/llnl_install_scripts/msub_llnl_rz_bgq_all_compilers.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#MSUB -l nodes=1
+#MSUB -q pbatch
+#MSUB -l walltime=8:00:00
+#MSUB -A wbronze
+#MSUB -j oe
+#MSUB -o m.out.r.uberenv.bgqos.all.compilers.%j.%N.txt
+#
+# usage: 
+#  cd {to directory with this script}
+#  msub -d `pwd` msub_llnl_rz_bgq_all_compilers.sh
+
+date
+. /usr/local/tools/dotkit/init.sh
+use python-2.7.3
+use cmake-3.1
+python ./llnl_rz_uberenv_install_bgqos_0_all_compilers.py
+date
+
