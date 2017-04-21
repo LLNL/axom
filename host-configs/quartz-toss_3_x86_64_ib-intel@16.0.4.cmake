6ec25d21ddc84f44bedf916bc52bfbe635ba7e30
Kenneth Weiss <weiss27@llnl.gov>
2017-04-20 20:15:12 -0700

    Quartz host-config files were placed in wrong directory.

--- host-configs/quartz-toss_3_x86_64_ib-intel@16.0.4.cmake
+++ host-configs/quartz-toss_3_x86_64_ib-intel@16.0.4.cmake
@@ -7,7 +7,7 @@
 ##################################
 
 # cmake from uberenv
-# cmake executable path: /usr/workspace/wsa/axom/thirdparty_libs/builds/2017_04_10_16_50_47/spack/opt/spack/toss_3_x86_64_ib/intel-16.0.4/cmake-3.3.1-pameyoqbnyiwmdxbeqwmiup7kswibf35/bin/cmake
+# cmake executable path: /usr/workspace/wsa/axom/thirdparty_libs/builds/2017_04_18_08_52_00/spack/opt/spack/toss_3_x86_64_ib/intel-16.0.4/cmake-3.3.1-pameyoqbnyiwmdxbeqwmiup7kswibf35/bin/cmake
 
 #######
 # using intel@16.0.4 compiler spec
@@ -25,36 +25,33 @@ set("ENABLE_FORTRAN" "ON" CACHE PATH "")
 set("CMAKE_Fortran_COMPILER" "/usr/tce/packages/intel/intel-16.0.4/bin/ifort" CACHE PATH "")
 
 # hdf5 from uberenv
-set("HDF5_DIR" "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_04_10_16_50_47/spack/opt/spack/toss_3_x86_64_ib/intel-16.0.4/hdf5-1.8.16-z6nt2tyk2j2eop2df6ghod6dnfhwei6s" CACHE PATH "")
+set("HDF5_DIR" "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_04_18_08_52_00/spack/opt/spack/toss_3_x86_64_ib/intel-16.0.4/hdf5-1.8.16-z6nt2tyk2j2eop2df6ghod6dnfhwei6s" CACHE PATH "")
 
 # conduit from uberenv
-set("CONDUIT_DIR" "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_04_10_16_50_47/spack/opt/spack/toss_3_x86_64_ib/intel-16.0.4/conduit-0.2.1-o2svxknpl6alrrdvuislszxqhljo4k3b" CACHE PATH "")
-
-# sparsehash headers from uberenv
-set("SPARSEHASH_DIR" "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_04_10_16_50_47/spack/opt/spack/toss_3_x86_64_ib/intel-16.0.4/sparsehash-headers-2.0.2-bjbnaamdnumhjyvmmpk57yxhm2yvuezy" CACHE PATH "")
+set("CONDUIT_DIR" "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_04_18_08_52_00/spack/opt/spack/toss_3_x86_64_ib/intel-16.0.4/conduit-0.2.1-o2svxknpl6alrrdvuislszxqhljo4k3b" CACHE PATH "")
 
 # boost headers from uberenv
-set("BOOST_DIR" "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_04_10_16_50_47/spack/opt/spack/toss_3_x86_64_ib/intel-16.0.4/boost-headers-1.58.0-22qg7in5tkbmwxpv2wszzejsmhqdpwmt" CACHE PATH "")
+set("BOOST_DIR" "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_04_18_08_52_00/spack/opt/spack/toss_3_x86_64_ib/intel-16.0.4/boost-headers-1.58.0-22qg7in5tkbmwxpv2wszzejsmhqdpwmt" CACHE PATH "")
 
 # python from uberenv
-set("PYTHON_EXECUTABLE" "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_04_10_16_50_47/spack/opt/spack/toss_3_x86_64_ib/intel-16.0.4/python-2.7.11-63apwbc3kjq5b3xpyjkcxpyf6yv5i354/bin/python" CACHE PATH "")
+set("PYTHON_EXECUTABLE" "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_04_18_08_52_00/spack/opt/spack/toss_3_x86_64_ib/intel-16.0.4/python-2.7.11-63apwbc3kjq5b3xpyjkcxpyf6yv5i354/bin/python" CACHE PATH "")
 
 # lua from uberenv
-set("LUA_DIR" "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_04_10_16_50_47/spack/opt/spack/toss_3_x86_64_ib/intel-16.0.4/lua-5.1.5-glh6zpy27o4lrbzvre4k6qu3rkhjawfu" CACHE PATH "")
+set("LUA_DIR" "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_04_18_08_52_00/spack/opt/spack/toss_3_x86_64_ib/intel-16.0.4/lua-5.1.5-glh6zpy27o4lrbzvre4k6qu3rkhjawfu" CACHE PATH "")
 
 # doxygen from uberenv
-set("DOXYGEN_EXECUTABLE" "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_04_10_16_50_47/spack/opt/spack/toss_3_x86_64_ib/intel-16.0.4/doxygen-1.8.11-tb3urqnfhpui4fp2br7uzk66ahyvmgjd/bin/doxygen" CACHE PATH "")
+set("DOXYGEN_EXECUTABLE" "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_04_18_08_52_00/spack/opt/spack/toss_3_x86_64_ib/intel-16.0.4/doxygen-1.8.11-tb3urqnfhpui4fp2br7uzk66ahyvmgjd/bin/doxygen" CACHE PATH "")
 
 # sphinx from uberenv
-set("SPHINX_EXECUTABLE" "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_04_10_16_50_47/spack/opt/spack/toss_3_x86_64_ib/intel-16.0.4/python-2.7.11-63apwbc3kjq5b3xpyjkcxpyf6yv5i354/bin/sphinx-build" CACHE PATH "")
+set("SPHINX_EXECUTABLE" "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_04_18_08_52_00/spack/opt/spack/toss_3_x86_64_ib/intel-16.0.4/python-2.7.11-63apwbc3kjq5b3xpyjkcxpyf6yv5i354/bin/sphinx-build" CACHE PATH "")
 
 # uncrustify from uberenv
-set("UNCRUSTIFY_EXECUTABLE" "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_04_10_16_50_47/spack/opt/spack/toss_3_x86_64_ib/intel-16.0.4/uncrustify-0.61-nzydqv27ioishmus56myotierqugxn54/bin/uncrustify" CACHE PATH "")
+set("UNCRUSTIFY_EXECUTABLE" "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_04_18_08_52_00/spack/opt/spack/toss_3_x86_64_ib/intel-16.0.4/uncrustify-0.61-nzydqv27ioishmus56myotierqugxn54/bin/uncrustify" CACHE PATH "")
 
 # lcov and genhtml from uberenv
-set("LCOV_PATH" "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_04_10_16_50_47/spack/opt/spack/toss_3_x86_64_ib/intel-16.0.4/lcov-1.11-fiwdecmhqvashhs323poq5mdr6uqnrio/usr/bin/lcov" CACHE PATH "")
+set("LCOV_PATH" "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_04_18_08_52_00/spack/opt/spack/toss_3_x86_64_ib/intel-16.0.4/lcov-1.11-fiwdecmhqvashhs323poq5mdr6uqnrio/usr/bin/lcov" CACHE PATH "")
 
-set("GENHTML_PATH" "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_04_10_16_50_47/spack/opt/spack/toss_3_x86_64_ib/intel-16.0.4/lcov-1.11-fiwdecmhqvashhs323poq5mdr6uqnrio/usr/bin/genhtml" CACHE PATH "")
+set("GENHTML_PATH" "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_04_18_08_52_00/spack/opt/spack/toss_3_x86_64_ib/intel-16.0.4/lcov-1.11-fiwdecmhqvashhs323poq5mdr6uqnrio/usr/bin/genhtml" CACHE PATH "")
 
 # Temporarily disable CXX11 on intel builds until we resolve issue ATK-619
 set("BLT_CXX_STD" "c++98" CACHE PATH "")
