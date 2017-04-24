6169d60f929f2a45c20b4d01e6ea46f27191a596
Arlie Capps <capps2@llnl.gov>
2017-04-24 11:28:43 -0700

    Proceeding with cleanup.

--- src/components/primal/examples/mesh_tester.cpp
+++ src/components/primal/examples/mesh_tester.cpp
@@ -73,18 +73,26 @@ typedef primal::Vector<double, 3> Vector3;
 typedef primal::Segment<double, 3> Segment3;
 
 
-typedef struct Input{
+typedef struct Input
+{
   std::string stlInput;
   std::string textOutputFile;
   int resolution;
-}Input;
+  int errorCode;
+
+  Input() : stlInput("src/components/primal/data/plane.stl"),
+            textOutputFile("meshTestResults.txt"),
+            resolution(10),
+            errorCode(0)
+  { };
+} Input;
 
 SpatialBoundingBox compute_bounds( mint::Mesh* mesh);
 Triangle3 getMeshTriangle(int i,  mint::Mesh* surface_mesh);
 bool shareVertices(Triangle3 t1, Triangle3 t2);
 bool trianglesAreCoplanar(Triangle3 t1, Triangle3 t2);
 bool trianglesOverlap(Triangle3 t1, Triangle3 t2);
-bool checkTT(Triangle3& t1, Triangle3& t2, std::ofstream& ofs, int i, int j);
+bool checkTT(Triangle3& t1, Triangle3& t2, std::ofstream& ofs, int i, int j, bool useTrianglesOverlap = true);
 void naiveAlgorithm(std::ofstream& ofs, mint::Mesh* surface_mesh);
 void vgAlgorithm(std::ofstream& ofs, mint::Mesh* surface_mesh, int resolution);
 void init_params(Input& params,int argc, char ** argv);
@@ -110,7 +118,7 @@ SpatialBoundingBox compute_bounds( mint::Mesh* mesh)
   return meshBB;
 }
 
-//helper function that retrieves the triangle at the given index
+// helper function that retrieves the triangle at the given index
 Triangle3 getMeshTriangle(int i,  mint::Mesh* surface_mesh)
 {
   SLIC_ASSERT(surface_mesh->getMeshNumberOfCellNodes(i) == 3);
@@ -120,28 +128,24 @@ Triangle3 getMeshTriangle(int i,  mint::Mesh* surface_mesh)
   primal::Point< double,3 > B1;
   primal::Point< double,3 > C1;
 
-  surface_mesh->getMeshNode( triCell[0],A1.data() );
-  //SLIC_INFO("A is " <<A1);
-  surface_mesh->getMeshNode( triCell[1],B1.data() );
-  //SLIC_INFO("B is " <<B1);
-  surface_mesh->getMeshNode( triCell[2],C1.data() );
-  //SLIC_INFO("C is " <<C1);
+  surface_mesh->getMeshNode(triCell[0], A1.data());
+  surface_mesh->getMeshNode(triCell[1], B1.data());
+  surface_mesh->getMeshNode(triCell[2], C1.data());
   Triangle3 t1 = Triangle3(A1,B1,C1);
-  //SLIC_INFO("T is " <<t1);
 
   return t1;
 }
-inline bool pointIsNearlyEqual(Point3& p1, Point3& p2, double EPS=1.0e-9) {
+
+inline bool pointIsNearlyEqual(Point3& p1, Point3& p2, double EPS=1.0e-9)
+{
   return axom::utilities::isNearlyEqual(p1[0], p2[0], EPS) && \
     axom::utilities::isNearlyEqual(p1[1], p2[1], EPS) && \
     axom::utilities::isNearlyEqual(p1[2], p2[2], EPS);
-
 }
 
-//check if any of the vertices are the same point (all of their points are within 1.0e-11 of eachother)
+// check if any of the vertices are the same point (all of their points are within 1.0e-11 of eachother)
 bool shareVertices(Triangle3 t1, Triangle3 t2)
 {
-  //SLIC_INFO("t1 is "<< t1 << " t2 is "<< t2);
   return ((pointIsNearlyEqual(t1[0], t2[0])) || \
           (pointIsNearlyEqual(t1[0], t2[1])) || \
           (pointIsNearlyEqual(t1[0], t2[2])) || \
@@ -151,11 +155,10 @@ bool shareVertices(Triangle3 t1, Triangle3 t2)
           (pointIsNearlyEqual(t1[2], t2[0])) || \
           (pointIsNearlyEqual(t1[2], t2[1])) || \
           (pointIsNearlyEqual(t1[2], t2[2])));
-
 }
 
-//It turns out that I didn't need the below function... I'm leaving this here in case it is needed after all
-bool trianglesAreCoplanar(Triangle3 t1, Triangle3 t2) {
+bool trianglesAreCoplanar(Triangle3 t1, Triangle3 t2)
+{
   //Step 11.1: Check if the triangles are coplanar
   Vector3 t1Normal = Vector3::cross_product(Vector3(t1[0], t1[1]), Vector3(t1[0], t1[2]));
   Vector3 t2Normal = Vector3::cross_product(Vector3(t2[2], t2[0]), Vector3(t2[2], t2[1]));
@@ -176,120 +179,138 @@ bool trianglesAreCoplanar(Triangle3 t1, Triangle3 t2) {
      axom::utilities::isNearlyEqual(dCj, 0.0, 1.0e-12)); 
 }
 
-//Requires t1 and t2 share at least 1 vertex
-bool trianglesIn(Triangle3 t1, Triangle3 t2) {
-  /*Step 11.1: given that t1 and t2 share a vertex, permute all possible vertex matching pairs
-    We have three cases: 
-    (1) triangles share all 3 vertices (are the same triangle)
-    (2) triangles share 2 vertices-- if they overlap not on the edge formed by these two vertices, 
-    it is because one of their vertices lies within the other. Note that this function must be called twice to check this case, as only one of 
-    the non-intersecting vertices need lie in the other triangle for an intersection to occur. 
-    (3) Triangles share 1 vertex -- if they overlap or intersect, one of the triangles two non-intersecting vertices must form
-    a segment that intersects the other. Note that this function must be called twice to check this case, as only one of the segments
-    formed by the non-intersecting vertices need intersect the other triangle for an intersection to occur.
+// Requires t1 and t2 share at least 1 vertex
+bool trianglesIn(Triangle3 t1, Triangle3 t2)
+{
+  /* Step 11.1: given that t1 and t2 share a vertex, permute all possible vertex 
+     matching pairs.  We have three cases: 
+     (1) triangles share all 3 vertices (are the same triangle)
+     (2) triangles share 2 vertices-- if they overlap not on the edge formed by
+         these two vertices, it is because one of their vertices lies within the
+         other.  Note that this function must be called twice to check this
+         case, as only one of the non-intersecting vertices need lie in the
+         other triangle for an intersection to occur. 
+     (3) Triangles share 1 vertex -- if they overlap or intersect, one of the
+         triangles two non-intersecting vertices must form a segment that
+         intersects the other.  Note that this function must be called twice
+         to check this case, as only one of the segments formed by the
+         non-intersecting vertices need intersect the other triangle for an
+         intersection to occur.
   */
   bool returning;
-  if ((pointIsNearlyEqual(t1[0],t2[0])) || (pointIsNearlyEqual(t1[0],t2[1])) ||(pointIsNearlyEqual(t1[0],t2[2]))) {
-    if ((pointIsNearlyEqual(t1[1],t2[0])) || (pointIsNearlyEqual(t1[1],t2[1])) ||(pointIsNearlyEqual(t1[1],t2[2]))){
-      if ((pointIsNearlyEqual(t1[2],t2[0])) || (pointIsNearlyEqual(t1[2],t2[1])) ||(pointIsNearlyEqual(t1[2],t2[2]))) {
-        return true; //Case 1: triangles are the same triangle, so they must be intersecting in a region
+  if ((pointIsNearlyEqual(t1[0],t2[0])) || (pointIsNearlyEqual(t1[0],t2[1])) || (pointIsNearlyEqual(t1[0],t2[2]))) {
+    if ((pointIsNearlyEqual(t1[1],t2[0])) || (pointIsNearlyEqual(t1[1],t2[1])) || (pointIsNearlyEqual(t1[1],t2[2]))){
+      if ((pointIsNearlyEqual(t1[2],t2[0])) || (pointIsNearlyEqual(t1[2],t2[1])) || (pointIsNearlyEqual(t1[2],t2[2]))) {
+        return true; // Case 1: triangles are the same triangle, so they must be intersecting in a region
       }
       else {
-        returning= trianglesAreCoplanar(t1, t2) && (t2.checkInTriangle(t1[2]));  //Case 2: t1.B and t1.A are a side on t2
-        return returning;
+        returning = trianglesAreCoplanar(t1, t2) && (t2.checkInTriangle(t1[2]));
+        return returning; // Case 2: t1.B and t1.A are a side on t2
       }
         
     }
-    else if ((pointIsNearlyEqual(t1[2],t2[0])) || (pointIsNearlyEqual(t1[2],t2[1])) ||(pointIsNearlyEqual(t1[2],t2[2]))) {
-      returning=  trianglesAreCoplanar(t1, t2) && (t2.checkInTriangle(t1[1])); //t1.C and t1.A are a side on t2
-      return returning;
+    else if ((pointIsNearlyEqual(t1[2],t2[0])) || (pointIsNearlyEqual(t1[2],t2[1])) || (pointIsNearlyEqual(t1[2],t2[2]))) {
+      returning = trianglesAreCoplanar(t1, t2) && (t2.checkInTriangle(t1[1]));
+      return returning; // t1.C and t1.A are a side on t2
     }
     else {
-      Segment3 s= Segment3(t1[1], t1[2]);
-      returning= primal::intersect(t2, s);  //t1.A is a shared vertex
+      Segment3 s = Segment3(t1[1], t1[2]);
+      returning = primal::intersect(t2, s);  // t1.A is a shared vertex
       return returning;
     }
 
   }
-  else if ((pointIsNearlyEqual(t1[1],t2[0])) || (pointIsNearlyEqual(t1[1],t2[1])) ||(pointIsNearlyEqual(t1[1],t2[2]))) {  //t1.A is not a shared vertex, at least one of t1.B or t1.C share a vertex with t2
-    if ((pointIsNearlyEqual(t1[2],t2[0])) || (pointIsNearlyEqual(t1[2],t2[1])) ||(pointIsNearlyEqual(t1[2],t2[2]))){
-      returning= trianglesAreCoplanar(t1, t2) && (t2.checkInTriangle(t1[0]));  //t1.B and t1.C are a side on t2
-      return returning;
+  else if ((pointIsNearlyEqual(t1[1],t2[0])) || (pointIsNearlyEqual(t1[1],t2[1])) || (pointIsNearlyEqual(t1[1],t2[2]))) {
+    // t1.A is not a shared vertex, at least one of t1.B or t1.C share a vertex with t2
+    if ((pointIsNearlyEqual(t1[2],t2[0])) || (pointIsNearlyEqual(t1[2],t2[1])) || (pointIsNearlyEqual(t1[2],t2[2]))) {
+      returning = trianglesAreCoplanar(t1, t2) && (t2.checkInTriangle(t1[0]));
+      return returning;  // t1.B and t1.C are a side on t2
     }
     else {
-      Segment3 s= Segment3(t1[0], t1[2]);
-      returning= primal::intersect(t2, s);  //t1.B is a shared vertex,  
-      return returning;
+      Segment3 s = Segment3(t1[0], t1[2]);
+      returning = primal::intersect(t2, s);
+      return returning; // t1.B is a shared vertex
     }
   }
-  else  { //t1.C must be a shared vertex, because t1.A and t1.B are not shared
-    Segment3 s= Segment3(t1[0], t1[1]);
-    returning= primal::intersect(t2, s);  //t1.B is a shared vertex,  
-    return returning;
+  else { // t1.C must be a shared vertex, because t1.A and t1.B are not shared
+    Segment3 s = Segment3(t1[0], t1[1]);
+    returning = primal::intersect(t2, s);
+    return returning; // t1.C is a shared vertex
   }
 }
 
 //Requires t1 and t2 share at least 1 vertex
-bool trianglesOverlap(Triangle3 t1, Triangle3 t2) {
-  return ((trianglesIn(t1, t2) || trianglesIn(t2,t1)));
-}
-
-bool checkTT(Triangle3& t1, Triangle3& t2, std::ofstream& ofs, int i, int j)
+bool trianglesOverlap(Triangle3 t1, Triangle3 t2)
 {
-  //Step 9:  Ignore these degenerate triangles, as we will eventually write out all
-  //degenerate triangles in main for loop
-  if (t2.degenerate()) return false;
-  //Step 10:  If two triangles share at least one vertice, they are either connected only along an edge/vertex or one of the 
-  //triangles has at least one vertex lying on the interior or along an edge (excluding the vertices) of the other triangle
-  if (shareVertices(t1, t2)) {
-    if (trianglesOverlap(t1, t2)) {
-      ofs<<"\n INTERSECTION FOUND Triangle 1 at index "<< i<<" is "<<t1<<"and triangle 2 at index "<<j<<"is "<< t2 << "\n";
-      return true;
-    }
-    return false;
-  } 
-  //Step 11.2: If no shared vertice, run the standard intersection test
-  return  primal::intersect(t1,  t2);
+  return ((trianglesIn(t1, t2) || trianglesIn(t2, t1)));
 }
 
-void naiveAlgorithm(std::ofstream& ofs, mint::Mesh* surface_mesh) {
-        
+bool checkTT(Triangle3& t1, Triangle3& t2, std::ofstream& ofs, int i, int j, bool useTrianglesOverlap)
+{
+  // Step 9:  Ignore degenerate triangles: we will eventually write out all
+  // degenerate triangles in main for loop.
+  if (t2.degenerate()) return false;
 
+  if (useTrianglesOverlap) {
+    // Step 10:  If two triangles share at least one vertex, they are either
+    // connected only along an edge/vertex or one of the triangles has at least
+    // one vertex lying on the interior or along an edge (excluding the vertices)
+    // of the other triangle.
+    if (shareVertices(t1, t2)) {
+      if (trianglesOverlap(t1, t2)) {
+        ofs << "\n INTERSECTION FOUND Triangle 1 at index " << i << " is " << t1
+            << "and triangle 2 at index " << j << " is " << t2 << "\n";
+        return true;
+      }
+      return false;
+    }
+  }
 
-  //Step 6-11 overview: For each triangle, check for intersections against every other 
-  //triangle with a greater index in the mesh, excluding degenerate triangles and
-  //triangles whose only intersection with the initial triangle occurs at a vertice
+  // Step 11.2: If no shared vertices, run the standard intersection test.
+  return primal::intersect(t1,  t2);
+}
+
+void naiveAlgorithm(std::ofstream& ofs, mint::Mesh* surface_mesh) 
+{
+  // Step 6-11 overview: For each triangle, check for intersections against
+  // every other triangle with a greater index in the mesh, excluding
+  // degenerate triangles and triangles whose only intersection with the
+  // initial triangle occurs at a vertex.
+
   const int ncells = surface_mesh->getMeshNumberOfCells();
   SLIC_INFO("Checking mesh with a total of "<< ncells<< " cells.");
 
   Triangle3 t1 = Triangle3();
   Triangle3 t2 = Triangle3();
-  int counter=0; //total intersections found, will be written to outfile at end
-  int degenerateCounter=0;  //total degenerate triangles found
-  //Step 6:  Iterate over all the triangles in the mesh
-  for (int i=0; i< ncells; i++) {
-    t1=getMeshTriangle(i,  surface_mesh);
-    //Step 7: Check if the triangle is degenerate, if so, write out to output file and skip
+  int counter = 0; // total intersections found, will be written to outfile
+  int degenerateCounter = 0;  // total degenerate triangles found
+
+  // Step 6:  Iterate over all the triangles in the mesh
+  for (int i = 0; i< ncells; i++) {
+    t1 = getMeshTriangle(i, surface_mesh);
+
+    // Step 7: Check if the triangle is degenerate, if so, write out to
+    // output file and skip
     if (t1.degenerate()) {
       degenerateCounter++;     
-      ofs<<"Warning, degenerate triangle at index "<< i<<" with value "<<t1;
+      ofs << "Warning, degenerate triangle at index " << i << ": " << t1;
       continue;
     }
-    //Step 8:  If the triangle is not degenerate, test against all other triangles that this
-    //triangle has not been checked against
-    for (int j=i+1; j< ncells; j++) {
-      t2=getMeshTriangle(j,  surface_mesh);
-      //Step into CheckTT for steps 9-11
+
+    // Step 8:  If the triangle is not degenerate, test against all other
+    // triangles that this triangle has not been checked against
+    for (int j = i + 1; j < ncells; j++) {
+      t2 = getMeshTriangle(j, surface_mesh);
+      // Actual intersection test is wrapped by checkTT()
       if (checkTT(t1, t2, ofs, i, j)) counter++;
     }
   }
-  //step 12, write results to outfile
-  ofs<<"A total of "<< counter <<  "  triangle intersections were found.";
-  ofs<<"A total of "<< degenerateCounter <<  " degenerate triangles were found.";
+  // step 12, write results to outfile
+  ofs << "A total of " << counter << " triangle intersections were found.";
+  ofs << "A total of " << degenerateCounter << " degenerate triangles were found.";
 }
 
 void vgAlgorithm(std::ofstream& ofs, mint::Mesh* surface_mesh, int resolution) {
-
 
   int counter=0;  //total intersections found, will be written to outfile at end
   int degenerateCounter=0; //total degenerate triangles found
@@ -407,20 +428,19 @@ void vgAlgorithm(std::ofstream& ofs, mint::Mesh* surface_mesh, int resolution) {
 }
 
 //helper function to update the parameters with the command line arguments
-void init_params(Input& params,int argc, char ** argv){
+void init_params(Input& params,int argc, char ** argv)
+{
   if((argc > 1)){
     std::string help = argv[1];
-    if(help == "--help"){
+    if(help == "--help") {
       std::cout << "Possible flags/usage are as follows" << std::endl
-                << "--resolution: resolution of virtual grid-- make this 1 if you want to run the naive algorithm" <<std::endl
+                << "--resolution: resolution of uniform grid.  Make this 1 "
+                << "if you want to run the naive algorithm." << std::endl
                 << "--infile -- the stl input file" << std::endl
-                << "--outfile -- the text output file containing the results" << std::endl;
-      exit(100);
-
-    }
-    if((argc % 2 ) != 1){
-      std::cerr << "Incorrect number of command line arguments, needs to be odd";
-      exit(200);
+                << "--outfile -- the text output file containing the " 
+                << "results" << std::endl;
+      params.errorCode = 1;
+      return;
     }
     for(int i = 1; i< argc;){
       std::string arg = argv[i];
@@ -445,7 +465,8 @@ void init_params(Input& params,int argc, char ** argv){
 
 
 
-int main( int argc, char** argv ) {
+int main( int argc, char** argv ) 
+{
   // STEP 0: Initialize SLIC Environment
   slic::initialize();
   slic::setLoggingMsgLevel( axom::slic::message::Debug );
@@ -463,14 +484,9 @@ int main( int argc, char** argv ) {
 
   //Step 1: Initialize default parameters and update parameters with command line arguments:
   Input params;
-  const std::string defaultFileName = "plane.stl";
-  const std::string defaultDir = "/g/g17/edesanto/axom/src/components/primal/data/";
-  params.stlInput = utilities::filesystem::joinPath(defaultDir, defaultFileName);
-  params.textOutputFile="meshTestResults.txt";
-  params.resolution=10;
-  init_params(params,argc,argv);
-  SLIC_ASSERT(params.resolution>0);
-   
+  init_params(params, argc, argv);
+  SLIC_ASSERT(params.resolution > 0);
+  SLIC_ERROR_IF( (params.errorCode > 0), "Error while parsing arguments" );
 
   // STEP 2: read file
   SLIC_INFO("Reading file: " <<  params.stlInput << "...\n");
@@ -481,7 +497,7 @@ int main( int argc, char** argv ) {
 
   // STEP 3: get surface mesh
   mint::Mesh* surface_mesh = new TriangleMesh( 3 );
-  reader-> getMesh( static_cast<TriangleMesh*>( surface_mesh ) );
+  reader->getMesh( static_cast<TriangleMesh*>( surface_mesh ) );
 
   // STEP 4: Delete the reader
   delete reader;
