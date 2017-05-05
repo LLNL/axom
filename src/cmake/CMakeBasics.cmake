7fcf690ac802dd2976cbe833ffb947a73e70b6db
George Zagaris <zagaris2@llnl.gov>
2017-05-05 11:23:07 -0700

    ENH: add check for C++/Fortran compatibility
    This addresses ATK-1024.

--- src/cmake/CMakeBasics.cmake
+++ src/cmake/CMakeBasics.cmake
@@ -65,9 +65,11 @@ endif()
 # Fortran Configuration
 ################################
 if(ENABLE_FORTRAN)
-    # Create macros for Fortran name mangling
+
+    # Check C/C++ compiler compatiblity with the Fortran compiler
     include(FortranCInterface)
     FortranCInterface_VERIFY()
+    FortranCInterface_VERIFY(CXX)
     
     if (ENABLE_MPI)
         # Determine if we should use fortran mpif.h header or fortran mpi module
