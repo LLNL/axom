a86cf3fb9c12971427d5bcaea5310a8b4c366de7
Michael H. Chang <chang28@llnl.gov>
2017-05-17 12:32:23 -0700

    Add gcc@4.9.3 to Coral

---
+++ host-configs/rzmanta-blueos_3_ppc64le_ib-gcc@4.9.3.cmake
@@ -0,0 +1,89 @@
+##################################
+# uberenv host-config
+#
+# This is a generated file, edit at own risk.
+##################################
+# blueos_3_ppc64le_ib-gcc@4.9.3
+##################################
+
+# cmake from uberenv
+# cmake executable path: /usr/workspace/wsrzc/chang28/atk/coral/axom/scripts/uberenv/llnl_install_scripts/uberenv_libs/spack/opt/spack/blueos_3_ppc64le_ib/gcc-4.9.3/cmake-3.3.1-e3jy27kc25f7qb7oeujvyxhdcejq3ijo/bin/cmake
+
+#######
+# using gcc@4.9.3 compiler spec
+#######
+
+# c compiler used by spack
+set(CMAKE_C_COMPILER "/usr/tcetmp/packages/gcc/gcc-4.9.3/bin/gcc" CACHE PATH "")
+
+# cpp compiler used by spack
+set(CMAKE_CXX_COMPILER "/usr/tcetmp/packages/gcc/gcc-4.9.3/bin/g++" CACHE PATH "")
+
+# fortran compiler used by spack
+# no fortran compiler
+
+set(ENABLE_FORTRAN OFF CACHE BOOL "")
+
+# Root directory for generated TPLs
+set(TPL_ROOT "/usr/workspace/wsrzc/chang28/atk/coral/axom/scripts/uberenv/llnl_install_scripts/uberenv_libs/spack/opt/spack/blueos_3_ppc64le_ib/gcc-4.9.3" CACHE PATH "")
+
+# hdf5 from uberenv
+set(HDF5_DIR "${TPL_ROOT}/hdf5-1.8.16-nj464kx3de5a7cnt3rzn57lltdku4x57" CACHE PATH "")
+
+# conduit from uberenv
+set(CONDUIT_DIR "${TPL_ROOT}/conduit-0.2.1-2pas54crsrghba7afbdi7kchzh2qb6nm" CACHE PATH "")
+
+# mfem from uberenv
+set(MFEM_DIR "${TPL_ROOT}/mfem-3.3-26n37iqtq6tggnhrxlr2vrulm46hcjps" CACHE PATH "")
+
+# boost headers from uberenv
+set(BOOST_DIR "${TPL_ROOT}/boost-headers-1.58.0-myxxuxc6xtfpl33e2ubcxr56co54uecn" CACHE PATH "")
+
+# python not build by uberenv
+
+# lua not build by uberenv
+
+# doxygen not built by uberenv
+
+# sphinx not built by uberenv
+
+# uncrustify not built by uberenv
+
+# lcov and genhtml from uberenv
+# lcov and genhtml not built by uberenv
+
+##################################
+# end uberenv host-config
+##################################
+##############################################################################
+# !---------------------------------------------------------------------------
+##############################################################################
+# Options added manually to 
+# lc blueos clang@4.0.0  host configs
+##############################################################################
+
+##############################################################################
+# MPI - manually added for now
+##############################################################################
+
+set(ENABLE_MPI ON CACHE BOOL "")
+
+set(MPI_HOME                 "/usr/tcetmp/packages/spectrum-mpi/spectrum-mpi-2017.04.03-gcc-4.9.3" CACHE PATH "")
+set(MPI_Fortran_HOME         "/usr/tcetmp/packages/xl/xl-beta-2017.05.08" CACHE PATH "")
+set(MPI_C_COMPILER           "${MPI_HOME}/bin/mpicc"   CACHE PATH "")
+set(MPI_CXX_COMPILER         "${MPI_HOME}/bin/mpicxx"  CACHE PATH "")
+set(MPI_Fortran_COMPILER     "${MPI_HOME}/bin/mpifort" CACHE PATH "")
+set(MPI_Fortran_LIBRARIES    "${MPI_Fortran_HOME}/lib" CACHE PATH "")
+set(MPI_Fortran_INCLUDE_PATH "${MPI_Fortran_HOME}/16.1.0/include" CACHE PATH "")
+
+set(MPIEXEC              "mpirun" CACHE PATH "")
+set(MPIEXEC_NUMPROC_FLAG "-n" CACHE PATH "")
+
+##############################################################################
+# SHROUD - manually added for now. Use a public build add to TPL later
+##############################################################################
+set(SHROUD_EXECUTABLE "/usr/apps/shroud/bin/shroud" CACHE PATH "")
+
+##############################################################################
+# !---------------------------------------------------------------------------
+##############################################################################
