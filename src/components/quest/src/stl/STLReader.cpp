3faf8d4adda8c0f74a49e6e38b7be5306fb7f183
Kenneth Weiss <weiss27@llnl.gov>
2017-06-06 11:25:28 -0700

    Moved some constants in Quest's STL reader to the implementation file. ATK-1038.
    Based on PR suggestion from Cyrus H.
    Also improved the documentation.

--- src/components/quest/src/stl/STLReader.cpp
+++ src/components/quest/src/stl/STLReader.cpp
@@ -16,11 +16,16 @@
 #include "axom/Types.hpp"
 #include "slic/slic.hpp"
 
-
 // C/C++ includes
 #include <cstddef>   // for NULL
 #include <fstream>   // for ifstream
 
+
+namespace
+{
+    const std::size_t  BINARY_HEADER_SIZE = 80; // bytes
+    const std::size_t  BINARY_TRI_SIZE = 50;    // bytes
+}
 
 
 //------------------------------------------------------------------------------
@@ -55,13 +60,10 @@ void STLReader::clear()
 //------------------------------------------------------------------------------
 bool STLReader::isAsciiFormat() const
 {
-    // An STL file with 'numTris' triangles is in binary format  
-    // if its file size (in bytes) is: 84 + 50 * numTris
-
     // The binary format consists of 
-    //    an 80 byte header, 
+    //    a header of size BINARY_HEADER_SIZE==80 bytes
     //    followed by a four byte int encoding the number of triangles
-    //    followed by the triangle data (50 bytes per triangle)
+    //    followed by the triangle data (BINARY_TRI_SIZE == 50 bytes per triangle)
 
     // Open the file
     std::ifstream ifs( m_fileName.c_str(), std::ios::in| std::ios::binary);
@@ -70,16 +72,16 @@ bool STLReader::isAsciiFormat() const
 
     // Find out the file size
     ifs.seekg(0, ifs.end);
-    int fileSize = ifs.tellg();
+    axom::common::int32 fileSize = ifs.tellg();
     
-    const int totalHeaderSize = (BINARY_HEADER_SIZE + sizeof(int));
+    const int totalHeaderSize = (BINARY_HEADER_SIZE + sizeof(axom::common::int32));
     if(fileSize < totalHeaderSize)
         return true;
     
     // Find the number of triangles (if the file were binary)
     int numTris = 0;
     ifs.seekg(BINARY_HEADER_SIZE, ifs.beg);
-    ifs.read( (char*)&numTris, sizeof(int));
+    ifs.read( (char*)&numTris, sizeof(axom::common::int32));
     
     // Check if the size matches our expectation
     int expectedBinarySize = totalHeaderSize + (numTris * BINARY_TRI_SIZE);
@@ -140,7 +142,7 @@ void STLReader::readBinarySTL()
   ifs.seekg(BINARY_HEADER_SIZE);
 
   // read the num faces and reserve room for the vertex positions
-  ifs.read( (char*)&m_num_faces, 4);
+  ifs.read( (char*)&m_num_faces, sizeof(axom::common::int32));
 
   m_num_nodes = m_num_faces * 3;
   m_nodes.reserve( m_num_nodes * 3);
