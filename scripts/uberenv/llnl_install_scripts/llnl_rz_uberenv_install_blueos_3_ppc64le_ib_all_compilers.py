5ad0628d8583bb94d6f4646d8553ff00522114bc
Michael H. Chang <chang28@llnl.gov>
2017-06-07 14:14:55 -0700

    Add clang@coral

--- scripts/uberenv/llnl_install_scripts/llnl_rz_uberenv_install_blueos_3_ppc64le_ib_all_compilers.py
+++ scripts/uberenv/llnl_install_scripts/llnl_rz_uberenv_install_blueos_3_ppc64le_ib_all_compilers.py
@@ -24,7 +24,8 @@ def main():
     # write info about this build
     write_build_info(pjoin(prefix,"info.json"))
     # spack specs for the cz chaos systems
-    specs = ["%gcc@4.9.3~devtools~python~lua"]
+    specs = ["%gcc@4.9.3~devtools~python~lua",
+             "%clang@coral~devtools~python~lua"]
     #specs = ["%clang@4.0.0"]
     # use uberenv to install for all specs
     for spec in specs:
