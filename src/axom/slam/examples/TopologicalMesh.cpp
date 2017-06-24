054ef1f20335288233d69b9d7c41a93d204f283d
Raine Yeh <yeh14@llnl.gov>
2017-06-23 17:27:50 -0700

    Updates slam TopologicalMesh example

--- src/axom/slam/examples/TopologicalMesh.cpp
+++ src/axom/slam/examples/TopologicalMesh.cpp
@@ -59,16 +59,12 @@ struct BasicTetMeshData
 struct Flat2DTriMeshData
 {
   //This is a square
-  std::vector<double> points = {0,      0,      1.0000, 0,      0,      1.0000,
-                                1.0000, 1.0000, 0.6557, 0.7060, 0.0357, 0.0318,
-                                0.8491, 0.2769, 0.9340, 0.0462, 0.6787, 0.0971,
-                                0.7577, 0.8235, 0.7431, 0.6948, 0.3922, 0.3171,
-                                0.6555, 0.9502, 0.1712, 0.0344};
-  std::vector<int> tri = {11, 2,  5,  5,  0, 13, 0,  5, 2,  11, 5,  13, 0,  1,
-                          13, 4,  2,  11, 4, 12, 2,  3, 2,  12, 13, 8,  11, 10,
-                          9,  4,  11, 6,  4, 9,  12, 4, 3,  9,  10, 3,  12, 9,
-                          4,  6,  10, 11, 8, 6,  6,  3, 10, 7,  6,  8,  7,  1,
-                          6,  13, 1,  8,  1, 3,  6,  8, 1,  7};
+  std::vector<double> points =
+    {-1, -1, -1, 1, 1, -1, 1, 1, -2, -2, -2, 2, 2, -2, 2, 2};
+  std::vector<int> tri = {
+
+    1, 3, 5, 4, 0, 5, 0, 1, 5, 0, 2, 1, 0, 4, 2,
+    3, 7, 5, 3, 2, 6, 3, 1, 2, 3, 6, 7, 2, 4, 6};
 };
 
 typedef axom::primal::Point2D Point2D;
@@ -84,9 +80,9 @@ typedef axom::primal::Triangle<double, 2> Triangle2D;
 		for(IndexType zidx = 0; zidx < tri_mesh.zone_set.size(); zidx++)
 		{
 
-			Point p0 = tri_mesh.node_coord[ tri_mesh.zn_rel[zidx][0] ];
-			Point p1 = tri_mesh.node_coord[ tri_mesh.zn_rel[zidx][1] ];
-			Point p2 = tri_mesh.node_coord[ tri_mesh.zn_rel[zidx][2] ];
+			Point p0 = tri_mesh.dcoord_map[ tri_mesh.dzn_rel[zidx][0] ];
+			Point p1 = tri_mesh.dcoord_map[ tri_mesh.dzn_rel[zidx][1] ];
+			Point p2 = tri_mesh.dcoord_map[ tri_mesh.dzn_rel[zidx][2] ];
 			Point p01 = p1 - p0;
 			Point p02 = p2 - p0;
 			//Do we assume the triangle is not degenerate?
@@ -116,18 +112,22 @@ typedef axom::primal::Triangle<double, 2> Triangle2D;
 
 IndexType find_containing_zone(const axom::slam::IAMesh<3, 2>& mesh,
                                Point2D query_pt,
-                               IndexType zone_i = 0)
+                               IndexType zone_i)
 {
+  SLIC_INFO("find_containing_zone " << zone_i);
   //assumes the point is definitely contained within a triangle
   //TODO fix that assumption ^
   SLIC_INFO("Query Pt " << query_pt);
-  SLIC_ASSERT(mesh.zn_rel[zone_i].size() == 3);  //should be a triangle...
+  SLIC_ASSERT(mesh.dzn_rel[zone_i].size() == 3);  //should be a triangle...
 
   while(1)
   {
-    Triangle2D tri(mesh.node_coord[mesh.zn_rel[zone_i][0]],
-                   mesh.node_coord[mesh.zn_rel[zone_i][1]],
-                   mesh.node_coord[mesh.zn_rel[zone_i][2]]);
+    SLIC_INFO("step into zone" << zone_i);
+    IndexType n0 = mesh.dzn_rel[zone_i][0];
+    IndexType n1 = mesh.dzn_rel[zone_i][1];
+    IndexType n2 = mesh.dzn_rel[zone_i][2];
+
+    Triangle2D tri(mesh.dcoord_map[n0], mesh.dcoord_map[n1], mesh.dcoord_map[n2]);
 
     axom::primal::Point3D bary_co = tri.barycentricCoords(query_pt);
     SLIC_INFO("bary_co " << bary_co);
@@ -137,17 +137,17 @@ IndexType find_containing_zone(const axom::slam::IAMesh<3, 2>& mesh,
     if(bary_co[0] > bary_co[1]) i = 1;
     if(bary_co[i] > bary_co[2]) i = 2;
 
-    if(bary_co[i] > 0)
-    {  //smaller than i -> outside of the triangle
-      return zone_i;
+    if(bary_co[i] >= 0)
+    {                 //smaller than i -> outside of the triangle
+      return zone_i;  //return if inside or on triangle
     }
 
-    zone_i = mesh.zz_rel[zone_i][2 - i];
+    zone_i = mesh.dzz_rel[zone_i][2 - i];
   }
 }
 
 bool is_point_in_circle(Point2D p0, Point2D p1, Point2D p2, Point2D q)
-{
+{  //todo const
   double det = axom::numerics::determinant(1.0,
                                            p0[0],
                                            p0[1],
@@ -172,16 +172,17 @@ bool is_point_in_circle(Point2D p0, Point2D p1, Point2D p2, Point2D q)
 	 */
 std::vector<IndexType> find_violating_zones(const axom::slam::IAMesh<3, 2>& mesh,
                                             Point2D query_pt,
-                                            IndexType zone_i)
+                                            IndexType q_zone_i)
 {
+  //SLIC_INFO("find_violating_zones from zone " << q_zone_i );
+  IndexType zone_i = q_zone_i;
   std::vector<IndexType> ret;
   std::list<IndexType> zone_list_to_check;
-  for(int i = 0; i < mesh.zz_rel[zone_i].size(); i++)
-  {
-    zone_list_to_check.push_back(mesh.zz_rel[zone_i][i]);
-  }
+  zone_list_to_check.push_back(zone_i);
+
   std::set<IndexType> checked_zones;
   checked_zones.insert(zone_i);
+  checked_zones.insert(-1);
 
   //starting from zone_i, which contains the point, try its neighbors, and the neighbor's neighbors
   //for point in circle test
@@ -191,21 +192,29 @@ std::vector<IndexType> find_violating_zones(const axom::slam::IAMesh<3, 2>& mesh
     zone_i = zone_list_to_check.front();
     zone_list_to_check.pop_front();
     if(zone_i < 0) continue;
-    Point2D p0 = mesh.node_coord[mesh.zn_rel[zone_i][0]];
-    Point2D p1 = mesh.node_coord[mesh.zn_rel[zone_i][1]];
-    Point2D p2 = mesh.node_coord[mesh.zn_rel[zone_i][2]];
+    IndexType n0 = mesh.dzn_rel[zone_i][0];
+    IndexType n1 = mesh.dzn_rel[zone_i][1];
+    IndexType n2 = mesh.dzn_rel[zone_i][2];
+    Point2D p0 = mesh.dcoord_map[n0];
+    Point2D p1 = mesh.dcoord_map[n1];
+    Point2D p2 = mesh.dcoord_map[n2];
     bool is_in_circle = is_point_in_circle(p0, p1, p2, query_pt);
-    SLIC_INFO("is in circle " << is_in_circle);
     if(is_in_circle)
     {
-      ret.push_back(zone_i);
-      for(int i = 0; i < mesh.zz_rel[zone_i].size(); i++)
+      bool is_repeat = false;
+      if(!is_repeat)
       {
-        std::set<IndexType>::iterator it =
-          checked_zones.find(mesh.zz_rel[zone_i][i]);
-        if(it != checked_zones.end())
+        ret.push_back(zone_i);
+        for(int i = 0; i < (int)mesh.dzz_rel[zone_i].size(); i++)
         {
-          zone_list_to_check.push_back(*it);
+          IndexType nbr_zone = mesh.dzz_rel[zone_i][i];
+
+          std::set<IndexType>::iterator it = checked_zones.find(nbr_zone);
+          if(it == checked_zones.end())
+          {
+            zone_list_to_check.push_back(nbr_zone);
+            checked_zones.insert(nbr_zone);
+          }
         }
       }
     }
@@ -213,6 +222,32 @@ std::vector<IndexType> find_violating_zones(const axom::slam::IAMesh<3, 2>& mesh
   return ret;
 }
 }  // namespace slamTopologicalMesh
+
+int printc = 1;
+void print_out_mesh(axom::slam::IAMesh<3, 2> ia_mesh2)
+{
+  using namespace slamTopologicalMesh;
+  SLIC_INFO("Printing out the resulting mesh:");
+  std::cout << "i=" << printc++ << std::endl;
+  std::cout << "nodes=[";
+  for(int i = 0; i < ia_mesh2.dcoord_map.actual_size(); i++)
+  {
+    Point2D p = ia_mesh2.dcoord_map[i];
+    std::cout << p[0] << "," << p[1] << ";" << std::endl;
+  }
+  std::cout << "];\n";
+  std::cout << "tri=[";
+  for(int i = 0; i < ia_mesh2.zone_set.actual_size(); i++)
+  {
+    if(ia_mesh2.zone_set.isValidEntry(i))
+    {
+      std::vector<int> nlist = ia_mesh2.getNodeInZone(i);
+      std::cout << nlist[0] << "," << nlist[1] << "," << nlist[2] << ";"
+                << std::endl;
+    }
+  }
+  std::cout << "];\n";
+}
 
 int main(/*int argc, char** argv*/)
 {
@@ -273,23 +308,30 @@ int main(/*int argc, char** argv*/)
   axom::slam::IAMesh<3, 2> ia_mesh2(basic_tri_mesh_data.points,
                                     basic_tri_mesh_data.tri);
 
+  print_out_mesh(ia_mesh2);
+
   //fake adding a number of random points
   srand(1234);
   int num_points = 10;
+  IndexType starting_zone = 0;
   for(int pt_i = 0; pt_i < num_points; pt_i++)
   {
-    double x = (double)rand() / (double)RAND_MAX;
-    double y = (double)rand() / (double)RAND_MAX;
+    double x = (double)rand() / (double)RAND_MAX * 2.0 - 1.0;
+    double y = (double)rand() / (double)RAND_MAX * 2.0 - 1.0;
     double pt_coord[2] = {x, y};
     Point2D new_pt(pt_coord, 2);
 
     //(assumes a mesh is already existing...)
     //find zone containing triangle
-    IndexType zone_i = find_containing_zone(ia_mesh2, new_pt, 0);
+    IndexType zone_i = find_containing_zone(ia_mesh2, new_pt, starting_zone);
 
     //find the delaunay cavity...
     std::vector<IndexType> zones_to_remove =
       find_violating_zones(ia_mesh2, new_pt, zone_i);
+    std::cout << "violating zones:";
+    for(unsigned int i = 0; i < zones_to_remove.size(); i++)
+      std::cout << zones_to_remove[i] << " ";
+    std::cout << std::endl;
 
     //remove each zones from the mesh, keeping a list of cavity edges and their connected zones
     typedef std::pair<IndexType, IndexType> IndexPairType;
@@ -299,12 +341,11 @@ int main(/*int argc, char** argv*/)
     for(unsigned int i = 0; i < zones_to_remove.size(); i++)
     {
       IndexType zone_i = zones_to_remove[i];
-      for(int j = 0; j < ia_mesh2.zz_rel[zone_i].size(); j++)
+      for(int j = 0; j < (int)ia_mesh2.dzn_rel[zone_i].size(); j++)
       {
-        IndexType z = ia_mesh2.zz_rel[zone_i][j];
-        if(z < 0) continue;
-        IndexType n1 = ia_mesh2.zn_rel[z][j / 2];
-        IndexType n2 = ia_mesh2.zn_rel[z][(j + 3) / 2];
+        IndexType z = zone_i;
+        IndexType n1 = ia_mesh2.dzn_rel[z][(j * 2) % 3];
+        IndexType n2 = ia_mesh2.dzn_rel[z][(1 + j * 2) % 3];
         CavityMapType::iterator iter = cavity_edges.find(IndexPairType(n2, n1));
         if(iter == cavity_edges.end())
         {
@@ -318,7 +359,14 @@ int main(/*int argc, char** argv*/)
       ia_mesh2.removeZone(zone_i);
     }
 
+    SLIC_INFO("After removing zones");
+    print_out_mesh(ia_mesh2);
+    std::cout << "pt = [" << pt_coord[0] << " " << pt_coord[1] << "];"
+              << std::endl;
+
     //new triangles from the cavity edges
+
+    IndexType new_pt_i = ia_mesh2.addNode(new_pt);
     for(CavityMapType::iterator iter = cavity_edges.begin();
         iter != cavity_edges.end();
         iter++)
@@ -327,14 +375,17 @@ int main(/*int argc, char** argv*/)
       IndexType n2 = iter->first.second;
       //IndexType nbr_zone_i = iter->second;
 
-      IndexType new_pt_i = ia_mesh2.addNode(new_pt);
-      ia_mesh2.addZone(n1, n2, new_pt_i);
-      SLIC_INFO("New cell: " << n1 << ", " << n2 << ", newpt:" << new_pt_i);
+      starting_zone = ia_mesh2.addZone(n1, n2, new_pt_i);
+      SLIC_INFO("New cell: " << starting_zone << " with " << n1 << ", " << n2
+                             << ", " << new_pt_i);
     }
-    //SLIC_INFO(zone_i);
+    SLIC_INFO("After adding zones");
+    print_out_mesh(ia_mesh2);
+    std::cout << "pt = [" << pt_coord[0] << " " << pt_coord[1] << "];"
+              << std::endl;
   }
 
-  ia_mesh2.zone_set.remove(0);
+  print_out_mesh(ia_mesh2);
 
   SLIC_INFO("Done!");
 
