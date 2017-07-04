5c6406603f77c25ee468fbd85ea23940d544ec84
Kenneth Weiss <weiss27@llnl.gov>
2017-07-03 20:00:46 -0700

    Fixes warning about unused variable in sidre tests.

--- src/components/sidre/tests/sidre_attribute.cpp
+++ src/components/sidre/tests/sidre_attribute.cpp
@@ -43,7 +43,7 @@ const int dump_yes = 1;
 const std::string name_size("size");
 const double size_small = 1.2;
 const double size_medium = 2.3;
-const double size_large = 3.4;
+// const double size_large = 3.4;
 
 //------------------------------------------------------------------------------
 // Create attribute in a Datastore
