693bbfa164ec8e4f06246b8e5acf40800d077b00
Kenneth Weiss <weiss27@llnl.gov>
2017-07-18 22:27:48 -0700

    Adds an endianness checker and swapper to axom utilities. ATK-1069.

--- src/components/axom_utils/src/utilities/Utilities.hpp
+++ src/components/axom_utils/src/utilities/Utilities.hpp
@@ -12,15 +12,17 @@
 /*!
  ******************************************************************************
  *
- * \file
+ * \file Utilities.hpp
  *
- * \brief   Header file containing utility functions.
+ * \brief Header file containing utility functions.
  *
  ******************************************************************************
  */
 
-#ifndef UTILITIES_HPP_
-#define UTILITIES_HPP_
+#ifndef AXOM_UTILITIES_HPP_
+#define AXOM_UTILITIES_HPP_
+
+#include "axom/Types.hpp"
 
 #include <cmath>
 #include <algorithm>
@@ -52,6 +54,84 @@ namespace utilities {
      T tmp = a;
      a = b; b = tmp;
   }
+
+  /**
+   * Tests the endianness of the system
+   *
+   * \return True, if the system is little endian, false otherwise
+   */
+  inline bool isLittleEndian()
+  {
+    // Note: Endianness test adapted from: https://stackoverflow.com/a/2103095
+
+    enum {
+      O32_LITTLE_ENDIAN = 0x03020100ul,
+      O32_BIG_ENDIAN    = 0x00010203ul,
+      O32_PDP_ENDIAN    = 0x01000302ul };
+
+    const union {
+      axom::common::uint8 raw[4];
+      axom::common::uint32 value;
+    }  host_order = { { 0, 1, 2, 3 } };
+
+    return host_order.value == O32_LITTLE_ENDIAN;
+  }
+
+
+
+  /*!
+   * \brief Swaps the endianness of the input value
+   *
+   * \param val The input value
+   * \return The value with endianness swapped
+   * \note Assumes endianness is either little or big (not PDP)
+   */
+  template < typename T >
+  T swapEndian(T val)
+  {
+    union
+    {
+      axom::common::uint8 raw[ sizeof(T) ];
+      T val;
+    } swp;
+
+    axom::common::uint8* src = reinterpret_cast<axom::common::uint8*>(&val);
+
+    switch(sizeof(T))
+    {
+    case 2:
+      swp.raw[0] = src[1];
+      swp.raw[1] = src[0];
+      break;
+
+    case 4:
+      swp.raw[0] = src[3];
+      swp.raw[1] = src[2];
+      swp.raw[2] = src[1];
+      swp.raw[3] = src[0];
+      break;
+
+    case 8:
+      swp.raw[0] = src[7];
+      swp.raw[1] = src[6];
+      swp.raw[2] = src[5];
+      swp.raw[3] = src[4];
+
+      swp.raw[4] = src[3];
+      swp.raw[5] = src[2];
+      swp.raw[6] = src[1];
+      swp.raw[7] = src[0];
+      break;
+
+    default:
+      swp.val = val;
+      break;
+    }
+
+    return swp.val;
+  }
+
+
 
 /*!
  * \brief Fuzzy comparison of two real valued quantities
@@ -93,4 +173,4 @@ namespace utilities {
 }  // ending brace for utilities namespace
 }  // ending brace for axom namespace
 
-#endif  // header include guard
+#endif  // AXOM_UTILITIES_HPP_
