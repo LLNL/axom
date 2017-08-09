615ec5f2fd905b48c6ba8b4f48b8c895a2f1bcda
Raine Yeh <yeh14@llnl.gov>
2017-08-09 08:47:27 -0700

    delaunay on tetrahedrons

--- src/axom/quest/Delaunay.hpp
+++ src/axom/quest/Delaunay.hpp
@@ -24,22 +24,29 @@ namespace axom
 {
 namespace quest
 {
+template <unsigned int TOPO_DIM = 2>
 class Delaunay
 {
 public:
-#define TOPO_DIM 2
-#define VERT_PER_ELEMENT TOPO_DIM + 1
-#define SPATIAL_DIM 2
+  enum
+  {
+    VERT_PER_ELEMENT = TOPO_DIM + 1,
+    SPATIAL_DIM = TOPO_DIM
+  };
 
   using DataType = double;
   using Indextype = int;
 
   using PointType = primal::Point<DataType, SPATIAL_DIM>;
+  using Point2DType = primal::Point<DataType, 2>;
+  using Point3DType = primal::Point<DataType, 3>;
+  using Point4DType = primal::Point<DataType, 4>;
   using IATriMeshType = slam::IAMesh<TOPO_DIM, SPATIAL_DIM, PointType>;
-  using Triangle2D = axom::primal::Triangle<DataType, SPATIAL_DIM>;
+  using Triangle2D = axom::primal::Triangle<DataType, 2>;
+  using Tetrahedron3D = axom::primal::Tetrahedron<DataType, 3>;
   using BoundingBox = axom::primal::BoundingBox<DataType, SPATIAL_DIM>;
 
-  using IndexListType = IATriMeshType::IndexListType;
+  using IndexListType = typename IATriMeshType::IndexListType;
   using IndexPairType = std::pair<IndexType, IndexType>;
 
 private:
@@ -67,12 +74,14 @@ private:
 
 public:
   /**
-   * \brief Default Constructor that creates the boundary square from -1,-1 to 1,1
+   * \brief Default Constructor. User need to call initializeBoundary(BoundPoint2DTypeingBox) before adding points.
    */
   Delaunay()
     : m_has_boundary(false)
     , m_num_removed_elements_since_last_compact(0)
-  { }
+  {
+    SLIC_ASSERT(TOPO_DIM == 2 || TOPO_DIM == 3);
+  }
 
   /**
    * \brief Defines the boundary of the triangulation.
@@ -87,17 +96,36 @@ public:
     DataType ymin = bb.getMin()[1];
     DataType ymax = bb.getMax()[1];
 
-    static const DataType point_arr[] =
-      {xmin, ymin, xmin, ymax, xmax, ymin, xmax, ymax};
-    std::vector<DataType> points(
-      point_arr,
-      point_arr + sizeof(point_arr) / sizeof(point_arr[0]));
+    std::vector<DataType> points;
+    std::vector<IndexType> elem;
 
-    static const IndexType tri_arr[] = {0, 2, 1, 3, 1, 2};
-    std::vector<IndexType> tri(tri_arr,
-                               tri_arr + sizeof(tri_arr) / sizeof(tri_arr[0]));
+    if(TOPO_DIM == 2)
+    {
+      static const DataType point_arr[] =
+        {xmin, ymin, xmin, ymax, xmax, ymin, xmax, ymax};
+      static const IndexType tri_arr[] = {0, 2, 1, 3, 1, 2};
+      points.assign(point_arr,
+                    point_arr + sizeof(point_arr) / sizeof(point_arr[0]));
+      elem.assign(tri_arr, tri_arr + sizeof(tri_arr) / sizeof(tri_arr[0]));
+    }
+    else if(TOPO_DIM == 3)
+    {
+      DataType zmin = bb.getMin()[2];
+      DataType zmax = bb.getMax()[2];
 
-    m_mesh = IATriMeshType(points, tri);
+      static const DataType point_arr[] = {
+        xmin, ymin, zmin, xmin, ymin, zmax, xmin, ymax, zmin, xmin, ymax, zmax,
+        xmax, ymin, zmin, xmax, ymin, zmax, xmax, ymax, zmin, xmax, ymax, zmax};
+
+      static const IndexType tet_arr[] = {3, 2, 4, 0, 3, 4, 1, 0, 3, 2, 6, 4,
+                                          3, 6, 7, 4, 3, 5, 1, 4, 3, 7, 5, 4};
+
+      points.assign(point_arr,
+                    point_arr + sizeof(point_arr) / sizeof(point_arr[0]));
+      elem.assign(tet_arr, tet_arr + sizeof(tet_arr) / sizeof(tet_arr[0]));
+    }
+
+    m_mesh = IATriMeshType(points, elem);
 
     m_has_boundary = true;
   }
@@ -105,7 +133,7 @@ public:
   /**
    * \brief Add a new point to the m_mesh, which triggers re-triangulation
    * \brief This function will traverse the m_mesh to find the element that contains this point,
-   * create the delaunay cavity, which takes out all the elements that contains the point it is sphere,
+   * create the delaunay cavity, which takes out all the elements that contains the point in its sphere,
    * and fill in the cavity with a delaunay ball.
    */
   void insertPoint(const PointType& new_pt)
@@ -119,23 +147,15 @@ public:
     SLIC_ASSERT_MSG(m_bounding_box.contains(new_pt),
                     "Error: new point is outside of the boundary box.");
 
-    //m_mesh.print_all();
-    //SLIC_INFO("DT: Inserting point "<<new_pt[0] <<"," <<new_pt[1]);
+    m_mesh.print_all();
+    SLIC_INFO("DT: Inserting point");
+    //new_pt.print(std::cout); std::cout << std::endl;
 
     IndexType element_i = findContainingElement(new_pt);
 
     //SLIC_INFO("DT: Containing element: "<<element_i );
 
     findCavityElements(new_pt, element_i);
-
-    /*
-    std::cout<<"DT: violating elements:" ;
-    for(unsigned int i=0; i<elements_to_remove.size(); i++)
-      std::cout<<elements_to_remove[i] <<" ";
-    std::cout<<std::endl;
-    */
-
-    //SLIC_ASSERT_MSG(elements_to_remove.size() > 0, "Error: New point is not contained in the m_mesh");
 
     createCavity();
 
@@ -147,24 +167,27 @@ public:
 
     delaunayBall(new_pt_i);
 
-    m_mesh.fixVertexNeighborhood(new_pt_i, new_elements);
+    m_mesh.fixVertexNeighbor(new_pt_i, new_elements);
 
     //SLIC_INFO("DT: After adding elements");
     //m_mesh.print_all();
 
-    //Make sure ee_rel has only 4 invalid
+    //Make sure ee_rel has only 4 invalid for triangles, 12 invalids for tetrahedrons
     int invalid_nbr_count = 0;
     for(int i = 0; i < m_mesh.ee_rel.size(); i++)
     {
       if(m_mesh.ee_rel.isValidEntry(i))
       {
-        for(int j = 0; j < VERT_PER_ELEMENT; j++)
+        for(int j = 0; j < (int)VERT_PER_ELEMENT; j++)
         {
           invalid_nbr_count += m_mesh.ee_rel[i][j] < 0;
         }
       }
     }
-    SLIC_ASSERT(invalid_nbr_count == 4);
+    SLIC_ASSERT(invalid_nbr_count == (TOPO_DIM == 2 ? 4 : 12));
+
+    SLIC_INFO("After insert");
+    m_mesh.print_all();
 
     //call compact() if there are too many invalid points
     if(m_num_removed_elements_since_last_compact > 64 &&
@@ -176,7 +199,7 @@ public:
 
     if(!m_mesh.isValid(true))
     {
-      SLIC_INFO("IA m_mesh is invalid after adding new point");
+      SLIC_WARNING("IA m_mesh is invalid after adding new point");
     }
   }
 
@@ -193,20 +216,26 @@ public:
   void writeToVTKFile(const std::string filename)
   {
     m_mesh.compact();
+    const int MINT_MESH_TYPE = TOPO_DIM == 2 ? MINT_TRIANGLE : MINT_TET;
 
-    axom::mint::UnstructuredMesh<MINT_TRIANGLE> mint_mesh(2);
+    axom::mint::UnstructuredMesh<MINT_MESH_TYPE> mint_mesh(SPATIAL_DIM);
+
     for(int i = 0; i < m_mesh.vertex_set.size(); i++)
     {
       if(!m_mesh.vertex_set.isValidEntry(i)) continue;
-      mint_mesh.insertNode(m_mesh.vcoord_map[i][0], m_mesh.vcoord_map[i][1]);
+
+      DataType coords[SPATIAL_DIM];
+      for(unsigned int j = 0; j < SPATIAL_DIM; j++)
+        coords[j] = m_mesh.vcoord_map[i][j];
+      mint_mesh.insertNode(coords);
     }
+
     for(int i = 0; i < m_mesh.ev_rel.size(); i++)
     {
       if(!m_mesh.ev_rel.isValidEntry(i)) continue;
 
       const int* ptr = &(m_mesh.ev_rel[i][0]);
-
-      mint_mesh.insertCell(ptr, MINT_TRIANGLE, 3);
+      mint_mesh.insertCell(ptr, MINT_MESH_TYPE, VERT_PER_ELEMENT);
     }
     mint_mesh.toVtkFile(filename);
   }
@@ -219,13 +248,14 @@ public:
   {
     if(m_has_boundary)
     {
-      //remove the boundary box, which will be the first 4 points.
-      m_mesh.removeVertex(0);
-      m_mesh.removeVertex(1);
-      m_mesh.removeVertex(2);
-      m_mesh.removeVertex(3);
+      //remove the boundary box, which will be the first 4 points for triangles, first 8 for tetrahedron
+      unsigned int num_boundary_pts = TOPO_DIM == 2 ? 4 : 8;
+      for(unsigned int i = 0; i < num_boundary_pts; i++) m_mesh.removeVertex(i);
 
       m_has_boundary = false;
+
+      SLIC_INFO("After removing boundary");
+      m_mesh.print_all();
     }
   }
 
@@ -235,15 +265,38 @@ public:
   const IATriMeshType* getMeshData() const { return &m_mesh; }
 
 private:
+  const Point2DType toPt2(Point2DType pt) const { return pt; }
+  const Point2DType toPt2(Point3DType) const
+  {
+    SLIC_ERROR("Not supposed to run!");
+    return Point2DType();
+  }
+  const Point3DType toPt3(Point2DType) const
+  {
+    SLIC_ERROR("Not supposed to run!");
+    return Point3DType();
+  }
+  const Point3DType toPt3(Point3DType pt) const { return pt; }
+
   Triangle2D getTriangle(IndexType element_idx)
   {
     IndexListType verts = m_mesh.getVertexInElement(element_idx);
 
-    Triangle2D tri(m_mesh.getVertexPoint(verts[0]),
-                   m_mesh.getVertexPoint(verts[1]),
-                   m_mesh.getVertexPoint(verts[2]));
-
+    Triangle2D tri(toPt2(m_mesh.getVertexPoint(verts[0])),
+                   toPt2(m_mesh.getVertexPoint(verts[1])),
+                   toPt2(m_mesh.getVertexPoint(verts[2])));
     return tri;
+  }
+
+  Tetrahedron3D getTetrahedron(IndexType element_idx)
+  {
+    IndexListType verts = m_mesh.getVertexInElement(element_idx);
+
+    Tetrahedron3D tet(toPt3(m_mesh.getVertexPoint(verts[0])),
+                      toPt3(m_mesh.getVertexPoint(verts[1])),
+                      toPt3(m_mesh.getVertexPoint(verts[2])),
+                      toPt3(m_mesh.getVertexPoint(verts[3])));
+    return tet;
   }
 
   /**
@@ -263,23 +316,40 @@ private:
     {
       //SLIC_INFO("DT: step into element "<<element_i);
 
-      Triangle2D tri = getTriangle(element_i);
-
-      axom::primal::Point3D bary_co = tri.barycentricCoords(query_pt);
-      //SLIC_INFO("bary_co " << bary_co);
+      const DataType* bary_arr;
+      if(TOPO_DIM == 2)
+      {
+        Triangle2D tri = getTriangle(element_i);
+        Point3DType bary_co = tri.barycentricCoords(toPt2(query_pt));
+        bary_arr = bary_co.data();
+        //SLIC_INFO("bary_co " << bary_co);
+      }
+      else if(TOPO_DIM == 3)
+      {
+        Tetrahedron3D tet = getTetrahedron(element_i);
+        Point4DType bary_co = tet.barycentricCoords(toPt3(query_pt));
+        bary_arr = bary_co.data();
+        //SLIC_INFO("bary_co " << bary_co);
+      }
+      else
+      {
+        SLIC_ERROR("Unsupported dimension");
+      }
 
       //Find the most negative
       IndexType i = 0;
-      if(bary_co[0] > bary_co[1]) i = 1;
-      if(bary_co[i] > bary_co[2]) i = 2;
+      for(unsigned int d = 1; d < VERT_PER_ELEMENT; d++)
+      {
+        if(bary_arr[i] > bary_arr[d]) i = d;
+      }
 
-      if(bary_co[i] >= 0)
+      if(bary_arr[i] >= 0)
       {                    // bary_val bigger than zero -> inside triangle
         return element_i;  //return if inside or on triangle
       }
 
       std::vector<IndexType> zlist = m_mesh.getElementNeighbor(element_i);
-      IndexType next_el = element_i = zlist[(i + 1) % 3];
+      IndexType next_el = element_i = zlist[(i + 1) % VERT_PER_ELEMENT];
 
       // Either there is a hole in the m_mesh, or the point is outside of the m_mesh.
       // Logically, this should never happen.
@@ -294,13 +364,31 @@ private:
    */
   bool findCavityElementsRec(const PointType& query_pt, IndexType element_idx)
   {
-    IndexListType verts = m_mesh.getVerticesInElement(element_idx);
+    IndexListType verts = m_mesh.getVertexInElement(element_idx);
     const PointType p0 = m_mesh.getVertexPoint(verts[0]);
     const PointType p1 = m_mesh.getVertexPoint(verts[1]);
     const PointType p2 = m_mesh.getVertexPoint(verts[2]);
-    bool is_in_circle = axom::primal::in_circle(query_pt, p0, p1, p2);
+    bool is_in_circle;
+    if(TOPO_DIM == 2)
+    {
+      is_in_circle =
+        axom::primal::in_circle(toPt2(query_pt), toPt2(p0), toPt2(p1), toPt2(p2));
+    }
+    else if(TOPO_DIM == 3)
+    {
+      const PointType p3 = m_mesh.getVertexPoint(verts[3]);
+      is_in_circle = axom::primal::in_sphere(toPt3(query_pt),
+                                             toPt3(p0),
+                                             toPt3(p1),
+                                             toPt3(p2),
+                                             toPt3(p3));
+    }
+    else
+    {
+      SLIC_ERROR("unsupported dimension.");
+    }
 
-    //SLIC_INFO(element_idx << " InCircle:" << is_in_circle);
+    //SLIC_INFO( "element " << element_idx << " is in Circle? " << is_in_circle);
 
     if(is_in_circle)
     {
@@ -308,14 +396,14 @@ private:
       cavity_element_list.push_back(element_idx);
 
       //check for each faces
-      IndexListType nbr_elements = m_mesh.getElementNeighbors(element_idx);
+      IndexListType nbr_elements = m_mesh.getElementNeighbor(element_idx);
       SLIC_ASSERT(nbr_elements.size() == TOPO_DIM + 1);
 
-      for(int face_i = 0; face_i < VERT_PER_ELEMENT; face_i++)
+      for(int face_i = 0; face_i < (int)VERT_PER_ELEMENT; face_i++)
       {
         IndexType nbr_elem = nbr_elements[face_i];
 
-        //SLIC_INFO("nbr "<< face_i <<" "<<nbr_elem);
+        //SLIC_INFO("this element's "<< face_i <<" neighbor is elem "<<nbr_elem);
 
         if(nbr_elem < 0 ||
            (checked_element_set.insert(nbr_elem).second
@@ -326,9 +414,16 @@ private:
             ))
         {
           IndexListType vlist = m_mesh.getElementFace(element_idx, face_i);
+          //For tetrahedron, if the element face is odd, the vertex order needs to be reversed.
+          if(TOPO_DIM == 3 && face_i % 2 == 1)
+          {
+            IndexType tmp = vlist[1];
+            vlist[1] = vlist[2];
+            vlist[2] = tmp;
+          }
           cavity_face_list.push_back(
             ElementFacePair<TOPO_DIM>(element_idx, &vlist[0]));
-          //SLIC_INFO("Added "<<vlist[0]<< " "<<vlist[1]);
+          //SLIC_INFO("Added vertices to cavity face list "<<vlist[0]<< " "<<vlist[1] << " " << vlist[2]);
         }
       }
       return false;
@@ -358,8 +453,12 @@ private:
     checked_element_set.insert(element_i);
     findCavityElementsRec(query_pt, element_i);
 
+    SLIC_ASSERT_MSG(cavity_element_list.size() > 0,
+                    "Error: New point is not contained in the mesh");
+    SLIC_ASSERT(cavity_face_list.size() > 0);
+
     /*
-    std::cout<<"violating elements: " ;
+    std::cout<<"cavity elements: " ;
     for(unsigned int i=0; i<cavity_element_list.size(); i++)
     {
       std::cout<< cavity_element_list[i] <<" ";
@@ -400,9 +499,12 @@ private:
 
     for(unsigned int i = 0; i < cavity_face_list.size(); i++)
     {
-      IndexType n0 = cavity_face_list[i].face_vidx[0];
-      IndexType n1 = cavity_face_list[i].face_vidx[1];
-      IndexType new_el = m_mesh.addElement(n0, n1, new_pt_i);
+      IndexType vlist[VERT_PER_ELEMENT];
+      for(unsigned int d = 0; d < VERT_PER_ELEMENT - 1; d++)
+        vlist[d] = cavity_face_list[i].face_vidx[d];
+      vlist[VERT_PER_ELEMENT - 1] = new_pt_i;
+
+      IndexType new_el = m_mesh.addElement(vlist);
       new_elements.push_back(new_el);
     }
 
--- src/axom/quest/examples/delaunay_triangulation.cpp
+++ src/axom/quest/examples/delaunay_triangulation.cpp
@@ -23,18 +23,14 @@ using namespace axom;
 
 double BOUNDING_BOX_DIM[] = {
   2.0,
+  2.0,
   2.0};  //This is the length of the bounding rectangle to contain the random points
-
-typedef axom::quest::Delaunay Delaunay;
 
 typedef int IndexType;
 typedef double DataType;
 
 typedef axom::primal::Point2D Point2D;
 typedef axom::primal::Point3D Point3D;
-typedef axom::primal::BoundingBox<DataType, 2> BoundingBox;
-
-typedef Delaunay::IndexListType IndexListType;
 
 enum InputStatus
 {
@@ -49,6 +45,7 @@ struct Input
   std::string outputVTKFile;
   int numRandPoints;
   bool outputSteps;
+  int dimension;
 
   InputStatus errorCode;
 
@@ -67,6 +64,9 @@ struct Input
               << "  --nrandpt(-n) [N]      The number of random points.  "
                  "Default N = 10.  "
               << std::endl
+              << "  --dim(-d) [D]          The dimension of the mesh. 2 = "
+                 "triangles in 2D, 3 = tetrahedrons in 3D."
+              << std::endl
               << "  --outputsteps(-s)      The intermediate steps will be "
                  "written out as VTK files"
               << std::endl
@@ -81,6 +81,7 @@ Input::Input(int argc, char** argv)
   : outputVTKFile("delaunay_output")
   , numRandPoints(10)
   , outputSteps(false)
+  , dimension(2)
   , errorCode(SUCCESS)
 {
   if(argc >= 2)
@@ -106,6 +107,10 @@ Input::Input(int argc, char** argv)
       {
         outputVTKFile = argv[++i];
       }
+      else if(arg == "--dim" || arg == "-d")
+      {
+        dimension = atoi(argv[++i]);
+      }
       else
       {
         errorCode = SHOWHELP;
@@ -115,25 +120,86 @@ Input::Input(int argc, char** argv)
     }
   }
 
-  /*
-    if (!canOpenFile(stlInput)) {
-        errorCode = CANTOPENFILE;
-        return;
-    }*/
-
   SLIC_INFO("Using parameter values: "
             << std::endl
             << "  nrandpt = " << numRandPoints << std::endl
+            << "  dimension = " << dimension << std::endl
             << "  outputSteps = " << outputSteps << std::endl
             << "  outfile = " << outputVTKFile << std::endl);
 }
 
+template <unsigned int dimension>
+int run_delaunay(int num_points, bool outputVTKsteps, std::string outputVTKFile)
+{
+  // Create Delaunay Triangulation on random points
+  typedef axom::quest::Delaunay<dimension> Delaunay;
+  typedef typename Delaunay::PointType PointType;
+  typedef axom::primal::BoundingBox<DataType, dimension> BoundingBox;
+  Delaunay dt;
+
+  double bb_min_arr[] = {-BOUNDING_BOX_DIM[0] / 2.0,
+                         -BOUNDING_BOX_DIM[1] / 2.0,
+                         -BOUNDING_BOX_DIM[2] / 2.0};
+  double bb_max_arr[] = {BOUNDING_BOX_DIM[0] / 2.0,
+                         BOUNDING_BOX_DIM[1] / 2.0,
+                         BOUNDING_BOX_DIM[2] / 2.0};
+  PointType bb_min(bb_min_arr);
+  PointType bb_max(bb_max_arr);
+  BoundingBox bb(bb_min, bb_max);
+  dt.initializeBoundary(bb);
+
+  //Adding a number of random points
+  time_t rseed_val = time(NULL);
+  SLIC_INFO("Using " << rseed_val << " as seed for rand()");
+  srand(rseed_val);
+
+  if(outputVTKsteps)
+  {
+    std::ostringstream stm;
+    stm << outputVTKFile << "_box.vtk";
+    dt.writeToVTKFile(stm.str());
+  }
+
+  for(int pt_i = 0; pt_i < num_points; pt_i++)
+  {
+    //SLIC_INFO("Adding point #" << pt_i );
+    //double x = (double)rand() / (double)RAND_MAX * BOUNDING_BOX_DIM[0] - BOUNDING_BOX_DIM[0]/2.0;
+    //double y = (double)rand() / (double)RAND_MAX * BOUNDING_BOX_DIM[1] - BOUNDING_BOX_DIM[1]/2.0;
+
+    double pt_coord[dimension];
+    for(unsigned int d = 0; d < dimension; d++)
+      pt_coord[d] = (double)rand() / (double)RAND_MAX * BOUNDING_BOX_DIM[d] -
+        BOUNDING_BOX_DIM[d] / 2.0;
+    PointType new_pt(pt_coord, dimension);
+
+    dt.insertPoint(new_pt);
+
+    if(outputVTKsteps)
+    {
+      std::ostringstream stm;
+      stm << outputVTKFile << std::setfill('0') << std::setw(2) << pt_i << ".vtk";
+      dt.writeToVTKFile(stm.str());
+    }
+  }
+
+  dt.removeBoundary();  //Remove the starting rectangular box
+
+  // Write the final mesh to a vtk file
+  std::ostringstream stm;
+  stm << outputVTKFile << ".vtk";
+  dt.writeToVTKFile(stm.str());
+
+  //dt.printMesh();
+
+  SLIC_INFO("Done!");
+
+  return 0;
+}
+
 int main(int argc, char** argv)
 {
-  int retval = EXIT_SUCCESS;
-
   // Initialize the SLIC logger
-  axom::slic::UnitTestLogger logger;
+  axom::slic::SimpleLogger logger;
 
   // Initialize default parameters and update with command line arguments:
   Input params(argc, argv);
@@ -153,56 +219,23 @@ int main(int argc, char** argv)
     }
   }
 
-  bool outputVTKsteps = params.outputSteps;
-  int num_points = params.numRandPoints;
+  const bool outputVTKsteps = params.outputSteps;
+  const int num_points = params.numRandPoints;
+  const std::string outputVTKFile = params.outputVTKFile;
+  const int dimension = params.dimension;
 
-  // Create Delaunay Triangulation on random points
-
-  Delaunay dt;
-
-  double bb_min_arr[] = {-BOUNDING_BOX_DIM[0] / 2.0, -BOUNDING_BOX_DIM[1] / 2.0};
-  double bb_max_arr[] = {BOUNDING_BOX_DIM[0] / 2.0, BOUNDING_BOX_DIM[1] / 2.0};
-  Point2D bb_min(bb_min_arr);
-  Point2D bb_max(bb_max_arr);
-  BoundingBox bb(bb_min, bb_max);
-  dt.initializeBoundary(bb);
-
-  //Adding a number of random points
-  time_t rseed_val = time(NULL);
-  SLIC_INFO("Using " << rseed_val << " as seed for rand()");
-  srand(rseed_val);
-
-  char fname[256];
-  for(int pt_i = 0; pt_i < num_points; pt_i++)
+  if(dimension == 2)
   {
-    //SLIC_INFO("Adding point #" << pt_i );
-    double x = (double)rand() / (double)RAND_MAX * BOUNDING_BOX_DIM[0] -
-      BOUNDING_BOX_DIM[0] / 2.0;
-    double y = (double)rand() / (double)RAND_MAX * BOUNDING_BOX_DIM[1] -
-      BOUNDING_BOX_DIM[1] / 2.0;
-    double pt_coord[2] = {x, y};
-    Point2D new_pt(pt_coord, 2);
-
-    dt.insertPoint(new_pt);
-
-    if(outputVTKsteps)
-    {
-      std::ostringstream stm;
-      stm << params.outputVTKFile << std::setfill('0') << std::setw(2) << pt_i
-          << ".vtk";
-      dt.writeToVTKFile(stm.str());
-    }
+    run_delaunay<2>(num_points, outputVTKsteps, outputVTKFile);
+  }
+  else if(dimension == 3)
+  {
+    run_delaunay<3>(num_points, outputVTKsteps, outputVTKFile);
+  }
+  else
+  {
+    params.showhelp();
   }
 
-  dt.removeBoundary();  //Remove the starting rectangular box
-
-  // Write the final mesh to a vtk file
-  sprintf(fname, "%s.vtk", params.outputVTKFile.c_str());
-  dt.writeToVTKFile(fname);
-
-  //dt.printMesh();
-
-  SLIC_INFO("Done!");
-
-  return retval;
+  return EXIT_SUCCESS;
 }
