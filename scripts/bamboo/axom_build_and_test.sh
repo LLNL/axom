7222c566948798ed75258b5af6aa9f3327b93eba
Michael H. Chang <chang28@llnl.gov>
2017-08-15 12:41:22 -0700

    Needs to seperate checkout from the build and test script.

--- scripts/bamboo/axom_build_and_test.sh
+++ scripts/bamboo/axom_build_and_test.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 # 
-# axom_build_and_test.sh "Debug" "" "clang@3.5.0" "develop" "cab" "chaos5" "/usr/workspace/wsrzd/axomdev/bamboo/rzmanta/build/clangcoral"
+# axom_build_and_test.sh "Debug" "" "clang@3.5.0" "cab" "chaos5" 
 #
 BUILD_TYPE=$1
 BUILD_OPT=$2
 COMPILER=$3
-BRANCH=$4
-HOST=$5
-SYSTEM_TYPE=$6
-WORKSPACE=$7
+HOST=$4
+SYSTEM_TYPE=$5
 
-ssh $HOST "
-   cd $WORSPACE && \
-   echo 'Cloning...' && \
-   echo '-----------------------------------------------------------------------' && \
-   axom_codeCheckout.sh $BRANCH && \
-   if [[ $SYSTEM_TYPE == 'chaos5' ] ||[ $SYSTEM_TYPE == 'toss3' ]]; then && \
-      srun -N1 --exclusive -ppdebug ./scripts/bamboo/${SYSTEM_TYPE}_build_test_uno_compiler.sh  '$BUILD_TYPE' '$BUILD_OPT' 'COMPILER'; && \
-   else && \
-      ./scripts/bamboo/${SYSTEM_TYPE}_build_test_uno_compiler.sh '$BUILD_TYPE' '$BUILD_OPT'  '$COMPILER' && \
-   fi && \
-   "
+if [ $SYSTEM_TYPE == "chaos5" ] || [ $SYSTEM_TYPE == "toss3" ]; then
+   SCRIPT="srun -N1 --exclusive -ppdebug ./scripts/bamboo/${SYSTEM_TYPE}_build_test_uno_compiler.sh";
+else
+   SCRIPT="./scripts/bamboo/${SYSTEM_TYPE}_build_test_uno_compiler.sh ";
+fi
+echo $SCRIPT
+
+cur_dir=`pwd`
+ssh $HOST "hostname; cd $cur_dir && \
+   pwd && \
+   $SCRIPT '$BUILD_TYPE' '$BUILD_OPT'  '$COMPILER'"
+
 if [ $? -ne 0 ]; then
    echo "Error: $SYSTEM_TYPE $HOST failed!"
    exit 1
