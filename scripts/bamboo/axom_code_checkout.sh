ef6c6954a8a0ae83d22e929c2f13115a3f06276f
Michael H. Chang <chang28@llnl.gov>
2017-08-17 09:48:11 -0700

    develop is the default branch

--- scripts/bamboo/axom_code_checkout.sh
+++ scripts/bamboo/axom_code_checkout.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-# axom_code_checkout.sh branch_name
+# axom_code_checkout.sh branch_name, if branch_name is NULL, develop is the default branch
 
 echo  axom_code_checkout.sh 1.0
 set -ev
  
 rm -rf axom
+
+BRANCH=develop
  
-git clone ssh://git@cz-bitbucket.llnl.gov:7999/atk/axom.git --branch $1 --recursive
+if [ $1 != "" ]; then
+   BRANCH=$1
+fi
+
+echo $BRANCH
+git clone ssh://git@cz-bitbucket.llnl.gov:7999/atk/axom.git --branch $BRANCH --recursive
 cd axom
 git branch
