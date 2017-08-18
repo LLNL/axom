fa8f1c3754f5b646ab67649c3460abb315fc5460
Michael H. Chang <chang28@llnl.gov>
2017-08-18 13:40:57 -0700

    Actually launch the code

--- scripts/bamboo/axom_launch.sh
+++ scripts/bamboo/axom_launch.sh
@@ -11,12 +11,12 @@ COMPILER=$3
 pwd
 echo $SYS_TYPE
 if [ $SYS_TYPE == "chaos_5_x86_64_ib" ]; then
-   echo srun -N1 --exclusive -ppdebug ./scripts/bamboo/chaos5_build_test_uno_compiler.sh '$BUILD_TYPE' '$BUILD_OPT'  '$COMPILER'
+   srun -N1 --exclusive -ppdebug ./scripts/bamboo/chaos5_build_test_uno_compiler.sh '$BUILD_TYPE' '$BUILD_OPT'  '$COMPILER'
 elif [ $SYS_TYPE == "toss_3_x86_64_ib" ]; then
-   echo srun -N1 --exclusive -ppdebug ./scripts/bamboo/toss3_build_test_uno_compiler.sh '$BUILD_TYPE' '$BUILD_OPT'  '$COMPILER'
+   srun -N1 --exclusive -ppdebug ./scripts/bamboo/toss3_build_test_uno_compiler.sh '$BUILD_TYPE' '$BUILD_OPT'  '$COMPILER'
 elif [ $SYS_TYPE == "bgqos_0" ]; then
-   echo  ./scripts/bamboo/bgq_build_test_uno_compiler.sh '$BUILD_TYPE' '$BUILD_OPT'  '$COMPILER'
+   ./scripts/bamboo/bgq_build_test_uno_compiler.sh '$BUILD_TYPE' '$BUILD_OPT'  '$COMPILER'
 elif [ $SYS_TYPE == "blueos_3_ppc64le_ib" ]; then
-   echo  ./scripts/bamboo/blueos_build_test_uno_compiler.sh '$BUILD_TYPE' '$BUILD_OPT'  '$COMPILER'
+    ./scripts/bamboo/blueos_build_test_uno_compiler.sh '$BUILD_TYPE' '$BUILD_OPT'  '$COMPILER'
 fi
 
