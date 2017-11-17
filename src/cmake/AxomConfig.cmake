2686ba2c54d776ba11b24accd042cb185280cc8c
Kenneth Weiss <weiss27@llnl.gov>
2017-11-16 17:23:19 -0800

    Removed spio from AxomConfig and config.hpp. ATK-1135
    Also removes spio from axom's config test.

--- src/cmake/AxomConfig.cmake
+++ src/cmake/AxomConfig.cmake
@@ -58,7 +58,7 @@ endif()
 
 
 ## Add a configuration define for each enabled axom component
-set(COMPS AXOM_UTILS LUMBERJACK SLIC SLAM SIDRE MINT PRIMAL QUEST SPIO)
+set(COMPS AXOM_UTILS LUMBERJACK SLIC SLAM SIDRE MINT PRIMAL QUEST)
 foreach(comp in ${COMPS})
     if( ENABLE_${comp} )
         set(AXOM_USE_${comp} TRUE)
