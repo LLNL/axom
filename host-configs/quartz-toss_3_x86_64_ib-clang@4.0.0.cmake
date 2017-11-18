b71184f29787a641ba51cb760b1a02b948671882
Rich Hornung <hornung1@llnl.gov>
2017-11-18 14:36:55 -0800

    New host config files for CZ toss3.
    gnu7.1.0 and intel18.0.0 yield fortran test failures.

--- host-configs/quartz-toss_3_x86_64_ib-clang@4.0.0.cmake
+++ host-configs/quartz-toss_3_x86_64_ib-clang@4.0.0.cmake
@@ -3,21 +3,21 @@
 #
 # This is a generated file, edit at own risk.
 ##################################
-# toss_3_x86_64_ib-gcc@4.9.3
+# toss_3_x86_64_ib-clang@4.0.0
 ##################################
 
 # cmake from uberenv
-# cmake executable path: /usr/workspace/wsa/axom/thirdparty_libs/builds/2017_11_18_08_52_48/spack/opt/spack/toss_3_x86_64_ib/gcc-4.9.3/cmake-3.8.2-s65hvfeklaae34jyaoy3ofd7jezjg5cl/bin/cmake
+# cmake executable path: /usr/workspace/wsa/axom/thirdparty_libs/builds/2017_11_18_08_52_48/spack/opt/spack/toss_3_x86_64_ib/clang-4.0.0/cmake-3.8.2-uc5ahergn5qm7tslfedpiplpmxqqfgus/bin/cmake
 
 #######
-# using gcc@4.9.3 compiler spec
+# using clang@4.0.0 compiler spec
 #######
 
 # c compiler used by spack
-set(CMAKE_C_COMPILER "/usr/tce/packages/gcc/gcc-4.9.3/bin/gcc" CACHE PATH "")
+set(CMAKE_C_COMPILER "/usr/tce/packages/clang/clang-4.0.0/bin/clang" CACHE PATH "")
 
 # cpp compiler used by spack
-set(CMAKE_CXX_COMPILER "/usr/tce/packages/gcc/gcc-4.9.3/bin/g++" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/usr/tce/packages/clang/clang-4.0.0/bin/clang++" CACHE PATH "")
 
 # fortran compiler used by spack
 set(ENABLE_FORTRAN ON CACHE BOOL "")
@@ -25,44 +25,44 @@ set(ENABLE_FORTRAN ON CACHE BOOL "")
 set(CMAKE_Fortran_COMPILER "/usr/tce/packages/gcc/gcc-4.9.3/bin/gfortran" CACHE PATH "")
 
 # Root directory for generated TPLs
-set(TPL_ROOT "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_11_18_08_52_48/spack/opt/spack/toss_3_x86_64_ib/gcc-4.9.3" CACHE PATH "")
+set(TPL_ROOT "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_11_18_08_52_48/spack/opt/spack/toss_3_x86_64_ib/clang-4.0.0" CACHE PATH "")
 
 # hdf5 from uberenv
-set(HDF5_DIR "${TPL_ROOT}/hdf5-1.8.16-v4jrbjlu2s6vl5a67kv3ydg6ssdsi2tj" CACHE PATH "")
+set(HDF5_DIR "${TPL_ROOT}/hdf5-1.8.16-vmpbfqdli4b5reg5gxkzhm7nx545jzlj" CACHE PATH "")
 
 # scr not built by uberenv
 
 # conduit from uberenv
-set(CONDUIT_DIR "${TPL_ROOT}/conduit-0.2.1-3felyqscssovjmznj4pd7qpuq23lzvyk" CACHE PATH "")
+set(CONDUIT_DIR "${TPL_ROOT}/conduit-0.2.1-drhvqhsk6q24f34y5ti2tgvumgq43d63" CACHE PATH "")
 
 # mfem from uberenv
-set(MFEM_DIR "${TPL_ROOT}/mfem-3.3.2-ub6rg4rafniyzeoecke3lfm6zpzbc5fs" CACHE PATH "")
+set(MFEM_DIR "${TPL_ROOT}/mfem-3.3.2-nnwwsm4ussloivedxx4i6gbgy75gjzls" CACHE PATH "")
 
 # boost headers from uberenv
-set(BOOST_DIR "${TPL_ROOT}/boost-headers-1.58.0-nvmps5cyz6fnof343goaptduus4n65cz" CACHE PATH "")
+set(BOOST_DIR "${TPL_ROOT}/boost-headers-1.58.0-prjrtwbbsultjswvxgv3llsdxujupwid" CACHE PATH "")
 
 # python from uberenv
-set(PYTHON_EXECUTABLE "${TPL_ROOT}/python-2.7.11-lea63zrc2ghlbkp23fwqihgaypyaunaa/bin/python" CACHE PATH "")
+set(PYTHON_EXECUTABLE "${TPL_ROOT}/python-2.7.11-txzcpqr53dowsgoz2e45xxodgto2f2kf/bin/python" CACHE PATH "")
 
 # lua from uberenv
-set(LUA_DIR "${TPL_ROOT}/lua-5.1.5-esnntgauhb5eur2ftxv4cpxdrxq3wcd4" CACHE PATH "")
+set(LUA_DIR "${TPL_ROOT}/lua-5.1.5-ycs4jfo3ljtsigqle52vntfwj5ssdxrq" CACHE PATH "")
 
 # doxygen from uberenv
-set(DOXYGEN_EXECUTABLE "${TPL_ROOT}/doxygen-1.8.11-f5xochsft45cexkd52mnwkxktcc2a4ds/bin/doxygen" CACHE PATH "")
+set(DOXYGEN_EXECUTABLE "${TPL_ROOT}/doxygen-1.8.11-j3yankkpbqqptrnc2rdajdlnrhbmw5gw/bin/doxygen" CACHE PATH "")
 
 # sphinx from uberenv
-set(SPHINX_EXECUTABLE "${TPL_ROOT}/python-2.7.11-lea63zrc2ghlbkp23fwqihgaypyaunaa/bin/sphinx-build" CACHE PATH "")
+set(SPHINX_EXECUTABLE "${TPL_ROOT}/python-2.7.11-txzcpqr53dowsgoz2e45xxodgto2f2kf/bin/sphinx-build" CACHE PATH "")
 
 # shroud from uberenv
-set(SHROUD_EXECUTABLE "${TPL_ROOT}/python-2.7.11-lea63zrc2ghlbkp23fwqihgaypyaunaa/bin/shroud" CACHE PATH "")
+set(SHROUD_EXECUTABLE "${TPL_ROOT}/python-2.7.11-txzcpqr53dowsgoz2e45xxodgto2f2kf/bin/shroud" CACHE PATH "")
 
 # uncrustify from uberenv
-set(UNCRUSTIFY_EXECUTABLE "${TPL_ROOT}/uncrustify-0.61-gfugzvve5jxq4p6kpozhysf76qtcwxcc/bin/uncrustify" CACHE PATH "")
+set(UNCRUSTIFY_EXECUTABLE "${TPL_ROOT}/uncrustify-0.61-ice6hujmc5ktfrzxx343qs4lduhdai5z/bin/uncrustify" CACHE PATH "")
 
 # lcov and genhtml from uberenv
-set(LCOV_PATH "${TPL_ROOT}/lcov-1.11-ms3k744sok6xi4ndnzcmwdxnhcqwpm3i/usr/bin/lcov" CACHE PATH "")
+set(LCOV_PATH "${TPL_ROOT}/lcov-1.11-k7s5ypbr7xsj67wxcmfkf7hehv5ss7xb/usr/bin/lcov" CACHE PATH "")
 
-set(GENHTML_PATH "${TPL_ROOT}/lcov-1.11-ms3k744sok6xi4ndnzcmwdxnhcqwpm3i/usr/bin/genhtml" CACHE PATH "")
+set(GENHTML_PATH "${TPL_ROOT}/lcov-1.11-k7s5ypbr7xsj67wxcmfkf7hehv5ss7xb/usr/bin/genhtml" CACHE PATH "")
 
 ##################################
 # end uberenv host-config
@@ -86,7 +86,7 @@ set(GENHTML_PATH "${TPL_ROOT}/lcov-1.11-ms3k744sok6xi4ndnzcmwdxnhcqwpm3i/usr/bin
 # !---------------------------------------------------------------------------
 ##############################################################################
 # Options added manually to 
-# lc toss3 gcc@4.9.3  host configs
+# lc toss3 clang@4.0.0  host configs
 ##############################################################################
 
 set(ENABLE_GTEST_DEATH_TESTS ON CACHE BOOL "")
@@ -94,9 +94,10 @@ set(ENABLE_GTEST_DEATH_TESTS ON CACHE BOOL "")
 ##############################################################################
 # MPI - manually added for now
 ##############################################################################
+
 set(ENABLE_MPI ON CACHE BOOL "")
 
-set(MPI_HOME             "/usr/tce/packages/mvapich2/mvapich2-2.2-gcc-4.9.3" CACHE PATH "")
+set(MPI_HOME             "/usr/tce/packages/mvapich2/mvapich2-2.2-clang-4.0.0" CACHE PATH "")
 set(MPI_C_COMPILER       "${MPI_HOME}/bin/mpicc"   CACHE PATH "")
 set(MPI_CXX_COMPILER     "${MPI_HOME}/bin/mpicxx"  CACHE PATH "")
 set(MPI_Fortran_COMPILER "${MPI_HOME}/bin/mpifort" CACHE PATH "")
--- host-configs/quartz-toss_3_x86_64_ib-intel@18.0.0.cmake
+++ host-configs/quartz-toss_3_x86_64_ib-intel@18.0.0.cmake
@@ -3,66 +3,66 @@
 #
 # This is a generated file, edit at own risk.
 ##################################
-# toss_3_x86_64_ib-gcc@4.9.3
+# toss_3_x86_64_ib-intel@18.0.0
 ##################################
 
 # cmake from uberenv
-# cmake executable path: /usr/workspace/wsa/axom/thirdparty_libs/builds/2017_11_18_08_52_48/spack/opt/spack/toss_3_x86_64_ib/gcc-4.9.3/cmake-3.8.2-s65hvfeklaae34jyaoy3ofd7jezjg5cl/bin/cmake
+# cmake executable path: /usr/workspace/wsa/axom/thirdparty_libs/builds/2017_11_18_08_52_48/spack/opt/spack/toss_3_x86_64_ib/intel-18.0.0/cmake-3.8.2-lnm37b5y7mhjihmm3kfu6i3tm3flu4kn/bin/cmake
 
 #######
-# using gcc@4.9.3 compiler spec
+# using intel@18.0.0 compiler spec
 #######
 
 # c compiler used by spack
-set(CMAKE_C_COMPILER "/usr/tce/packages/gcc/gcc-4.9.3/bin/gcc" CACHE PATH "")
+set(CMAKE_C_COMPILER "/usr/tce/packages/intel/intel-18.0.0/bin/icc" CACHE PATH "")
 
 # cpp compiler used by spack
-set(CMAKE_CXX_COMPILER "/usr/tce/packages/gcc/gcc-4.9.3/bin/g++" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/usr/tce/packages/intel/intel-18.0.0/bin/icpc" CACHE PATH "")
 
 # fortran compiler used by spack
 set(ENABLE_FORTRAN ON CACHE BOOL "")
 
-set(CMAKE_Fortran_COMPILER "/usr/tce/packages/gcc/gcc-4.9.3/bin/gfortran" CACHE PATH "")
+set(CMAKE_Fortran_COMPILER "/usr/tce/packages/intel/intel-18.0.0/bin/ifort" CACHE PATH "")
 
 # Root directory for generated TPLs
-set(TPL_ROOT "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_11_18_08_52_48/spack/opt/spack/toss_3_x86_64_ib/gcc-4.9.3" CACHE PATH "")
+set(TPL_ROOT "/usr/workspace/wsa/axom/thirdparty_libs/builds/2017_11_18_08_52_48/spack/opt/spack/toss_3_x86_64_ib/intel-18.0.0" CACHE PATH "")
 
 # hdf5 from uberenv
-set(HDF5_DIR "${TPL_ROOT}/hdf5-1.8.16-v4jrbjlu2s6vl5a67kv3ydg6ssdsi2tj" CACHE PATH "")
+set(HDF5_DIR "${TPL_ROOT}/hdf5-1.8.16-irsil6zovibxmnqsutxmdxrl3ywbt5j4" CACHE PATH "")
 
 # scr not built by uberenv
 
 # conduit from uberenv
-set(CONDUIT_DIR "${TPL_ROOT}/conduit-0.2.1-3felyqscssovjmznj4pd7qpuq23lzvyk" CACHE PATH "")
+set(CONDUIT_DIR "${TPL_ROOT}/conduit-0.2.1-3wmqrefb443252p7vaocb5n6kgofutsv" CACHE PATH "")
 
 # mfem from uberenv
-set(MFEM_DIR "${TPL_ROOT}/mfem-3.3.2-ub6rg4rafniyzeoecke3lfm6zpzbc5fs" CACHE PATH "")
+set(MFEM_DIR "${TPL_ROOT}/mfem-3.3.2-xkrjwgamcyfelfu7o5k3rvvtosyhbt6e" CACHE PATH "")
 
 # boost headers from uberenv
-set(BOOST_DIR "${TPL_ROOT}/boost-headers-1.58.0-nvmps5cyz6fnof343goaptduus4n65cz" CACHE PATH "")
+set(BOOST_DIR "${TPL_ROOT}/boost-headers-1.58.0-n5v2bxq776e4jx22zmfnhbrgdfgafsjs" CACHE PATH "")
 
 # python from uberenv
-set(PYTHON_EXECUTABLE "${TPL_ROOT}/python-2.7.11-lea63zrc2ghlbkp23fwqihgaypyaunaa/bin/python" CACHE PATH "")
+set(PYTHON_EXECUTABLE "${TPL_ROOT}/python-2.7.11-mksmsbeuhjj3lhacndedeilqusfbofdl/bin/python" CACHE PATH "")
 
 # lua from uberenv
-set(LUA_DIR "${TPL_ROOT}/lua-5.1.5-esnntgauhb5eur2ftxv4cpxdrxq3wcd4" CACHE PATH "")
+set(LUA_DIR "${TPL_ROOT}/lua-5.1.5-qm3wvjesm74bvrs4efyrumq54n4daa7r" CACHE PATH "")
 
 # doxygen from uberenv
-set(DOXYGEN_EXECUTABLE "${TPL_ROOT}/doxygen-1.8.11-f5xochsft45cexkd52mnwkxktcc2a4ds/bin/doxygen" CACHE PATH "")
+set(DOXYGEN_EXECUTABLE "${TPL_ROOT}/doxygen-1.8.11-plu3e4wj5xynk4qbiukouzqvyzqylutt/bin/doxygen" CACHE PATH "")
 
 # sphinx from uberenv
-set(SPHINX_EXECUTABLE "${TPL_ROOT}/python-2.7.11-lea63zrc2ghlbkp23fwqihgaypyaunaa/bin/sphinx-build" CACHE PATH "")
+set(SPHINX_EXECUTABLE "${TPL_ROOT}/python-2.7.11-mksmsbeuhjj3lhacndedeilqusfbofdl/bin/sphinx-build" CACHE PATH "")
 
 # shroud from uberenv
-set(SHROUD_EXECUTABLE "${TPL_ROOT}/python-2.7.11-lea63zrc2ghlbkp23fwqihgaypyaunaa/bin/shroud" CACHE PATH "")
+set(SHROUD_EXECUTABLE "${TPL_ROOT}/python-2.7.11-mksmsbeuhjj3lhacndedeilqusfbofdl/bin/shroud" CACHE PATH "")
 
 # uncrustify from uberenv
-set(UNCRUSTIFY_EXECUTABLE "${TPL_ROOT}/uncrustify-0.61-gfugzvve5jxq4p6kpozhysf76qtcwxcc/bin/uncrustify" CACHE PATH "")
+set(UNCRUSTIFY_EXECUTABLE "${TPL_ROOT}/uncrustify-0.61-ck2wou52pejemppw3qtnh6jnbojijk7h/bin/uncrustify" CACHE PATH "")
 
 # lcov and genhtml from uberenv
-set(LCOV_PATH "${TPL_ROOT}/lcov-1.11-ms3k744sok6xi4ndnzcmwdxnhcqwpm3i/usr/bin/lcov" CACHE PATH "")
+set(LCOV_PATH "${TPL_ROOT}/lcov-1.11-63uc76dz2mzrzip46gnrvpt7iiyhgxox/usr/bin/lcov" CACHE PATH "")
 
-set(GENHTML_PATH "${TPL_ROOT}/lcov-1.11-ms3k744sok6xi4ndnzcmwdxnhcqwpm3i/usr/bin/genhtml" CACHE PATH "")
+set(GENHTML_PATH "${TPL_ROOT}/lcov-1.11-63uc76dz2mzrzip46gnrvpt7iiyhgxox/usr/bin/genhtml" CACHE PATH "")
 
 ##################################
 # end uberenv host-config
@@ -86,7 +86,7 @@ set(GENHTML_PATH "${TPL_ROOT}/lcov-1.11-ms3k744sok6xi4ndnzcmwdxnhcqwpm3i/usr/bin
 # !---------------------------------------------------------------------------
 ##############################################################################
 # Options added manually to 
-# lc toss3 gcc@4.9.3  host configs
+# lc toss3 intel@18.0.0  host configs
 ##############################################################################
 
 set(ENABLE_GTEST_DEATH_TESTS ON CACHE BOOL "")
@@ -96,7 +96,7 @@ set(ENABLE_GTEST_DEATH_TESTS ON CACHE BOOL "")
 ##############################################################################
 set(ENABLE_MPI ON CACHE BOOL "")
 
-set(MPI_HOME             "/usr/tce/packages/mvapich2/mvapich2-2.2-gcc-4.9.3" CACHE PATH "")
+set(MPI_HOME             "/usr/tce/packages/mvapich2/mvapich2-2.2-intel-18.0.0" CACHE PATH "")
 set(MPI_C_COMPILER       "${MPI_HOME}/bin/mpicc"   CACHE PATH "")
 set(MPI_CXX_COMPILER     "${MPI_HOME}/bin/mpicxx"  CACHE PATH "")
 set(MPI_Fortran_COMPILER "${MPI_HOME}/bin/mpifort" CACHE PATH "")
