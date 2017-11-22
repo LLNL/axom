ba43d4d1e354e4de4b6502d7a766029b847d4042
Lee Taylor <ltaylor@llnl.gov>
2017-11-22 11:30:57 -0800

    Change to C style comments

--- src/components/sidre/src/interface/c_fortran/csidresplicer.c
+++ src/components/sidre/src/interface/c_fortran/csidresplicer.c
@@ -1,21 +1,29 @@
-!
-!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-!Copyright (c) 2017, Lawrence Livermore National Security, LLC.
-!
-!Produced at the Lawrence Livermore National Laboratory
-!
-!LLNL-CODE-741217
-!
-!All rights reserved.
-!
-!This file is part of Axom.
-!
-!For details about use and distribution, please read axom/LICENSE.
-!
-!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-!
+/*
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Copyright (c) 2017, Lawrence Livermore National Security, LLC.
+ *
+ * Produced at the Lawrence Livermore National Laboratory
+ *
+ * LLNL-CODE-741217
+ *
+ * All rights reserved.
+ *
+ * This file is part of Axom.
+ *
+ * For details about use and distribution, please read axom/LICENSE.
+ *
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
 
-!C code that will be inserted into code via shroud splicer blocks
+/*!
+ ******************************************************************************
+ *
+ * \file csidresplicer.c
+ *
+ * \brief   C code that will be inserted into code via shroud splicer blocks
+ *
+ ******************************************************************************
+ */
 
 // splicer begin C_definitions
 // equivalent to C_LOC
