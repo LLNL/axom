5979289250713ebb5a3839adb41aff54b427dd0c
Kenneth Weiss <weiss27@llnl.gov>
2017-11-29 18:30:28 -0800

    Disabled Fortran in the MSVC-Intel host-config. ATK-1145.
    Visual Studio was not generating modules for the Fortran interfaces to our components.
    
    Misc:  Cleaned up the host-configs a bit.

--- host-configs/other/sqa-uno-MSVC-INTEL.cmake
+++ host-configs/other/sqa-uno-MSVC-INTEL.cmake
@@ -12,31 +12,40 @@
 # For details about use and distribution, please read axom/LICENSE.
 #------------------------------------------------------------------------------
 # host-config for 'sqa-uno' Windows machine
-# using the intel 18 toolchain for visual studio 15
-# Run the following to configure 64-bit configurations from a build dir:
-#   cmake -G "Visual Studio 15 2017 Win64"                  \
-#         -T "Intel C++ Compiler 18.0"                      \
-#         -C ..\host-configs\other\sqa-uno-MSVC-intel.cmake \
+# using the Intel 18 toolchain for Visual Studio 15
+#
+# Run the following from a build dir for a 64-bit configuration:
+#   cmake -G "Visual Studio 15 2017 Win64"                   \
+#         -T "Intel C++ Compiler 18.0"                       \
+#         -C ..\host-configs\other\sqa-uno-MSVC-intel.cmake  \
 #         <path-to-axom>
 #------------------------------------------------------------------------------
 
-## Setup Axom components 
+### Setup Axom components
+
 # Disable Sidre until we can successfully link to HDF5 and Conduit on Windows
-set(ENABLE_FORTRAN ON CACHE BOOL "")
 set(ENABLE_SIDRE OFF CACHE BOOL "")
 #set(HDF5_DIR    ... CACHE PATH "")
 #set(CONDUIT_DIR ... CACHE PATH "")
 
-## Setup some devtool/TPL paths
-# Set the HOME variable -- it is %USERPROFILE% in Windows
+# Disable Fortran since Visual Studio is not generating modules for our component Fortan interfaces
+set(ENABLE_FORTRAN OFF CACHE BOOL "")
+
+
+### Setup some devtool/TPL paths
+
+# Set the HOME variable (%USERPROFILE% in Windows)
 string(REPLACE "\\" "/" HOME "$ENV{USERPROFILE}")
+
 set(BOOST_DIR "${HOME}/Code/boost" CACHE PATH "")
-# assumes graphviz 'dot' is in PATH
+
+# Note: Doxygen assumes graphviz 'dot' is in PATH
 set(DOXYGEN_EXECUTABLE "${HOME}/Chocolatey/bin/doxygen.exe" CACHE PATH "") 
+
 # Note: uncrustify disabled since this version is different than uberenv version and changes whitespace
 # set(UNCRUSTIFY_EXECUTABLE "${HOME}/Code/UniversalIndentGUI/indenters/uncrustify.exe" CACHE PATH "")
 
-## Setup MPI
+# Setup MPI
 set(ENABLE_MPI ON CACHE BOOL "")
 set(MPI_HOME              "C:/Program Files (x86)/IntelSWTools/compilers_and_libraries_2018.1.156/windows/mpi/intel64" CACHE PATH "")
 set(MPI_C_COMPILER        "${MPI_HOME}/bin/mpiicc.bat" CACHE PATH "")
@@ -44,7 +53,6 @@ set(MPI_CXX_COMPILER      "${MPI_HOME}/bin/mpiicpc.bat" CACHE PATH "")
 set(MPI_Fortran_COMPILER  "${MPI_HOME}/bin/mpiifort.bat" CACHE PATH "")
 
 
-
-## Set some additional options
+### Set some additional options
 set(ENABLE_FOLDERS ON CACHE BOOL "")
 set(ENABLE_GTEST_DEATH_TESTS ON CACHE BOOL "")
--- host-configs/other/sqa-uno-MSVC.cmake
+++ host-configs/other/sqa-uno-MSVC.cmake
@@ -11,32 +11,46 @@
 #
 # For details about use and distribution, please read axom/LICENSE.
 #------------------------------------------------------------------------------
-# host-config for 'sqa-uno' Windows machine-- using visual studio 15
-# Run the following from a build dir to configure for 32- and 64-bit configurations:
-#   cmake -G "Visual Studio 15 2017"       -C ..\host-configs\other\sqa-uno-MSVC.cmake <path-to-axom>
-#   cmake -G "Visual Studio 15 2017 Win64" -C ..\host-configs\other\sqa-uno-MSVC.cmake <path-to-axom>
+# host-config for 'sqa-uno' Windows machine-- using Visual Studio 15
+#
+# Run the following from a build dir for a 32-bit configuration
+#   cmake -G "Visual Studio 15 2017"                       \
+#         -C ..\host-configs\other\sqa-uno-MSVC.cmake      \
+#         <path-to-axom>
+#
+# Run the following from a build dir for a 64-bit configuration
+#   cmake -G "Visual Studio 15 2017 Win64"               \
+#         -C ..\host-configs\other\sqa-uno-MSVC.cmake    \
+#         <path-to-axom> 
 #------------------------------------------------------------------------------
 
-## Setup Axom components
+### Setup Axom components
+
 # Disable Sidre until we can successfully link to HDF5 and Conduit on Windows
 set(ENABLE_SIDRE OFF CACHE BOOL "")
 #set(HDF5_DIR    ... CACHE PATH "")
 #set(CONDUIT_DIR ... CACHE PATH "")
 
-## Setup some devtool/TPL paths
-# Set the HOME variable -- it is %USERPROFILE% in Windows
+
+### Setup some devtool/TPL paths
+
+# Set the HOME variable (%USERPROFILE% in Windows)
 string(REPLACE "\\" "/" HOME "$ENV{USERPROFILE}")
+
 set(BOOST_DIR "${HOME}/Code/boost" CACHE PATH "")
-# assumes graphviz 'dot' is in PATH
+
+# Note: Doxygen assumes graphviz 'dot' is in PATH
 set(DOXYGEN_EXECUTABLE "${HOME}/Chocolatey/bin/doxygen.exe" CACHE PATH "") 
+
 # Note: uncrustify disabled since this version is different than uberenv version and changes whitespace
 # set(UNCRUSTIFY_EXECUTABLE "${HOME}/Code/UniversalIndentGUI/indenters/uncrustify.exe" CACHE PATH "")
 
-## Setup MPI
+# Setup MPI
 set(ENABLE_MPI ON CACHE BOOL "")
-set(MPI_GUESS_LIBRARY_NAME "MSMPI" CACHE STRING "")
 set(MPI_HOME  "C:/Program Files/Microsoft HPC Pack 2008 R2" CACHE PATH "")
+set(MPI_GUESS_LIBRARY_NAME "MSMPI" CACHE STRING "")
 
-## Set some additional options
+
+### Set some additional options
 set(ENABLE_FOLDERS ON CACHE BOOL "")
 set(ENABLE_GTEST_DEATH_TESTS ON CACHE BOOL "")
