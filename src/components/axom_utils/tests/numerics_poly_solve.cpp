9d095d39a0e3f6422412ec8c6bbcb8c360eb4b65
Arlie Capps <capps2@llnl.gov>
2017-12-12 13:19:39 -0800

    Added tests for quadratic solver.

--- src/components/axom_utils/tests/numerics_poly_solve.cpp
+++ src/components/axom_utils/tests/numerics_poly_solve.cpp
@@ -17,6 +17,7 @@
 
 // Axom includes
 #include "axom_utils/poly_solve.hpp"
+#include "axom_utils/Utilities.hpp" // for isNearlyEqual()
 
 // Google Test include
 #include "gtest/gtest.h"
@@ -26,7 +27,9 @@ int count_mismatches(double *standard, double *test, int n)
   int mcount = 0;
 
   for (int i = 0; i < n; ++i) {
-    mcount += (standard[i] != test[i]);
+    if (! axom::utilities::isNearlyEqual(standard[i], test[i])) {
+      ++mcount;
+    }
   }
 
   return mcount;
@@ -86,13 +89,14 @@ TEST( numerics_poly_solve, poly_solve_linear )
   }
 
   {
-    // Not sure exactly how this should work.
     SCOPED_TRACE("X-axis");
     coeff[0] = 0; coeff[1] = 0;
     roots[0] = 0;
     expected[0] = 0;
     rc = axom::numerics::solve_linear(coeff, roots, n);
+    // rc == 0 because there are real solutions
     EXPECT_EQ(rc, 0);
+    // n == -1 because there are infinitely many solutions
     EXPECT_EQ(n, -1);
     EXPECT_EQ(count_mismatches(expected, roots, 1), 0);
   }
@@ -100,6 +104,71 @@ TEST( numerics_poly_solve, poly_solve_linear )
 
 TEST( numerics_poly_solve, poly_solve_quadratic )
 {
+  double coeff[3];
+  double roots[2];
+  double expected[2];
+  int n = 2;
+  int rc;
+
+  {
+    // y = (x + 2.3)(x + 2.3)
+    SCOPED_TRACE("Double root at x = -2.3");
+    coeff[0] = 5.29; coeff[1] = 4.6; coeff[2] = 1;
+    roots[0] = 0; roots[1] = 0;
+    expected[0] = -2.3; expected[1] = -2.3;
+    rc = axom::numerics::solve_quadratic(coeff, roots, n);
+    EXPECT_EQ(rc, 0);
+    EXPECT_EQ(n, 1);
+    EXPECT_EQ(count_mismatches(expected, roots, 2), 0);
+  }
+
+  {
+    // y = (-x + 1.5)(x - 1.5)
+    SCOPED_TRACE("Double root at x = 1.5 (opening down)");
+    coeff[0] = -2.25; coeff[1] = 3; coeff[2] = -1;
+    roots[0] = 0; roots[1] = 0;
+    expected[0] = 1.5; expected[1] = 1.5;
+    rc = axom::numerics::solve_quadratic(coeff, roots, n);
+    EXPECT_EQ(rc, 0);
+    EXPECT_EQ(n, 1);
+    EXPECT_EQ(count_mismatches(expected, roots, 2), 0);
+  }
+
+  {
+    // y = 3.2(x + 0.7)(x - 2)
+    SCOPED_TRACE("Roots at -0.7 and 2");
+    coeff[0] = -4.48; coeff[1] = -4.16; coeff[2] = 3.2;
+    roots[0] = 0; roots[1] = 0;
+    expected[0] = 2; expected[1] = -0.7;
+    rc = axom::numerics::solve_quadratic(coeff, roots, n);
+    EXPECT_EQ(rc, 0);
+    EXPECT_EQ(n, 2);
+    EXPECT_EQ(count_mismatches(expected, roots, 2), 0);
+  }
+
+  {
+    // y = 0.1x^2 + 0.2x + 6
+    SCOPED_TRACE("No real roots (opening up)");
+    coeff[0] = 6; coeff[1] = 0.2; coeff[2] = 0.1;
+    roots[0] = 0; roots[1] = 0;
+    expected[0] = 0; expected[1] = 0;
+    rc = axom::numerics::solve_quadratic(coeff, roots, n);
+    EXPECT_EQ(rc, -1);
+    EXPECT_EQ(n, 0);
+    EXPECT_EQ(count_mismatches(expected, roots, 2), 0);
+  }
+
+  {
+    // y = -5x^2 + 0.2x -20
+    SCOPED_TRACE("No real roots (opening up)");
+    coeff[0] = 6; coeff[1] = 0.2; coeff[2] = 0.1;
+    roots[0] = 0; roots[1] = 0;
+    expected[0] = 0; expected[1] = 0;
+    rc = axom::numerics::solve_quadratic(coeff, roots, n);
+    EXPECT_EQ(rc, -1);
+    EXPECT_EQ(n, 0);
+    EXPECT_EQ(count_mismatches(expected, roots, 2), 0);
+  }
 }
 
 TEST( numerics_poly_solve, poly_solve_cubic )
