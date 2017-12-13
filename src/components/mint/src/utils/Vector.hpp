280ed0a3e4350bb65247aa631acb68ff9564924b
George Zagaris <zagaris2@llnl.gov>
2017-12-12 16:01:48 -0800

    ENH: add copyright to Vector class

--- src/components/mint/src/utils/Vector.hpp
+++ src/components/mint/src/utils/Vector.hpp
@@ -1,3 +1,20 @@
+/*
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Copyright (c) 2017, Lawrence Livermore National Security, LLC.
+ *
+ * Produced at the Lawrence Livermore National Laboratory
+ *
+ * LLNL-CODE-741217
+ *
+ * All rights reserved.
+ *
+ * This file is part of Axom.
+ *
+ * For details about use and distribution, please read axom/LICENSE.
+ *
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
 #ifndef MINT_UTILS_VECTOR_HXX_
 #define MINT_UTILS_VECTOR_HXX_
 
