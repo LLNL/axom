4ef24f75f81c6a4d23bf585698f97259346ad240
George Zagaris <zagaris2@llnl.gov>
2017-12-13 10:46:52 -0800

    ENH: add mint::database wrapper layer for Sidre
    This commit adds a thin wrapper layer for Sidre in Mint.
    
    A centralized DataStore instance can be registered with
    Mint as follows:
    ```
    mint::database::set( my_datastore );
    ```
    
    Once a datastore is registered, views can be accessed:
    ```
    sidre::View* view = mint::database::get_view( "path/to/view" );
    ```

---
+++ src/components/mint/src/mesh/database.hpp
@@ -0,0 +1,59 @@
+/*
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Copyright (c) 2017, Lawrence Livermore National Security, LLC.
+ *
+ * Produced at the Lawrence Livermore National Laboratory
+ *
+ * LLNL-CODE-741217
+ *
+ * All rights reserved.
+ *
+ * This file is part of Axom.
+ *
+ * For details about use and distribution, please read axom/LICENSE.
+ *
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+#ifndef MINT_DATABASE_HPP_
+#define MINT_DATABASE_HPP_
+
+#include <string> // for C++ string
+
+namespace axom
+{
+
+// Forward Declarations
+namespace sidre
+{
+  class View;
+  class DataStore;
+}
+
+namespace mint
+{
+
+namespace database
+{
+
+/*!
+ * \brief Sets the datastore instance that will be used.
+ * \param [in] db pointer to the datastore
+ * \pre db != AXOM_NULLPTR
+ */
+void set( sidre::DataStore* db );
+
+/*!
+ * \brief Returns a sidre::View instance along the given path.
+ * \param [in] path the path of the requested view.
+ * \return view pointer to the view.
+ * \note If the view does not exist, AXOM_NULLPTR is returned.
+ */
+sidre::View* get_view( const std::string& path );
+
+} /* namespace database */
+} /* namespace mint */
+} /* namespace axom */
+
+
+#endif /* MINT_DATABASE_HPP_ */
