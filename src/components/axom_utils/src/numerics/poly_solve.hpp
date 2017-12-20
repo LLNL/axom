d48552303d76f33fbebccebf10fa2c2459328f28
Arlie Capps <capps2@llnl.gov>
2017-12-19 21:46:21 -0800

    Added analytic cubic solver with tests.
    The analytic cubic polynomial solver is based on Cardano's solution,
    as set forth in various textbooks and papers (referenced in comments
    in the source code).  This can get numerically unstable: the method
    uses division by cube root of a number, which can rapidly propagate
    noise.  The paper by Kopp cited in the source discusses that author's
    work on eigenproblems of small matrices: use the analytic solution
    with a fall-back to the slow, but very accurate, Jacobi's method.  We
    may need to do something similar here.

--- src/components/axom_utils/src/numerics/poly_solve.hpp
+++ src/components/axom_utils/src/numerics/poly_solve.hpp
@@ -42,7 +42,8 @@ namespace numerics
 int solve_linear( const double* coeff, double* roots, int& numRoots );
 
 /*!
- * \brief Solves a quadratic equation of the form \f$ ax^2 + bx + c = 0 \f$.
+ * \brief Solves a quadratic equation of the form \f$ ax^2 + bx + c = 0 \f$,
+ * using the quadratic formula.
  *
  * \param [in] coeff Equation coefficients: coeff[i] multiplies \f$ x^i \f$.
  * \param [out] roots The roots of the equation.
@@ -55,6 +56,38 @@ int solve_quadratic( const double* coeff, double* roots, int& numRoots );
 
 /*!
  * \brief Solves a cubic equation of the form \f$ ax^3 + bx^2 + cx + d = 0 \f$.
+ *
+ * A closed-form solution for cubic equations was published in Cardano's
+ * *Ars Magna* of 1545.  This can be summarized as follows:
+ *
+ * Start with the cubic equation
+ * 
+ *   \f[ x^3 + bx^2 + cx + d = 0. \f]
+ *
+ * Define the following:
+ *
+ *   \f[ p = b^2 - 3c \f]
+ *   \f[ q = -\frac{27}{2}d - b^3 + \frac{9}{2}cb \f]
+ *   \f[ t = 2p^{-3/2}q \f]
+ *   \f[ y = \frac{3}{\sqrt{p}}(x + \frac{1}{3}b) \f]
+ *
+ * Then the original cubic equation can be written as \f$ y^3 - 3y = t \f$
+ * with a solution \f$ y = \frac{1}{u} + u \f$, with
+ *
+ *   \f[ u = \sqrt[3]{\frac{t}{2} \pm \sqrt{\frac{t^2}{4} - 1}}. \f]
+ *
+ * Because the cubic is an odd function, there can be either one, two, or three
+ * real roots.  In the case of one real root, the root can either be tripled or
+ * single with two complex roots.  In the case of two real roots, one will be
+ * doubled.  If the discriminant \f$ d = -27t^2 - 4(-3^3) \f$ is zero, the
+ * equation has doubled or tripled roots.  If \f$ d > 0, \f$ there are three
+ * distinct real roots, and if \f$ d > 0, \f$ there is one real root.
+ *
+ * See J. Kopp, Efficient numerical diagonalization of hermitian 3x3 matrices,
+ * Int.J.Mod.Phys. C19:523-548, 2008 (https://arxiv.org/abs/physics/0610206)
+ * and G. A. Korn and T. M. Korn, "Mathematical Handbook for Scientists and
+ * Engineers," QA37 K84 1968 in the library; section 1.8-3 "Cubic Equations",
+ * p. 23.
  *
  * \param [in] coeff Equation coefficients: coeff[i] multiplies \f$ x^i \f$.
  * \param [out] roots The roots of the equation.
@@ -116,7 +149,7 @@ int solve_quadratic( const double* coeff, double* roots, int& numRoots )
     numRoots = 1;
     roots[0] = roots[1] = -coeff[1] / (2*coeff[2]);
   } else if (discriminant < 0) {
-    // No unique real roots
+    // No real roots
     numRoots = 0;
   } else {
     // Two real roots
@@ -129,9 +162,97 @@ int solve_quadratic( const double* coeff, double* roots, int& numRoots )
   return status;
 }
 
+inline double cuberoot(double x)
+{
+  // pow(x, y) returns NaN for negative finite x and noninteger y.
+  if (x < 0) {
+    return -pow(-x, 1. / 3.);
+  } else {
+    return  pow( x, 1. / 3.);
+  }
+}
+
 int solve_cubic( const double* coeff, double* roots, int& numRoots )
 {
   int status = -1;
+
+  // Here I use variable names as presented in Korn & Korn:
+  // x^3 + ax^2 + bx + c = 0
+  double cubecoeff = coeff[3];
+  double a = coeff[2];
+  double b = coeff[1];
+  double c = coeff[0];
+
+  if (utilities::isNearlyEqual(cubecoeff, 0.)) {
+    // If this system is nearly quadratic, solve it as such.
+    return solve_quadratic(coeff, roots, numRoots);
+  }
+
+  a = a / cubecoeff;
+  b = b / cubecoeff;
+  c = c / cubecoeff;
+
+  // Note that p and q differ by a multiplicative constant from Korn,
+  // because they're always used with that multiplication.
+  double p = (-a*a + 3*b) / 9;                 //  1/3 Korn's p
+  double q = (a*(-2*a*a + 9*b) - 27*c) / 54;   // -1/2 Korn's q
+
+  double Q = p*p*p + q*q;           // actual discriminant == -108Q
+  // the term chVar occurs because we've changed variables
+  // (x = y - a/3) and we need to change back to x.
+  double chVar = -a / 3.;
+
+  if (utilities::isNearlyEqual(Q, 0.)) {
+    // We have three real roots, and at least two are equal.
+    if (utilities::isNearlyEqual(q, 0.)) {
+      numRoots = 1;
+    } else {
+      numRoots = 2;
+    }
+    status = 0;
+
+    double cuberootq = cuberoot(q);
+    roots[0] = chVar + 2*cuberootq;
+    roots[1] = chVar -   cuberootq;
+    roots[2] = roots[1];
+  } else if (Q > 0) {
+    // We have one real root, and two complex roots.
+    // Right now we're calculating the real root only, but the complex roots
+    // can be easily added by un-commenting the calculation.
+    numRoots = 1;
+    status = 0;
+
+    double sqrtQ = sqrt(Q);
+    double A = cuberoot(q + sqrtQ);
+    double B = cuberoot(q - sqrtQ);
+
+    roots[0] = chVar + A + B;
+    roots[1] = 0;
+    roots[2] = 0;
+    // double imagpart = sqrt(3) * (A - B)/2;
+    // double stavg = (A + B)/2;
+    // Here note use of imaginary i:
+    // roots[1] = chVar - stavg - i*imagpart;
+    // roots[2] = chVar - stavg + i*imagpart;
+  } else {
+    // Q < 0, and we have three distinct real roots.
+    // The case for Q == 0 is a special case of the case for Q > 0, and
+    // both correspond to Cardano's method proper, reported in section
+    // 1.8-3 of Korn, where the quadratic term ax^2 is eliminated by a
+    // change of variable x = y - a/3.  For "irreducible" cubics (this
+    // case, where the variable substitution didn't eliminate the quadratic),
+    // we use the trigonometric solution to the cubic equation, reported
+    // in section 1.8-4a of Korn.
+    numRoots = 3;
+    status = 0;
+
+    double alpha = acos(q / sqrt(-p*p*p));
+    double m = 2 * sqrt(-p);
+
+    roots[0] = chVar + m*cos(alpha / 3.);
+    roots[1] = chVar - m*cos((alpha + M_PI) / 3.);
+    roots[2] = chVar - m*cos((alpha - M_PI) / 3.);
+  }
 
   return status;
 }
--- src/components/axom_utils/tests/numerics_poly_solve.cpp
+++ src/components/axom_utils/tests/numerics_poly_solve.cpp
@@ -22,12 +22,15 @@
 // Google Test include
 #include "gtest/gtest.h"
 
-int count_mismatches(double *standard, double *test, int n)
+int count_mismatches(double *standard, double *test, int n, 
+                     double thresh = 1.0e-8);
+
+int count_mismatches(double *standard, double *test, int n, double thresh)
 {
   int mcount = 0;
 
   for (int i = 0; i < n; ++i) {
-    if (! axom::utilities::isNearlyEqual(standard[i], test[i])) {
+    if (! axom::utilities::isNearlyEqual(standard[i], test[i], thresh)) {
       ++mcount;
     }
   }
@@ -173,6 +176,73 @@ TEST( numerics_poly_solve, poly_solve_quadratic )
 
 TEST( numerics_poly_solve, poly_solve_cubic )
 {
+  double coeff[4];
+  double roots[3];
+  double expected[3];
+  int n = 3;
+  int rc;
+
+  {
+    // y = (x - 1.2)^3 = 1.0 x^3 - 3.6 x^2 + 4.32 x - 1.728
+    SCOPED_TRACE("Triple root at x = 1.2 (NOTE: LOOSE TOLERANCE HERE)");
+    coeff[0] = -1.728; coeff[1] = 4.32; coeff[2] = -3.6; coeff[3] = 1;
+    roots[0] = 0; roots[1] = 0; roots[2] = 0;
+    expected[0] = 1.2; expected[1] = 1.2; expected[2] = 1.2;
+    rc = axom::numerics::solve_cubic(coeff, roots, n);
+    EXPECT_EQ(rc, 0);
+    EXPECT_EQ(n, 1);
+    EXPECT_EQ(count_mismatches(expected, roots, 3, 5.0e-5), 0);
+  }
+
+  {
+    // y = -x^3 - x + 10
+    SCOPED_TRACE("Single real root at x = 2");
+    coeff[0] = 10; coeff[1] = -1; coeff[2] = 0; coeff[3] = -1;
+    roots[0] = 0; roots[1] = 0; roots[2] = 0;
+    expected[0] = 2; expected[1] = 0; expected[2] = 0;
+    rc = axom::numerics::solve_cubic(coeff, roots, n);
+    EXPECT_EQ(rc, 0);
+    EXPECT_EQ(n, 1);
+    EXPECT_EQ(count_mismatches(expected, roots, 3), 0);
+  }
+
+  {
+    // y = x^3 + x^2 - 8*x - 12
+    SCOPED_TRACE("Two real roots, at x = 3, twice at x = -2");
+    coeff[0] = -12; coeff[1] = -8; coeff[2] = 1; coeff[3] = 1;
+    roots[0] = 0; roots[1] = 0; roots[2] = 0;
+    expected[0] = 3; expected[1] = -2; expected[2] = -2;
+    rc = axom::numerics::solve_cubic(coeff, roots, n);
+    EXPECT_EQ(rc, 0);
+    EXPECT_EQ(n, 2);
+    EXPECT_EQ(count_mismatches(expected, roots, 3), 0);
+  }
+
+  {
+    // y = (x + 0.8)(-x + 1)(x - 8) = -3x^2 + 24.6x^2 - 2.4x - 19.2
+    SCOPED_TRACE("Three real roots, at x = -0.8, 1, 8");
+    coeff[0] = -19.2; coeff[1] = -2.4; coeff[2] = 24.6; coeff[3] = -3;
+    roots[0] = 0; roots[1] = 0; roots[2] = 0;
+    expected[0] = 8; expected[1] = 1; expected[2] = -0.8;
+    rc = axom::numerics::solve_cubic(coeff, roots, n);
+    EXPECT_EQ(rc, 0);
+    EXPECT_EQ(n, 3);
+    EXPECT_EQ(count_mismatches(expected, roots, 3), 0);
+  }
+
+  {
+    // y = 4.3(x + 38)(x + 1)(x - 0.001) 
+    //   = 4.3x^3 + 167.6957x^2 + 163.2323x - 0.1634
+    SCOPED_TRACE("Three real roots, at x = -38, -1, 0.001");
+    coeff[0] = -0.1634; coeff[1] = 163.2323; coeff[2] = 167.6957; 
+    coeff[3] = 4.3;
+    roots[0] = 0; roots[1] = 0; roots[2] = 0;
+    expected[0] = 0.001; expected[1] = -1; expected[2] = -38;
+    rc = axom::numerics::solve_cubic(coeff, roots, n);
+    EXPECT_EQ(rc, 0);
+    EXPECT_EQ(n, 3);
+    EXPECT_EQ(count_mismatches(expected, roots, 3), 0);
+  }
 }
 
 
