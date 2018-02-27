8c7b9707661cb37fe467d1563178fc65560bd70e
George Zagaris <zagaris2@llnl.gov>
2018-02-27 11:39:36 -0800

    ENH: improve error message in Plane constructor.

--- src/components/primal/src/geometry/Plane.hpp
+++ src/components/primal/src/geometry/Plane.hpp
@@ -326,8 +326,7 @@ Plane< T, NDIMS >::Plane( const T* x1, const T* x2, const T* x3 )
   }
 
   SLIC_ERROR_IF( degenerate,
-      "cannot construct plane from supplied points." <<
-      "Supplied points form a degenerate line(in 2D) or triangle(in 3D)" );
+     "Supplied points form a degenerate " << (NDIMS==2) ? "line" : "triangle");
 
   numerics::normalize( m_normal, NDIMS );
   m_offset = numerics::dot_product( m_normal, x1, NDIMS );
