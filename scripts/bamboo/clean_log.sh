c919e595668d6e587d239d26ad4bc2f18dbdf1d3
Chris White <white238@llnl.gov>
2018-03-09 14:18:46 -0800

    add missed file from last checkin, update cron jobs

--- scripts/bamboo/clean_log.sh
+++
@@ -1,3 +0,0 @@
-find /g/g16/atk/bambooAgent/asctoolkit.cab.llnl.gov/atlassian*.log.* -mtime +90 -exec rm {} \;
-find /g/g16/atk/bambooAgent/asctoolkit.cab2.llnl.gov/atlassian*.log.* -mtime +90 -exec rm {} \;
-find /g/g16/atk/bambooAgent/asctoolkit.vulcan.llnl.gov/atlassian*.log.* -mtime +90 -exec rm {} \;
