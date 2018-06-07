a8ec10e93e27db67de758f5001c59e1d8b82ff16
George Zagaris <zagaris2@llnl.gov>
2018-06-07 11:49:01 -0700

    STYLE: don't use axom:: within the axom namespace

--- src/components/quest/src/stl/STLReader.hpp
+++ src/components/quest/src/stl/STLReader.hpp
@@ -79,7 +79,7 @@ public:
    * \param [in,out] mesh pointer to the unstructured mesh.
    * \pre mesh != AXOM_NULLPTR.
    */
-  void getMesh( axom::mint::UnstructuredMesh< mint::SINGLE_SHAPE >* mesh );
+  void getMesh( mint::UnstructuredMesh< mint::SINGLE_SHAPE >* mesh );
 
 private:
   /*!
