8358effe954e4fcf785665c61be803186dfd5d7b
Raine Yeh <yeh14@llnl.gov>
2018-06-11 10:09:34 -0700

    Incorporate BivariateSet and BivariateMap in the process of developing dense MultiMat field

--- src/axom/multimat/multimat.cpp
+++ src/axom/multimat/multimat.cpp
@@ -22,11 +22,18 @@ MultiMat::MultiMat(DataLayout d, SparcityLayout s): MultiMat() {
   m_sparcityLayout = s;
 }
 
+axom::multimat::MultiMat::~MultiMat()
+{
+  for (auto mapPtr : m_mapVec) {
+    delete mapPtr;
+  }
+}
+
 void MultiMat::setNumberOfMat(int n){
   assert(n > 0);
   m_nmats = n;
 
-  m_matSet = SetType(0, m_nmats);
+  m_matSet = RangeSetType(0, m_nmats);
   assert(m_matSet.isValid());
 }
 
@@ -35,7 +42,7 @@ void MultiMat::setNumberOfCell(int c)
   assert(c > 0);
   m_ncells = c;
 
-  m_cellSet = SetType(0, m_ncells);
+  m_cellSet = RangeSetType(0, m_ncells);
   assert(m_cellSet.isValid());
 }
 
@@ -47,66 +54,79 @@ void MultiMat::setCellMatRel(vector<bool>& vecarr)
 
   assert(m_dataLayout == LAYOUT_CELL_DOM); //for now assumes cell dominant
 
-  if (m_sparcityLayout == LAYOUT_SPARSE)
-  {
-    //Set-up the variable cardinality relation
-    m_cell2matRel_beginsVec.resize(m_cellSet.size() + 1, -1);
+  //Set-up the cell/mat relation
+  m_cell2matRel_beginsVec.resize(m_cellSet.size() + 1, -1);
 
-    SetPosType curIdx = SetPosType();
-    for (SetPosType i = 0; i < m_ncells; ++i)
+  SetPosType curIdx = SetPosType();
+  for (SetPosType i = 0; i < m_ncells; ++i)
+  {
+    m_cell2matRel_beginsVec[i] = curIdx;
+    for (SetPosType j = 0; j < m_nmats; ++j)
     {
-      m_cell2matRel_beginsVec[i] = curIdx;
-      for (SetPosType j = 0; j < m_nmats; ++j)
-      {
-        if (vecarr[i*m_nmats + j]) {
-          m_cell2matRel_indicesVec.push_back(j);
-          ++curIdx;
-        }
+      if (vecarr[i*m_nmats + j]) {
+        m_cell2matRel_indicesVec.push_back(j);
+        ++curIdx;
       }
     }
-    m_cell2matRel_beginsVec[m_ncells] = curIdx;
+  }
+  m_cell2matRel_beginsVec[m_ncells] = curIdx;
 
-    m_cell2matRel = StaticVariableRelationType(&m_cellSet, &m_matSet);
-    m_cell2matRel.bindBeginOffsets(m_cellSet.size(), &m_cell2matRel_beginsVec);
-    m_cell2matRel.bindIndices(m_cell2matRel_indicesVec.size(), &m_cell2matRel_indicesVec);
+  m_cell2matRel = StaticVariableRelationType(&m_cellSet, &m_matSet);
+  m_cell2matRel.bindBeginOffsets(m_cellSet.size(), &m_cell2matRel_beginsVec);
+  m_cell2matRel.bindIndices(m_cell2matRel_indicesVec.size(), &m_cell2matRel_indicesVec);
 
-    assert(m_cell2matRel.isValid());
+  assert(m_cell2matRel.isValid());
 
-    cout << "indice total size: " << m_cell2matRel_indicesVec.size() << endl;
-    cout << "cellmatrel total size: " << m_cell2matRel.totalSize() << endl;
-    cout << "fromset size: " << m_cellSet.size() << endl;
+  cout << "indice total size: " << m_cell2matRel_indicesVec.size() << endl;
+  cout << "cellmatrel total size: " << m_cell2matRel.totalSize() << endl;
+  cout << "fromset size: " << m_cellSet.size() << endl;
 
+  if (m_sparcityLayout == LAYOUT_SPARSE)
+  {
     //a set of mapped relation, which is a subset of the cartesian of cell & mat
     m_cellMatNZSet = MappedRelationSetType(&m_cell2matRel);
   }
   else if (m_sparcityLayout == LAYOUT_DENSE) {
-    assert(false); //TODO
+    if(m_dataLayout == LAYOUT_CELL_DOM)
+      m_cellMatProdSet = ProductSetType(&m_cellSet, &m_matSet);
+    else if(m_dataLayout==LAYOUT_MAT_DOM)
+      m_cellMatProdSet = ProductSetType(&m_matSet, &m_cellSet);
+    else assert(false);
   }
   else assert(false);
 
 }
 
-MultiMat::RelationSet MultiMat::getMatInCell(int c)
+MultiMat::IdSubSet MultiMat::getMatInCell(int c)
 {
-  if (m_dataLayout == LAYOUT_SPARSE)
-    return m_cell2matRel[c];
-  else if (m_dataLayout == LAYOUT_DENSE)
-    assert(false); //TODO
+  if (m_dataLayout == LAYOUT_SPARSE) {
+    return m_cell2matRel[c]; //returns a RelationSet / OrderedSet with STLindirection
+  }
+  //else if (m_dataLayout == LAYOUT_DENSE)
+  //  return m_cell2matRel[c]; //since the relation is currently only stored sparse, return the same thing.
   else assert(false);
 }
 
-MultiMat::RangeSetType axom::multimat::MultiMat::getIndexingSetOfCell(int c)
+MultiMat::IndexSet axom::multimat::MultiMat::getIndexingSetOfCell(int c)
 {
-  assert(0 <= c && c < m_ncells);
-  int start_idx = m_cell2matRel_beginsVec[c];
-  int end_idx = m_cell2matRel_beginsVec[c + 1];
-
-  return RangeSetType::SetBuilder().range(start_idx, end_idx);
+  if (m_sparcityLayout == LAYOUT_SPARSE) {
+    assert(0 <= c && c < m_ncells);
+    int start_idx = m_cell2matRel_beginsVec[c];
+    int end_idx = m_cell2matRel_beginsVec[c + 1];
+    return RangeSetType::SetBuilder().range(start_idx, end_idx);
+  }
+  else if (m_sparcityLayout == LAYOUT_DENSE) {
+    //return m_cellMatProdSet.getRow(c);
+    int size1 = m_cellMatProdSet.set1Size();
+    int size2 = m_cellMatProdSet.set2Size();
+    return RangeSetType::SetBuilder().range(c*size2, (c + 1)*size2 - 1);
+  }
+  else assert(false);
 }
 
 void axom::multimat::MultiMat::convertLayout(DataLayout new_layout, SparcityLayout new_sparcity)
 {
-  if (new_layout == m_dataLayout)
+  if (new_layout == m_dataLayout && new_sparcity == m_sparcityLayout)
     return;
 
   //TODO
@@ -128,6 +148,11 @@ std::string axom::multimat::MultiMat::getLayoutAsString()
     assert(false);
     return "";
   }
+}
+
+SparcityLayout axom::multimat::MultiMat::getSparcityLayout()
+{
+  return m_sparcityLayout;
 }
 
 void axom::multimat::MultiMat::printSelf()
@@ -162,7 +187,11 @@ axom::multimat::MultiMat::SetType* axom::multimat::MultiMat::get_mapped_set(Fiel
     map_set = &m_matSet;
     break;
   case PER_CELL_MAT:
-    map_set = &m_cellMatNZSet;
+    if (m_sparcityLayout == LAYOUT_SPARSE)
+      map_set = &m_cellMatNZSet;
+    else if (m_sparcityLayout == LAYOUT_DENSE)
+      map_set = &m_cellMatProdSet;
+    else assert(false);
     break;
   default:
     assert(false);
