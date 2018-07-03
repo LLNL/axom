3653f071139006de9af0a73de45bc6c5b4c63e85
Cyrus Harrison <cyrush@llnl.gov>
2018-07-03 16:53:39 -0700

    add strawmen for using-with examples

---
+++ src/examples/using-with-cmake/FindAxom.cmake
@@ -0,0 +1,59 @@
+#------------------------------------------------------------------------------
+# Copyright (c) 2017-2018, Lawrence Livermore National Security, LLC.
+#
+# Produced at the Lawrence Livermore National Laboratory
+#
+# LLNL-CODE-741217
+#
+# All rights reserved.
+#
+# This file is part of Axom.
+#
+# For details about use and distribution, please read axom/LICENSE.
+#------------------------------------------------------------------------------
+###############################################################################
+#
+# Setup Axom
+#
+###############################################################################
+#
+# Expects AXOM_DIR to point to a Conduit installations.
+#
+# This file defines the following CMake variables:
+#  AXOM_FOUND - If Axom was found
+#  AXOM_INCLUDE_DIRS - The Axom include directories
+#
+###############################################################################
+
+###############################################################################
+# Check for AXOM_DIR
+###############################################################################
+if(NOT AXOM_DIR)
+    MESSAGE(FATAL_ERROR "Could not find Axom. Axom requires explicit AXOM_DIR.")
+endif()
+
+###############################################################################
+#TODO Axom needs to export a main "axom.cmake" to make this easier and more 
+# useful
+###############################################################################
+
+if(NOT EXISTS ${AXOM_DIR}/lib/cmake/axom_utils-targets.cmake)
+    MESSAGE(FATAL_ERROR "Could not find Axom CMake include file (${AXOM_DIR}/lib/cmake/axom_utils-targets.cmake)")
+endif()
+
+###############################################################################
+# Import Conduit's CMake targets
+###############################################################################
+include(${AXOM_DIR}/lib/cmake/axom_utils-targets.cmake)
+
+###############################################################################
+# Set remaning CMake variables 
+###############################################################################
+# we found Conduit
+set(AXOM_FOUND TRUE)
+# provide location of the headers in AXOM_INCLUDE_DIRS
+set(AXOM_INCLUDE_DIRS ${AXOM_DIR}/include/)
+
+
+
+
