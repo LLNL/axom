0d20387b667613aa0aa9a04164e82775a14e0005
George Zagaris <zagaris2@llnl.gov>
2018-07-16 12:05:30 -0700

    ENH: improvements to the jacobi_eigensolve routine
    This commit makes some minor enhancements to the
    jacobi_eigensolve method. Specifically:
    * Adds TOL as an optional parameter to the method
    * Take into account the optional numIterations parameter which
      when specified, it stores the actual number of iterations for
      the jacobi iterative algorithm
    * Improve the doxygen documentation of the method.
    * Improve the unit test and add checks
    
    This commit resolves in part ATK-1218.

--- src/components/axom_utils/src/numerics/jacobi_eigensolve.hpp
+++ src/components/axom_utils/src/numerics/jacobi_eigensolve.hpp
@@ -23,35 +23,57 @@
 
 #include "axom_utils/Matrix.hpp"       // for numerics::Matrix
 #include "axom_utils/eigen_sort.hpp"   // for numerics::eigen_sort()
+#include "axom_utils/Utilities.hpp"    // for abs(), isNearlyEqual()
 
 namespace axom
 {
 namespace numerics
 {
 
-constexpr double TOL = 1.e-18;
-constexpr int JACOBI_EIGENSOLVE_SUCCESS = 0;
-constexpr int JACOBI_EIGENSOLVE_FAILURE = -1;
+constexpr double JACOBI_DEFAULT_TOLERANCE   = 1.e-18;
+constexpr int JACOBI_DEFAULT_MAX_ITERATIONS = 20;
+constexpr int JACOBI_EIGENSOLVE_SUCCESS     = 0;
+constexpr int JACOBI_EIGENSOLVE_FAILURE     = -1;
 
 
 /*!
  * \brief Computes the eigenvalues and eigenvectors of a real symmetric matrix
  *  using the Jacobi iteration method.
  *
- * \param [in] B the matrix whose eigenpairs will be computed
- * \param [in] n the number of eigenvalues to compute (e.g. dim(A))
- * \param [in,out] V matrix to store eigenvectors
- * \param [in,out] lambdas buffer to store eigenvalues in ascending order
- * \param [in] numIterations number of iterations in Jacobi method
+ * \param [in]  A the input matrix whose eigenpairs will be computed
+ * \param [out] V matrix to store the eigenvectors of the input matrix
+ * \param [out] lambdas buffer where the computed eigenvalues will be stored.
+ * \param [in] maxIterations the maximum number of iterations (optional)
+ * \param [in] numIterations the number of actual iterations (optional)
+ * \param [in] TOL convergence tolerance. Default is set to 1.e-18 (optional)
+ *
+ * \return status returns JACOBI_EIGENSOLVE_SUCCESS on success, otherwise,
+ *  JACOBI_EIGENSOLVE_FAILURE is returned.
  *
  * \note The supplied matrix is expected to be a real symmetric matrix.
+ *
+ * \note The Jacobi method is absolutely foolproof for all real symmetric
+ *  matrices. It is efficient for moderate size matrices and generally can
+ *  evaluate the smaller eigenvalues with better relative accuracy than other
+ *  methods that convert the matrix to tridiagonal form, e.g., QR. However,
+ *  for matrices of order greater than, e.g., 20, the Jacobi algorithm is
+ *  generally slower by a significant constant factor.
+ *
+ * \note The Jacobi iteration will generally converge within 6-15 iterations.
+ *  The default max number of iterations is set to 20, but, the caller may
+ *  specify a different value if necessary.
+ *
+ * \pre A.isSquare() == true
+ * \pre V.isSquare() == true
+ * \pre lambdas != AXOM_NULLPTR
  */
 template < typename T >
 int jacobi_eigensolve( Matrix < T > A,
                        Matrix< T >& V,
                        T* lambdas,
-                       int maxIterations=160,
-                       int* numIterations=AXOM_NULLPTR );
+                       int maxIterations=JACOBI_DEFAULT_MAX_ITERATIONS,
+                       int* numIterations=AXOM_NULLPTR,
+                       T TOL=JACOBI_DEFAULT_TOLERANCE );
 
 //------------------------------------------------------------------------------
 // IMPLEMENTATION
@@ -61,7 +83,8 @@ int jacobi_eigensolve( Matrix < T > A,
                        Matrix< T >& V,
                        T* lambdas,
                        int maxIterations,
-                       int* numIterations )
+                       int* numIterations,
+                       T TOL )
 {
   int n = A.getNumRows();
 
@@ -73,7 +96,7 @@ int jacobi_eigensolve( Matrix < T > A,
   assert("pre: lambdas vector is null" && (lambdas != AXOM_NULLPTR));
 
 
-  if ( !A.isSquare() )
+  if ( !A.isSquare() || !V.isSquare() )
   {
     return JACOBI_EIGENSOLVE_FAILURE;
   }
@@ -82,23 +105,16 @@ int jacobi_eigensolve( Matrix < T > A,
   T *bw = new T[n]; // temp array
   T *zw = new T[n]; // temp array
 
+  // initialize
   for (int i = 0; i < n; ++i)
   {
-    lambdas[i] = A(i,i);
-    bw[i] = lambdas[i];
-    zw[i] = 0.0;
+    lambdas[ i ] = A(i,i);
+    bw[ i ]      = lambdas[i];
+    zw[ i ]      = 0.0;
 
     for (int j = 0; j < n; ++j)
     {
-      if (i == j)
-      {
-        V(i,j) = 1.0;
-      }
-      else
-      {
-        V(i,j) = 0.0;
-      }
-
+      V( i,j ) = ( i==j )? 1.0 : 0.0;
     }
 
   }
@@ -119,7 +135,7 @@ int jacobi_eigensolve( Matrix < T > A,
       }
     }
 
-    T thresh = sqrt(sum)/(4.0* (T) n);
+    const T thresh = sqrt(sum) / (4.0*static_cast< T >( n ) );
 
     if (utilities::isNearlyEqual( thresh, 0.0, TOL ))
     {
@@ -132,34 +148,39 @@ int jacobi_eigensolve( Matrix < T > A,
       for (int q = p+1; q < n; ++q)
       {
 
-        T gapq = 10.0 * fabs(A(p,q));
-        T termp = gapq + fabs(lambdas[p]);
-        T termq = gapq + fabs(lambdas[q]);
+        T gapq  = 10.0 * utilities::abs(A(p,q));
+        T termp = gapq + utilities::abs(lambdas[p]);
+        T termq = gapq + utilities::abs(lambdas[q]);
 
         // the Jacobi iteration ignores off diagonal elements close to zero
         if ( 4 < iter &&
-             termp == fabs(lambdas[p]) &&
-             termq == fabs(lambdas[q]) )
+             termp == utilities::abs(lambdas[p]) &&
+             termq == utilities::abs(lambdas[q]) )
         {
           A(p,q) = 0.0;
         }
-        else if ( thresh <= fabs(A(p,q)) )
+        else if ( thresh <= utilities::abs(A(p,q)) )
         {
 
           T h = lambdas[q] - lambdas[p];
-          T term = fabs(h) + gapq;
+          T term = utilities::abs(h) + gapq;
 
           T t;
 
-          if (term == fabs(h))
+          if (term == utilities::abs(h))
           {
             t = A(p,q)/h;
-          } else {
+          }
+          else
+          {
             T theta = 0.5 * h / A(p,q);
-            t = 1.0 / ( fabs(theta) + sqrt(1.0 + theta*theta) );
+            t = 1.0 / ( utilities::abs(theta) + sqrt(1.0 + theta*theta) );
 
             if (theta < 0.0)
+            {
               t = -t;
+            }
+
           }
 
           // compute Givens rotation terms: c = cos(theta), s = sin(theta)
@@ -179,23 +200,23 @@ int jacobi_eigensolve( Matrix < T > A,
           // perform the rotation using information from upper triangle of A
           for (int j = 0; j < p; ++j)
           {
-            T g1 = A(j,p);
-            T g2 = A(j,q);
+            T g1   = A(j,p);
+            T g2   = A(j,q);
             A(j,p) = g1 - s * (g2 + g1 * tau);
             A(j,q) = g2 + s * (g1 - g2 * tau);
           }
 
           for (int j = p+1; j < q; ++j) {
-            T g1 = A(p,j);
-            T g2 = A(j,q);
+            T g1   = A(p,j);
+            T g2   = A(j,q);
             A(p,j) = g1 - s * (g2 + g1 * tau);
             A(j,q) = g2 + s * (g1 - g2 * tau);
           }
 
           for (int j = q+1; j < n; ++j)
           {
-            T g1 = A(p,j);
-            T g2 = A(q,j);
+            T g1   = A(p,j);
+            T g2   = A(q,j);
             A(p,j) = g1 - s * (g2 + g1 * tau);
             A(q,j) = g2 + s * (g1 - g2 * tau);
           }
@@ -203,8 +224,8 @@ int jacobi_eigensolve( Matrix < T > A,
           // accumulate results into eigenvector matrix
           for (int j = 0; j < n; ++j)
           {
-            T g1 = V(j,p);
-            T g2 = V(j,q);
+            T g1   = V(j,p);
+            T g2   = V(j,q);
             V(j,p) = g1 - s * (g2 + g1 * tau);
             V(j,q) = g2 + s * (g1 - g2 * tau);
           }
@@ -218,6 +239,12 @@ int jacobi_eigensolve( Matrix < T > A,
       bw[i] += zw[i];
       lambdas[i] = bw[i];
       zw[i] = 0.0;
+    }
+
+    // update number of actual iterations (if specified)
+    if ( numIterations != AXOM_NULLPTR )
+    {
+      (*numIterations)++;
     }
 
   } // END for all iterations
--- src/components/axom_utils/tests/numerics_jacobi_eigensolve.cpp
+++ src/components/axom_utils/tests/numerics_jacobi_eigensolve.cpp
@@ -19,12 +19,15 @@
 #include "axom/Types.hpp"                    // for AXOM_NULLPTR
 
 #include "axom_utils/Matrix.hpp"             // for numerics::Matrix
-#include "axom_utils/Utilities.hpp"          // for random_real()
+#include "axom_utils/Utilities.hpp"          // for random_real()/isNearlyEqual
 #include "axom_utils/jacobi_eigensolve.hpp"  // for jacobi_eigensolve()
 #include "axom_utils/matvecops.hpp"          // for matrix operators
 
 // gtest includes
 #include "gtest/gtest.h"
+
+// C/C++ includes
+#include <sstream> // for std::ostringstream
 
 // namespace aliases
 namespace numerics  = axom::numerics;
@@ -33,45 +36,67 @@ namespace utilities = axom::utilities;
 TEST( numerics_jacobi_eigensolve, random_symmetric_matrix )
 {
   using IndexType = typename numerics::Matrix< double >::IndexType;
+  constexpr double TOL   = 1.e-12;
+  constexpr int MAX_SIZE = 41;
 
   // 40x40 test. Generate random symmetric matrix A
-  int N = 40;
-  numerics::Matrix<double> A_test(N,N);
-  for (IndexType i=0; i < N; ++i)
+  for ( int k=2; k < MAX_SIZE; ++k  )
   {
-    for (IndexType j=0; j <= i; ++j)
+    const int N = k;
+
+    std::ostringstream oss;
+    oss << "checking [" << N << " x " << N << "] matrix";
+    SCOPED_TRACE( oss.str() );
+
+    numerics::Matrix<double> A_test(N,N);
+    for (IndexType i=0; i < N; ++i)
     {
-      // A_test(i,j) = (double) rand()/ (double) RAND_MAX;
-      A_test(i,j) = utilities::random_real<double> (0.0, 1.0);
-      A_test(j,i) = A_test(i,j);
+      for (IndexType j=0; j <= i; ++j)
+      {
+        A_test(i,j) = utilities::random_real<double> (0.0, 1.0);
+        A_test(j,i) = A_test(i,j);
+      }
     }
-  }
 
-  double *lambdas = new double[N];
-  numerics::Matrix<double> V(N,N);
+    double *lambdas = new double[N];
+    numerics::Matrix<double> V(N,N);
 
-  numerics::jacobi_eigensolve<double>( A_test, V, lambdas);
+    int numIterations = 0;
+    numerics::jacobi_eigensolve( A_test, V, lambdas,
+                                 numerics::JACOBI_DEFAULT_MAX_ITERATIONS,
+                                 &numIterations,
+                                 numerics::JACOBI_DEFAULT_TOLERANCE );
 
-  // compute A*V - V*lambda
-  numerics::Matrix<double> tmp(N,N);
-  numerics::Matrix<double> test(N,N);
-  numerics::matrix_multiply(A_test, V, tmp);
+    EXPECT_TRUE( numIterations > 0 );
+    EXPECT_TRUE( numIterations < 16 );
 
-  for (IndexType i=0; i < N; ++i)
-  {
-    for (IndexType j=0; j < N; ++j)
+    // compute A*V - V*lambda
+    numerics::Matrix<double> tmp(N,N);
+    numerics::Matrix<double> test(N,N);
+    numerics::matrix_multiply(A_test, V, tmp);
+
+    for (IndexType i=0; i < N; ++i)
     {
-      test(i,j) = tmp(i,j) - V(i,j)*lambdas[j];
+      for (IndexType j=0; j < N; ++j)
+      {
+        test(i,j) = tmp(i,j) - V(i,j)*lambdas[j];
+        EXPECT_TRUE( utilities::isNearlyEqual( test(i,j), 0.0, TOL ) );
+      }
     }
-  }
 
-  double p1norm    = numerics::matrix_norm( test, numerics::P1_NORM );
-  double inftynorm = numerics::matrix_norm( test, numerics::INF_NORM );
-  double frobnorm  = numerics::matrix_norm( test, numerics::FROBENIUS_NORM );
+    double p1norm    = numerics::matrix_norm( test, numerics::P1_NORM );
+    double inftynorm = numerics::matrix_norm( test, numerics::INF_NORM );
+    double frobnorm  = numerics::matrix_norm( test, numerics::FROBENIUS_NORM );
 
-  std::cout << "p1norm: "    << p1norm    << std::endl;
-  std::cout << "inftynorm: " << inftynorm << std::endl;
-  std::cout << "frobnorm: "  << frobnorm  << std::endl;
+    EXPECT_TRUE( utilities::isNearlyEqual( p1norm, 0.0, TOL ) );
+    EXPECT_TRUE( utilities::isNearlyEqual( inftynorm, 0.0, TOL ) );
+    EXPECT_TRUE( utilities::isNearlyEqual( frobnorm, 0.0, TOL ) );
+
+    delete [] lambdas;
+    lambdas = AXOM_NULLPTR;
+
+  } // END for all matrix sizes
+
 }
 
 //------------------------------------------------------------------------------
