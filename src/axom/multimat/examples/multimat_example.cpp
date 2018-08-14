9e29c7e64ac3762cfbd966505d6dfbcd7c31979a
Raine Yeh <yeh14@llnl.gov>
2018-08-14 16:15:57 -0700

    doxygen for multimat tests

--- src/axom/multimat/examples/multimat_example.cpp
+++ src/axom/multimat/examples/multimat_example.cpp
@@ -1,3 +1,28 @@
+/*
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Copyright (c) 2017-2018, Lawrence Livermore National Security, LLC.
+ *
+ * Produced at the Lawrence Livermore National Laboratory
+ *
+ * LLNL-CODE-741217
+ *
+ * All rights reserved.
+ *
+ * This file is part of Axom.
+ *
+ * For details about use and distribution, please read axom/LICENSE.
+ *
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+
+
+
+/**
+ * \file multimat_example.cpp
+ *
+ * \brief Some examples using MultiMat
+ */
 
 #include "multimat/multimat.hpp"
 
