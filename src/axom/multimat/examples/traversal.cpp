edb4a211236da5570eb7e37fa6d3f1adff9ddf82
Raine Yeh <yeh14@llnl.gov>
2018-08-15 12:16:59 -0700

    some fix-ups

--- src/axom/multimat/examples/traversal.cpp
+++ src/axom/multimat/examples/traversal.cpp
@@ -10,7 +10,7 @@
  *
  * This file is part of Axom.
  *
- * For details about use and distribution, please read axom/LICENSE.
+ * details about use and distribution, please read axom/LICENSE.
  *
  *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  */
@@ -24,30 +24,52 @@
 
 #include "axom/multimat/multimat.hpp"
 
-#include <ctime>
+#include "axom/slic/core/UnitTestLogger.hpp"
+#include "axom/core/utilities/Timer.hpp"
+
+#include <cstdlib>   // for std::rand(), RAND_MAX
 
 using namespace std;
 using namespace axom::multimat;
 
 
-std::clock_t start_time;
-void start_timer() {
-  start_time = std::clock();
+double getRandomDouble(double low, double high)
+{
+  const double delta = high - low;
+  const double c = static_cast< double >(std::rand()) /
+                   static_cast< double >(RAND_MAX);
+  return (delta*c + low);
 }
-double end_timer() {
-  double duration = (std::clock() - start_time) / (double)CLOCKS_PER_SEC;
-  return duration;
-}
 
 
-void various_traversal_methods(int nmats, int ncells, int ncomp, bool use_sparse) {
+/**
+ * \brief Show different ways to traverse the values inside MultiMat.
+ * 
+ * The two fields used are "Cell Array" for 1d array
+ * where there is an entry for each cell, and "CellMat Array" where
+ * there is an entry for each cell and each material. 
+ * 
+ * Each traversal loops access the value and, for the sparse layouts, the index
+ * of the material (assuming a cell-dominant layout).
+ * 
+ * Note that some index access are commented out to avoid compiler warnings,
+ * but are valid index accessing code.
+ * 
+ */
+void various_traversal_methods(int nmats, int ncells, int ncomp,
+                               bool use_sparse, double fill_percentage) {
 
-  MultiMat mm(DataLayout::CELL_CENTRIC, use_sparse ? SparcityLayout::SPARSE : SparcityLayout::DENSE);
-  
+  axom::utilities::Timer timer;
+
+  MultiMat mm(DataLayout::CELL_CENTRIC,
+              (use_sparse ? SparcityLayout::SPARSE : SparcityLayout::DENSE));
+
   int nfilled = 0;
   std::vector<bool> cellMatRel(nmats * ncells, false);
-  for (int i = 0; i < nmats*ncells; i++) {
-    if (i % 3 == 1) {
+  for (int i = 0 ; i < nmats*ncells ; i++)
+  {
+    if (getRandomDouble(0,1) < fill_percentage)
+    {
       cellMatRel[i] = true;
       nfilled++;
     }
@@ -58,21 +80,26 @@ void various_traversal_methods(int nmats, int ncells, int ncomp, bool use_sparse
   mm.setCellMatRel(cellMatRel);
 
   //create the std::vector data for the field arrays
-  std::vector<double> cell_arr1(ncells*ncomp);
+  std::vector<double> cell_arr(ncells*ncomp);
   double c_sum = 0;
-  for (int i = 0; i < ncells; ++i) {
-    for (int comp = 0; comp < ncomp; ++comp) {
-      cell_arr1[i*ncomp + comp] = (double)i * 2.0 + comp * 0.01;
-      c_sum += cell_arr1[i*ncomp + comp];
+  for (int i = 0 ; i < ncells ; ++i)
+  {
+    for (int comp = 0 ; comp < ncomp ; ++comp)
+    {
+      cell_arr[i*ncomp + comp] = (double)i * 2.0 + comp * 0.01;
+      c_sum += cell_arr[i*ncomp + comp];
     }
   }
-    
+
   std::vector<double> cellmat_arr;
   cellmat_arr.resize( (use_sparse ? nfilled : nmats * ncells) *ncomp);
   double x_sum = 0;
-  for (unsigned int i = 0; i < cellmat_arr.size()/ncomp; i++) {
-    if (use_sparse || cellMatRel[i]) {
-      for (int comp = 0; comp < ncomp; ++comp) {
+  for (unsigned int i = 0 ; i < cellmat_arr.size()/ncomp ; i++)
+  {
+    if (use_sparse || cellMatRel[i])
+    {
+      for (int comp = 0 ; comp < ncomp ; ++comp)
+      {
         cellmat_arr[i*ncomp + comp] = (double)i * 1.1 + comp * 0.001;
         x_sum += cellmat_arr[i*ncomp + comp];
       }
@@ -81,214 +108,246 @@ void various_traversal_methods(int nmats, int ncells, int ncomp, bool use_sparse
 
   //create volfrac array
   std::vector<double> volfrac_arr(ncells*nmats, 0);
-  for (auto i = 0; i < ncells; ++i)
+  for (auto i = 0 ; i < ncells ; ++i)
   {
     int matcount = 0;
-    for (auto m = 0; m < nmats; ++m) {
+    for (auto m = 0 ; m < nmats ; ++m)
+    {
       if (cellMatRel[i*nmats + m])
         matcount+=1;
     }
-    for (auto m = 0; m < nmats; ++m) {
+    for (auto m = 0 ; m < nmats ; ++m)
+    {
       if (cellMatRel[i*nmats + m])
         volfrac_arr[i*nmats + m] = 1.0 / (double)matcount;
     }
   }
 
   mm.setVolfracField(volfrac_arr.data());
-  mm.addField("Cell Array"   , FieldMapping::PER_CELL    , &cell_arr1[0], ncomp);
-  mm.addField("CellMat Array", FieldMapping::PER_CELL_MAT, &cellmat_arr[0], ncomp);
-
-  //convert layout
-  mm.convertLayoutToSparse();
+  mm.addField("Cell Array",
+              FieldMapping::PER_CELL, &cell_arr[0], ncomp);
+  mm.addField("CellMat Array",
+              FieldMapping::PER_CELL_MAT, &cellmat_arr[0], ncomp);
 
   double sum = 0;
 
   //Different accessing methods ...
-  
+
   //get the volfrac field
   MultiMat::Field2D<double>& volfrac_map = mm.getVolfracField();
   MultiMat::Field2D<double>& volfrac_map2 = mm.get2dField<double>("Volfrac");
-  assert(&volfrac_map == &volfrac_map2);
-  
+  SLIC_ASSERT(&volfrac_map == &volfrac_map2);
+  //volfrac field is access the same way as a regular Field2d
+
   // --------- returning SLAM map and submap -----------
-  printf("\nAccess from SLAM map (and submap)\n");
+  SLIC_INFO("\n -- Access from SLAM map (and submap) -- ");
   sum = 0;
-  start_timer();
+  timer.reset();
+  timer.start();
   {
     MultiMat::Field1D<double>& map = mm.get1dField<double>("Cell Array");
-    assert(ncomp == map.stride());
-    assert(ncomp == map.numComp());
-    for (int i = 0; i < mm.getNumberOfCells(); i++) {
-      for (int comp = 0; comp < map.numComp(); comp++)
+    SLIC_ASSERT(ncomp == map.stride());
+    SLIC_ASSERT(ncomp == map.numComp());
+    for (int i = 0 ; i < mm.getNumberOfCells() ; i++)
+    {
+      for (int comp = 0 ; comp < map.numComp() ; comp++)
       {
-        double val = map(i, comp);                               //<----
-        assert(val == map[ i*map.numComp() + comp] );    //1d bracket access
+        double val = map(i, comp);                           //<----
+        SLIC_ASSERT(val == map[ i*map.numComp() + comp] );   //1d bracket access
         sum += val;
       }
     }
   }
-  cout << end_timer() << "\t";
-  assert(c_sum == sum);
+  timer.stop();
+  SLIC_INFO("  Field1D: " << timer.elapsed() << " sec");
+  SLIC_ASSERT(c_sum == sum);
 
   sum = 0;
-  start_timer();
+  timer.reset();
+  timer.start();
   {
     MultiMat::Field2D<double>& map2d = mm.get2dField<double>("CellMat Array");
-    assert(ncomp == map2d.stride());
-    assert(ncomp == map2d.numComp());
-    for (int i = 0; i < map2d.firstSetSize(); i++)
+    SLIC_ASSERT(ncomp == map2d.stride());
+    SLIC_ASSERT(ncomp == map2d.numComp());
+    for (int i = 0 ; i < map2d.firstSetSize() ; i++)
     {
       MultiMat::IdSet rel_set = map2d.indexSet(i);
       MultiMat::SubField<double> submap = map2d(i);
-      assert(rel_set.size() == submap.size());
-      for (int k = 0; k < submap.size(); k++)
+      SLIC_ASSERT(rel_set.size() == submap.size());
+      for (int k = 0 ; k < submap.size() ; k++)
       {
         int idx = submap.index(k);  //mat id
         int idx2 = rel_set[k]; //another way to get mat id
-        assert(idx == idx2); 
+        SLIC_ASSERT(idx == idx2);
 
-        for (int c = 0; c < submap.numComp(); ++c) {
+        for (int c = 0 ; c < submap.numComp() ; ++c)
+        {
           double val = submap.value(k, c);                 //<----------
-          assert( val == submap(k,c) );                    //operator () access
-          assert( val == submap[ k*submap.numComp()+c ] );  //1d bracket access
-          sum += val;         
+          SLIC_ASSERT( val == submap(k,c) );               //operator () access
+          SLIC_ASSERT( val == submap[ k*submap.numComp()+c ] ); //bracket access
+          sum += val;
         }
       }
     }
   }
-  cout << end_timer() << "\t";
-  assert(x_sum == sum);
-  
+  timer.stop();
+  SLIC_INFO("  Field2D: " << timer.elapsed() << " sec");
+  SLIC_ASSERT(x_sum == sum);
+
 
   // ------- Dense Access ----------
-  printf("\nDense Access via map\n-\t");
+  SLIC_INFO("\n -- Dense Access via map-- ");
   sum = 0;
-  start_timer();
+  timer.reset();
+  timer.start();
   {
     MultiMat::Field2D<double>& map = mm.get2dField<double>("CellMat Array");
-    
-    for (int i = 0; i < mm.getNumberOfCells(); i++) {
-      for (int m = 0; m < mm.getNumberOfMaterials(); m++) {
-        for (int c = 0; c < map.numComp(); ++c) 
+
+    for (int i = 0 ; i < mm.getNumberOfCells() ; i++)
+    {
+      for (int m = 0 ; m < mm.getNumberOfMaterials() ; m++)
+      {
+        for (int c = 0 ; c < map.numComp() ; ++c)
         {
-          double* valptr = map.findValue(i, m, c); //<---- contains a hidden for-loop for sparse layouts
-          if (valptr) 
-            sum += *valptr;           
+          double* valptr = map.findValue(i, m, c);
+          // ^ contains a hidden for-loop for sparse layouts, O(row_size) time
+          if (valptr)
+            sum += *valptr;
         }
       }
     }
   }
-  cout << end_timer() << "\t";
-  assert(x_sum == sum);
-   
+  timer.stop();
+  SLIC_INFO("  Field2D: " << timer.elapsed() << " sec");
+  SLIC_ASSERT(x_sum == sum);
+
 
   if (mm.getSparcityLayout() == SparcityLayout::SPARSE)
   {
     // ------------ return index set --------------
-    printf("\nAccess by Map with indexing set\n-\t");
+    SLIC_INFO("\n -- Access by Map with indexing set-- ");
     sum = 0;
-    start_timer();
+    timer.reset();
+    timer.start();
     {
-      //Note you can achieve the same thing by using a Map pointer to point to a bivariateMap object
+      //Note you can achieve the same thing by using
+      // a Map pointer to point to a bivariateMap object
       //MultiMat::Field1D<double>& map = mm.get1dField<double>("CellMat Array");
 
       MultiMat::Field2D<double>& map = mm.get2dField<double>("CellMat Array");
 
-      for (int i = 0; i < mm.getNumberOfCells(); i++)
+      for (int i = 0 ; i < mm.getNumberOfCells() ; i++)
       {
-        MultiMat::IdSet setOfMaterialsInThisCell = mm.getMatInCell(i); //the materials (by id) in this cell
-        MultiMat::IndexSet indexSet = mm.getIndexingSetOfCell(i); //the indices into the maps
-        assert(setOfMaterialsInThisCell.size() == indexSet.size());
-        for (int j = 0; j < indexSet.size(); j++)
+        //the materials (by id) in this cell
+        MultiMat::IdSet setOfMaterialsInThisCell = mm.getMatInCell(i);
+        //the indices into the maps
+        MultiMat::IndexSet indexSet = mm.getIndexingSetOfCell(i);
+
+        SLIC_ASSERT(setOfMaterialsInThisCell.size() == indexSet.size());
+        for (int j = 0 ; j < indexSet.size() ; j++)
         {
-          int mat_id = setOfMaterialsInThisCell.at(j);
-          for (int comp = 0; comp < map.numComp(); ++comp) {
+          //int idx = setOfMaterialsInThisCell.at(j); //mat_id
+          for (int comp = 0 ; comp < map.numComp() ; ++comp)
+          {
             double val = map[indexSet[j]*map.numComp() + comp];   //<-----
-            //assert(val == map(indexSet[j], comp)); //if 1dMap is used, this is also valid
+
+            //if 1dMap is used, this is also valid
+            //SLIC_ASSERT(val == map(indexSet[j], comp));
+
             sum += val;
           }
         }
       }
     }
-    cout << end_timer() << "\t";
-    assert(x_sum == sum);
+    timer.stop();
+    SLIC_INFO("  Field2D: " << timer.elapsed() << " sec");
+    SLIC_ASSERT(x_sum == sum);
   }
 
 
   // ---------- using iterator with Map and Submap -------------
-  printf("\nWith Map (and Submap) iterators\n");
+  SLIC_INFO("\n -- With Map (and Submap) iterators -- ");
   sum = 0;
-  start_timer();
+  timer.reset();
+  timer.start();
   {
     MultiMat::Field1D<double>& map = mm.get1dField<double>("Cell Array");
-    for (MultiMat::Field1D<double>::iterator iter = map.begin(); iter != map.end(); iter++)
+    for (MultiMat::Field1D<double>::iterator iter = map.begin() ;
+         iter != map.end() ; iter++)
     {
-      for(int comp=0; comp<iter.numComp(); ++comp)
+      for(int comp=0 ; comp<iter.numComp() ; ++comp)
         sum += iter(comp);              //<----
     }
   }
-  cout << end_timer() << "\t";
-  assert(c_sum == sum);
-
+  timer.stop();
+  SLIC_INFO("  Field1D: " << timer.elapsed() << " sec");
+  SLIC_ASSERT(c_sum == sum);
 
   sum = 0;
-  start_timer();
+  timer.reset();
+  timer.start();
   {
     MultiMat::Field2D<double>& map2d = mm.get2dField<double>("CellMat Array");
-    for (int i = 0; i < map2d.firstSetSize(); i++) 
+    for (int i = 0 ; i < map2d.firstSetSize() ; i++)
     {
       MultiMat::SubField<double> submap = map2d(i);
-      for (auto iter = submap.begin(); iter != submap.end(); iter++) 
+      for (auto iter = submap.begin() ; iter != submap.end() ; iter++)
       {
-        for (int comp = 0; comp < map2d.numComp(); ++comp)
+        //int idx = iter.index();  //get the index
+        for (int comp = 0 ; comp < map2d.numComp() ; ++comp)
         {
           sum += iter(comp);                  //<----
-          assert(iter(comp) == iter.value(comp));  //another way to get the value
-          int idx = iter.index();
+          SLIC_ASSERT(iter(comp) == iter.value(comp)); //value()
         }
-        //assert(*iter, iter.value());   //2 ways to get the first component value
+        SLIC_ASSERT(*iter == iter.value());  //2 ways to get the first component
       }
     }
   }
-  cout << end_timer() << "\t";
-  assert(x_sum == sum);
+  timer.stop();
+  SLIC_INFO("  Field2D: " << timer.elapsed() << " sec");
+
+  SLIC_ASSERT(x_sum == sum);
 
 
   // ---------- iterator for BivariateMap ------------
-  printf("\nWith Map iterators - begin(i) and end(i)\n-\t");
+  SLIC_INFO("\n -- With Map iterators - begin(i) and end(i) -- ");
   sum = 0;
-  start_timer();
+  timer.reset();
+  timer.start();
   {
     MultiMat::Field2D<double>& map2d = mm.get2dField<double>("CellMat Array");
-    for (int i = 0; i < mm.getNumberOfCells();/* map2d.firstSetSize(); */i++)
+    for (int i = 0 ; i < mm.getNumberOfCells() /*map2d.firstSetSize()*/ ; i++)
     {
-      for (MultiMat::SubField<double>::SubMapIterator iter = map2d.begin(i); iter != map2d.end(i); iter++)
+      for (MultiMat::SubField<double>::SubMapIterator iter = map2d.begin(i) ;
+           iter != map2d.end(i) ; iter++)
       {
-        for (int comp = 0; comp < map2d.numComp(); ++comp)
+        // int idx = iter.index(); get the index
+        for (int comp = 0 ; comp < map2d.numComp() ; ++comp)
         {
           sum += iter(comp);          //<----
-          assert(iter(comp) == iter.value(comp));  //another way to get the value
-          int idx = iter.index();
+          SLIC_ASSERT(iter(comp) == iter.value(comp));  //value()
         }
-        assert(iter(0) == *iter); //2 ways to get the first component value
-        assert(iter(0) == iter.value()); 
+        SLIC_ASSERT(iter(0) == *iter); //2 ways to get the first component
+        SLIC_ASSERT(iter(0) == iter.value());
       }
     }
   }
-  cout << end_timer() << "\t";
-  assert(x_sum == sum);
+  timer.stop();
+  SLIC_INFO("  Field2D: " << timer.elapsed() << " sec");
+  SLIC_ASSERT(x_sum == sum);
 
 
 
-  // ---------- range-based for-loop, only works if there is 1 component ------------
+  // -------- range-based for-loop, only works if there is 1 component ---------
   if( ncomp == 1 )
   {
-    printf("\nWith range-based for-loop of Map \n-\t");
+    SLIC_INFO("\n -- With range-based for-loop of Map -- ");
     sum = 0;
-    start_timer();
-    
+    timer.reset();
+    timer.start();
+
     MultiMat::Field2D<double>& map2d = mm.get2dField<double>("CellMat Array");
-    for (int i = 0; i < mm.getNumberOfCells(); i++)
+    for (int i = 0 ; i < mm.getNumberOfCells() ; i++)
     {
       MultiMat::SubField<double> submap = map2d(i);
       for (double val : submap)
@@ -296,51 +355,94 @@ void various_traversal_methods(int nmats, int ncells, int ncomp, bool use_sparse
         sum += val;          //<----
       }
     }
-    
-    cout << end_timer() << "\t";
-    assert(x_sum == sum);
+
+    timer.stop();
+    SLIC_INFO("  Field2D: " << timer.elapsed() << " sec");
+    SLIC_ASSERT(x_sum == sum);
   }
 
 
   // ---------- flat iterator ------------
-  printf("\nWith BivariateMap flat iterators \n-\t");
+  SLIC_INFO("\n -- With BivariateMap flat iterators -- ");
   sum = 0;
-  start_timer();
+  timer.reset();
+  timer.start();
   {
     MultiMat::Field2D<double>& map2d = mm.get2dField<double>("CellMat Array");
-    for (auto iter = map2d.begin(); iter != map2d.end(); ++iter)
+    for (auto iter = map2d.begin() ; iter != map2d.end() ; ++iter)
     {
-      int cell_id = iter.firstIndex();
-      int mat_id = iter.secondIndex();
-      for (int comp = 0; comp < map2d.numComp(); ++comp)
+      //get the indices
+      //int cell_id = iter.firstIndex();
+      //int mat_id = iter.secondIndex();
+      for (int comp = 0 ; comp < map2d.numComp() ; ++comp)
       {
         double val = iter(comp);          //<----
-        assert(val == iter.value(comp));  //another way to get the value
+        SLIC_ASSERT(val == iter.value(comp));  //another way to get the value
         sum += val;
       }
-      assert(iter(0) == *iter); //2 ways to get the first component value
-      assert(iter(0) == iter.value());
-      
+      SLIC_ASSERT(iter(0) == *iter); //2 ways to get the first component value
+      SLIC_ASSERT(iter(0) == iter.value());
+
     }
   }
-  cout << end_timer() << "\t";
-  assert(x_sum == sum);
+  timer.stop();
+  SLIC_INFO("  Field2D: " << timer.elapsed() << " sec");
+  SLIC_ASSERT(x_sum == sum);
 
-  cout << endl;
+  SLIC_INFO("\n");
 }
 
+void usage()
+{
+  SLIC_WARNING("Usage: ./multimat_traversal_ex "
+    << "<num_cells> <num_mats> <num_comp> "
+    << "<bool use_sparse> <fill percentage>");
+  SLIC_WARNING("  example: ./multimat_traversal_ex 10000 50 3 1 0.2");
+}
 
 int main(int argc, char** argv)
 {
+  axom::slic::UnitTestLogger logger;
+
   //default options
-  bool use_sparse = true;
+  int ncells = 10000;
   int nmats = 50;
-  int ncells = 2000;
-  int ncomp = 2;
+  int ncomp = 3;
+  bool use_sparse = true;
+  double fill_percentage = .2;
 
+  if (argc != 1 && argc != 6)
+  {
+    usage();
+    return 1;
+  }
 
-  various_traversal_methods(nmats, ncells, ncomp, use_sparse);
+  if (argc == 6)
+  {
+    ncells = stoi(argv[1]);
+    nmats = stoi(argv[2]);
+    ncomp = stoi(argv[3]);
+    int sparse = stoi(argv[4]);
+    if (sparse == 0)
+      use_sparse = false;
+    else if (sparse == 1)
+      use_sparse = true;
+    else
+    {
+      usage();
+      return 1;
+    }
+    fill_percentage = stod(argv[5]);
+  }
 
-  cout << "\nPress Enter to terminate...";
-  cin.ignore();
+  SLIC_INFO("Using arguments:");
+  SLIC_INFO(" number of cells:      " << ncells);
+  SLIC_INFO(" number of materials:  " << nmats);
+  SLIC_INFO(" number of components: " << ncomp);
+  SLIC_INFO(" use sparse:           " << (use_sparse ? "yes" : "no"));
+  SLIC_INFO(" fill percentage:      " << fill_percentage);
+
+  various_traversal_methods(nmats, ncells, ncomp, use_sparse, fill_percentage);
+
+  return 0;
 }
