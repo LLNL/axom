f9d894ef5a6e528497e1f6922c6b2c070f598d41
Stephanie Dempsey <dempsey11@llnl.gov>
2018-10-18 11:27:03 -0700

    script for installing on mac

---
+++ scripts/llnl_scripts/build-and-test-mac.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ev 
+
+WORKSPACE=`pwd`/../..
+cd $WORKSPACE
+
+echo "~~~~~~~~~~~~ Building tpls ~~~~~~~~~~~"
+cd ./scripts/uberenv
+/usr/bin/python ./uberenv.py --spec="^conduit@master" --prefix="uberenv_libs" # uberenv_libs is the default dir name
+
+echo "~~~~~~~~~~~~ Building axom ~~~~~~~~~~~"
+./config-build.py -hc ./scripts/uberenv/uberenv_libs/*.llnl.gov-darwin-x86_64-clang@9.0.0.cmake -bp ./build
+cd build
+make
+
+echo "~~~~~~~~~~~~ Testing axom ~~~~~~~~~~~"
+make test
+
