496f170f31bd7affaad92110ac33610ffb3ea9d6
Stephanie Dempsey <dempsey11@llnl.gov>
2018-10-18 13:06:47 -0700

    fixed an error to move back to the top level for config-build.

--- scripts/llnl_scripts/build-and-test-mac.sh
+++ scripts/llnl_scripts/build-and-test-mac.sh
@@ -10,6 +10,7 @@ cd ./scripts/uberenv
 /usr/bin/python ./uberenv.py --spec="^conduit@master" --prefix="uberenv_libs" # uberenv_libs is the default dir name
 
 echo "~~~~~~~~~~~~ Building axom ~~~~~~~~~~~"
+cd ../..
 ./config-build.py -hc ./scripts/uberenv/uberenv_libs/*.llnl.gov-darwin-x86_64-clang@9.0.0.cmake -bp ./build
 cd build
 make
