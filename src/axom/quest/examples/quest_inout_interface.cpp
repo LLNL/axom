bd795b132dd0deb0ef7c057bbdace6a410fed839
Kenneth Weiss <weiss27@llnl.gov>
2018-12-12 17:46:28 -0800

    Updated quest_inout_interface example to use new quest inout API
    Note: Removed calls to quest_signed_distance API since this has its own example.

--- src/axom/quest/examples/quest_inout_interface.cpp
+++ src/axom/quest/examples/quest_inout_interface.cpp
@@ -18,274 +18,245 @@
 /*!
  * \file quest_inout_interface.cpp
  *
- * \brief Simple example that exercises the quest_inout interface which 
- * determine if a point is contained within an enclosed volume defined 
- * by a surface mesh.
+ * \brief Simple example that exercises the C-style quest_inout interface
+ * which determines if a point is contained within an enclosed volume
+ * defined by a surface mesh.
  */
 
 // Axom includes
 #include "axom/core.hpp"
-#include "axom/quest.hpp"
 #include "axom/slic.hpp"
+#include "axom/primal.hpp"
+
+#include "axom/quest/interface/inout_query.hpp"
 
 #ifdef AXOM_USE_MPI
   #include <mpi.h>
-#endif  // AXOM_USE_MPI
-
+#endif
 
 // C/C++ includes
-#include <cmath>     // for signbit()
-#include <cstdlib>   // for std::rand(), RAND_MAX
-#include <ctime>     // for time
-#include <iostream>  // for std::cout
+#include <vector>
 
+namespace slic = axom::slic;
+namespace primal = axom::primal;
+namespace quest = axom::quest;
 
-using namespace axom;
-
-typedef std::vector<double> CoordsVec;
+typedef primal::Point<double, 3> PointType;
+typedef primal::BoundingBox<double, 3> BoxType;
+typedef std::vector<PointType> CoordsVec;
 
 //------------------------------------------------------------------------------
-template < typename T >
-T getRandomDouble( T low, T high )
-{
-  const T delta = high-low;
-  const T c = static_cast< T >( std::rand() ) / static_cast< T >( RAND_MAX );
-  return ( delta*c+low );
-}
-
-template < typename T >
-T scaleAndOffset( T rangeMin, T rangeMax, T val)
-{
-  return rangeMin + (rangeMax - rangeMin) * val;
-}
-
-
-void outputMeshStats()
-{
-  // Obtain and log the mesh bounding box
-  double bbMin[3], bbMax[3];
-  quest::mesh_min_bounds(bbMin);
-  quest::mesh_max_bounds(bbMax);
-  SLIC_INFO(
-    "Mesh bounding box: "
-    << "{ lower: (" << bbMin[0] <<"," << bbMin[1] <<","<< bbMin[2] << ")"
-    << "; upper: (" << bbMax[0] <<"," << bbMax[1] <<","<< bbMax[2] << ")}" );
-
-  // Obtain and log the mesh center of mass
-  double cm[3];
-  quest::mesh_center_of_mass(cm);
-  SLIC_INFO(
-    "Mesh center of mass: "
-    << "(" << cm[0] <<"," << cm[1] <<","<< cm[2] << ")"    );
-}
-
-void runQuestDistance(const std::string& fileName, const CoordsVec& points)
-{
-  constexpr int DIMENSION     = 3;
-  constexpr int MAX_LEVELS    = 20;
-  constexpr int MAX_OCCUPANCY = 25;
-
-  // set signed distance parameters
-  quest::signed_distance_set_dimension( DIMENSION );
-  quest::signed_distance_set_max_levels( MAX_LEVELS );
-  quest::signed_distance_set_max_occupancy( MAX_OCCUPANCY );
-
-  // initialize the signed distance query
-#ifdef AXOM_USE_MPI
-  quest::signed_distance_init( fileName, MPI_COMM_WORLD );
-#else
-  quest::signed_distance_init( fileName );
-#endif
-
-  CoordsVec coords[3];
-  {
-    int nOrigPts = points.size()/3;
-
-    double bbMin[3], bbMax[3];
-    quest::signed_distance_get_mesh_bounds( bbMin, bbMax );
-
-    // Reserve space and add the mesh BB center
-    for(int i=0 ; i< 3 ; ++i)
-    {
-      coords[i].reserve(nOrigPts+2);
-      coords[i].push_back( scaleAndOffset(bbMin[i], bbMax[i], 0.5));
-      coords[i].push_back( scaleAndOffset(bbMin[i], bbMax[i], 0.5));
-    }
-
-    // Scale and add the random points from input parameter
-    for(int j=0 ; j< nOrigPts ; ++j)
-    {
-      for(int i=0 ; i< 3 ; ++i)
-      {
-        coords[i].push_back(
-          scaleAndOffset(bbMin[i], bbMax[i], points[j*3 + i]));
-      }
-    }
-  }
-
-
-  int nPoints = coords[0].size();
-  for(int i=0 ; i< nPoints ; ++i)
-  {
-    // scale points within bounding box of mesh
-    const double x = coords[0][i];
-    const double y = coords[1][i];
-    const double z = coords[2][i];
-
-    const double phi = quest::signed_distance_evaluate( x,y,z );
-    const int ins    = ( std::signbit( phi ) != 0) ? -1 : 1;
-
-    SLIC_INFO(
-      "Point (" << x << ", " << y << ", " << z << ") "
-                << "is " << (ins ? "inside" : "outside") << " surface."
-                <<" Distance is " << phi << ".");
-  }
-
-  quest::signed_distance_finalize();
-}
-
-void runQuestContainment(const std::string& fileName, const CoordsVec& points)
-{
-  const bool useDistance = false;
-  const int unusedVar = -1;
-
-  #ifdef AXOM_USE_MPI
-  quest::initialize( MPI_COMM_WORLD, fileName, useDistance, 3, unusedVar,
-                     unusedVar);
-  #else
-  quest::initialize( fileName, useDistance, 3, unusedVar, unusedVar );
-  #endif
-
-  outputMeshStats();
-
-  CoordsVec coords[3];
-  {
-    int nOrigPts = points.size()/3;
-
-    double bbMin[3], bbMax[3], cMass[3];
-    quest::mesh_min_bounds(bbMin);
-    quest::mesh_max_bounds(bbMax);
-    quest::mesh_center_of_mass(cMass);
-
-    // Reserve space and add the mesh BB center and center of mass
-    for(int i=0 ; i< 3 ; ++i)
-    {
-      coords[i].reserve(nOrigPts+2);
-      coords[i].push_back( scaleAndOffset(bbMin[i], bbMax[i], 0.5));
-      coords[i].push_back( scaleAndOffset(cMass[i], cMass[i], 0.5));
-    }
-
-    // Scale and add the random points from input parameter
-    for(int j=0 ; j< nOrigPts ; ++j)
-    {
-      for(int i=0 ; i< 3 ; ++i)
-      {
-        coords[i].push_back(
-          scaleAndOffset(bbMin[i], bbMax[i], points[j*3 + i]));
-      }
-    }
-  }
-
-
-  int nPoints = coords[0].size();
-  for(int i=0 ; i< nPoints ; ++i)
-  {
-    // scale points within bounding box of mesh
-    const double x = coords[0][i];
-    const double y = coords[1][i];
-    const double z = coords[2][i];
-    const int ins = quest::inside( x,y,z);
-
-    SLIC_INFO(
-      "Point (" << x << ", " << y << ", " << z << ") "
-                << "is " << (ins ? "inside" : "outside") << " surface." );
-  }
-
-  quest::finalize();
-}
-
 
 /*!
- * \brief A simple example illustrating the use of the Quest C-Style interface.
- * \param [in] argc argument counter.
- * \param [in] argv argument vector.
- * \return rc return code.
+ * \brief Utility function to generate a random set of query points
+ * within a given bounding box
  *
- * \note To run the example
- * \verbatim
- *
- *   [mpirun -np N] ./quest_interface_ex <stl_file>
- *
- * \endverbatim
+ * \param [out] queryPoints The generated set of query points
+ * \param [in] bbox The bounding box for the query points
+ * \param [in] numPoints The number of points to generate
  */
-int main( int argc, char** argv )
+void generateQueryPoints(CoordsVec& queryPoints, BoxType const& bbox,
+                         int numPoints)
 {
-#ifdef AXOM_USE_MPI
-  // Initialize MPI
-  MPI_Init( &argc, &argv );
-#endif
+  queryPoints.clear();
+  queryPoints.reserve(numPoints);
+  for(int i=0 ; i< numPoints ; ++i)
+  {
+    double x = axom::utilities::random_real(bbox.getMin()[0], bbox.getMax()[0]);
+    double y = axom::utilities::random_real(bbox.getMin()[1], bbox.getMax()[1]);
+    double z = axom::utilities::random_real(bbox.getMin()[2], bbox.getMax()[2]);
+    queryPoints.push_back( PointType::make_point(x,y,z));
+  }
+}
 
+/*!
+ * \brief Utility function to initialize the logger
+ */
+void initializeLogger()
+{
   // Initialize Logger
-  axom::slic::initialize();
-  axom::slic::setLoggingMsgLevel( axom::slic::message::Info );
+  slic::initialize();
+  slic::setLoggingMsgLevel( axom::slic::message::Info );
 
-  axom::slic::LogStream* logStream;
+  slic::LogStream* logStream;
 
 #ifdef AXOM_USE_MPI
   std::string fmt = "[<RANK>][<LEVEL>]: <MESSAGE>\n";
   #ifdef AXOM_USE_LUMBERJACK
   const int RLIMIT = 8;
   logStream =
-    new axom::slic::LumberjackStream(&std::cout,MPI_COMM_WORLD, RLIMIT, fmt);
+    new slic::LumberjackStream(&std::cout,MPI_COMM_WORLD, RLIMIT, fmt);
   #else
   logStream =
-    new axom::slic::SynchronizedStream(&std::cout,MPI_COMM_WORLD, fmt);
+    new slic::SynchronizedStream(&std::cout,MPI_COMM_WORLD, fmt);
   #endif
 #else
   std::string fmt = "[<LEVEL>]: <MESSAGE>\n";
-  logStream = new axom::slic::GenericOutputStream(&std::cout, fmt);
+  logStream = new slic::GenericOutputStream(&std::cout, fmt);
 #endif // AXOM_USE_MPI
 
-  axom::slic::addStreamToAllMsgLevels( logStream );
+  slic::addStreamToAllMsgLevels( logStream );
+}
 
+
+/*!
+ * \brief Utility function to finalize the logger
+ */
+void finalizeLogger()
+{
+  if(slic::isInitialized())
+  {
+    slic::flushStreams();
+    slic::finalize();
+  }
+}
+
+/*!
+ * \brief Utility function for a clean abort that finalizes
+ * quest_inout, slic and mpi
+ */
+void cleanAbort()
+{
+  if(quest::inout_initialized())
+  {
+    quest::inout_finalize();
+  }
+
+  finalizeLogger();
+
+  axom::utilities::processAbort();
+}
+
+/*!
+ * \brief A simple example illustrating the use of the Quest C-Style interface.
+ *
+ * \note To run the example
+ * \verbatim
+ *
+ *   [mpirun -np N] ./quest_inout_interface_ex <stl_file>
+ *
+ * \endverbatim
+ */
+int main( int argc, char** argv )
+{
+  // -- Initialize MPI
+#ifdef AXOM_USE_MPI
+  MPI_Init( &argc, &argv );
+#endif
+
+  // -- Initialize logger
+  initializeLogger();
+
+  // -- Parse command line options
   if(argc != 2)
   {
   #ifdef AXOM_USE_MPI
-    SLIC_WARNING("Usage: [mpirun -np N] ./quest_interface_ex <stl_file>");
+    SLIC_WARNING("Usage: [mpirun -np N] ./quest_inout_interface_ex <stl_file>");
   #else
-    SLIC_WARNING("Usage: ./quest_interface_ex <stl_file>");
+    SLIC_WARNING("Usage: ./quest_inout_interface_ex <stl_file>");
   #endif
-    axom::slic::finalize();
-    axom::utilities::processAbort();
+    cleanAbort();
   }
 
-  // Generate the query points
   std::string fileName = std::string(argv[1]);
+  bool isVerbose = false;
+  const int npoints = 100000;
 
-  std::srand( time(NULL) );
-  const int npoints = 10;
-  const double lb = 0.;
-  const double ub = 1.;
-  CoordsVec ptVec;
-  ptVec.reserve(3*npoints);
-  for ( int ipnt=0 ; ipnt < npoints ; ++ipnt )
+  int rc = quest::QUEST_INOUT_SUCCESS;
+
+  // -- Set quest_inout options
+  quest::inout_set_verbose(isVerbose);
+  if(rc != quest::QUEST_INOUT_SUCCESS)
   {
-    ptVec.push_back( getRandomDouble(lb,ub) );
-    ptVec.push_back( getRandomDouble(lb,ub) );
-    ptVec.push_back( getRandomDouble(lb,ub) );
+    cleanAbort();
   }
 
-  SLIC_INFO("**Running distance queries using quest interface...");
-  runQuestDistance(fileName, ptVec);
-  SLIC_INFO("--");
+  // -- Initialize quest_inout
+  axom::utilities::Timer timer;
+  SLIC_INFO("Initializing quest_inout...");
+  timer.start();
+  {
+    #ifdef AXOM_USE_MPI
+    rc = quest::inout_init( fileName, MPI_COMM_WORLD);
+    #else
+    rc = quest::inout_init( fileName);
+    #endif
+  }
+  timer.stop();
+  if(rc != quest::QUEST_INOUT_SUCCESS)
+  {
+    cleanAbort();
+  }
+  else
+  {
+    SLIC_INFO("  initialization took " << timer.elapsed() << " seconds.");
+  }
 
-  SLIC_INFO("**Running containment queries using quest interface...");
-  runQuestContainment(fileName, ptVec);
-  SLIC_INFO("--");
+
+  // -- Query mesh bounding box and center of mass
+  double bbMin[3], bbMax[3], cMass[3];
+  rc = quest::inout_mesh_min_bounds(bbMin);
+  if(rc != quest::QUEST_INOUT_SUCCESS)
+  {
+    cleanAbort();
+  }
+
+  rc = quest::inout_mesh_max_bounds(bbMax);
+  if(rc != quest::QUEST_INOUT_SUCCESS)
+  {
+    cleanAbort();
+  }
+
+  rc = quest::inout_mesh_center_of_mass(cMass);
+  if(rc != quest::QUEST_INOUT_SUCCESS)
+  {
+    cleanAbort();
+  }
+
+  BoxType bbox = BoxType(PointType(bbMin), PointType(bbMax));
+  SLIC_INFO("Mesh bounding box: " << bbox );
+  SLIC_INFO("Mesh center of mass: " << PointType(cMass) );
 
 
-  axom::slic::finalize();
+  // -- Generate query points
+  CoordsVec queryPoints;
+  generateQueryPoints(queryPoints, bbox, npoints);
 
+  // -- Run the queries
+  int numInside=0;
+  SLIC_INFO("Querying mesh with " << npoints << " query points...");
+  timer.start();
+  for(auto& pt : queryPoints)
+  {
+    const double x = pt[0];
+    const double y = pt[1];
+    const double z = pt[2];
+
+    const bool ins = quest::inout_inside(x,y,z);
+    numInside += ins ? 1 : 0;
+  }
+  timer.stop();
+
+  // -- Output some query statistics
+  {
+    SLIC_INFO("  queries took " << timer.elapsed() << " seconds.");
+
+    const double rate =  queryPoints.size() / timer.elapsed();
+    SLIC_INFO("  query rate: " <<  rate  << " queries per second.");
+
+    const double intPercent = (100 * numInside) / queryPoints.size();
+    SLIC_INFO("  " << numInside << " of " << queryPoints.size()
+                   << " (" << intPercent << "%) "
+                   << " of the query points were contained in the surface.");
+  }
+
+  // -- Finalize quest_inout
+  quest::inout_finalize();
+
+  // -- Finalize logger
+  finalizeLogger();
+
+  // -- Finalize MPI
 #ifdef AXOM_USE_MPI
   MPI_Finalize();
 #endif
