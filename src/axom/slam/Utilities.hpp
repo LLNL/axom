3965a3a4c9e70fe55624128e7ed1e541bf079ae1
Kenneth Weiss <weiss27@llnl.gov>
2018-12-27 13:44:52 -0800

    Removes slam's 'findFileInAncestorDirs()' utility function
    This was an awkward function and is no longer needed now that axom's
    config file has a path to the data directory (AXOM_DATA_DIR).

--- src/axom/slam/Utilities.hpp
+++ src/axom/slam/Utilities.hpp
@@ -48,19 +48,6 @@ struct TypeToString<double>
   static std::string    to_string(){return "double"; }
 };
 
-
-/**
- * \brief Simple utility function to search through ancestor directories to find
- *  a file
-
- * \param [in] fileName The path to the original file.
- * \param [in] numAncestors The maximum number of ancestor directories to try
- * \return A string representing a valid path to the file when this can be
- *  found, else, will return the original fileName.
- */
-std::string
-findFileInAncestorDirs(const std::string& fileName, int numAncestors = 4);
-
 } // end namespace util
 } // end namespace slam
 } // end namespace axom
