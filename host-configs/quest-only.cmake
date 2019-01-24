68db30f5c460e0bf3fe52a6cf1301851066901e3
Lee Taylor <ltaylor@llnl.gov>
2019-01-24 11:57:11 -0800

    Add copyright notice

--- host-configs/quest-only.cmake
+++ host-configs/quest-only.cmake
@@ -1,3 +1,18 @@
+##################################
+#
+# Copyright (c) 2019, Lawrence Livermore National Security, LLC.
+#
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# LLNL-CODE-741217
+#
+# All rights reserved.
+#
+# This file is part of Axom.
+#
+# For details about use and distribution, please read axom/LICENSE.
+#
+##################################
 ##############################################################################
 # Minimal host-config (cmake cache file) to build quest
 #
