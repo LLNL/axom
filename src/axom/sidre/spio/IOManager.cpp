681c3b7844f636d09a2e49cd8592ffeac62d5756
Noah S. Elliott <elliott22@llnl.gov>
2019-03-04 17:08:14 -0800

    Add error if trying to read on a reduced number of processors.

--- src/axom/sidre/spio/IOManager.cpp
+++ src/axom/sidre/spio/IOManager.cpp
@@ -723,6 +723,8 @@ void IOManager::readSidreHDF5(sidre::Group* datagroup,
   int num_files = getNumFilesFromRoot(root_file);
   int num_groups = getNumGroupsFromRoot(root_file);
   SLIC_ASSERT(num_files > 0);
+  SLIC_ERROR_IF(num_groups > m_comm_size,
+                "IOManager cannot read from files written by more ranks than are being used in the current run.");
 
   if (m_baton)
   {
