b60b2b1d50b582456a74469116b6d4ef594fbba2
Rich Hornung <hornung1@llnl.gov>
2019-03-18 13:06:21 -0700

    Added a script to make a gzipped release tarball.
    When you make a release on Github, the tarfiles it generates don't properly
    include Git submodules. This script generates a gzipped tarfile named for
    the release that can be linked to the Github release for users to download.
    
    It strips out all the git stuff using 'git archive'.
    
    Here's the recipe to use the script:
    
    1) Edit the script and set the 'VERSION' variable to the release number. This
       is an extra step, but it's easy and makes the script useful for other things
       such as generating a complete tarfile of Axom from any commit and give it
       to someone.
    2) In the top-level Axom directory, type: ./scripts/make_release_tarball.sh
    3) The generated gzipped tarfile in the top-level Axom directory is good to go.
       Use it like you would a clone of the RAJA repo.

---
+++ scripts/make_release_tarball.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+###############################################################################
+# Copyright (c) 2017-19, Lawrence Livermore National Security, LLC.
+#
+# Produced at the Lawrence Livermore National Laboratory
+#
+# LLNL-CODE-741217
+#       
+# All rights reserved.
+#   
+# This file is part of Axom.
+#   
+# For details about use and distribution, please read axom/LICENSE.
+#   
+###############################################################################
+
+TAR_CMD=gtar
+VERSION=0.2.9
+
+git archive --prefix=Axom-${VERSION}/ -o Axom-${VERSION}.tar HEAD 2> /dev/null
+
+echo "Running git archive submodules..."
+
+p=`pwd` && (echo .; git submodule foreach) | while read entering path; do
+    temp="${path%\'}";
+    temp="${temp#\'}";
+    path=$temp;
+    [ "$path" = "" ] && continue;
+    (cd $path && git archive --prefix=Axom-${VERSION}/$path/ HEAD > $p/tmp.tar && ${TAR_CMD} --concatenate --file=$p/Axom-${VERSION}.tar $p/tmp.tar && rm $p/tmp.tar);
+done
+
+gzip Axom-${VERSION}.tar
