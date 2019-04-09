fd5a33178076c92db45693d6341a8d42972f0279
Chris White <white238@llnl.gov>
2019-04-09 16:42:33 -0700

    Fix build issues due to overzealous search/replace

--- src/axom/sidre/spio/interface/c_fortran/wrapfspio.f
+++ src/axom/sidre/spio/interface/c_fortran/wrapfspio.f
@@ -1,9 +1,9 @@
 ! wrapfspio.f
 ! This is generated code, do not edit
-# Copyright (c) 2017-2019, Lawrence Livermore National Security, LLC and
-# other Axom Project Developers. See the top-level COPYRIGHT file for details.
-#
-# SPDX-License-Identifier: (BSD-3-Clause)
+! Copyright (c) 2017-2019, Lawrence Livermore National Security, LLC and
+! other Axom Project Developers. See the top-level COPYRIGHT file for details.
+!
+! SPDX-License-Identifier: (BSD-3-Clause)
 !>
 !! \file wrapfspio.f
 !! \brief Shroud generated wrapper for SPIO library
--- src/axom/slic/interface/c_fortran/wrapfslic.f
+++ src/axom/slic/interface/c_fortran/wrapfslic.f
@@ -1,9 +1,9 @@
 ! wrapfslic.f
 ! This is generated code, do not edit
-# Copyright (c) 2017-2019, Lawrence Livermore National Security, LLC and
-# other Axom Project Developers. See the top-level COPYRIGHT file for details.
-#
-# SPDX-License-Identifier: (BSD-3-Clause)
+! Copyright (c) 2017-2019, Lawrence Livermore National Security, LLC and
+! other Axom Project Developers. See the top-level COPYRIGHT file for details.
+!
+! SPDX-License-Identifier: (BSD-3-Clause)
 !>
 !! \file wrapfslic.f
 !! \brief Shroud generated wrapper for SLIC library
