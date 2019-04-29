ed5d2dda335dff7e0d0fdc15f9179908b676e10b
Rich Hornung <hornung1@llnl.gov>
2019-04-29 15:38:48 -0700

    First attempt to get basic g++ linux build working.

---
+++ scripts/azure-pipelines/linux-build_and_test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+##############################################################################
+# Copyright (c) 2017-2019, Lawrence Livermore National Security, LLC and
+# other Axom Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (BSD-3-Clause)
+##############################################################################
+
+env
+function or_die () {
+    "$@"
+    local status=$?
+    if [[ $status != 0 ]] ; then
+        echo ERROR $status command: $@
+        exit $status
+    fi
+}
+
+or_die mkdir azure-linux-build
+cd azure-linux-build
+if [[ "$DO_BUILD" == "yes" ]] ; then
+    or_die cmake -DCMAKE_CXX_COMPILER="${COMPILER}" ${CMAKE_EXTRA_FLAGS} ../
+    if [[ ${CMAKE_EXTRA_FLAGS} == *COVERAGE* ]] ; then
+      or_die make -j 3
+    else
+      or_die make -j 3 VERBOSE=1
+    fi
+    if [[ "${DO_TEST}" == "yes" ]] ; then
+      or_die ctest -T test --output-on-failure -V
+    fi
+    if [[ "${DO_MEMCHECK}" == "yes" ]] ; then
+      or_die ctest -T memcheck
+    fi
+fi
+
+exit 0
