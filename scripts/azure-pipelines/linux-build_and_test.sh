b94cff680680bfd2f6ce9c49734611322836b9a7
Rich Hornung <hornung1@llnl.gov>
2019-04-30 15:07:34 -0700

    Added azure builds and tests for basic linux configuration.
    Three versions of gcc, two versions of clang.

---
+++ scripts/azure-pipelines/linux-build_and_test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+##############################################################################
+# Copyright (c) 2017-2019, Lawrence Livermore National Security, LLC and
+# other Axom Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (BSD-3-Clause)
+##############################################################################
+
+env
+function or_die () {
+    "$@"
+    local status=$?
+    if [[ $status != 0 ]] ; then
+        echo ERROR $status command: $@
+        exit $status
+    fi
+}
+
+or_die mkdir azure-${Test_Target}-build
+cd azure-${Test_Target}-build
+if [[ "$DO_BUILD" == "yes" ]] ; then
+    or_die cmake -DCMAKE_CXX_COMPILER="${COMPILER}" ${CMAKE_EXTRA_FLAGS} ../src
+    if [[ ${CMAKE_EXTRA_FLAGS} == *COVERAGE* ]] ; then
+      or_die make -j 3
+    else
+      or_die make -j 3 VERBOSE=1
+    fi
+    if [[ "${DO_TEST}" == "yes" ]] ; then
+      or_die ctest -T test --output-on-failure -V
+    fi
+    if [[ "${DO_MEMCHECK}" == "yes" ]] ; then
+      or_die ctest -T memcheck
+    fi
+fi
+
+exit 0
