e93fd4592a77412061ddb9901462abe174655ca5
George Zagaris <zagaris2@llnl.gov>
2019-05-13 10:37:46 -0700

    DOC: update copyright

--- src/axom/primal/spatial_acceleration/linear_bvh/linear_bvh_builder.hpp
+++ src/axom/primal/spatial_acceleration/linear_bvh/linear_bvh_builder.hpp
@@ -1,19 +1,8 @@
-/*
- *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * Copyright (c) 2017-2019, Lawrence Livermore National Security, LLC.
- *
- * Produced at the Lawrence Livermore National Laboratory
- *
- * LLNL-CODE-741217
- *
- * All rights reserved.
- *
- * This file is part of Axom.
- *
- * For details about use and distribution, please read axom/LICENSE.
- *
- *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- */
+// Copyright (c) 2017-2019, Lawrence Livermore National Security, LLC and
+// other Axom Project Developers. See the top-level COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
 
 #ifndef AXOM_PRIMAL_BVH_LINEAR_BUILDER_H_
 #define AXOM_PRIMAL_BVH_LINEAR_BUILDER_H_
