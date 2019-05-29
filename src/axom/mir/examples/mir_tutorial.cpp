d13a5eb437a36c85c5717f476f10c7d3e9a05589
Sterbentz <sterbentz3@lonestarr.llnl.gov>
2019-05-29 08:14:31 -0700

    Builds a simple mir program.

---
+++ src/axom/mir/examples/mir_tutorial.cpp
@@ -0,0 +1,42 @@
+// Copyright (c) 2017-2019, Lawrence Livermore National Security, LLC and
+// other Axom Project Developers. See the top-level COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
+#include "axom/core.hpp"  // for axom macros
+#include "axom/mir.hpp"  // for Mir classes & functions
+
+
+
+// C/C++ includes
+#include <cmath>          // for definition of M_PI, exp()
+
+// namespace aliases
+//namespace mir     = axom::mir;
+namespace numerics = axom::numerics;
+
+/*!
+ * \file
+ *
+ * \brief Various code snippets/examples used for the Mir tutorial section.
+ *
+ * \note These examples are designed to illustrate specific Mir
+ *  concepts and capabilities. Consult the Tutorial section of Mir's
+ *  User Guide for more details.
+ *
+ */
+
+
+/*!
+ * \brief Tutorial main
+ */
+int main( int AXOM_NOT_USED(argc), char** AXOM_NOT_USED(argv) )
+{
+
+  std::cout << "hello from the mir_tutorial!" << std::endl;
+
+  return 0;
+}
+
+
+
