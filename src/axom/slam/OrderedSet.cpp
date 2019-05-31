b5053b733346b53536a995991aed71e6d9ef6e43
Kenneth Weiss <weiss27@llnl.gov>
2019-05-30 23:19:06 -0700

    Restores Slam's OrderedSet.cpp

---
+++ src/axom/slam/OrderedSet.cpp
@@ -0,0 +1,25 @@
+// Copyright (c) 2017-2019, Lawrence Livermore National Security, LLC and
+// other Axom Project Developers. See the top-level COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
+/**
+ * \file OrderedSet.cpp
+ */
+
+#include "OrderedSet.hpp"
+
+namespace axom
+{
+namespace slam
+{
+
+namespace policies
+{
+
+const NullSet<> NoSubset::s_nullSet;
+NullSet<> VirtualParentSubset::s_nullSet;
+
+} // namespace policies
+} // namespace slam
+} // namespace axom
