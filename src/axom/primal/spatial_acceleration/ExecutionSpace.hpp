f9c6240371bc887107cf100776e8c4e23f2f9bd1
George Zagaris <zagaris2@llnl.gov>
2019-06-04 17:27:31 -0700

    ENH: add execution space traits class
    This commit adds an execution_space traits class that binds
    the user-perscribed execution space to the corresponding
    RAJA execution policies, used internally, and the Umpire
    allocator.

---
+++ src/axom/primal/spatial_acceleration/ExecutionSpace.hpp
@@ -0,0 +1,165 @@
+// Copyright (c) 2017-2019, Lawrence Livermore National Security, LLC and
+// other Axom Project Developers. See the top-level COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
+
+#ifndef AXOM_PRIMAL_EXECUTIONSPACE_HPP_
+#define AXOM_PRIMAL_EXECUTIONSPACE_HPP_
+
+#include "axom/config.hpp"
+
+// RAJA includes
+#include "RAJA/RAJA.hpp"
+
+// Umpire includes
+#include "umpire/config.hpp"
+#include "umpire/ResourceManager.hpp"
+#include "umpire/op/MemoryOperationRegistry.hpp"
+
+#include "axom/core/memory_management.hpp"
+
+/*!
+ * \file
+ *
+ * \brief Defines the list of available execution spaces for spatial
+ *  acceleration data-structures.
+ *
+ * \note The execution space is provided as a template argument to
+ *
+ */
+
+namespace axom
+{
+namespace primal
+{
+
+/// \name Execution Spaces
+/// @{
+
+/*!
+ * \brief Indicates parallel execution on the GPU with CUDA.
+ */
+template < int BLOCK_SIZE >
+struct CUDA_EXEC{ };
+
+/*!
+ * \brief Indicates sequential execution on the CPU.
+ */
+struct SEQ_EXEC{ };
+
+/*!
+ * \brief Indicates parallel execution on the CPU using OpenMP.
+ */
+struct OMP_EXEC{ };
+
+/// @}
+
+
+/// \name Execution Space Traits
+/// @{
+
+
+/*!
+ * \brief The execution_space is a traits class.
+ *
+ *  The execution_space traits class binds the execution space to:
+ *  * corresponding RAJA execution policies and
+ *  * the default memory allocator to use.
+ *
+ *  \tparam ExecSpace the execution space
+ *
+ */
+template < typename ExecSpace >
+struct execution_space
+{
+  using raja_exec   = void;
+  using raja_reduce = void;
+  using raja_atomic = void;
+
+  static constexpr bool valid() { return false; };
+  static constexpr char* name() { return (char*)"[UNDEFINED]"; };
+  static umpire::Allocator allocator()
+  {
+    return axom::getAllocator( umpire::resource::Host );
+  };
+
+};
+
+//------------------------------------------------------------------------------
+
+
+#if defined(AXOM_USE_CUDA) && defined(AXOM_USE_RAJA)
+
+/*!
+ * \brief execution_space traits specialization for CUDA_EXEC
+ *
+ * \tparam BLOCK_SIZE the number of CUDA threads to launch
+ */
+template < int BLOCK_SIZE >
+struct execution_space< CUDA_EXEC< BLOCK_SIZE > >
+{
+  using raja_exec   = RAJA::cuda_exec< BLOCK_SIZE >;
+  using raja_reduce = RAJA::cude_reduce< BLOCK_SIZE >;
+  using raja_atomic = RAJA::atomic::cuda_atomic;
+
+  static constexpr bool valid() noexcept { return true; };
+  static constexpr char* name() noexcept { return (char*)"[CUDA_EXEC]"; };
+  static umpire::Allocator allocator() noexcept
+  {
+    return axom::getAllocator( umpire::resource::Unified);
+  };
+
+};
+
+#endif
+
+//------------------------------------------------------------------------------
+
+#if defined(AXOM_USE_RAJA) && defined(AXOM_USE_OPENMP)
+
+/*!
+ * \brief execution_space traits specialization for OMP_EXEC
+ */
+template < >
+struct execution_space< OMP_EXEC >
+{
+  using raja_exec   = RAJA::omp_parallel_for_exec;
+  using raja_reduce = RAJA::omp_reduce;
+  using raja_atomic = RAJA::atomic::omp_atomic;
+
+  static constexpr bool valid() noexcept { return true; };
+  static constexpr char* name() noexcept { return (char*)"[OMP_EXEC]"; };
+  static umpire::Allocator allocator() noexcept
+  {
+    return axom::getAllocator( umpire::resource::Host );
+  };
+
+};
+
+#endif
+
+/*!
+ * \brief execution_space traits specialization for SEQ_EXEC
+ */
+template < >
+struct execution_space< SEQ_EXEC >
+{
+  using raja_exec   = RAJA::loop_exec;
+  using raja_reduce = RAJA::loop_reduce;
+  using raja_atomic = RAJA::atomic::loop_atomic;
+
+  static constexpr bool valid() noexcept { return true; };
+  static constexpr char* name() noexcept { return (char*)"[SEQ_EXEC]"; };
+  static umpire::Allocator allocator() noexcept
+  {
+    return axom::getAllocator( umpire::resource::Host );
+  };
+
+};
+
+/// @}
+} /* namespace primal */
+} /* namespace axom   */
+
+#endif /* AXOM_PRIMAL_EXECUTIONSPACE_HPP_ */
---
+++ src/axom/primal/tests/primal_execution_space.cpp
@@ -0,0 +1,167 @@
+// Copyright (c) 2017-2019, Lawrence Livermore National Security, LLC and
+// other Axom Project Developers. See the top-level COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
+#include "axom/config.hpp"  // for compile time definitions
+
+// slic
+#include "axom/slic.hpp"
+
+// primal includes
+#include "axom/primal/spatial_acceleration/ExecutionSpace.hpp"
+
+#include "umpire/Umpire.hpp"  // for Umpire
+#include "RAJA/RAJA.hpp"      // for RAJA
+
+// for gtest macros
+#include "gtest/gtest.h"
+
+// C/C++ includes
+#include <cstring>     // for strlen(),strcmp()
+#include <type_traits> // for std::is_same
+
+// namespace aliases
+namespace primal = axom::primal;
+
+//------------------------------------------------------------------------------
+//  HELPER METHODS
+//------------------------------------------------------------------------------
+namespace
+{
+
+template< typename ExecSpace >
+void check_valid( )
+{
+  SLIC_INFO( "checking execution space:" <<
+              primal::execution_space< ExecSpace >::name() );
+  EXPECT_TRUE( primal::execution_space< ExecSpace >::valid() );
+  EXPECT_TRUE( strlen( primal::execution_space< ExecSpace >::name() ) > 0 );
+}
+
+//------------------------------------------------------------------------------
+template < typename ExecSpace >
+void check_invalid( )
+{
+  SLIC_INFO( "checking execution space:" <<
+              primal::execution_space< ExecSpace >::name() );
+  EXPECT_FALSE( primal::execution_space< ExecSpace >::valid() );
+  EXPECT_EQ( strcmp(primal::execution_space< ExecSpace >::name(),"[UNDEFINED]"),
+             0 );
+
+}
+
+//------------------------------------------------------------------------------
+template < typename ExecSpace,
+           typename RajaExec,
+           typename RajaReduce,
+           typename RajaAtomic,
+           umpire::resource::MemoryResourceType UmpireAllocator >
+void check_execution_mappings( )
+{
+  SLIC_INFO( "checking execution space: " <<
+             primal::execution_space< ExecSpace >::name());
+
+  using exec   = typename primal::execution_space< ExecSpace >::raja_exec;
+  using reduce = typename primal::execution_space< ExecSpace >::raja_reduce;
+  using atomic = typename primal::execution_space< ExecSpace >::raja_atomic;
+
+  bool valid_raja_exec   = std::is_same< exec, RajaExec >::value;
+  bool valid_raja_reduce = std::is_same< reduce, RajaReduce >::value;
+  bool valid_raja_atomic = std::is_same< atomic, RajaAtomic >::value;
+
+  EXPECT_TRUE( valid_raja_exec );
+  EXPECT_TRUE( valid_raja_reduce );
+  EXPECT_TRUE( valid_raja_atomic );
+
+  umpire::Allocator allocator = primal::execution_space<ExecSpace>::allocator();
+  EXPECT_EQ( allocator.getId(), UmpireAllocator );
+}
+
+} /* end anonymous namespace */
+
+//------------------------------------------------------------------------------
+//  UNIT TESTS
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+TEST( primal_execution_space, check_valid )
+{
+  check_valid< primal::SEQ_EXEC >( );
+  check_valid< primal::OMP_EXEC >( );
+#ifdef AXOM_USE_CUDA
+  check_valid< primal::CUDA_EXEC >( );
+#endif
+}
+
+//------------------------------------------------------------------------------
+TEST( primal_execution_space, check_invalid )
+{
+  struct InvalidSpace { };
+  check_invalid< InvalidSpace >( );
+}
+
+//------------------------------------------------------------------------------
+TEST( primal_execution_space, check_seq_exec )
+{
+  check_valid< primal::SEQ_EXEC >( );
+
+  check_execution_mappings< primal::SEQ_EXEC,
+                            RAJA::loop_exec,
+                            RAJA::loop_reduce,
+                            RAJA::atomic::loop_atomic,
+                            umpire::resource::Host >( );
+
+}
+
+//------------------------------------------------------------------------------
+#ifdef AXOM_USE_OPENMP
+TEST( primal_execution_space, check_omp_exec )
+{
+
+  check_valid< primal::OMP_EXEC >( );
+
+  check_execution_mappings< primal::OMP_EXEC,
+                            RAJA::omp_parallel_for_exec,
+                            RAJA::omp_reduce,
+                            RAJA::atomic::omp_atomic,
+                            umpire::resource::Host >( );
+
+}
+#endif
+
+//------------------------------------------------------------------------------
+#ifdef AXOM_USE_CUDA
+TEST( primal_execution_space, check_cuda_exec )
+{
+  constexpr int BLOCK_SIZE = 256;
+
+  check_valid< primal::CUDA_EXEC< BLOCK_SIZE > >( );
+
+  check_execution_mappings< primal::CUDA_EXEC< BLOCK_SIZE >,
+                            RAJA::cuda_exec< BLOCK_SIZE >,
+                            RAJA::cuda_reduce< BLOCK_SIZE >,
+                            RAJA::atomic::cuda_atomic,
+                            umpire::resource::Unified >( );
+
+}
+#endif
+
+//------------------------------------------------------------------------------
+#include "axom/slic/core/UnitTestLogger.hpp"
+using axom::slic::UnitTestLogger;
+
+int main(int argc, char* argv[])
+{
+  int result = 0;
+
+  ::testing::InitGoogleTest(&argc, argv);
+
+  UnitTestLogger logger;  // create & initialize test logger,
+
+  // finalized when exiting main scope
+
+  result = RUN_ALL_TESTS();
+
+  return result;
+}
