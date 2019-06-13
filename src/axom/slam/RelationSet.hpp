2b507bae6618765e2d2e5c728f924cb608e821e3
Kenneth Weiss <weiss27@llnl.gov>
2019-06-12 18:06:25 -0700

    Optimizes access to Submap indices() in some cases by adding flag to avoid indirection
    For Submaps over simple ProductSets, this avoid computing the index 'i' as '(i + j * nrows) % nrows'.

--- src/axom/slam/RelationSet.hpp
+++ src/axom/slam/RelationSet.hpp
@@ -164,7 +164,7 @@ public:
 
   ElementType at(PositionType pos) const override
   {
-    verifyPosition(pos);
+    verifyPositionImpl(pos);
     return (*m_relation->relationData())[pos];
   }
 
@@ -226,16 +226,26 @@ private:
            s2 >= 0 && s2 < m_relation->size(s1);
   }
 
-  void verifyPosition(PositionType AXOM_DEBUG_PARAM(sPos) ) const override
+  void verifyPosition(PositionType sPos) const override
   { //override function from RangeSet, overloading to avoid warning in compiler
+    verifyPositionImpl(sPos );
+  }
+
+  void verifyPositionImpl(PositionType AXOM_DEBUG_PARAM(sPos) ) const
+  {
     SLIC_ASSERT_MSG(
       sPos >= 0 && sPos < size(),
       "SLAM::RelationSet -- requested out-of-range element at position "
       << sPos << ", but set only has " << size() << " elements.");
   }
 
-  void verifyPosition(PositionType AXOM_DEBUG_PARAM(s1),
-                      PositionType AXOM_DEBUG_PARAM(s2) ) const override
+  void verifyPosition(PositionType s1, PositionType s2 ) const override
+  {
+    verifyPositionImpl(s1,s2);
+  }
+
+  void verifyPositionImpl(PositionType AXOM_DEBUG_PARAM(s1),
+                          PositionType AXOM_DEBUG_PARAM(s2) ) const
   {
     SLIC_ASSERT_MSG(
       isValidIndex(s1, s2),
