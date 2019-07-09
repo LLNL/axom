89153cef8cc7287630f1e32388b01452b6e0804f
uncrustify-robot <axom-dev@llnl.gov>
2019-07-08 18:14:45 -0700

    Uncrustified primal

--- src/axom/primal/geometry/BezierCurve.hpp
+++ src/axom/primal/geometry/BezierCurve.hpp
@@ -24,7 +24,7 @@
 namespace axom
 {
 namespace primal
-{ 
+{
 
 // Forward declare the templated classes and operator functions
 template < typename T, int NDIMS >
@@ -32,10 +32,11 @@ class BezierCurve;
 
 /*! \brief Overloaded output operator for Bezier Curves*/
 template < typename T,int NDIMS >
-std::ostream& operator<<(std::ostream & os, const BezierCurve< T,NDIMS > & bCurve);
+std::ostream& operator<<(std::ostream & os,
+                         const BezierCurve< T,NDIMS > & bCurve);
 
 /*!
- * \class BezierCurve 
+ * \class BezierCurve
  *
  * \brief Represents a Bezier Curve defined by an array of points
  * \tparam T the coordinate type, e.g., double, float, etc.
@@ -72,13 +73,15 @@ public:
     m_controlpoints.reserve(ord+1);
     m_controlpoints.resize(ord+1);
   }
-  
+
   /*!
-   * \brief Constructor for a Bezier Curve from a list of Points 
-   * \verbatim {x_0, x_1, x_2, x_3, y_0, y_1, y_2, y_3, z_0, z_1, z_2, z_3}  
+   * \brief Constructor for a Bezier Curve from a list of Points
+   * \verbatim {x_0, x_1, x_2, x_3, y_0, y_1, y_2, y_3, z_0, z_1, z_2, z_3}
    *
-   * \param [in] pts an array with (n+1)*NDIMS entries, ordered by coordinate then by control point order
-   * \param [in] ord number of control points minus 1 for which to reserve control point space
+   * \param [in] pts an array with (n+1)*NDIMS entries, ordered by coordinate
+   * then by control point order
+   * \param [in] ord number of control points minus 1 for which to reserve
+   * control point space
    * \pre order is not negative
    *
    */
@@ -93,7 +96,7 @@ public:
     SLIC_ASSERT(pts != nullptr);
 
     m_controlpoints.reserve(ord+1);
-    
+
     T tempar[NDIMS];
     for ( int i = 0 ; i <= ord ; i++)
     {
@@ -106,10 +109,11 @@ public:
   }
 
   /*!
-   * \brief Constructor for a Bezier Curve from an array of coordinates 
+   * \brief Constructor for a Bezier Curve from an array of coordinates
    *
    * \param [in] pts a vector with ord+1 points in it
-   * \param [in] ord number of control points minus 1 for which to reserve control point space
+   * \param [in] ord number of control points minus 1 for which to reserve
+   * control point space
    * \pre order is not negative
    *
    */
@@ -155,96 +159,104 @@ public:
   PointType& operator[](int idx) { return m_controlpoints[idx]; }
   /*! Retrieves the control point at index idx */
   const PointType& operator[](int idx) const { return m_controlpoints[idx]; }
- 
+
   /* Checks equality of two Bezier Curve */
-  friend inline bool operator==(const BezierCurve< T, NDIMS>& lhs, const BezierCurve< T, NDIMS>& rhs)
+  friend inline bool operator==(const BezierCurve< T, NDIMS>& lhs,
+                                const BezierCurve< T, NDIMS>& rhs)
   {
     return lhs.m_controlpoints == rhs.m_controlpoints;
   }
 
-  friend inline bool operator!=(const BezierCurve< T, NDIMS>& lhs, const BezierCurve< T, NDIMS>& rhs)
+  friend inline bool operator!=(const BezierCurve< T, NDIMS>& lhs,
+                                const BezierCurve< T, NDIMS>& rhs)
   {
     return !(lhs == rhs);
   }
 
   std::vector< Point< T, NDIMS > > getControlPoints() const
   {
-    return m_controlpoints; 
+    return m_controlpoints;
   }
 
   /*!
- * \brief Evaluates a Bezier Curve at particular parameter value between 0 and 1
- *
- * \param [in] t parameter value between 0 and 1 at which to evaluate
- * \return p the value of the Bezier Curve at t
- *
- */
+   * \brief Evaluates a Bezier Curve at a particular parameter value \a t
+   *
+   * \param [in] t parameter value at which to evaluate
+   * \return p the value of the Bezier Curve at t
+   *
+   * \note We typically evaluate the curve at \a t between 0 and 1
+   */
 
   Point< T, NDIMS > eval_bezier(T t)
   {
     Point< T, NDIMS > ptval(0.0,NDIMS);
     int ord = m_controlpoints.size()-1;
     T dCarray[ord+1];
-    
-    for ( int i=0; i < NDIMS; i++)
+
+    for ( int i=0 ; i < NDIMS ; i++)
     {
-       for ( int j=0 ; j <= ord ; j++)
-       {
-         dCarray[j] = m_controlpoints[j][i];
-       }
-       for ( int j=1 ; j <= ord ; j++)
-       {
-         for ( int k=0 ; k <= ord-j ; k++)
-         {
-           dCarray[k]=(1-t)*dCarray[k]+t*dCarray[k+1];  
-         }        
-       }
-       ptval[i]=dCarray[0];
+      for ( int j=0 ; j <= ord ; j++)
+      {
+        dCarray[j] = m_controlpoints[j][i];
+      }
+      for ( int j=1 ; j <= ord ; j++)
+      {
+        for ( int k=0 ; k <= ord-j ; k++)
+        {
+          dCarray[k]=(1-t)*dCarray[k]+t*dCarray[k+1];
+        }
+      }
+      ptval[i]=dCarray[0];
     }
-  
+
     return ptval;
   }
-  
-  /*!
- * \brief Splits a Bezier Curve into two Bezier Curves at particular parameter value between 0 and 1
- *
- * \param [in] t parameter value between 0 and 1 at which to evaluate
- * \param [in] c1, c2 two BezierCurves objects to store output in
- * \param [out] c1, c2 two new Bezier Curves that split the original
- * \return p the value of the Bezier Curve at t
- *
- */
 
-  void split_bezier(T t, BezierCurve< T, NDIMS >& c1, BezierCurve< T, NDIMS>& c2) const
-  { 
+  /*!
+   * \brief Splits a Bezier Curve into two Bezier Curves at particular parameter
+   * value between 0 and 1
+   *
+   * \param [in] t parameter value between 0 and 1 at which to evaluate
+   * \param [in] c1, c2 two BezierCurves objects to store output in
+   * \param [out] c1, c2 two new Bezier Curves that split the original
+   * \return p the value of the Bezier Curve at t
+   *
+   */
+
+  void split_bezier(T t,
+                    BezierCurve< T, NDIMS >& c1,
+                    BezierCurve< T, NDIMS>& c2) const
+  {
     int ord = m_controlpoints.size()-1;
     T* dCarray = new T[NDIMS*2*(ord+1)];
-    for ( int i=0; i < NDIMS; i++)
+    for ( int i=0 ; i < NDIMS ; i++)
     {
-       for ( int j=0 ; j <= ord ; j++)
-       {
-         dCarray[i*((ord+1))+j] = m_controlpoints[j][i];
-       }
-       dCarray[NDIMS*(ord+1)+(i)*((ord+1))]= dCarray[i*((ord+1))];
-       for ( int j=1 ; j <= ord ; j++)
-       {
-         for ( int k=0 ; k <= ord-j ; k++)
-         {
-           dCarray[i*((ord+1))+k]=(1-t)*dCarray[i*((ord+1))+k]+t*dCarray[i*((ord+1))+k+1];
-         }       
-         dCarray[NDIMS*(ord+1)+(i)*((ord+1))+j]=dCarray[i*((ord+1))];
-       }
+      for ( int j=0 ; j <= ord ; j++)
+      {
+        dCarray[i*((ord+1))+j] = m_controlpoints[j][i];
+      }
+      dCarray[NDIMS*(ord+1)+(i)*((ord+1))]= dCarray[i*((ord+1))];
+      for ( int j=1 ; j <= ord ; j++)
+      {
+        for ( int k=0 ; k <= ord-j ; k++)
+        {
+          dCarray[i*((ord+1))+k]=(1-t)*dCarray[i*((ord+1))+k]
+                                  + t*dCarray[i*((ord+1))+k+1];
+        }
+        dCarray[NDIMS*(ord+1)+(i)*((ord+1))+j]=dCarray[i*((ord+1))];
+      }
     }
-       c2=BezierCurve< T, NDIMS> (dCarray,ord);
-       c1=BezierCurve< T, NDIMS> (dCarray+NDIMS*(ord+1),ord);
-       delete [] dCarray;
-       return;
+    c2=BezierCurve< T, NDIMS> (dCarray,ord);
+    c1=BezierCurve< T, NDIMS> (dCarray+NDIMS*(ord+1),ord);
+    delete [] dCarray;
+    return;
   }
 
   /*!
    * \brief
    *
-   * \param [in] tol a tolerance parameter controlling definition of near-linearity
+   * \param [in] tol a tolerance parameter controlling definition of
+   * near-linearity
    * \param [out] boolean TRUE if c1 is near-linear
    */
 
@@ -253,11 +265,11 @@ public:
     int ord1 = m_controlpoints.size()-1;
     SegmentType linear1(m_controlpoints[0],m_controlpoints[ord1]);
     double d1=0.0;
-    for (int i=1; i<ord1; i++) // Only adds interior, since line is defined by endpoints
+    for (int i=1 ; i<ord1 ; i++) // line defined by endpoints; only add interior
     {
       d1=d1+squared_distance(m_controlpoints[i],linear1);
     }
-  return (d1<tol); // Note: tolerance is squared
+    return (d1<tol); // Note: tolerance is squared
   }
 
   /*!
@@ -267,12 +279,12 @@ public:
    * \return A reference to the modified ostream
    */
 
-  std::ostream& print(std::ostream& os) const 
+  std::ostream& print(std::ostream& os) const
   {
     const int sz = m_controlpoints.size()-1;
 
     os <<"{" << sz <<"-degree Bezier Curve:";
-    for (int i=0 ; i< sz; ++i)
+    for (int i=0 ; i< sz ; ++i)
     {
       os << m_controlpoints[i] << ",";
     }
@@ -285,7 +297,7 @@ public:
   /*!
    * \brief Simple check for validity of a Bezier Curve
    *
-   * Initial check is that the BezierCurve has two or more control points 
+   * Initial check is that the BezierCurve has two or more control points
    * \return True, if the Bezier Curve is valid, False otherwise
    */
   bool isValid() const
@@ -300,7 +312,8 @@ private:
 /// Free functions implementing BezierCurve's operators
 //------------------------------------------------------------------------------
 template < typename T, int NDIMS >
-std::ostream& operator<<(std::ostream & os, const BezierCurve< T,NDIMS > & bCurve)
+std::ostream& operator<<(std::ostream & os,
+                         const BezierCurve< T,NDIMS > & bCurve)
 {
   bCurve.print(os);
   return os;
--- src/axom/primal/tests/primal_bezier_curve.cpp
+++ src/axom/primal/tests/primal_bezier_curve.cpp
@@ -5,7 +5,7 @@
 
 /* /file bezier_test.cpp
  * /brief This file tests the BezierCurve.hpp and eval_bezier.hpp files
-*/
+ */
 
 #include "gtest/gtest.h"
 
@@ -21,10 +21,11 @@ TEST( primal_beziercurve, bezier_constructor )
   static const int DIM = 3;
   typedef double CoordType;
   typedef primal::BezierCurve< CoordType, DIM > BezierCurveType;
-  SLIC_INFO("\nprimal: testing default bezier constructor ") ;
+  SLIC_INFO("primal: testing default bezier constructor ");
   BezierCurveType bCurve;
 
-  EXPECT_EQ(bCurve.getControlPoints() ,(std::vector< Point< CoordType, DIM > >()));
+  EXPECT_EQ(bCurve.getControlPoints(),
+            (std::vector< Point< CoordType, DIM > >()));
 }
 
 //----------------------------------------------------------------------------------
@@ -33,10 +34,10 @@ TEST( primal_beziercurve, bezier_order_constructor )
   static const int DIM = 3;
   typedef double CoordType;
   typedef primal::BezierCurve< CoordType, DIM > BezierCurveType;
-  SLIC_INFO("\nprimal: testing bezier order constructor ") ;
+  SLIC_INFO("primal: testing bezier order constructor ");
 
   BezierCurveType bCurve(1);
-  
+
   EXPECT_EQ(bCurve.getOrder(), 1);
 }
 
@@ -47,20 +48,23 @@ TEST( primal_beziercurve, bezier_add_controlpoints )
   typedef double CoordType;
   typedef primal::Point< CoordType, DIM > PointType;
   typedef primal::BezierCurve< CoordType, DIM > BezierCurveType;
-  SLIC_INFO("\nprimal: testing adding control points to empty beziercurve");
+  SLIC_INFO("primal: testing adding control points to empty beziercurve");
 
   BezierCurveType bCurve;
-  
+
   CoordType coords[6] = {.6,1.2,1.0,0.0,1.6,1.8};
-  bCurve.addControlpoint( PointType::make_point( coords[0], coords[1], coords[2] ) );
-  bCurve.addControlpoint( PointType::make_point( coords[3], coords[4], coords[5] ) );
-  
+  bCurve.addControlpoint(
+    PointType::make_point( coords[0], coords[1], coords[2] ) );
+  bCurve.addControlpoint(
+    PointType::make_point( coords[3], coords[4], coords[5] ) );
+
   EXPECT_EQ(bCurve.getOrder(), 1);
-  for (int i=0; i<DIM; i++)
+  for (int i=0 ; i<DIM ; i++)
   {
-    for (int j=0; j<2; j++)
-    { 
-      EXPECT_TRUE(utilities::isNearlyEqual(bCurve[j][i],coords[j*DIM+i], 1.0e-14));
+    for (int j=0 ; j<2 ; j++)
+    {
+      EXPECT_TRUE(utilities::isNearlyEqual(bCurve[j][i],coords[j*DIM+i],
+                                           1.0e-14));
     }
   }
 }
@@ -73,17 +77,21 @@ TEST( primal_beziercurve, bezier_point_array_constructor )
   typedef primal::Point< CoordType, DIM > PointType;
   typedef primal::BezierCurve< CoordType, DIM > BezierCurveType;
   SLIC_INFO("\nprimal: testing point array constructor");
-  
+
   CoordType coords[6] = {.6,1.2,1.0,0.0,1.6,1.8};
-  PointType pts[2] = {PointType::make_point(coords[0], coords[1], coords[2]),PointType::make_point(coords[3], coords[4], coords[5])}; 
+  PointType pts[2] = {
+    PointType::make_point(coords[0], coords[1], coords[2]),
+    PointType::make_point(coords[3], coords[4], coords[5])
+  };
 
   BezierCurveType bCurve(pts,1);
   EXPECT_EQ(bCurve.getOrder(), 1);
-  for (int i=0; i<DIM; i++)
+  for (int i=0 ; i<DIM ; i++)
   {
-    for (int j=0; j<2; j++)
-    { 
-      EXPECT_TRUE(utilities::isNearlyEqual(bCurve[j][i],coords[j*DIM+i], 1.0e-14));
+    for (int j=0 ; j<2 ; j++)
+    {
+      EXPECT_TRUE(utilities::isNearlyEqual(bCurve[j][i],coords[j*DIM+i],
+                                           1.0e-14));
     }
   }
 }
@@ -95,17 +103,19 @@ TEST( primal_beziercurve, bezier_coordinate_array_constructor )
   typedef double CoordType;
   typedef primal::BezierCurve< CoordType, DIM > BezierCurveType;
   SLIC_INFO("\nprimal: testing coordinate array constructor");
-  
+
   CoordType coords[6] = {.6,1.2,1.0,0.0,1.6,1.8};
-  CoordType coordsToPass[6] = {coords[0], coords[3], coords[1],coords[4], coords[2], coords[5]}; 
+  CoordType coordsToPass[6] = {coords[0], coords[3], coords[1],
+                               coords[4], coords[2], coords[5]};
 
   BezierCurveType bCurve(coordsToPass,1);
   EXPECT_EQ(bCurve.getOrder(), 1);
-  for (int i=0; i<DIM; i++)
+  for (int i=0 ; i<DIM ; i++)
   {
-    for (int j=0; j<2; j++)
-    { 
-      EXPECT_TRUE(utilities::isNearlyEqual(bCurve[j][i],coords[j*DIM+i], 1.0e-14));
+    for (int j=0 ; j<2 ; j++)
+    {
+      EXPECT_TRUE(utilities::isNearlyEqual(bCurve[j][i],coords[j*DIM+i],
+                                           1.0e-14));
     }
   }
 }
@@ -118,7 +128,7 @@ TEST( primal_beziercurve, bezier_evaluation )
   typedef primal::Point< CoordType, DIM > PointType;
   typedef primal::BezierCurve< CoordType, DIM > BezierCurveType;
   SLIC_INFO("\nprimal: testing bezier evaluation");
-  
+
   const int nbr_points = 4;
   PointType data[nbr_points];
   data[0] = PointType::make_point(0.6, 1.2, 1.0);
@@ -126,13 +136,16 @@ TEST( primal_beziercurve, bezier_evaluation )
   data[2] = PointType::make_point(2.9, 2.4, 2.3);
   data[3] = PointType::make_point(3.2, 3.5, 3.0);
   BezierCurveType b2Curve(data, nbr_points-1);
-  
-  PointType midtval = PointType::make_point(2.05,2.0875,2.0375);  
-  for ( int i=0; i<DIM; i++)
+
+  PointType midtval = PointType::make_point(2.05,2.0875,2.0375);
+  for ( int i=0 ; i<DIM ; i++)
   {
-    EXPECT_TRUE(utilities::isNearlyEqual(b2Curve[0][i], b2Curve.eval_bezier(0.0)[i]));
-    EXPECT_TRUE(utilities::isNearlyEqual(b2Curve[nbr_points-1][i], b2Curve.eval_bezier(1.0)[i]));
-    EXPECT_TRUE(utilities::isNearlyEqual((midtval[i]), b2Curve.eval_bezier(0.5)[i]));
+    EXPECT_TRUE(utilities::isNearlyEqual(b2Curve[0][i],
+                                         b2Curve.eval_bezier(0.0)[i]));
+    EXPECT_TRUE(utilities::isNearlyEqual(b2Curve[nbr_points-1][i],
+                                         b2Curve.eval_bezier(1.0)[i]));
+    EXPECT_TRUE(utilities::isNearlyEqual((midtval[i]),
+                                         b2Curve.eval_bezier(0.5)[i]));
   }
 }
 
@@ -144,7 +157,7 @@ TEST( primal_beziercurve, bezier_split )
   typedef primal::Point< CoordType, DIM > PointType;
   typedef primal::BezierCurve< CoordType, DIM > BezierCurveType;
   SLIC_INFO("\nprimal: testing bezier splitting");
-  
+
   const int nbr_points = 4;
   PointType data[nbr_points];
   data[0] = PointType::make_point(0.6, 1.2, 1.0);
@@ -153,40 +166,38 @@ TEST( primal_beziercurve, bezier_split )
   data[3] = PointType::make_point(3.2, 3.5, 3.0);
   BezierCurveType b2Curve(data, nbr_points-1);
 
-  BezierCurveType b3Curve(3);// Checks that split works with order constructor input
-  BezierCurveType b4Curve;// Checks that split works with default constructor input
+  BezierCurveType b3Curve(3); // Checks split_bezier with order constructor
+  BezierCurveType b4Curve; // Checks split_bezier with default constructor
   b2Curve.split_bezier(.5,b3Curve,b4Curve);
-  
-  CoordType b3Coords[12] = {.6, .95, 1.525, 2.05, 1.2, 1.4, 1.7, 2.0875, 1.0, 1.4, 1.725, 2.0375};
-  CoordType b4Coords[12] = {2.05, 2.575, 3.05, 3.2, 2.0875, 2.475, 2.95, 3.5, 2.0375, 2.35, 2.65, 3.0};
+
+  CoordType b3Coords[12] = {.6, .95, 1.525, 2.05,
+                            1.2, 1.4, 1.7, 2.0875,
+                            1.0, 1.4, 1.725, 2.0375};
+  CoordType b4Coords[12] = {2.05, 2.575, 3.05, 3.2,
+                            2.0875, 2.475, 2.95, 3.5,
+                            2.0375, 2.35, 2.65, 3.0};
   BezierCurveType b3True(b3Coords,3);
   BezierCurveType b4True(b4Coords,3);
-  for ( int i=0; i<DIM; i++)
+  for ( int i=0 ; i<DIM ; i++)
   {
-    for ( int j=0; j<(nbr_points-1); j++)
+    for ( int j=0 ; j<(nbr_points-1) ; j++)
     {
       EXPECT_DOUBLE_EQ(b3Curve[j][i], b3True[j][i]);
       EXPECT_DOUBLE_EQ(b4Curve[j][i], b4True[j][i]);
     }
   }
-  
+
 }
 
 int main(int argc, char* argv[])
-   {
-     int result = 0;
-   
-     ::testing::InitGoogleTest(&argc, argv);
-   
-     axom::slic::UnitTestLogger logger;  // create & initialize test logger,
-   
-     // finalized when exiting main scope
-   
-     result = RUN_ALL_TESTS();
-   
+{
+  int result = 0;
 
-     return result;
-   }
+  ::testing::InitGoogleTest(&argc, argv);
 
+  axom::slic::UnitTestLogger logger;     // create & initialize test logger,
 
+  result = RUN_ALL_TESTS();
 
+  return result;
+}
