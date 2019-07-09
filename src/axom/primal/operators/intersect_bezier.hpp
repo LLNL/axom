d87d1ed739d70c656858952bc44c1594a9f54e24
uncrustify-robot <axom-dev@llnl.gov>
2019-07-08 18:14:45 -0700

    Uncrustified primal

--- src/axom/primal/operators/intersect_bezier.hpp
+++ src/axom/primal/operators/intersect_bezier.hpp
@@ -36,17 +36,17 @@ namespace primal
  * \return status true iff c1 intersects with c2, otherwise false.
  *
  * \param c1, c2 BezierCurve objects to intersect
- * \param sp, tp vector of type T parameter space intersection points (t-values and s-values)
- *  for c1 and c2, respectively
+ * \param sp, tp vector of type T parameter space intersection points (t-values
+ * and s-values) for c1 and c2, respectively
  */
 template < typename T, int NDIMS>
 bool intersect_bezier( const BezierCurve< T, NDIMS>& c1,
-    const BezierCurve< T, NDIMS>& c2,
-    std::vector< T >& sp,
-    std::vector< T >& tp)
-  {
-    int ord1=c1.getOrder();
-    int ord2=c2.getOrder();
+                       const BezierCurve< T, NDIMS>& c2,
+                       std::vector< T >& sp,
+                       std::vector< T >& tp)
+{
+  int ord1=c1.getOrder();
+  int ord2=c2.getOrder();
   if ( c1.is_linear(1e-31) && c2.is_linear(1e-31))
   {
     T s;
@@ -62,14 +62,14 @@ bool intersect_bezier( const BezierCurve< T, NDIMS>& c1,
   {
     BezierCurve< T, NDIMS> c3(ord1); BezierCurve< T, NDIMS> c4(ord1);
     c1.split_bezier(.5,c3,c4);
-    
-    std::vector<Point<T, NDIMS>> Ptv3 = c3.getControlPoints();
+
+    std::vector<Point<T, NDIMS> > Ptv3 = c3.getControlPoints();
     BoundingBox < T, NDIMS> b3(Ptv3.data(),ord1+1);
-    
-    std::vector<Point<T, NDIMS>> Ptv4 = c4.getControlPoints();
+
+    std::vector<Point<T, NDIMS> > Ptv4 = c4.getControlPoints();
     BoundingBox < T, NDIMS> b4(Ptv4.data(),ord1+1);
-    
-    std::vector<Point<T, NDIMS>> Ptv2 = c2.getControlPoints();
+
+    std::vector<Point<T, NDIMS> > Ptv2 = c2.getControlPoints();
     BoundingBox < T, NDIMS> b2(Ptv2.data(),ord1+1);
     bool checkint = false;
     int intcount = sp.size();
@@ -79,9 +79,9 @@ bool intersect_bezier( const BezierCurve< T, NDIMS>& c1,
       checkint=intersect_bezier(c2,c3,tp,sp);
       if (checkint)
       {
-        for (int i=intcount; i<static_cast<int>(sp.size()); i++)
+        for (int i=intcount ; i<static_cast<int>(sp.size()) ; i++)
         {
-        sp[i] = 0.5*sp[i];
+          sp[i] = 0.5*sp[i];
         }
       }
     }
@@ -91,7 +91,7 @@ bool intersect_bezier( const BezierCurve< T, NDIMS>& c1,
       checkint = intersect_bezier(c2,c4,tp,sp);
       if (checkint)
       {
-        for (int i=intcount; i<static_cast<int>(sp.size()); i++)
+        for (int i=intcount ; i<static_cast<int>(sp.size()) ; i++)
         {
           sp[i] = .5+0.5*sp[i];
         }
@@ -107,19 +107,23 @@ bool intersect_bezier( const BezierCurve< T, NDIMS>& c1,
   {
     return false;
   }
-  }
+}
 
 /*!
  * \brief Intersects two segments defined by their end points (a,d) and (c,b)
- * 
+ *
  * \param [in] a,d,c,b the endpoints of the segments
- * \param [out] The parametrized s and t values at which intersection occurs (note these could be outside [0,1]
+ * \param [out] The parametrized s and t values at which intersection occurs
+ * (note these could be outside [0,1]
  */
 
 template < typename T, int NDIMS>
-void intersect_2d_linear( const Point<T,NDIMS> &a, const Point<T,NDIMS> &d, const Point<T,NDIMS> &c, const Point<T,NDIMS> &b, T &s, T &t)
+void intersect_2d_linear( const Point<T,NDIMS> &a, const Point<T,NDIMS> &d,
+                          const Point<T,NDIMS> &c, const Point<T,NDIMS> &b,
+                          T &s, T &t)
 {
-  T determ = a[1]*b[0]-a[0]*b[1]-a[1]*c[0]+a[0]*c[1]+b[1]*d[0]-c[1]*d[0]-b[0]*d[1]+c[0]*d[1];
+  T determ =  a[1]*b[0]-a[0]*b[1] -a[1]*c[0]+a[0]*c[1]
+             +b[1]*d[0]-c[1]*d[0] -b[0]*d[1]+c[0]*d[1];
   s = (1.0/determ)*((b[1]-c[1])*(b[0]-a[0])+(c[0]-b[0])*(b[1]-a[1]));
   t = 1.0-(1.0/determ)*((a[1]-d[1])*(b[0]-a[0])+(d[0]-a[0])*(b[1]-a[1]));
 }
