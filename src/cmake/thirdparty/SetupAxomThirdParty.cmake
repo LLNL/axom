f7480e1c7e6a81060ef1c388c7b12533daa5543c
Chris White <white238@llnl.gov>
2019-08-29 16:58:00 -0700

    update conduit to master

--- src/cmake/thirdparty/SetupAxomThirdParty.cmake
+++ src/cmake/thirdparty/SetupAxomThirdParty.cmake
@@ -11,40 +11,38 @@
 # UMPIRE
 ################################
 if (UMPIRE_DIR)
-  include(cmake/thirdparty/FindUmpire.cmake)
-  blt_register_library( NAME umpire
-                        INCLUDES ${UMPIRE_INCLUDE_DIRS}
-                        TREAT_INCLUDES_AS_SYSTEM ON
-                        LIBRARIES umpire )
+    include(cmake/thirdparty/FindUmpire.cmake)
+    blt_register_library( NAME      umpire
+                          INCLUDES  ${UMPIRE_INCLUDE_DIRS}
+                          LIBRARIES umpire
+                          TREAT_INCLUDES_AS_SYSTEM ON)
 else()
-  message(STATUS "Umpire support is OFF")
+    message(STATUS "Umpire support is OFF")
 endif()
+
 
 ################################
 # RAJA
 ################################
 if (RAJA_DIR)
-  include(cmake/thirdparty/FindRAJA.cmake)
-  blt_register_library( NAME raja
-                        INCLUDES ${RAJA_INCLUDE_DIR}
-                        TREAT_INCLUDES_AS_SYSTEM ON
-                        LIBRARIES ${RAJA_LIB_DIR}/libRAJA.a )
+    include(cmake/thirdparty/FindRAJA.cmake)
+    blt_register_library( NAME      raja
+                          INCLUDES  ${RAJA_INCLUDE_DIR}
+                          LIBRARIES ${RAJA_LIB_DIR}/libRAJA.a
+                          TREAT_INCLUDES_AS_SYSTEM ON)
 else()
-  message(STATUS "RAJA support is OFF" )
+    message(STATUS "RAJA support is OFF" )
 endif()
+
 
 ################################
 # Conduit
 ################################
 if (CONDUIT_DIR)
     include(cmake/thirdparty/FindConduit.cmake)
-    blt_register_library( NAME conduit
-                          INCLUDES ${CONDUIT_INCLUDE_DIRS}
-                          LIBRARIES conduit
-                          TREAT_INCLUDES_AS_SYSTEM ON)
-    blt_register_library( NAME conduit_relay
-                          INCLUDES ${CONDUIT_INCLUDE_DIRS}
-                          LIBRARIES conduit_relay
+    blt_register_library( NAME      conduit
+                          INCLUDES  ${CONDUIT_INCLUDE_DIRS}
+                          LIBRARIES conduit::conduit
                           TREAT_INCLUDES_AS_SYSTEM ON)
 else()
     message(STATUS "Conduit support is OFF")
@@ -56,8 +54,8 @@ endif()
 ################################
 if (HDF5_DIR)
     include(cmake/thirdparty/SetupHDF5.cmake)
-    blt_register_library( NAME hdf5
-                          INCLUDES ${HDF5_INCLUDE_DIRS}
+    blt_register_library( NAME      hdf5
+                          INCLUDES  ${HDF5_INCLUDE_DIRS}
                           LIBRARIES ${HDF5_LIBRARIES}
                           TREAT_INCLUDES_AS_SYSTEM ON)
 else()
@@ -70,8 +68,8 @@ endif()
 ################################
 if (MFEM_DIR)
     include(cmake/thirdparty/FindMFEM.cmake)
-    blt_register_library( NAME mfem
-                          INCLUDES ${MFEM_INCLUDE_DIRS}
+    blt_register_library( NAME      mfem
+                          INCLUDES  ${MFEM_INCLUDE_DIRS}
                           LIBRARIES ${MFEM_LIBRARY}
                           TREAT_INCLUDES_AS_SYSTEM ON)
 else()
@@ -91,7 +89,7 @@ if(EXISTS ${SHROUD_EXECUTABLE})
                     ERROR_VARIABLE SHROUD_cmake_error
                     OUTPUT_STRIP_TRAILING_WHITESPACE )
     if(${SHROUD_cmake_error})
-       message(FATAL_ERROR "Error from Shroud: ${SHROUD_cmake_error}")
+        message(FATAL_ERROR "Error from Shroud: ${SHROUD_cmake_error}")
     endif()
     include(${CMAKE_CURRENT_BINARY_DIR}/SetupShroud.cmake)
 else()
@@ -104,8 +102,8 @@ endif()
 ################################
 if (SCR_DIR)
     include(cmake/thirdparty/FindSCR.cmake)
-    blt_register_library( NAME scr
-                          INCLUDES ${SCR_INCLUDE_DIRS}
+    blt_register_library( NAME      scr
+                          INCLUDES  ${SCR_INCLUDE_DIRS}
                           LIBRARIES ${SCR_LIBRARY}
                           TREAT_INCLUDES_AS_SYSTEM ON)
 else()
