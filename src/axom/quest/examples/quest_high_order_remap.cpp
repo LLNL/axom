ab1b1eea0de9551c7ad70608891c609948492d06
Kenny Weiss <kenny@kennyweiss.com>
2019-10-08 11:00:55 -0700

    Rely on Axom data submodule for mesh files in quest high order remap example

--- src/axom/quest/examples/quest_high_order_remap.cpp
+++ src/axom/quest/examples/quest_high_order_remap.cpp
@@ -1,5 +1,5 @@
-// Copyright (c) 2017-2019, Lawrence Livermore National Security, LLC and
-// other Axom Project Developers. See the top-level COPYRIGHT file for details.
+// Copyright (c) 2017-2021, Lawrence Livermore National Security, LLC and
+// other Axom Project Developers. See the top-level LICENSE file for details.
 //
 // SPDX-License-Identifier: (BSD-3-Clause)
 
@@ -370,7 +370,11 @@ public:
     }
     // create the target mesh
     {
-      auto* mesh = new mfem::Mesh("./disc-nurbs-80.mesh", 1, 1);
+      // NOTE (KW): For now, assume we have AXOM_DATA_DIR
+      namespace fs = axom::utilities::filesystem;
+      std::string fname = fs::joinPath(AXOM_DATA_DIR, "mfem/disc-nurbs.mesh");
+
+      auto* mesh = new mfem::Mesh(fname.c_str(), 1, 1);
       if(mesh->NURBSext)
       {
         int order = tgt_ord;
