966638f5baaabc0bb58434105f876cbb65a8fcdc
George Zagaris <zagaris2@llnl.gov>
2019-10-22 14:27:53 -0700

    ENH: generalize execution space traits
    This commit, in part, resolves #56

--- src/axom/core/execution_space.hpp
+++ src/axom/core/execution_space.hpp
@@ -4,39 +4,102 @@
 // SPDX-License-Identifier: (BSD-3-Clause)
 
 
-#ifndef AXOM_SPIN_EXECUTIONSPACE_HPP_
-#define AXOM_SPIN_EXECUTIONSPACE_HPP_
+#ifndef AXOM_EXECUTIONSPACE_HPP_
+#define AXOM_EXECUTIONSPACE_HPP_
 
 #include "axom/config.hpp"
 #include "axom/core/memory_management.hpp"
 
-#if !defined(AXOM_USE_RAJA) || !defined(AXOM_USE_UMPIRE)
-#error *** The execution_space traits class requires RAJA and Umpire ***
+// RAJA includes
+#ifdef AXOM_USE_RAJA
+#include "RAJA/RAJA.hpp"
 #endif
 
-
-// RAJA includes
-#include "RAJA/RAJA.hpp"
-
 // Umpire includes
+#ifdef AXOM_USE_UMPIRE
 #include "umpire/Umpire.hpp"
+#endif
 
 /*!
  * \file
  *
- * \brief Defines the list of available execution spaces for spatial
- *  acceleration data-structures.
+ * \brief Defines the list of available execution spaces for axom.
  *
- * \note The execution space is provided as a template argument to
+ * The list of defined execution spaces are the following:
  *
+ *  * <b>SEQ_EXEC<b> <br />
+ *
+ *    Indicates sequential execution on the CPU. Always defined.
+ *
+ *    When using this execution space, the data must reside on CPU/host memory.
+ *
+ *  * <b>OMP_EXEC<b> <br />
+ *
+ *    Indicates parallel execution with OpenMP on the CPU.
+ *
+ *    Defined when AXOM_USE_OPENMP and AXOM_USE_RAJA are defined. In addition,
+ *    using this execution space requires linking to a RAJA that is configured
+ *    with OpenMp, i.e., RAJA_ENABLE_OPENMP must be defined in the generated
+ *    RAJA/config.hpp.
+ *
+ *    The default memory allocator when using this execution space is HOST.
+ *
+ *    When using this execution space, the data must reside on CPU/host memory.
+ *
+ *  * <b>CUDA_EXEC<BLOCKSIZE></b> <br />
+ *
+ *    Indicates parallel execution with CUDA on the GPU.
+ *
+ *    Defined when AXOM_USE_CUDA and AXOM_USE_RAJA are defined. In  addition,
+ *    using this execution space requires linking to a RAJA that is configured
+ *    with CUDA, i.e., RAJA_ENABLE_CUDA must be defined in the generated
+ *    RAJA/config.hpp.
+ *
+ *    The CUDA_EXEC execution space strictly requires Umpire for memory
+ *    management. The data must reside either on device memory which, is only
+ *    accessible on the GPU, or on Unified memory which, is accessible on both
+ *    the CPU or GPU. Note, when using Unified memory, the hardware transfers
+ *    the data accordingly as needed. Consequently, using unified memory must
+ *    be handled with extreme care, in order to avoid the latencies associated
+ *    with data transfers between the CPU and GPU.
+ *
+ *    The default memory allocator when using this execution space is UNIFIED.
+ *
+ * The execution spaces bind the corresponding RAJA execution policies and
+ * default memory space.
  */
 
 namespace axom
 {
-namespace spin
+
+/*!
+ * \brief The execution_space is a traits class that binds the execution
+ *  space to a corresponding RAJA execution policies and default memory
+ *  allocator.
+ *
+ * \tparam ExecSpace the execution space
+ *
+ * \note This class is specialized for each execution space.
+ *
+ */
+template < typename ExecSpace >
+struct execution_space
 {
+  using loop_policy   = void;
+  using reduce_policy = void;
+  using atomic_policy = void;
+
+  static constexpr bool valid() noexcept { return false; };
+  static constexpr char* name() noexcept { return (char*)"[UNDEFINED]"; };
+  static int allocatorID() noexcept { return axom::INVALID_ALLOCATOR_ID; };
+};
 
 /// \name Execution Spaces
+/// @{
+
+//-----------------------------------------------------------| SEQ_EXEC |-------
+
+/// \name SEQ_EXEC
 /// @{
 
 /*!
@@ -45,56 +108,105 @@ namespace spin
 struct SEQ_EXEC{ };
 
 /*!
- * \brief Indicates parallel execution on the CPU using OpenMP.
+ * \brief execution_space traits specialization for SEQ_EXEC
  */
-#ifdef AXOM_USE_OPENMP
-struct OMP_EXEC{ };
+template < >
+struct execution_space< SEQ_EXEC >
+{
+#ifdef AXOM_USE_RAJA
+  using loop_policy   = RAJA::loop_exec;
+  using reduce_policy = RAJA::loop_reduce;
+  using atomic_policy = RAJA::loop_atomic;
+#else
+  using loop_policy   = void;
+  using reduce_policy = void;
+  using atomic_policy = void;
 #endif
 
+  static constexpr bool valid() noexcept { return true; };
+  static constexpr char* name() noexcept { return (char*)"[SEQ_EXEC]"; };
+  static int allocatorID() noexcept
+  {
+#ifdef AXOM_USE_UMPIRE
+    return axom::getResourceAllocatorID( umpire::resource::Host );
+#else
+    return 0;
+#endif
+  };
+
+};
+
+/// @}
+
+//-----------------------------------------------------------| OMP_EXEC |-------
+
+/// \name OMP_EXEC
+/// @{
+
+#if defined(AXOM_USE_OPENMP) && defined(AXOM_USE_RAJA)
+
+#if !defined(RAJA_ENABLE_OPENMP)
+#error *** OMP_EXEC requires an OpenMP enabled RAJA ***
+#endif
+
+/*!
+ * \brief Indicates parallel execution on the CPU using OpenMP.
+ */
+struct OMP_EXEC{ };
+
+/*!
+ * \brief execution_space traits specialization for OMP_EXEC
+ */
+template < >
+struct execution_space< OMP_EXEC >
+{
+ using loop_policy   = RAJA::omp_parallel_for_exec;
+ using reduce_policy = RAJA::omp_reduce;
+ using atomic_policy = RAJA::omp_atomic;
+
+ static constexpr bool valid() noexcept { return true; };
+ static constexpr char* name() noexcept { return (char*)"[OMP_EXEC]"; };
+
+ static int allocatorID() noexcept
+ {
+#ifdef AXOM_USE_UMPIRE
+   return axom::getResourceAllocatorID(umpire::resource::Host);
+#else
+   return 0;
+#endif
+ };
+
+};
+
+#endif
+
+/// @}
+
+//----------------------------------------------------------| CUDA_EXEC |-------
+
+/// \name CUDA_EXEC
+/// @{
+
+#if defined(AXOM_USE_CUDA) && defined(AXOM_USE_RAJA) && defined(AXOM_USE_UMPIRE)
+
+#if !defined(RAJA_ENABLE_CUDA)
+#error *** CUDA_EXEC requires a CUDA enabled RAJA ***
+#endif
+
+#if !defined(UMPIRE_ENABLE_CUDA)
+#error *** CUDA_EXEC requires a CUDA enabled UMPIRE ***
+#endif
+
+#if !defined(UMPIRE_ENABLE_UM)
+#error *** CUDA_EXEC requires UMPIRE configured with UMPIRE_ENABLE_UM ***
+#endif
 
 /*!
  * \brief Indicates parallel execution on the GPU with CUDA.
  * \tparam BLOCK_SIZE the number of CUDA threads in a block.
  */
-#if defined(AXOM_USE_CUDA) && defined(RAJA_ENABLE_CUDA)
 template < int BLOCK_SIZE >
 struct CUDA_EXEC{ };
-#endif
-
-/// @}
-
-
-/// \name Execution Space Traits
-/// @{
-
-
-/*!
- * \brief The execution_space is a traits class.
- *
- *  The execution_space traits class binds the execution space to:
- *  * corresponding RAJA execution policies and
- *  * the default memory allocator to use.
- *
- *  \tparam ExecSpace the execution space
- *
- */
-template < typename ExecSpace >
-struct execution_space
-{
-  using raja_exec   = void;
-  using raja_reduce = void;
-  using raja_atomic = void;
-
-  static constexpr bool valid() noexcept { return false; };
-  static constexpr char* name() noexcept { return (char*)"[UNDEFINED]"; };
-  static int allocatorID() noexcept
-  { return axom::INVALID_ALLOCATOR_ID; };
-};
-
-//------------------------------------------------------------------------------
-
-
-#if defined(AXOM_USE_CUDA) && defined(AXOM_USE_RAJA)
 
 /*!
  * \brief execution_space traits specialization for CUDA_EXEC
@@ -104,9 +216,9 @@ struct execution_space
 template < int BLOCK_SIZE >
 struct execution_space< CUDA_EXEC< BLOCK_SIZE > >
 {
-  using raja_exec   = RAJA::cuda_exec< BLOCK_SIZE >;
-  using raja_reduce = RAJA::cuda_reduce;
-  using raja_atomic = RAJA::cuda_atomic;
+  using loop_policy   = RAJA::cuda_exec< BLOCK_SIZE >;
+  using reduce_policy = RAJA::cuda_reduce;
+  using atomic_policy = RAJA::cuda_atomic;
 
   static constexpr bool valid() noexcept { return true; };
   static constexpr char* name() noexcept { return (char*)"[CUDA_EXEC]"; };
@@ -116,46 +228,8 @@ struct execution_space< CUDA_EXEC< BLOCK_SIZE > >
 
 #endif
 
-//------------------------------------------------------------------------------
-
-#if defined(AXOM_USE_RAJA) && defined(AXOM_USE_OPENMP)
-
-/*!
- * \brief execution_space traits specialization for OMP_EXEC
- */
-template < >
-struct execution_space< OMP_EXEC >
-{
-  using raja_exec   = RAJA::omp_parallel_for_exec;
-  using raja_reduce = RAJA::omp_reduce;
-  using raja_atomic = RAJA::omp_atomic;
-
-  static constexpr bool valid() noexcept { return true; };
-  static constexpr char* name() noexcept { return (char*)"[OMP_EXEC]"; };
-  static int allocatorID() noexcept
-  { return axom::getResourceAllocatorID(umpire::resource::Host); };
-};
-
-#endif
-
-/*!
- * \brief execution_space traits specialization for SEQ_EXEC
- */
-template < >
-struct execution_space< SEQ_EXEC >
-{
-  using raja_exec   = RAJA::loop_exec;
-  using raja_reduce = RAJA::loop_reduce;
-  using raja_atomic = RAJA::loop_atomic;
-
-  static constexpr bool valid() noexcept { return true; };
-  static constexpr char* name() noexcept { return (char*)"[SEQ_EXEC]"; };
-  static int allocatorID() noexcept
-  { return axom::getResourceAllocatorID(umpire::resource::Host); };
-};
-
 /// @}
-} /* namespace spin */
+
 } /* namespace axom   */
 
 #endif /* AXOM_SPIN_EXECUTIONSPACE_HPP_ */
