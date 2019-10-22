af717ed5954e670c6241251edeab243d14e9cf68
George Zagaris <zagaris2@llnl.gov>
2019-10-22 16:29:07 -0700

    COMP: refactor execution_space test
    This commit, in part, resolves #56

--- src/axom/core/tests/core_execution_space.cpp
+++ src/axom/core/tests/core_execution_space.cpp
@@ -5,14 +5,16 @@
 
 #include "axom/config.hpp"  // for compile time definitions
 
-// slic
-#include "axom/slic.hpp"
-
 // spin includes
-#include "axom/spin/execution_space.hpp"
+#include "axom/core/execution_space.hpp"
 
+#ifdef AXOM_USE_UMPIRE
 #include "umpire/Umpire.hpp"  // for Umpire
+#endif
+
+#ifdef AXOM_USE_RAJA
 #include "RAJA/RAJA.hpp"      // for RAJA
+#endif
 
 // for gtest macros
 #include "gtest/gtest.h"
@@ -20,9 +22,6 @@
 // C/C++ includes
 #include <cstring>     // for strlen(),strcmp()
 #include <type_traits> // for std::is_same
-
-// namespace aliases
-namespace spin = axom::spin;
 
 //------------------------------------------------------------------------------
 //  HELPER METHODS
@@ -33,25 +32,26 @@ namespace
 template< typename ExecSpace >
 void check_valid( )
 {
-  SLIC_INFO( "checking execution space:" <<
-              spin::execution_space< ExecSpace >::name() );
-  EXPECT_TRUE( spin::execution_space< ExecSpace >::valid() );
-  EXPECT_TRUE( strlen( spin::execution_space< ExecSpace >::name() ) > 0 );
+  std::cout << "checking execution space:" <<
+               axom::execution_space< ExecSpace >::name() << std::endl;
+
+  EXPECT_TRUE( axom::execution_space< ExecSpace >::valid() );
+  EXPECT_TRUE( strlen( axom::execution_space< ExecSpace >::name() ) > 0 );
 }
 
 //------------------------------------------------------------------------------
 template < typename ExecSpace >
 void check_invalid( )
 {
-  SLIC_INFO( "checking execution space:" <<
-              spin::execution_space< ExecSpace >::name() );
+  std::cout << "checking execution space:" <<
+              axom::execution_space< ExecSpace >::name() << std::endl;
 
-  EXPECT_FALSE( spin::execution_space< ExecSpace >::valid() );
+  EXPECT_FALSE( axom::execution_space< ExecSpace >::valid() );
 
-  EXPECT_EQ( spin::execution_space< ExecSpace >::allocatorID(),
+  EXPECT_EQ( axom::execution_space< ExecSpace >::allocatorID(),
              axom::INVALID_ALLOCATOR_ID );
 
-  EXPECT_EQ( strcmp(spin::execution_space< ExecSpace >::name(),"[UNDEFINED]"),
+  EXPECT_EQ( strcmp(axom::execution_space< ExecSpace >::name(),"[UNDEFINED]"),
              0 );
 }
 
@@ -62,12 +62,12 @@ template < typename ExecSpace,
            typename RajaAtomic >
 void check_execution_mappings( int expectedAllocatorID )
 {
-  SLIC_INFO( "checking execution space: " <<
-             spin::execution_space< ExecSpace >::name());
+  std::cout << "checking execution space: " <<
+               axom::execution_space< ExecSpace >::name() << std::endl;
 
-  using exec   = typename spin::execution_space< ExecSpace >::raja_exec;
-  using reduce = typename spin::execution_space< ExecSpace >::raja_reduce;
-  using atomic = typename spin::execution_space< ExecSpace >::raja_atomic;
+  using exec   = typename axom::execution_space< ExecSpace >::loop_policy;
+  using reduce = typename axom::execution_space< ExecSpace >::reduce_policy;
+  using atomic = typename axom::execution_space< ExecSpace >::atomic_policy;
 
   bool valid_raja_exec   = std::is_same< exec, RajaExec >::value;
   bool valid_raja_reduce = std::is_same< reduce, RajaReduce >::value;
@@ -77,7 +77,7 @@ void check_execution_mappings( int expectedAllocatorID )
   EXPECT_TRUE( valid_raja_reduce );
   EXPECT_TRUE( valid_raja_atomic );
 
-  int allocatorID = spin::execution_space< ExecSpace >::allocatorID();
+  int allocatorID = axom::execution_space< ExecSpace >::allocatorID();
   EXPECT_EQ( expectedAllocatorID, allocatorID );
 }
 
@@ -88,33 +88,33 @@ void check_execution_mappings( int expectedAllocatorID )
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
-TEST( spin_execution_space, check_valid )
+TEST( core_execution_space, check_valid )
 {
-  check_valid< spin::SEQ_EXEC >( );
+  check_valid< axom::SEQ_EXEC >( );
 
 #ifdef AXOM_USE_OPENMP
-  check_valid< spin::OMP_EXEC >( );
+  check_valid< axom::OMP_EXEC >( );
 #endif
 
 #ifdef AXOM_USE_CUDA
-  check_valid< spin::CUDA_EXEC< 256 > >( );
+  check_valid< axom::CUDA_EXEC< 256 > >( );
 #endif
 }
 
 //------------------------------------------------------------------------------
-TEST( spin_execution_space, check_invalid )
+TEST( core_execution_space, check_invalid )
 {
   struct InvalidSpace { };
   check_invalid< InvalidSpace >( );
 }
 
 //------------------------------------------------------------------------------
-TEST( spin_execution_space, check_seq_exec )
+TEST( core_execution_space, check_seq_exec )
 {
-  check_valid< spin::SEQ_EXEC >( );
+  check_valid< axom::SEQ_EXEC >( );
 
   int allocator_id = axom::getResourceAllocatorID( umpire::resource::Host );
-  check_execution_mappings< spin::SEQ_EXEC,
+  check_execution_mappings< axom::SEQ_EXEC,
                             RAJA::loop_exec,
                             RAJA::loop_reduce,
                             RAJA::loop_atomic >( allocator_id );
@@ -123,13 +123,13 @@ TEST( spin_execution_space, check_seq_exec )
 
 //------------------------------------------------------------------------------
 #ifdef AXOM_USE_OPENMP
-TEST( spin_execution_space, check_omp_exec )
+TEST( core_execution_space, check_omp_exec )
 {
 
-  check_valid< spin::OMP_EXEC >( );
+  check_valid< axom::OMP_EXEC >( );
 
   int allocator_id = axom::getResourceAllocatorID( umpire::resource::Host );
-  check_execution_mappings< spin::OMP_EXEC,
+  check_execution_mappings< axom::OMP_EXEC,
                             RAJA::omp_parallel_for_exec,
                             RAJA::omp_reduce,
                             RAJA::omp_atomic >( allocator_id );
@@ -139,14 +139,14 @@ TEST( spin_execution_space, check_omp_exec )
 
 //------------------------------------------------------------------------------
 #ifdef AXOM_USE_CUDA
-TEST( spin_execution_space, check_cuda_exec )
+TEST( core_execution_space, check_cuda_exec )
 {
   constexpr int BLOCK_SIZE = 256;
 
-  check_valid< spin::CUDA_EXEC< BLOCK_SIZE > >( );
+  check_valid< axom::CUDA_EXEC< BLOCK_SIZE > >( );
 
   int allocator_id = axom::getResourceAllocatorID( umpire::resource::Unified );
-  check_execution_mappings< spin::CUDA_EXEC< BLOCK_SIZE >,
+  check_execution_mappings< axom::CUDA_EXEC< BLOCK_SIZE >,
                             RAJA::cuda_exec< BLOCK_SIZE >,
                             RAJA::cuda_reduce,
                             RAJA::cuda_atomic >( allocator_id );
@@ -155,16 +155,11 @@ TEST( spin_execution_space, check_cuda_exec )
 #endif
 
 //------------------------------------------------------------------------------
-#include "axom/slic/core/UnitTestLogger.hpp"
-using axom::slic::UnitTestLogger;
-
 int main(int argc, char* argv[])
 {
   int result = 0;
 
   ::testing::InitGoogleTest(&argc, argv);
-
-  UnitTestLogger logger;  // create & initialize test logger,
 
   // finalized when exiting main scope
 
