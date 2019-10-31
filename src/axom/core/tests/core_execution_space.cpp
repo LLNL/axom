0b892b093ec593b1bf5774f52ec87b6210578f17
George Zagaris <zagaris2@llnl.gov>
2019-10-31 15:37:33 -0700

    ENH: improve testing of execution space traits
    Add tests for 2d/3d loop policies + minor clean up.
    
    This commit, in part, resolves #56

--- src/axom/core/tests/core_execution_space.cpp
+++ src/axom/core/tests/core_execution_space.cpp
@@ -57,29 +57,37 @@ void check_invalid( )
 
 //------------------------------------------------------------------------------
 template < typename ExecSpace,
-           typename RajaExec,
-           typename RajaReduce,
-           typename RajaAtomic,
-           typename RajaSync >
+           typename LoopPolicy,
+           typename Loop2dPolicy,
+           typename Loop3dPolicy,
+           typename ReducePolicy,
+           typename AtomicPolicy,
+           typename SyncPolicy >
 void check_execution_mappings( int expectedAllocatorID )
 {
   std::cout << "checking execution space: " <<
                axom::execution_space< ExecSpace >::name() << std::endl;
 
-  using exec   = typename axom::execution_space< ExecSpace >::loop_policy;
-  using reduce = typename axom::execution_space< ExecSpace >::reduce_policy;
-  using atomic = typename axom::execution_space< ExecSpace >::atomic_policy;
-  using sync   = typename axom::execution_space< ExecSpace >::sync_policy;
+  using loop_pol   = typename axom::execution_space< ExecSpace >::loop_policy;
+  using loop2d_pol = typename axom::execution_space< ExecSpace >::loop2d_policy;
+  using loop3d_pol = typename axom::execution_space< ExecSpace >::loop3d_policy;
+  using reduce_pol = typename axom::execution_space< ExecSpace >::reduce_policy;
+  using atomic_pol = typename axom::execution_space< ExecSpace >::atomic_policy;
+  using sync_pol   = typename axom::execution_space< ExecSpace >::sync_policy;
 
-  bool valid_raja_exec   = std::is_same< exec, RajaExec >::value;
-  bool valid_raja_reduce = std::is_same< reduce, RajaReduce >::value;
-  bool valid_raja_atomic = std::is_same< atomic, RajaAtomic >::value;
-  bool valid_raja_sync   = std::is_same< sync, RajaSync >::value;
+  bool valid_loop_policy   = std::is_same< loop_pol, LoopPolicy >::value;
+  bool valid_loop2d_policy = std::is_same< loop2d_pol, Loop2dPolicy >::value;
+  bool valid_loop3d_policy = std::is_same< loop3d_pol, Loop3dPolicy >::value;
+  bool valid_reduce_policy = std::is_same< reduce_pol, ReducePolicy >::value;
+  bool valid_atomic_policy = std::is_same< atomic_pol, AtomicPolicy >::value;
+  bool valid_sync_policy   = std::is_same< sync_pol, SyncPolicy >::value;
 
-  EXPECT_TRUE( valid_raja_exec );
-  EXPECT_TRUE( valid_raja_reduce );
-  EXPECT_TRUE( valid_raja_atomic );
-  EXPECT_TRUE( valid_raja_sync );
+  EXPECT_TRUE( valid_loop_policy );
+  EXPECT_TRUE( valid_loop2d_policy );
+  EXPECT_TRUE( valid_loop3d_policy );
+  EXPECT_TRUE( valid_reduce_policy );
+  EXPECT_TRUE( valid_atomic_policy );
+  EXPECT_TRUE( valid_sync_policy );
 
   int allocatorID = axom::execution_space< ExecSpace >::allocatorID();
   EXPECT_EQ( expectedAllocatorID, allocatorID );
@@ -117,9 +125,33 @@ TEST( core_execution_space, check_seq_exec )
 {
   check_valid< axom::SEQ_EXEC >( );
 
+  /* *INDENT-OFF* */
+  using raja_loop2d_policy =
+      RAJA::KernelPolicy<
+               RAJA::statement::For< 1, RAJA::loop_exec,   // j
+                 RAJA::statement::For< 0, RAJA::loop_exec, // i
+                   RAJA::statement::Lambda< 0 >
+                 > // END i
+               > // END j
+            >; // END kernel
+
+  using raja_loop3d_policy =
+      RAJA::KernelPolicy<
+              RAJA::statement::For< 2, RAJA::loop_exec,       // k
+                 RAJA::statement::For< 1, RAJA::loop_exec,    // j
+                    RAJA::statement::For< 0, RAJA::loop_exec, // i
+                       RAJA::statement::Lambda< 0 >
+                    > // END i
+                  > // END j
+               > // END k
+            >; // END kernel
+    /* *INDENT-ON* */
+
   int allocator_id = axom::getResourceAllocatorID( umpire::resource::Host );
   check_execution_mappings< axom::SEQ_EXEC,
                             RAJA::loop_exec,
+                            raja_loop2d_policy,
+                            raja_loop3d_policy,
                             RAJA::loop_reduce,
                             RAJA::loop_atomic,
                             void >( allocator_id );
@@ -133,9 +165,23 @@ TEST( core_execution_space, check_omp_exec )
 
   check_valid< axom::OMP_EXEC >( );
 
+  /* *INDENT-OFF* */
+  using raja_loop2d_policy = RAJA::KernelPolicy<
+      RAJA::statement::Collapse< RAJA::omp_parallel_collapse_exec,
+                                 RAJA::ArgList< 1,0 >,
+                                 RAJA::statement::Lambda< 0 > > >;
+
+  using raja_loop3d_policy = RAJA::KernelPolicy<
+      RAJA::statement::Collapse< RAJA::omp_parallel_collapse_exec,
+                                 RAJA::ArgList< 2,1,0 >,
+                                 RAJA::statement::Lambda< 0 > > >;
+  /* *INDENT-ON* */
+
   int allocator_id = axom::getResourceAllocatorID( umpire::resource::Host );
   check_execution_mappings< axom::OMP_EXEC,
                             RAJA::omp_parallel_for_exec,
+                            raja_loop2d_policy,
+                            raja_loop3d_policy,
                             RAJA::omp_reduce,
                             RAJA::omp_atomic,
                             RAJA::omp_synchronize >( allocator_id );
@@ -151,9 +197,37 @@ TEST( core_execution_space, check_cuda_exec )
 
   check_valid< axom::CUDA_EXEC< BLOCK_SIZE > >( );
 
+  /* *INDENT-OFF* */
+  using raja_loop2d_policy =
+      RAJA::KernelPolicy<
+            RAJA::statement::CudaKernelFixed< 256,
+              RAJA::statement::For<1, RAJA::cuda_block_x_loop,
+                RAJA::statement::For<0, RAJA::cuda_thread_x_loop,
+                  RAJA::statement::Lambda<0>
+                >
+              >
+            >
+          >;
+
+  using raja_loop3d_policy =
+      RAJA::KernelPolicy<
+        RAJA::statement::CudaKernelFixed< 256,
+          RAJA::statement::For<2, RAJA::cuda_block_x_loop,
+            RAJA::statement::For<1, RAJA::cuda_block_y_loop,
+              RAJA::statement::For<0, RAJA::cuda_thread_x_loop,
+                RAJA::statement::Lambda<0>
+              >
+            >
+          >
+        >
+      >;
+  /* *INDENT-ON* */
+
   int allocator_id = axom::getResourceAllocatorID( umpire::resource::Unified );
   check_execution_mappings< axom::CUDA_EXEC< BLOCK_SIZE >,
                             RAJA::cuda_exec< BLOCK_SIZE >,
+                            raja_loop2d_policy,
+                            raja_loop3d_policy,
                             RAJA::cuda_reduce,
                             RAJA::cuda_atomic,
                             RAJA::cuda_synchronize >( allocator_id );
