8529d100ed1c3f3f332da88b9dd5f617847ed774
George Zagaris <zagaris2@llnl.gov>
2019-11-01 14:46:21 -0700

    ENH: generic for_all() and synchronize() routines
    Implement generic for_all() traversal routines and synchronize()
    primitive in axom/core based on mint::for_all() and mint::synchronize().
    Since the execution space is consolidated and defined at the top level,
    it makes more sense for the generic for_all() traversal and
    synchronize() routines to be defined in axom/core.

---
+++ src/axom/core/execution/for_all.hpp
@@ -0,0 +1,70 @@
+// Copyright (c) 2017-2019, Lawrence Livermore National Security, LLC and
+// other Axom Project Developers. See the top-level COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
+#ifndef AXOM_CORE_EXECUTION_FOR_ALL_HPP_
+#define AXOM_CORE_EXECUTION_FOR_ALL_HPP_
+
+#include "axom/config.hpp"                         /* compile time defs */
+#include "axom/core/execution/execution_space.hpp" /* execution_space traits */
+#include "axom/core/Macros.hpp"                    /* for axom Macros */
+#include "axom/core/Types.hpp"                     /* for axom::IndexType */
+
+namespace axom
+{
+
+/// \name Generic Loop Traversal Functions
+/// @{
+
+/*!
+ * \brief
+ *
+ * \param begin
+ * \param end
+ * \param kernel
+ */
+template < typename ExecSpace, typename KernelType >
+inline void for_all( const IndexType& begin, const IndexType& end,
+                     KernelType&& kernel ) noexcept
+{
+  AXOM_STATIC_ASSERT( execution_space< ExecSpace >::valid() );
+
+#ifdef AXOM_USE_RAJA
+
+  using loop_exec = typename execution_space< ExecSpace >::loop_policy;
+  RAJA::forall< loop_exec >( RAJA::RangeSegment( begin, end ),
+                             std::forward< KernelType >( kernel ) );
+
+#else
+
+  constexpr bool is_serial = std::is_same< ExecSpace, SEQ_EXEC >::value;
+  AXOM_STATIC_ASSERT( is_serial );
+  for ( IndexType i=begin; i < end; ++i )
+  {
+    kernel( i );
+  }
+
+#endif
+
+}
+
+/*!
+ * \brief
+ *
+ * \param N
+ * \param kernel
+ *
+ */
+template < typename ExecSpace, typename KernelType >
+inline void for_all( const IndexType& N, KernelType&& kernel ) noexcept
+{
+  AXOM_STATIC_ASSERT( execution_space< ExecSpace >::valid() );
+  for_all< ExecSpace >( 0, N, std::forward< KernelType >( kernel ) );
+}
+
+/// @}
+
+} /* namespace axom */
+
+#endif /* AXOM_CORE_EXECUTION_FOR_ALL_HPP_ */
