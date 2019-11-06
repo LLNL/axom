c5d8542e08de9577f0011635e0a562ae921de0d3
Brian Manh Hien Han <han12@rzansel15.coral.llnl.gov>
2019-11-05 16:15:57 -0800

    Added policy for OpenMP; CUDA policy commented out

--- src/axom/spin/examples/naive_intersect_raja.cpp
+++ src/axom/spin/examples/naive_intersect_raja.cpp
@@ -2,13 +2,19 @@
 // other Axom Project Developers. See the top-level COPYRIGHT file for details.
 //
 // SPDX-License-Identifier: (BSD-3-Clause)
-//
+
+
 // Axom primitives
 #include "axom/primal/geometry/Point.hpp"
 #include "axom/primal/geometry/Triangle.hpp"
 
 // Axom operations
 #include "axom/primal/operators/intersect.hpp"
+
+#include "axom/slic.hpp"
+
+// Testing memory_management
+#include "axom/core/memory_management.hpp"
 
 // C++ headers
 #include <cmath> // do we need this?
@@ -20,9 +26,6 @@
 
 // RAJA 
 #include "RAJA/RAJA.hpp"
-
-// Testing memory_management
-#include "axom/core/memory_management.hpp"
 
 /*
   CUDA_BLOCK_SIZE - specifies the number of threads in a CUDA thread block
@@ -123,8 +126,8 @@ void driveUniformGrid()
   findTriIntersectionsNaively(trisIntersect, naiveclashesIntersect);
 
   std::cout << "----- driveUniformGrid -----" << std::endl;
-  printPairs(" clashes found by naive algorithm for non-intersecting triangles:", naiveclashesNonIntersect);
-  printPairs(" clashes found by naive algorithm for intersecting triangles:", naiveclashesIntersect);
+  printPairs(" clashes found by naive algorithm for non-intersecting triangles (0 is expected):", naiveclashesNonIntersect);
+  printPairs(" clashes found by naive algorithm for intersecting triangles (5 is expected):", naiveclashesIntersect);
   std::cout << std::endl;
 }
 
@@ -142,14 +145,17 @@ void try_axom_umpire(umpire::Allocator allocator)
     c[i] = 0;
   }
 
-  using EXEC_POL = RAJA::cuda_exec<CUDA_BLOCK_SIZE>;
+  using EXEC_POL = RAJA::seq_exec;
+  //using EXEC_POL = RAJA::cuda_exec<CUDA_BLOCK_SIZE>;
 
   //add
-  RAJA::forall< EXEC_POL >(RAJA::RangeSegment(0, 10), [=] RAJA_DEVICE (int i) { 
+  RAJA::forall< EXEC_POL >(RAJA::RangeSegment(0, 10), [=] AXOM_HOST_DEVICE (int i) { 
+  //RAJA::forall< EXEC_POL >(RAJA::RangeSegment(0, 10), [=] (int i) { 
     c[i] = a[i] + b[i]; 
   });    
 
   //Print
+  std::cout << "----- try_axom_umpire -----" << std::endl;
   for (int i = 0; i < 10; ++i) {
     std::cout << c[i] << std::endl;
   }
@@ -196,6 +202,8 @@ void try_RAJA_assignment(umpire::Allocator allocator)
     trie[i][0] = p[3];
   }); 
 
+  std::cout << "----- try_RAJA_assignment -----" << std::endl;
+  std::cout << "Trying assignment in RAJA loop (1st point should all be the same)" << std::endl;
   std::cout << trie[0] << std::endl;
   std::cout << trie[1] << std::endl;
   std::cout << trie[2] << std::endl;
@@ -214,7 +222,6 @@ void try_intersect(umpire::Allocator allocator)
   //Setup data structures
   TriangleType * tri = axom::allocate <TriangleType> (6, allocator);
   PointType * p = axom::allocate <PointType> (5, allocator);
-  int * numIntersect = axom::allocate <int> (1, allocator);
   
   p[0] = PointType::make_point(0.0,  1.0, 0.0);
   p[1] = PointType::make_point(-1.0,  0.0, 0.0);
@@ -236,52 +243,85 @@ void try_intersect(umpire::Allocator allocator)
   tri[4] = t4;
   tri[5] = t5;
 
-
-
   RAJA::RangeSegment row_range(0, 6);
   RAJA::RangeSegment col_range(0, 6);
 
-  using ATOMIC_POL_SEQ = RAJA::seq_atomic;
-  using KERNEL_EXEC_POL_SEQ = 
+  // Sequential Policies
+  // using REDUCE_POL = RAJA::seq_reduce;
+  // using ATOMIC_POL= RAJA::seq_atomic;
+  // using KERNEL_EXEC_POL = 
+  //   RAJA::KernelPolicy<
+  //     RAJA::statement::For<1, RAJA::seq_exec,
+  //       RAJA::statement::For<0, RAJA::seq_exec,
+  //         RAJA::statement::Lambda<0>
+  //       >
+  //     >
+  //   >;
+  
+
+  // OpenMP Policies
+  using REDUCE_POL = RAJA::omp_reduce;
+  using ATOMIC_POL= RAJA::omp_atomic;
+  using KERNEL_EXEC_POL = 
     RAJA::KernelPolicy<
-      RAJA::statement::For<1, RAJA::seq_exec,
-        RAJA::statement::For<0, RAJA::seq_exec,
+      RAJA::statement::For<1, RAJA::omp_parallel_for_exec,
+        RAJA::statement::For<0, RAJA::loop_exec,
           RAJA::statement::Lambda<0>
         >
       >
     >;
+  
+
+  // CUDA Policies
+  // using REDUCE_POL = RAJA::cuda_reduce;
+  // using ATOMIC_POL= RAJA::cuda_atomic;
+  // using KERNEL_EXEC_POL = 
+  //   RAJA::KernelPolicy<
+  //     RAJA::statement::CudaKernelFixed<CUDA_BLOCK_SIZE,
+  //       RAJA::statement::For<1, RAJA::cuda_block_x_loop,
+  //         RAJA::statement::For<0, RAJA::cuda_thread_x_loop,
+  //           RAJA::statement::Lambda<0>
+  //         >
+  //       >
+  //     >
+  //   >;
+
+
+  RAJA::ReduceSum< REDUCE_POL, int > numIntersect(0);  
 
   // RAJA loop to find size to initialize (number of intersections)
-  RAJA::kernel<KERNEL_EXEC_POL_SEQ>( RAJA::make_tuple(col_range, row_range),
-    [=] (int col, int row) {
+    // NOte : reduction more optimal
+  RAJA::kernel<KERNEL_EXEC_POL>( RAJA::make_tuple(col_range, row_range),
+    [=] AXOM_HOST_DEVICE (int col, int row) {
     if (row > col)
     {
       if (intersect (tri[row], tri[col]))
       {
-        RAJA::atomicAdd<ATOMIC_POL_SEQ>(numIntersect, 1);
+        numIntersect += 1;
       }
     }
   });
   
   // Print size
-  std::cout << "Number of intersections is " << numIntersect[0] << "(Expected was 5)" << std::endl;
+  std::cout << "----- try_intersect -----" << std::endl;
+  std::cout << "Number of intersections is " << numIntersect.get() << " (Expected was 5)" << std::endl;
 
   // Allocation to hold intersection pairs and counter to know where to store
-  int * intersections = axom::allocate <int> (numIntersect[0] * 2, allocator);
+  int * intersections = axom::allocate <int> (numIntersect.get() * 2, allocator);
   int * counter = axom::allocate <int> (1, allocator);
   
   counter[0] = 0;
 
   // RAJA loop to populate with intersections
-  RAJA::kernel<KERNEL_EXEC_POL_SEQ>( RAJA::make_tuple(col_range, row_range),
-    [=] (int col, int row) {
+  RAJA::kernel<KERNEL_EXEC_POL>( RAJA::make_tuple(col_range, row_range),
+    [=] AXOM_HOST_DEVICE (int col, int row) {
     if (row > col)
     {
       if (intersect (tri[row], tri[col]))
       {
-        intersections[counter[0]] = row;
-        intersections[counter[0] + 1] = col;
-        RAJA::atomicAdd<ATOMIC_POL_SEQ>(counter, 2);
+        auto idx = RAJA::atomicAdd<ATOMIC_POL>(counter, 2);
+        intersections[idx] = row;
+        intersections[idx+1] = col;
 
       }
     }
@@ -289,7 +329,7 @@ void try_intersect(umpire::Allocator allocator)
 
   // Print intersection pairs
   std::cout << "Intersection pairs were: " << std::endl;
-  for (int i = 0 ; i < numIntersect[0] * 2 ; i = i + 2)
+  for (int i = 0 ; i < numIntersect.get() * 2 ; i = i + 2)
   {
     std::cout << intersections[i] << "   " << intersections[i + 1] << std::endl;
   }
@@ -297,7 +337,6 @@ void try_intersect(umpire::Allocator allocator)
   // Deallocate 
   axom::deallocate(tri);
   axom::deallocate(p);
-  axom::deallocate(numIntersect);
   axom::deallocate(intersections);
   axom::deallocate(counter);
 }
@@ -307,11 +346,26 @@ void try_intersect(umpire::Allocator allocator)
 int main(int argc, char** argv)
 {
 
+   #ifdef AXOM_USE_UMPIRE
+    std::cout << "UMPIRE IS USED!!!" << std::endl;
+  #endif
+
+  #if defined(RAJA_ENABLE_OPENMP)
+    std::cout << "OPENMP IS ENABLED!!!" << std::endl;
+
+    umpire::Allocator allocator= axom::getAllocator( axom::getResourceAllocatorID (umpire::resource::Host));
+  #endif
+
+  #if defined(RAJA_ENABLE_CUDA)
+    std::cout << "CUDA IS ENABLED!!!" << std::endl;
+    umpire::Allocator allocator= axom::getAllocator( axom::getResourceAllocatorID (umpire::resource::Unified));
+  #endif
+
   // Deal with unused variables
   AXOM_DEBUG_VAR(argc);
   AXOM_DEBUG_VAR(argv);
 
-  umpire::Allocator allocator= axom::getAllocator( axom::getResourceAllocatorID (umpire::resource::Unified));
+  
 
   driveUniformGrid();
   try_axom_umpire(allocator);
