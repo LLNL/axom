758c742503b8c602aabf15c25076305782386b2e
Kenneth Weiss <weiss27@llnl.gov>
2019-12-26 14:01:39 -0800

    Improves consistency in sidre::Group's logging messages
    All logging calls now include the Graoup's pathName().
    This is implemented by adding a macro -- SIDRE_GROUP_LOG_PREPEND.
    
    Misc:
    * Also includes a bugfix for some checking code that protects against dereferencing a null ptr
    * Adds an optimization to Group::getPathName() to only call Group::getPath() once

--- src/axom/sidre/core/Group.hpp
+++ src/axom/sidre/core/Group.hpp
@@ -13,11 +13,14 @@
  ******************************************************************************
  */
 
-#ifndef DATAGROUP_HPP_
-#define DATAGROUP_HPP_
+#ifndef SIDRE_GROUP_HPP_
+#define SIDRE_GROUP_HPP_
 
+// axom headers
 #include "axom/config.hpp"
 #include "axom/core/Macros.hpp"
+#include "axom/core/Types.hpp"
+#include "axom/slic.hpp"
 
 // Standard C++ headers
 #include <memory>
@@ -32,11 +35,7 @@
 #include "hdf5.h"
 #endif
 
-// Other axom headers
-#include "axom/slic/interface/slic.hpp"
-#include "axom/core/Types.hpp"
-
-// Sidre project headers
+// Sidre headers
 #include "SidreTypes.hpp"
 #include "View.hpp"
 
@@ -177,12 +176,14 @@ public:
    */
   std::string getPathName() const
   {
-    if (getPath().length() < 1)
+    const auto p = getPath();
+
+    if (p.length() < 1)
     {
       return getName();
     }
 
-    return getPath() + getPathDelimiter() + getName();
+    return p + getPathDelimiter() + getName();
   }
 
   /*!
@@ -1634,4 +1635,4 @@ private:
 } /* end namespace sidre */
 } /* end namespace axom */
 
-#endif /* DATAGROUP_HPP_ */
+#endif /* SIDRE_GROUP_HPP_ */
