dc939cb8fd4537b5fd8a4b2aaf687715a3889515
Chris White <white238@llnl.gov>
2020-04-03 12:53:33 -0700

    Adding echos

--- scripts/azure-pipelines/linux-build_and_test.sh
+++ scripts/azure-pipelines/linux-build_and_test.sh
@@ -25,18 +25,22 @@ echo HOST_CONFIG
 echo $HOST_CONFIG
 
 if [[ "$DO_BUILD" == "yes" ]] ; then
+    echo "~~~~~~ RUNNING CMAKE ~~~~~~~~"
     or_die ./config-build.py -hc /home/axom/axom/host-configs/docker/${HOST_CONFIG}.cmake -DENABLE_GTEST_DEATH_TESTS=ON
     or_die cd build-$HOST_CONFIG-debug
+    echo "~~~~~~ BUILDING ~~~~~~~~"
     if [[ ${CMAKE_EXTRA_FLAGS} == *COVERAGE* ]] ; then
         or_die make -j 10
     else
         or_die make -j 10 VERBOSE=1
     fi
     if [[ "${DO_TEST}" == "yes" ]] ; then
+        echo "~~~~~~ RUNNING TESTS ~~~~~~~~"
         make CTEST_OUTPUT_ON_FAILURE=1 test ARGS='-T Test -VV -j8'
     fi
     if [[ "${DO_MEMCHECK}" == "yes" ]] ; then
-      or_die ctest -T memcheck
+        echo "~~~~~~ RUNNING MEMCHECK ~~~~~~~~"
+        or_die ctest -T memcheck
     fi
 fi
 
@@ -44,6 +48,7 @@ find ./axom/sidre -type d -exec chmod 755 {} \;
 find ./axom/sidre -type f -exec chmod 644 {} \;
 
 if [[ "$DO_CLEAN" == "yes" ]] ; then
+    echo "~~~~~~ CLEANING BUILD DIRECTORY ~~~~~~~~"
     make clean
 fi
 
