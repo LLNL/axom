e81ce66f16dea7fa8f01277cd5087b42dfa84a87
Kae Suarez <suarez29@llnl.gov>
2020-07-14 10:32:07 -0700

    Adding Map.hpp as a source file.

---
+++ src/axom/core/Map.hpp
@@ -0,0 +1,19 @@
+//Copyright (c) 2017-2020, Lawrence Livermore National Security, LLC and
+//other Axom Project Developers. See the top-level COPYRIGHT file for details.
+//
+//SPDX-License-Identifier: (BSD-3-Clause)
+
+#ifndef AXOM_MAP_HPP_
+#define AXOM_MAP_HPP_
+
+namespace axom
+{
+
+
+
+
+
+
+} /* namespace axom */
+
+#endif /* AXOM_MAP_HPP_ */
