00cbdcc81c941405978b731335543c2b8fc4b90e
Aesha Parekh <aeshabparekh@gmail.com>
2020-07-22 16:34:04 -0700

    added documentation for utils

--- src/axom/inlet/inlet_utils.hpp
+++ src/axom/inlet/inlet_utils.hpp
@@ -8,6 +8,16 @@ namespace axom
 namespace inlet
 {
 
+
+/*!
+  *****************************************************************************
+  * \brief This function is used to mark if anything went wrong during the 
+  * defining phase of inlet so verify() will properly fail.
+  *
+  * \param [in] root Pointer to the Sidre Root Group where the warning flag 
+  * will be set.
+  *****************************************************************************
+  */
 void setWarningFlag(axom::sidre::Group* root);
 
 }
