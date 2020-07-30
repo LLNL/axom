54e304e589d77ea93bdaf160f7fa9848026d14d5
George Zagaris <zagaris2@llnl.gov>
2020-07-30 16:31:23 -0700

    ENH: add axom::getVersion() convenience method

--- src/axom/core/tests/utils_about.cpp
+++ src/axom/core/tests/utils_about.cpp
@@ -5,9 +5,24 @@
 
 #include "gtest/gtest.h"
 
+#include "axom/config.hpp" 
 #include "axom/core/utilities/About.hpp"
+
+// C/C++ includes
+#include <sstream> // for std::ostringstream
 
 TEST(core_about,print_about)
 {
   axom::about();
+}
+
+//-----------------------------------------------------------------------------
+TEST(core_about,get_version)
+{
+  std::ostringstream EXPECTED_VERSION_STRING;
+  EXPECTED_VERSION_STRING << AXOM_VERSION_FULL << "-";
+  EXPECTED_VERSION_STRING << AXOM_VERSION_EXTRA;
+
+  std::string axom_version = axom::getVersion();
+  EXPECT_EQ( EXPECTED_VERSION_STRING.str(), axom_version );
 }
