134d2d72da6ae46900cdcc58d0dee0a2714c1421
Chris White <white238@llnl.gov>
2020-08-04 17:03:09 -0700

    further loosen variants

--- scripts/uberenv/packages/axom/package.py
+++ scripts/uberenv/packages/axom/package.py
@@ -124,8 +124,7 @@ class Axom(CMakePackage, CudaPackage):
         depends_on('umpire cuda_arch={0}'.format(sm_),
                    when='+umpire cuda_arch={0}'.format(sm_))
 
-    depends_on("mfem~hypre~metis~zlib", when="+mfem")
-    depends_on("mfem~hypre~metis~zlib~mpi", when="+mfem~mpi")
+    depends_on("mfem~mpi", when="+mfem~mpi")
 
     depends_on("python", when="+python")
 
