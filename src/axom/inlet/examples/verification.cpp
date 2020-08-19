a68c2ebce5c01a563de759a5b4577d01018ed20a
Aesha Parekh <aeshabparekh@gmail.com>
2020-08-19 11:50:46 -0700

    Added comments for new example

--- src/axom/inlet/examples/verification.cpp
+++ src/axom/inlet/examples/verification.cpp
@@ -31,12 +31,16 @@ void example() {
     return false;
   });
 
+  // We expect verification to be unsuccessful since the only Field
+  // in vector is x but 2 dimensions are expected
   myInlet->verify() ? std::cout << "Verification was successful\n" 
                     : std::cout << "Verification was unsuccessful\n";
   
    v->addInt("y");
   std::cout << "After adding a new dimension:\n";
 
+  // We expect the verification to succeed because vector now contains
+  // both x and y to match the 2 dimensions
   myInlet->verify() ? std::cout << "Verification was successful\n" 
                     : std::cout << "Verification was unsuccessful\n";
 
