de2dc99760f43658a9ac5bcf7001cce79a893962
Chris White <white238@llnl.gov>
2020-08-20 18:07:37 -0700

    silence warning, clarify output

--- src/axom/inlet/examples/verification.cpp
+++ src/axom/inlet/examples/verification.cpp
@@ -44,14 +44,16 @@ void example() {
 
   // We expect verification to be unsuccessful since the only Field
   // in vector is x but 2 dimensions are expected
+  std::cout << "This should fail due to a missing dimension:\n";
   myInlet->verify() ? std::cout << "Verification was successful\n" 
                     : std::cout << "Verification was unsuccessful\n";
   
+  // Add required dimension to schema
   v->addInt("y");
-  std::cout << "After adding a new dimension:\n";
 
   // We expect the verification to succeed because vector now contains
   // both x and y to match the 2 dimensions
+  std::cout << "After adding the required dimension:\n";
   myInlet->verify() ? std::cout << "Verification was successful\n" 
                     : std::cout << "Verification was unsuccessful\n";
   // _inlet_workflow_verification_end
