9d6b290db4ab543ba2e0e9a019db021d9535678a
Josh Essman <essman1@llnl.gov>
2020-09-22 16:34:27 -0500

    build: add new guard option to AxomOptions.cmake

--- src/cmake/AxomOptions.cmake
+++ src/cmake/AxomOptions.cmake
@@ -10,6 +10,7 @@ option(AXOM_ENABLE_ANNOTATIONS "Enables code annotations to facilitate performan
 option(AXOM_ENABLE_SPARSEHASH "Enables Sparsehash." ON)
 option(AXOM_ENABLE_ALL_COMPONENTS "Enables all components by default" ON)
 option(AXOM_USE_64BIT_INDEXTYPE "Use 64-bit integers for axom::IndexType" OFF)
+option(AXOM_ENABLE_MFEM_SIDRE_DATACOLLECTION "Enable Axom's version of the MFEM SidreDataCollection" OFF)
 
 if(NOT CMAKE_CONFIGURATION_TYPES)
     if(CMAKE_BUILD_TYPE MATCHES "(Debug|RelWithDebInfo)")
