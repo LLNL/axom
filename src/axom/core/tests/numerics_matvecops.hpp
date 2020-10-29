61ab64caa560d44bbb208fcd6b85fe71c036e0e1
Chris White <white238@llnl.gov>
2020-10-29 15:14:59 -0700

    remove shortening of namespace is hopefully tripping up msvc, move spio test to lower mpi tasks

--- src/axom/core/tests/numerics_matvecops.hpp
+++ src/axom/core/tests/numerics_matvecops.hpp
@@ -8,9 +8,6 @@
 #include "axom/core/numerics/matvecops.hpp"
 #include "axom/core/utilities/Utilities.hpp"
 
-namespace numerics = axom::numerics;
-namespace utilities = axom::utilities;
-
 namespace
 {
 /*!
@@ -35,17 +32,17 @@ void expect_vector_eq(const double* u, const double* v, int N)
 //------------------------------------------------------------------------------
 TEST(numerics_matvecops, check_invalid_matrix_addition)
 {
-  numerics::Matrix<int> A(2, 2);
-  numerics::Matrix<int> B(3, 3);
-  numerics::Matrix<int> C(3, 3);
-  numerics::Matrix<int> C2(3, 3);
+  axom::numerics::Matrix<int> A(2, 2);
+  axom::numerics::Matrix<int> B(3, 3);
+  axom::numerics::Matrix<int> C(3, 3);
+  axom::numerics::Matrix<int> C2(3, 3);
 
-  EXPECT_FALSE(numerics::matrix_add(A, B, C));
+  EXPECT_FALSE(axom::numerics::matrix_add(A, B, C));
   EXPECT_EQ(C.getNumColumns(), 1);
   EXPECT_EQ(C.getNumRows(), 1);
   EXPECT_EQ(C(0, 0), 0);
 
-  EXPECT_FALSE(numerics::matrix_add(B, C2, A));
+  EXPECT_FALSE(axom::numerics::matrix_add(B, C2, A));
   EXPECT_EQ(A.getNumColumns(), 1);
   EXPECT_EQ(A.getNumRows(), 1);
   EXPECT_EQ(A(0, 0), 0);
@@ -55,11 +52,11 @@ TEST(numerics_matvecops, check_invalid_matrix_addition)
 TEST(numerics_matvecops, matrix_add)
 {
   constexpr int N = 3;
-  numerics::Matrix<int> I1 = numerics::Matrix<int>::identity(N);
-  numerics::Matrix<int> I2 = numerics::Matrix<int>::identity(N);
+  axom::numerics::Matrix<int> I1 = axom::numerics::Matrix<int>::identity(N);
+  axom::numerics::Matrix<int> I2 = axom::numerics::Matrix<int>::identity(N);
 
-  numerics::Matrix<int> A(N, N);
-  EXPECT_TRUE(numerics::matrix_add(I1, I2, A));
+  axom::numerics::Matrix<int> A(N, N);
+  EXPECT_TRUE(axom::numerics::matrix_add(I1, I2, A));
 
   for(int i = 0; i < N; ++i)
   {
@@ -74,17 +71,17 @@ TEST(numerics_matvecops, matrix_add)
 //------------------------------------------------------------------------------
 TEST(numerics_matvecops, check_invalid_matrix_subtraction)
 {
-  numerics::Matrix<int> A(2, 2);
-  numerics::Matrix<int> B(3, 3);
-  numerics::Matrix<int> C(3, 3);
-  numerics::Matrix<int> C2(3, 3);
+  axom::numerics::Matrix<int> A(2, 2);
+  axom::numerics::Matrix<int> B(3, 3);
+  axom::numerics::Matrix<int> C(3, 3);
+  axom::numerics::Matrix<int> C2(3, 3);
 
-  EXPECT_FALSE(numerics::matrix_subtract(A, B, C));
+  EXPECT_FALSE(axom::numerics::matrix_subtract(A, B, C));
   EXPECT_EQ(C.getNumColumns(), 1);
   EXPECT_EQ(C.getNumRows(), 1);
   EXPECT_EQ(C(0, 0), 0);
 
-  EXPECT_FALSE(numerics::matrix_subtract(B, C2, A));
+  EXPECT_FALSE(axom::numerics::matrix_subtract(B, C2, A));
   EXPECT_EQ(A.getNumColumns(), 1);
   EXPECT_EQ(A.getNumRows(), 1);
   EXPECT_EQ(A(0, 0), 0);
@@ -96,11 +93,11 @@ TEST(numerics_matvecops, matrix_subtract)
   constexpr int ZERO = 0;
   constexpr int N = 3;
 
-  numerics::Matrix<int> I1 = numerics::Matrix<int>::identity(N);
-  numerics::Matrix<int> I2 = numerics::Matrix<int>::identity(N);
+  axom::numerics::Matrix<int> I1 = axom::numerics::Matrix<int>::identity(N);
+  axom::numerics::Matrix<int> I2 = axom::numerics::Matrix<int>::identity(N);
 
-  numerics::Matrix<int> A(N, N);
-  EXPECT_TRUE(numerics::matrix_subtract(I1, I2, A));
+  axom::numerics::Matrix<int> A(N, N);
+  EXPECT_TRUE(axom::numerics::matrix_subtract(I1, I2, A));
 
   for(int i = 0; i < N; ++i)
   {
@@ -114,16 +111,16 @@ TEST(numerics_matvecops, matrix_subtract)
 //------------------------------------------------------------------------------
 TEST(numerics_matvecops, check_invalid_matrix_multiplication)
 {
-  numerics::Matrix<int> A(2, 3);
-  numerics::Matrix<int> B(3, 2);
-  numerics::Matrix<int> C(3, 3);
+  axom::numerics::Matrix<int> A(2, 3);
+  axom::numerics::Matrix<int> B(3, 2);
+  axom::numerics::Matrix<int> C(3, 3);
 
-  EXPECT_FALSE(numerics::matrix_multiply(A, B, C));
+  EXPECT_FALSE(axom::numerics::matrix_multiply(A, B, C));
   EXPECT_EQ(C.getNumColumns(), 1);
   EXPECT_EQ(C.getNumRows(), 1);
   EXPECT_EQ(C(0, 0), 0);
 
-  EXPECT_FALSE(numerics::matrix_multiply(A, C, B));
+  EXPECT_FALSE(axom::numerics::matrix_multiply(A, C, B));
   EXPECT_EQ(B.getNumColumns(), 1);
   EXPECT_EQ(B.getNumRows(), 1);
   EXPECT_EQ(B(0, 0), 0);
@@ -133,7 +130,7 @@ TEST(numerics_matvecops, check_invalid_matrix_multiplication)
 TEST(numerics_matvecops, matrix_matrix_multiply)
 {
   // Setup test matrix A
-  numerics::Matrix<int> A(2, 3);
+  axom::numerics::Matrix<int> A(2, 3);
   A(0, 0) = 1;
   A(0, 1) = 2;
   A(0, 2) = 3;
@@ -142,7 +139,7 @@ TEST(numerics_matvecops, matrix_matrix_multiply)
   A(1, 2) = 6;
 
   // Setup test matrix B
-  numerics::Matrix<int> B(3, 2);
+  axom::numerics::Matrix<int> B(3, 2);
   B(0, 0) = 1;
   B(0, 1) = 2;
   B(1, 0) = 3;
@@ -150,17 +147,17 @@ TEST(numerics_matvecops, matrix_matrix_multiply)
   B(2, 0) = 5;
   B(2, 1) = 6;
 
-  numerics::Matrix<int> C(2, 2);
-  EXPECT_TRUE(numerics::matrix_multiply(A, B, C));
+  axom::numerics::Matrix<int> C(2, 2);
+  EXPECT_TRUE(axom::numerics::matrix_multiply(A, B, C));
 
   EXPECT_EQ(22, C(0, 0));
   EXPECT_EQ(28, C(0, 1));
   EXPECT_EQ(49, C(1, 0));
   EXPECT_EQ(64, C(1, 1));
 
-  numerics::Matrix<int> I2 = numerics::Matrix<int>::identity(2);
-  numerics::Matrix<int> C2(2, 2);
-  EXPECT_TRUE(numerics::matrix_multiply(C, I2, C2));
+  axom::numerics::Matrix<int> I2 = axom::numerics::Matrix<int>::identity(2);
+  axom::numerics::Matrix<int> C2(2, 2);
+  EXPECT_TRUE(axom::numerics::matrix_multiply(C, I2, C2));
 
   EXPECT_EQ(22, C2(0, 0));
   EXPECT_EQ(28, C2(0, 1));
@@ -174,8 +171,8 @@ TEST(numerics_matvecops, matrix_scalar_multiply)
   constexpr int N = 3;
   constexpr int SCALAR = 2;
 
-  numerics::Matrix<int> A = numerics::Matrix<int>::identity(N);
-  numerics::matrix_scalar_multiply(A, SCALAR);
+  axom::numerics::Matrix<int> A = axom::numerics::Matrix<int>::identity(N);
+  axom::numerics::matrix_scalar_multiply(A, SCALAR);
 
   for(int i = 0; i < N; ++i)
   {
@@ -191,7 +188,7 @@ TEST(numerics_matvecops, matrix_scalar_multiply)
 TEST(numerics_matvecops, matrix_vector_multiply)
 {
   constexpr int N = 2;
-  numerics::Matrix<int> A(N, N);
+  axom::numerics::Matrix<int> A(N, N);
   A(0, 0) = 1;
   A(0, 1) = 2;
   A(1, 0) = 3;
@@ -201,7 +198,7 @@ TEST(numerics_matvecops, matrix_vector_multiply)
   int v[2] = {1, 2};
   int expected[2] = {5, 11};
 
-  numerics::matrix_vector_multiply(A, v, res);
+  axom::numerics::matrix_vector_multiply(A, v, res);
 
   for(int i = 0; i < N; ++i)
   {
@@ -212,10 +209,10 @@ TEST(numerics_matvecops, matrix_vector_multiply)
 //------------------------------------------------------------------------------
 TEST(numerics_matvecops, check_invalid_matrix_transpose)
 {
-  numerics::Matrix<int> A(2, 3);
-  numerics::Matrix<int> M(10, 10);
+  axom::numerics::Matrix<int> A(2, 3);
+  axom::numerics::Matrix<int> M(10, 10);
 
-  EXPECT_FALSE(numerics::matrix_transpose(A, M));
+  EXPECT_FALSE(axom::numerics::matrix_transpose(A, M));
   EXPECT_EQ(M.getNumRows(), 1);
   EXPECT_EQ(M.getNumColumns(), 1);
   EXPECT_EQ(M(0, 0), 0);
@@ -226,7 +223,7 @@ TEST(numerics_matvecops, matrix_transpose)
 {
   // Setup matrix: (1 2 3)
   //               (4 5 6)
-  numerics::Matrix<int> A(2, 3);
+  axom::numerics::Matrix<int> A(2, 3);
   A(0, 0) = 1;
   A(0, 1) = 2;
   A(0, 2) = 3;
@@ -235,8 +232,8 @@ TEST(numerics_matvecops, matrix_transpose)
   A(1, 2) = 6;
 
   // Matrix transpose
-  numerics::Matrix<int> M(A.getNumColumns(), A.getNumRows());
-  EXPECT_TRUE(numerics::matrix_transpose(A, M));
+  axom::numerics::Matrix<int> M(A.getNumColumns(), A.getNumRows());
+  EXPECT_TRUE(axom::numerics::matrix_transpose(A, M));
 
   EXPECT_EQ(1, M(0, 0));
   EXPECT_EQ(2, M(1, 0));
@@ -265,18 +262,18 @@ TEST(numerics_matvecops, matrix_norm)
   };
   // clang-format on
 
-  numerics::Matrix<double> A(N, N, data, ZERO_COPY);
+  axom::numerics::Matrix<double> A(N, N, data, ZERO_COPY);
 
   // STEP 1: test p1-norm
-  double p1norm = numerics::matrix_norm(A, numerics::P1_NORM);
+  double p1norm = axom::numerics::matrix_norm(A, axom::numerics::P1_NORM);
   EXPECT_DOUBLE_EQ(p1norm, EXPECTED_P1_NORM);
 
   // STEP 2: test infinity-norm
-  double inftynorm = numerics::matrix_norm(A, numerics::INF_NORM);
+  double inftynorm = axom::numerics::matrix_norm(A, axom::numerics::INF_NORM);
   EXPECT_DOUBLE_EQ(inftynorm, EXPECTED_INFTY_NORM);
 
   // STEP 3: test frobenius norm
-  double frobnorm = numerics::matrix_norm(A, numerics::FROBENIUS_NORM);
+  double frobnorm = axom::numerics::matrix_norm(A, axom::numerics::FROBENIUS_NORM);
   EXPECT_DOUBLE_EQ(frobnorm, EXPECTED_FROBENIOUS_NORM);
 }
 
@@ -294,16 +291,16 @@ TEST(numerics_matvecops, linspace)
   const double x0 = -2.0;
   const double x1 = 3.0;
 
-  EXPECT_FALSE(numerics::linspace(x0, x1, vc, 0));
-  EXPECT_FALSE(numerics::linspace(x0, x1, vc, 1));
+  EXPECT_FALSE(axom::numerics::linspace(x0, x1, vc, 0));
+  EXPECT_FALSE(axom::numerics::linspace(x0, x1, vc, 1));
 
-  EXPECT_TRUE(numerics::linspace(x0, x1, vc, 6));
+  EXPECT_TRUE(axom::numerics::linspace(x0, x1, vc, 6));
   expect_vector_eq(vc, v1, 6);
 
-  EXPECT_TRUE(numerics::linspace(x1, x0, vc, 6));
+  EXPECT_TRUE(axom::numerics::linspace(x1, x0, vc, 6));
   expect_vector_eq(vc, v2, 6);
 
-  EXPECT_TRUE(numerics::linspace(0.0, 1.0, vc, 3));
+  EXPECT_TRUE(axom::numerics::linspace(0.0, 1.0, vc, 3));
   expect_vector_eq(vc, v3, 3);
 }
 
@@ -323,19 +320,19 @@ TEST(numerics_matvecops, vector_cross_product)
 
   double w[3];
 
-  numerics::cross_product(e1, e2, w);
+  axom::numerics::cross_product(e1, e2, w);
   expect_vector_eq(w, e3, NDIMS);
 
-  numerics::cross_product(e2, e1, w);
+  axom::numerics::cross_product(e2, e1, w);
   expect_vector_eq(w, me3, NDIMS);
 
-  numerics::cross_product(e3, e1, w);
+  axom::numerics::cross_product(e3, e1, w);
   expect_vector_eq(w, e2, NDIMS);
 
-  numerics::cross_product(e2, e3, w);
+  axom::numerics::cross_product(e2, e3, w);
   expect_vector_eq(w, e1, NDIMS);
 
-  numerics::cross_product(u, v, w);
+  axom::numerics::cross_product(u, v, w);
   expect_vector_eq(w, u_x_v, NDIMS);
 }
 
@@ -351,17 +348,17 @@ TEST(numerics_matvecops, vector_dot_product)
   u[0] = 1.;
   v[1] = 1.;
 
-  EXPECT_NEAR(numerics::dot_product<double>(u, v, dim), 0., EPS);
+  EXPECT_NEAR(axom::numerics::dot_product<double>(u, v, dim), 0., EPS);
 
   u[1] = 1.;
   v[0] = 1.;
 
-  EXPECT_NEAR(numerics::dot_product<double>(u, v, dim), 2., EPS);
+  EXPECT_NEAR(axom::numerics::dot_product<double>(u, v, dim), 2., EPS);
 
   u[5] = 10.;
   v[5] = -10.;
 
-  EXPECT_NEAR(numerics::dot_product<double>(u, v, dim), -98., EPS);
+  EXPECT_NEAR(axom::numerics::dot_product<double>(u, v, dim), -98., EPS);
 
   // free up allocated memory
   delete[] u;
@@ -386,13 +383,13 @@ TEST(numerics_matvecops, vector_make_orthogonal)
   v[1] = 1.;
   v[2] = 1.;
 
-  numerics::make_orthogonal<double>(u, v, dim);
+  axom::numerics::make_orthogonal<double>(u, v, dim);
 
   EXPECT_NEAR(u[0], 1. - ONE_THIRD, EPS);
   EXPECT_NEAR(u[1], 1. - ONE_THIRD, EPS);
   EXPECT_NEAR(u[2], -1. - ONE_THIRD, EPS);
 
-  numerics::make_orthogonal(u, u, dim);
+  axom::numerics::make_orthogonal(u, u, dim);
 
   EXPECT_NEAR(u[0], 0., EPS);
   EXPECT_NEAR(u[1], 0., EPS);
@@ -420,7 +417,7 @@ TEST(numerics_matvecops, vector_orthonormalize)
   u[1] = 1.;
   v[0] = 2.;
 
-  EXPECT_TRUE(numerics::orthonormalize<double>(basis, size, dim));
+  EXPECT_TRUE(axom::numerics::orthonormalize<double>(basis, size, dim));
 
   EXPECT_NEAR(u[0], ONE_OVER_SQRT_TWO, EPS);
   EXPECT_NEAR(u[1], ONE_OVER_SQRT_TWO, EPS);
@@ -431,7 +428,7 @@ TEST(numerics_matvecops, vector_orthonormalize)
   EXPECT_NEAR(v[2], 0., EPS);
 
   // doesn't span
-  EXPECT_FALSE(numerics::orthonormalize<double>(basis, dim, dim));
+  EXPECT_FALSE(axom::numerics::orthonormalize<double>(basis, dim, dim));
 
   // free up allocated memory
   delete[] basis;
@@ -456,21 +453,21 @@ TEST(numerics_matvecops, vector_normalize)
   v[1] = 3.;
   v[2] = 1.;
 
-  EXPECT_TRUE(numerics::normalize<double>(u, dim));
+  EXPECT_TRUE(axom::numerics::normalize<double>(u, dim));
 
   EXPECT_NEAR(u[0], ONE_OVER_SQRT_THREE, EPS);
   EXPECT_NEAR(u[1], ONE_OVER_SQRT_THREE, EPS);
   EXPECT_NEAR(u[2], ONE_OVER_SQRT_THREE, EPS);
 
-  EXPECT_TRUE(numerics::normalize<double>(v, dim));
+  EXPECT_TRUE(axom::numerics::normalize<double>(v, dim));
 
   EXPECT_NEAR(v[0], 0., EPS);
   EXPECT_NEAR(v[1], 3. * ONE_OVER_SQRT_TEN, EPS);
   EXPECT_NEAR(v[2], ONE_OVER_SQRT_TEN, EPS);
 
   // now make u = (0, 0, 0)
-  numerics::make_orthogonal(u, u, dim);
+  axom::numerics::make_orthogonal(u, u, dim);
 
   // normalize should fail
-  EXPECT_FALSE(numerics::normalize<double>(u, dim));
+  EXPECT_FALSE(axom::numerics::normalize<double>(u, dim));
 }
