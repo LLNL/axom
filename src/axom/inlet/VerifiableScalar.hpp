2ac557e073ddaa6a378fc3c70dd7c777d75f489e
Josh Essman <essman1@llnl.gov>
2020-12-08 11:40:20 -0600

    cleanup: have VerifiableScalar inherit from Verifiable as its interface is just a subset

--- src/axom/inlet/VerifiableScalar.hpp
+++ src/axom/inlet/VerifiableScalar.hpp
@@ -14,10 +14,7 @@
 #ifndef INLET_VERIFIABLE_SCALAR_HPP
 #define INLET_VERIFIABLE_SCALAR_HPP
 
-#include <memory>
-#include <functional>
-#include <vector>
-#include <initializer_list>
+#include "axom/inlet/Verifiable.hpp"
 
 namespace axom
 {
@@ -40,48 +37,9 @@ class Field;
  * values and discrete sets of valid values.
  *******************************************************************************
  */
-class VerifiableScalar
+class VerifiableScalar : public Verifiable<Field>
 {
 public:
-  // Should not be reassignable
-  VerifiableScalar& operator=(const VerifiableScalar&) = delete;
-  /*!
-   *****************************************************************************
-   * \brief Set the required status of this object.
-   *
-   * Set whether this object is required, or not, to be in the input file.
-   * The default behavior is to not be required.
-   *
-   * \param [in] isRequired Boolean value of whether object is required
-   *
-   * \return Reference to calling object, for chaining
-   *****************************************************************************
-   */
-  virtual VerifiableScalar& required(bool isRequired = true) = 0;
-
-  /*!
-   *****************************************************************************
-   * \brief Return the required status.
-   *
-   * Return that this object is required, or not, to be in the input file.
-   * The default behavior is to not be required.
-   *
-   * \return Boolean value of whether this object is required
-   *****************************************************************************
-   */
-  virtual bool isRequired() const = 0;
-
-  /*!
-   *****************************************************************************
-   * \brief Registers the function object that will verify this object's contents
-   * during the verification stage.
-   * 
-   * \param [in] The function object.
-   *****************************************************************************
-  */
-  virtual VerifiableScalar& registerVerifier(
-    std::function<bool(const axom::inlet::Field&)> lambda) = 0;
-
   /*!
    *****************************************************************************
    * \brief Set the default value of this object.
@@ -243,13 +201,6 @@ public:
    *****************************************************************************
   */
   virtual VerifiableScalar& validValues(const std::initializer_list<double>& set) = 0;
-
-  /*!
-   *****************************************************************************
-   * \brief Verifies the object to make sure it satisfies the imposed requirements
-   *****************************************************************************
-  */
-  virtual bool verify() const = 0;
 };
 
 }  // namespace inlet
