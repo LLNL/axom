793cfb43b4e0d8c7405561881d6848f3fda3d7a1
Noah S. Elliott <elliott22@llnl.gov>
2020-12-16 11:29:15 -0800

    New host-configs for TPL builds with new conduit

--- host-configs/rzgenie-toss_3_x86_64_ib-clang@9.0.0.cmake
+++ host-configs/rzgenie-toss_3_x86_64_ib-clang@9.0.0.cmake
@@ -30,9 +30,9 @@ set(BLT_EXE_LINKER_FLAGS " -Wl,-rpath,/usr/tce/packages/clang/clang-9.0.0/lib" C
 #------------------------------------------------------------------------------
 
 # Root directory for generated TPLs
-set(TPL_ROOT "/usr/WS1/axom/libs/toss_3_x86_64_ib/2020_11_25_18_21_13/clang-9.0.0" CACHE PATH "")
+set(TPL_ROOT "/usr/WS1/axom/libs/toss_3_x86_64_ib/2020_12_15_18_47_29/clang-9.0.0" CACHE PATH "")
 
-set(CONDUIT_DIR "${TPL_ROOT}/conduit-0.5.1" CACHE PATH "")
+set(CONDUIT_DIR "${TPL_ROOT}/conduit-0.6.0" CACHE PATH "")
 
 set(MFEM_DIR "${TPL_ROOT}/mfem-4.1.0" CACHE PATH "")
 
--- host-configs/rzgenie-toss_3_x86_64_ib-intel@19.0.4.cmake
+++ host-configs/rzgenie-toss_3_x86_64_ib-intel@19.0.4.cmake
@@ -30,9 +30,9 @@ set(CMAKE_Fortran_FLAGS "-gcc-name=/usr/tce/packages/gcc/gcc-8.1.0/bin/gcc" CACH
 #------------------------------------------------------------------------------
 
 # Root directory for generated TPLs
-set(TPL_ROOT "/usr/WS1/axom/libs/toss_3_x86_64_ib/2020_11_25_18_21_13/intel-19.0.4" CACHE PATH "")
+set(TPL_ROOT "/usr/WS1/axom/libs/toss_3_x86_64_ib/2020_12_15_18_47_29/intel-19.0.4" CACHE PATH "")
 
-set(CONDUIT_DIR "${TPL_ROOT}/conduit-0.5.1" CACHE PATH "")
+set(CONDUIT_DIR "${TPL_ROOT}/conduit-0.6.0" CACHE PATH "")
 
 set(MFEM_DIR "${TPL_ROOT}/mfem-4.1.0" CACHE PATH "")
 
