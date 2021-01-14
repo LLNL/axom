661db5021f615dc2281251a80171b290fadc186f
Esteban Pauli <pauli2@llnl.gov>
2021-01-13 17:08:18 -0800

    Added whether klee is enabled to the configuration.

---
+++ src/axom/klee/tests/klee_config.cpp
@@ -0,0 +1,21 @@
+// Copyright (c) 2017-2020, Lawrence Livermore National Security, LLC and
+// other Axom Project Developers. See the top-level COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
+#include "axom/config.hpp"
+
+#include "gtest/gtest.h"
+
+namespace axom
+{
+namespace klee {
+
+TEST(KleeConfig, defines) {
+#ifndef AXOM_USE_KLEE
+  FAIL() << "AXOM_ENABLE_KLEE not defined";
+#endif
+}
+
+}  // namespace klee
+}  // namespace axom
