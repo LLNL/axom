d4fc79dbdfbcb066755e607def0993aeec747ed8
Hennis <hennis1@luz.llnl.gov>
2021-01-14 16:54:14 -0800

    updated build src et al to use host configs

--- scripts/llnl_scripts/build_tpls.py
+++ scripts/llnl_scripts/build_tpls.py
@@ -8,14 +8,12 @@
 
 """
  file: build_tpls.py
-
  description: 
   uses uberenv to install tpls for the set of compilers we want
   for current machine.
-
 """
 
-from llnl_lc_build_tools import *
+from common_build_functions import *
 
 from optparse import OptionParser
 
@@ -30,21 +28,24 @@ def parse_args():
                       dest="directory",
                       default="",
                       help="Location to build all TPL's, timestamp directory will be created (Defaults to shared location)")
-    # Directory to do all the building
+    # Spack spec to use for the build
     parser.add_option("-s", "--spec",
                       dest="spec",
                       default="",
                       help="Spack spec to build (defaults to all available on SYS_TYPE)")
-    # Whether to archive results
-    parser.add_option("-a", "--archive",
-                      dest="archive",
+    parser.add_option("-v", "--verbose",
+                      action="store_true",
+                      dest="verbose",
+                      default=False,
+                      help="Output logs to screen as well as to files")
+    parser.add_option("-m", "--mirror",
+                      dest="mirror",
                       default="",
-                      help="Archive build results under given name (Defaults to off)")
-
+                      help="Mirror location to use (defaults to shared location)")
     ###############
     # parse args
     ###############
-    opts, extras = parser.parse_args()
+    opts, _ = parser.parse_args()
     # we want a dict b/c the values could 
     # be passed without using optparse
     opts = vars(opts)
@@ -65,22 +66,12 @@ def main():
 
     repo_dir = get_repo_dir()
 
-    if opts["archive"] != "":
-        job_name = opts["archive"]
-    else:
-        job_name = get_username() + "/" + os.path.basename(__file__)
-
     try:
         original_wd = os.getcwd()
         os.chdir(repo_dir)
 
         timestamp = get_timestamp()
-        res = full_build_and_test_of_tpls(builds_dir, job_name, timestamp, opts["spec"])
-
-        if opts["archive"] != "":
-            # Get information for archiving
-            archive_base_dir = get_archive_base_dir()
-            archive_tpl_logs(builds_dir, job_name, timestamp)
+        res = full_build_and_test_of_tpls(builds_dir, timestamp, opts["spec"], opts["verbose"], opts["mirror"])
     finally:
         os.chdir(original_wd)
 
