43568a2b67c6dcb7549b5e0d8060b0adbe825d35
Chris White <white238@llnl.gov>
2021-01-27 00:46:06 -0800

    Simplify/clarify example

--- src/axom/inlet/examples/fields.cpp
+++ src/axom/inlet/examples/fields.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2020, Lawrence Livermore National Security, LLC and
+// Copyright (c) 2017-2021, Lawrence Livermore National Security, LLC and
 // other Axom Project Developers. See the top-level COPYRIGHT file for details.
 //
 // SPDX-License-Identifier: (BSD-3-Clause)
@@ -46,47 +46,58 @@ int main()
   // Create Inlet with LuaReader and the Sidre Group which Inlet will use
   axom::inlet::Inlet inlet(std::move(lr), ds.getRoot());
 
-  // Define and store the values in the input file
   // _inlet_simple_types_fields_add_start
+
+  // Define and store the values in the input file
+
+  // Add an optional top-level boolean
   inlet.addBool("a_simple_bool", "A description of a_simple_bool");
+
+  // Add an optional top-level integer
   inlet.addInt("a_simple_int", "A description of a_simple_int");
+
+  // Add a required top-level double
   inlet.addDouble("a_simple_double", "A description of a_simple_double").required();
+
+  // Add an optional top-level string
   inlet.addString("a_simple_string", "A description of a_simple_string");
 
+  // Add an optional top-level integer with a default value of 17 if not defined by the user
   inlet.addInt("a_defaulted_int", "An int that has a default value").defaultValue(17);
 
-  inlet.addString("does_not_exist", "Should not be in your input file");
+  // Add an optional top-level string not defined in the input file for example purposes
+  inlet.addString("does_not_exist", "Shows that not all fields need to be present in input file");
   // _inlet_simple_types_fields_add_end
 
-  // Access values stored in the Datastore via Inlet
   // _inlet_simple_types_fields_access_start
-  bool a_simple_bool = inlet["a_simple_bool"];
-  int a_simple_int = inlet["a_simple_int"];
+  // Access values stored in the Datastore via Inlet
+
+  // Check if input file contained info before accessing optional fields
+  if (inlet.contains("a_simple_bool")) {
+    bool a_simple_bool = inlet["a_simple_bool"];
+    std::cout << "a_simple_bool = " << a_simple_bool << std::endl;
+  }
+
+  if (inlet.contains("a_simple_int")) {
+    int a_simple_int = inlet["a_simple_int"];
+    std::cout << "a_simple_int = " << a_simple_int << std::endl;
+  }
+
+  // Because this field was marked required, we do not have to call contains before accessing
   double a_simple_double = inlet["a_simple_double"];
-  std::string a_simple_string = inlet["a_simple_string"];
-
-  int a_defaulted_int = inlet["a_defaulted_int"];
-  // _inlet_simple_types_fields_access_end
-
-  std::cout << "a_simple_bool = " << a_simple_bool << std::endl;
-  std::cout << "a_simple_int = " << a_simple_int << std::endl;
   std::cout << "a_simple_double = " << a_simple_double << std::endl;
-  std::cout << "a_simple_string = " << a_simple_string << std::endl;
+
+  if (inlet.contains("a_simple_string")) {
+    std::string a_simple_string = inlet["a_simple_string"];
+    std::cout << "a_simple_string = " << a_simple_string << std::endl;
+  }
+
+  // If the user did not provide a value, the default value will be used.  Safe to use
+  // without checking contains
+  int a_defaulted_int = inlet["a_defaulted_int"];
   std::cout << "a_defaulted_int = " << a_defaulted_int << std::endl;
 
-  // _inlet_simple_types_fields_contains_start
-  if(inlet.contains("does_not_exist"))
-  {
-    std::cout << "Error: Inlet should not have contained key 'does_not_exist' "
-              << std::endl;
-    return 1;
-  }
-  else
-  {
-    std::cout << "Key 'does_not_exist' did not exist in Inlet. Success!"
-              << std::endl;
-  }
-  // _inlet_simple_types_fields_contains_end
+  // _inlet_simple_types_fields_access_end
 
   return 0;
 }
