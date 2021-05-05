ef442aab571a69cfe05585a23ea0f996ec8348e9
Kenneth Weiss <weiss27@llnl.gov>
2021-05-05 09:21:23 -0700

    Revert changes for building a parallel MFEM in uberenv
    We'll tackle that in a future PR.

--- scripts/spack/packages/axom/package.py
+++ scripts/spack/packages/axom/package.py
@@ -112,7 +112,7 @@ class Axom(CachedCMakePackage, CudaPackage):
         depends_on('umpire cuda_arch={0}'.format(sm_),
                    when='+umpire cuda_arch={0}'.format(sm_))
 
-    depends_on("mfem+mpi", when="+mfem+mpi")
+    depends_on("mfem", when="+mfem")
     depends_on("mfem~mpi", when="+mfem~mpi")
 
     depends_on("python", when="+python")
