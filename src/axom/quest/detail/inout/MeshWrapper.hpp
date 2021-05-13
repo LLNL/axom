0d4759d3db798ef21ec71d15e4ce11efe71043b0
Kenneth Weiss <weiss27@llnl.gov>
2021-05-12 22:02:47 -0700

    Finished generalizing InOutOctree's MeshWrapper
    We now have a 2D instance of MeshWrapper for meshes composed of segments
    and a 3D instance for meshes composed of triangles.

--- src/axom/quest/detail/inout/MeshWrapper.hpp
+++ src/axom/quest/detail/inout/MeshWrapper.hpp
@@ -55,6 +55,7 @@ public:
   using VertexIndexMap = slam::Map<slam::Set<VertexIndex>, VertexIndex>;
   using VertexPositionMap = slam::Map<slam::Set<VertexIndex>, SpacePt>;
 
+  /// Always DIM verts since we're representing a d-dimensional simplicial mesh in dimension d
   static constexpr int NUM_CELL_VERTS = DIM;
   using STLIndirection =
     slam::policies::STLVectorIndirection<VertexIndex, VertexIndex>;
@@ -69,17 +70,27 @@ public:
                                                   MeshVertexSet>;
   using CellVertIndices = typename CellVertexRelation::RelationSubset;
 
-  /** \brief A vertex index to indicate that there is no associated vertex */
-  static const VertexIndex NO_VERTEX = -1;
+  // \brief A vertex index to indicate that there is no associated vertex
+  static constexpr VertexIndex NO_VERTEX = -1;
 
-  /** \brief A vertex index to indicate that there is no associated vertex */
-  static const CellIndex NO_CELL = -1;
+  /// \brief A vertex index to indicate that there is no associated vertex
+  static constexpr CellIndex NO_CELL = -1;
 
 protected:
   SimplexMeshWrapper(SurfaceMesh*& meshPtr)
     : m_surfaceMesh(meshPtr)
     , m_vertexPositions(&m_vertexSet)
+    , m_cellToVertexRelation()
   { }
+
+private:
+  /// Utility functions to get a pointer to the derived type (part of CRTP pattern)
+  Derived* getDerived() { return static_cast<Derived*>(this); }
+  /// Utility functions to get a const pointer to the derived type (part of CRTP pattern)
+  const Derived* getDerived() const
+  {
+    return static_cast<const Derived*>(this);
+  }
 
 public:
   /** Predicate to determine if the wrapped surface mesh has been reindexed */
@@ -149,6 +160,106 @@ public:
     return m_vertexPositions[idx];
   }
 
+  /**
+     * \brief Returns the indices of the boundary vertices of the element
+     * of the wrapped surface mesh with the given index
+     *
+     * \param idx The index of an element within the surface mesh
+     */
+  CellVertIndices cellVertexIndices(CellIndex idx) const
+  {
+    return m_cellToVertexRelation[idx];
+  }
+
+  /**
+   * \brief Finds the index of a vertex in cell \a c1 that is not in cell \a c0
+   *
+   * \param c0 The index of the first cell
+   * \param c1 The index of the second cell
+   * \pre \a c0 and \a c1 must be distinct cells
+   * \return The index of a vertex in \a c1 that is not in \a c0; NO_VERTEX if one does not exist
+   */
+  VertexIndex distinctVertex(CellIndex c0, CellIndex c1) const
+  {
+    SLIC_ASSERT_MSG(c0 != c1,
+                    "Expected two different cell indices in "
+                    "quest::MeshWrapper::findDistinctVertex,"
+                      << " got cell index " << c0 << " twice.");
+
+    // Find a vertex from the local surface that is not incident in the first cell
+    CellVertIndices cvRel0 = cellVertexIndices(c0);
+    CellVertIndices cvRel1 = cellVertexIndices(c1);
+
+    for(int i = 0; i < NUM_CELL_VERTS; ++i)
+      if(!incidentInVertex(cvRel0, cvRel1[i])) return cvRel1[i];
+
+    SLIC_ASSERT_MSG(
+      false,
+      fmt::format("There should be a vertex in cell {} that was not in cell {}",
+                  c1,
+                  c0));
+    return NO_VERTEX;
+  }
+
+  /**
+   * \brief Determine if the two given cells have a vertex in common
+   *
+   * \param c0 The index of the first cell
+   * \param c1 The index of the second cell
+   * \param [out] sharedVert The index of the shared vertex, if it exists
+   * \return true if the two cells have a vertex
+   * in common (returned in sharedVert), false otherwise
+   */
+  bool haveSharedVertex(CellIndex c0, CellIndex c1, VertexIndex& sharedVert) const
+  {
+    // There are two cells  -- check that they have at least one common vertex
+    CellVertIndices cvRel0 = cellVertexIndices(c0);
+    CellVertIndices cvRel1 = cellVertexIndices(c1);
+
+    for(int i = 0; i < NUM_CELL_VERTS; ++i)
+    {
+      if(getDerived()->incidentInVertex(cvRel0, cvRel1[i]))
+      {
+        sharedVert = cvRel1[i];
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * \brief Determine if the three given cells have a vertex in common
+   *
+   * \param c0 The index of the first cell
+   * \param c1 The index of the second cell
+   * \param c2 The index of the second cell
+   * \param [out] sharedVert The index of the shared vertex, if it exists
+   * \return true if the three cells have a vertex in common
+   *  (returned in sharedVert), false otherwise
+   */
+  bool haveSharedVertex(CellIndex c0,
+                        CellIndex c1,
+                        CellIndex c2,
+                        VertexIndex& sharedVert) const
+  {
+    CellVertIndices c0Verts = cellVertexIndices(c0);
+    CellVertIndices c1Verts = cellVertexIndices(c1);
+    CellVertIndices c2Verts = cellVertexIndices(c2);
+
+    for(int i = 0; i < NUM_CELL_VERTS; ++i)
+    {
+      // check if a vertex from the third cell is in the first and second
+      if(getDerived()->incidentInVertex(c0Verts, c2Verts[i]) &&
+         getDerived()->incidentInVertex(c1Verts, c2Verts[i]))
+      {
+        sharedVert = c2Verts[i];
+        return true;
+      }
+    }
+
+    return false;
+  }
+
 protected:
   SurfaceMesh*& m_surfaceMesh;  // ref to pointer to allow changing the mesh
 
@@ -156,6 +267,9 @@ protected:
   MeshElementSet m_elementSet {0};
 
   VertexPositionMap m_vertexPositions;
+
+  std::vector<VertexIndex> m_cv_data;
+  CellVertexRelation m_cellToVertexRelation;
 
   bool m_meshWasReindexed {false};
 };
@@ -166,7 +280,6 @@ class MeshWrapper<2> : public SimplexMeshWrapper<2, MeshWrapper<2>>
 public:
   static constexpr int DIM = 2;
   using Base = SimplexMeshWrapper<DIM, MeshWrapper<DIM>>;
-  using SpaceCell = primal::Segment<double, DIM>;
   using Base::CellIndex;
   using Base::GeometricBoundingBox;
   using Base::SpacePt;
@@ -174,6 +287,134 @@ public:
   using Base::VertexIndex;
   using Base::VertexIndexMap;
   using Base::VertexPositionMap;
+
+  /// \brief A constant for the number of boundary vertices in an edge */
+  static constexpr int NUM_EDGE_VERTS = DIM;
+  using SpaceCell = primal::Segment<double, NUM_EDGE_VERTS>;
+
+public:
+  /// \brief Constructor for a mesh wrapper */
+  MeshWrapper(SurfaceMesh*& meshPtr) : Base(meshPtr) { }
+
+  /**
+   * \brief Helper function to compute the bounding box of an edge
+   *
+   * \param idx The edges's index within the surface mesh
+   */
+  GeometricBoundingBox cellBoundingBox(CellIndex idx) const
+  {
+    // Get the ids of the verts bounding this edge
+    CellVertIndices vertIds = cellVertexIndices(idx);
+    return GeometricBoundingBox(vertexPosition(vertIds[0]),
+                                vertexPosition(vertIds[1]));
+  }
+
+  /**
+   * \brief Utility function to retrieve the positions of the edge's vertices
+   *
+   * \return A SpaceCell (Segment) whose vertices are positioned in space
+   */
+  SpaceCell cellPositions(CellIndex idx) const
+  {
+    CellVertIndices verts = cellVertexIndices(idx);
+    return SpaceCell(vertexPosition(verts[0]), vertexPosition(verts[1]));
+  }
+
+  /// \brief Checks whether the indexed triangle contains a reference to the given vertex
+  bool incidentInVertex(const CellVertIndices& ids, VertexIndex vIdx) const
+  {
+    return (ids[0] == vIdx) || (ids[1] == vIdx);
+  }
+
+  /**
+   * \brief Reindexes the mesh vertices and edge indices using the given map
+   *
+   * \param numVertices The number of vertices in the new mesh
+   * \param vertexIndexMap A mapping from the old vertex indices to the new ones
+   * \note This step clears out the original mesh,
+   * which can be reconstructed using the regenerateSurfaceMesh() function
+   */
+  void reindexMesh(int numVertices, const VertexIndexMap& vertexIndexMap)
+  {
+    // Create a vertex set on the new vertices and grab coordinates from the old ones
+    m_vertexSet = MeshVertexSet(numVertices);
+    m_vertexPositions = VertexPositionMap(&m_vertexSet);
+
+    int numOrigVertices = numMeshVertices();
+    for(int i = 0; i < numOrigVertices; ++i)
+    {
+      const VertexIndex& vInd = vertexIndexMap[i];
+      m_vertexPositions[vInd] = getMeshVertexPosition(i);
+    }
+
+    // Update the vertex IDs of the triangles to the new vertices
+    // and create a SLAM relation on these
+    int numOrigEdges = numMeshCells();
+
+    m_cv_data.clear();
+    m_cv_data.reserve(NUM_EDGE_VERTS * numOrigEdges);
+    for(axom::IndexType i = 0; i < numOrigEdges; ++i)
+    {
+      // Grab relation from mesh
+      using UMesh = mint::UnstructuredMesh<mint::SINGLE_SHAPE>;
+      axom::IndexType* vertIds =
+        static_cast<UMesh*>(m_surfaceMesh)->getCellNodeIDs(i);
+
+      // Remap the vertex IDs
+      for(int j = 0; j < NUM_EDGE_VERTS; ++j)
+        vertIds[j] = vertexIndexMap[vertIds[j]];
+
+      // Add to relation if not degenerate edge
+      // (namely, we need 2 unique vertex IDs)
+      if((vertIds[0] != vertIds[1]))
+      {
+        m_cv_data.push_back(vertIds[0]);
+        m_cv_data.push_back(vertIds[1]);
+      }
+    }
+
+    m_elementSet =
+      MeshElementSet(static_cast<int>(m_cv_data.size()) / NUM_EDGE_VERTS);
+    m_cellToVertexRelation = CellVertexRelation(&m_elementSet, &m_vertexSet);
+    m_cellToVertexRelation.bindIndices(static_cast<int>(m_cv_data.size()),
+                                       &m_cv_data);
+
+    // Delete old mesh, and NULL its pointer
+    delete m_surfaceMesh;
+    m_surfaceMesh = nullptr;
+
+    m_meshWasReindexed = true;
+  }
+
+  /// \brief Add the vertex positions and edge boundary relations to the surface mesh
+  void regenerateSurfaceMesh()
+  {
+    if(m_surfaceMesh != nullptr)
+    {
+      delete m_surfaceMesh;
+      m_surfaceMesh = nullptr;
+    }
+
+    using UMesh = mint::UnstructuredMesh<mint::SINGLE_SHAPE>;
+    UMesh* edgeMesh =
+      new UMesh(3, mint::SEGMENT, m_vertexSet.size(), m_elementSet.size());
+
+    // Add vertices to the mesh (i.e. vertex positions)
+    for(int i = 0; i < m_vertexSet.size(); ++i)
+    {
+      const SpacePt& pt = vertexPosition(i);
+      edgeMesh->appendNode(pt[0], pt[1]);
+    }
+
+    // Add edges to the mesh (i.e. boundary vertices)
+    for(int i = 0; i < m_elementSet.size(); ++i)
+    {
+      const CellIndex* tv = &cellVertexIndices(i)[0];
+      edgeMesh->appendCell(tv);
+    }
+
+    m_surfaceMesh = edgeMesh;
+  }
 };
 
 template <>
@@ -190,33 +431,19 @@ public:
   using Base::VertexIndexMap;
   using Base::VertexPositionMap;
 
-  /** \brief A constant for the number of boundary vertices in a triangle */
-  static const int NUM_TRI_VERTS = 3;
+  /// \brief A constant for the number of boundary vertices in a triangle */
+  static constexpr int NUM_TRI_VERTS = 3;
   using SpaceCell = primal::Triangle<double, DIM>;
 
 public:
-  /** \brief Constructor for a mesh wrapper */
-  MeshWrapper(SurfaceMesh*& meshPtr)
-    : Base(meshPtr)
-    , m_triangleToVertexRelation()
-  { }
+  /// \brief Constructor for a mesh wrapper */
+  MeshWrapper(SurfaceMesh*& meshPtr) : Base(meshPtr) { }
 
   /**
-     * \brief Returns the indices of the boundary vertices of the element
-     * of the wrapped surface mesh with the given index
-     *
-     * \param idx The index of an element within the surface mesh
-     */
-  CellVertIndices cellVertexIndices(CellIndex idx) const
-  {
-    return m_triangleToVertexRelation[idx];
-  }
-
-  /**
-     * \brief Helper function to compute the bounding box of a triangle
-     *
-     * \param idx The triangle's index within the surface mesh
-     */
+   * \brief Helper function to compute the bounding box of a triangle
+   *
+   * \param idx The triangle's index within the surface mesh
+   */
   GeometricBoundingBox cellBoundingBox(CellIndex idx) const
   {
     // Get the ids of the verts bounding this triangle
@@ -230,11 +457,10 @@ public:
   }
 
   /**
-     * \brief Utility function to retrieve the positions of the triangle's
-     * vertices
-     *
-     * \return A triangle instance whose vertices are positioned in space
-     */
+   * \brief Utility function to retrieve the positions of the triangle's vertices
+   *
+   * \return A SpaceCell (Triangle) whose vertices are positioned in space
+   */
   SpaceCell cellPositions(CellIndex idx) const
   {
     CellVertIndices verts = cellVertexIndices(idx);
@@ -243,120 +469,20 @@ public:
                      vertexPosition(verts[2]));
   }
 
-  /**
-     * \brief Checks whether the indexed triangle contains a reference to the
-     * given vertex
-     */
-  bool incidentInVertex(const CellVertIndices& triVerts, VertexIndex vIdx) const
+  /// \brief Checks whether the indexed triangle contains a reference to the given vertex
+  bool incidentInVertex(const CellVertIndices& ids, VertexIndex vIdx) const
   {
-    return (triVerts[0] == vIdx) || (triVerts[1] == vIdx) ||
-      (triVerts[2] == vIdx);
+    return (ids[0] == vIdx) || (ids[1] == vIdx) || (ids[2] == vIdx);
   }
 
   /**
-     * \brief Finds the index of a vertex in triangle t1 that is not in triangle
-     * t0
-     *
-     * \param t0 The index of the first triangle
-     * \param t1 The index of the second triangle
-     * \pre t0 and t1 must be distinct triangles
-     * \return The index of a vertex in t1 that is not in t0,
-     *         (NO_VERTEX if one does not exist)
-     */
-  VertexIndex distinctVertex(CellIndex t0, CellIndex t1) const
-  {
-    SLIC_ASSERT_MSG(
-      t0 != t1,
-      "Expected two different triangle indices in "
-        << "quest::MeshWrapper::findDistinctVertex, got triangle index " << t0
-        << " twice.");
-
-    // Find a vertex from the local surface that is not incident in the first
-    // triangle
-    CellVertIndices tvRel0 = cellVertexIndices(t0);
-    CellVertIndices tvRel1 = cellVertexIndices(t1);
-
-    for(int i = 0; i < NUM_TRI_VERTS; ++i)
-      if(!incidentInVertex(tvRel0, tvRel1[i])) return tvRel1[i];
-
-    SLIC_ASSERT_MSG(
-      false,
-      fmt::format("There should be a vertex in triangle {} that was not in {}",
-                  t1,
-                  t0));
-    return NO_VERTEX;
-  }
-
-  /**
-     * \brief Determine if the two given triangles have a vertex in common
-     *
-     * \param t0 The index of the first triangle
-     * \param t1 The index of the second triangle
-     * \param [out] sharedVert The index of the shared vertex, if it exists
-     * \return true if the two triangles have a vertex
-     * in common (returned in sharedVert), false otherwise
-     */
-  bool haveSharedVertex(CellIndex t0, CellIndex t1, VertexIndex& sharedVert) const
-  {
-    // There are two triangles -- check that they have at least one common
-    // vertex
-    CellVertIndices tvRel0 = cellVertexIndices(t0);
-    CellVertIndices tvRel1 = cellVertexIndices(t1);
-
-    for(int i = 0; i < NUM_TRI_VERTS; ++i)
-    {
-      if(incidentInVertex(tvRel0, tvRel1[i]))
-      {
-        sharedVert = tvRel1[i];
-        return true;
-      }
-    }
-    return false;
-  }
-
-  /**
-     * \brief Determine if the three given triangles have a vertex in common
-     *
-     * \param t0 The index of the first triangle
-     * \param t1 The index of the second triangle
-     * \param t2 The index of the second triangle
-     * \param [out] sharedVert The index of the shared vertex, if it exists
-     * \return true if the three triangles have a vertex in common
-     *  (returned in sharedVert), false otherwise
-     */
-  bool haveSharedVertex(CellIndex t0,
-                        CellIndex t1,
-                        CellIndex t2,
-                        VertexIndex& sharedVert) const
-  {
-    CellVertIndices t0Verts = cellVertexIndices(t0);
-    CellVertIndices t1Verts = cellVertexIndices(t1);
-    CellVertIndices t2Verts = cellVertexIndices(t2);
-
-    for(int i = 0; i < NUM_TRI_VERTS; ++i)
-    {
-      // check if a vertex from the third triangle is in the first and second
-      if(incidentInVertex(t0Verts, t2Verts[i]) &&
-         incidentInVertex(t1Verts, t2Verts[i]))
-      {
-        sharedVert = t2Verts[i];
-        return true;
-      }
-    }
-
-    return false;
-  }
-
-  /**
-     * \brief Reindexes the mesh vertices and triangle indices using the given
-     * map
-     *
-     * \param numVertices The number of vertices in the new mesh
-     * \param vertexIndexMap A mapping from the old vertex indices to the new
-     * ones
-     * \note This step clears out the original mesh,
-     * which can be reconstructed using the regenerateSurfaceMesh() function
-     */
+   * \brief Reindexes the mesh vertices and triangle indices using the given map
+   *
+   * \param numVertices The number of vertices in the new mesh
+   * \param vertexIndexMap A mapping from the old vertex indices to the new ones
+   * \note This step clears out the original mesh,
+   * which can be reconstructed using the regenerateSurfaceMesh() function
+   */
   void reindexMesh(int numVertices, const VertexIndexMap& vertexIndexMap)
   {
     // Create a vertex set on the new vertices and grab coordinates from the old ones
@@ -374,8 +500,8 @@ public:
     // and create a SLAM relation on these
     int numOrigTris = numMeshCells();
 
-    m_tv_data.clear();
-    m_tv_data.reserve(NUM_TRI_VERTS * numOrigTris);
+    m_cv_data.clear();
+    m_cv_data.reserve(NUM_TRI_VERTS * numOrigTris);
     for(axom::IndexType i = 0; i < numOrigTris; ++i)
     {
       // Grab relation from mesh
@@ -392,17 +518,17 @@ public:
       if((vertIds[0] != vertIds[1]) && (vertIds[1] != vertIds[2]) &&
          (vertIds[2] != vertIds[0]))
       {
-        m_tv_data.push_back(vertIds[0]);
-        m_tv_data.push_back(vertIds[1]);
-        m_tv_data.push_back(vertIds[2]);
+        m_cv_data.push_back(vertIds[0]);
+        m_cv_data.push_back(vertIds[1]);
+        m_cv_data.push_back(vertIds[2]);
       }
     }
 
     m_elementSet =
-      MeshElementSet(static_cast<int>(m_tv_data.size()) / NUM_TRI_VERTS);
-    m_triangleToVertexRelation = CellVertexRelation(&m_elementSet, &m_vertexSet);
-    m_triangleToVertexRelation.bindIndices(static_cast<int>(m_tv_data.size()),
-                                           &m_tv_data);
+      MeshElementSet(static_cast<int>(m_cv_data.size()) / NUM_TRI_VERTS);
+    m_cellToVertexRelation = CellVertexRelation(&m_elementSet, &m_vertexSet);
+    m_cellToVertexRelation.bindIndices(static_cast<int>(m_cv_data.size()),
+                                       &m_cv_data);
 
     // Delete old mesh, and NULL its pointer
     delete m_surfaceMesh;
@@ -411,8 +537,7 @@ public:
     m_meshWasReindexed = true;
   }
 
-  /** \brief Add the vertex positions and triangle boundary
-     *   relations to the surface mesh */
+  /// \brief Add the vertex positions and triangle boundary relations to the surface mesh
   void regenerateSurfaceMesh()
   {
     if(m_surfaceMesh != nullptr)
@@ -441,10 +566,6 @@ public:
 
     m_surfaceMesh = triMesh;
   }
-
-private:
-  std::vector<VertexIndex> m_tv_data;
-  CellVertexRelation m_triangleToVertexRelation;
 };
 
 }  // namespace quest
