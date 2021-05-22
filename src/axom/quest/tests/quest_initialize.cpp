911710cffcc3edc9f3bae059542164fea442f2ed
format-robot <axom-dev@llnl.gov>
2021-05-21 18:38:07 -0700

    Prepends/patches copyright on some files

--- src/axom/quest/tests/quest_initialize.cpp
+++ src/axom/quest/tests/quest_initialize.cpp
@@ -1,4 +1,7 @@
-
+// Copyright (c) 2017-2021, Lawrence Livermore National Security, LLC and
+// other Axom Project Developers. See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
 
 // Axom includes
 #include "axom/mint.hpp"
