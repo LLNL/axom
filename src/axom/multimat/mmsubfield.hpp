49254f4706d9b457e0428b9d0113bc6b4b04238c
format-robot <axom-dev@llnl.gov>
2021-05-21 18:38:07 -0700

    Prepends/patches copyright on some files

--- src/axom/multimat/mmsubfield.hpp
+++ src/axom/multimat/mmsubfield.hpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2017-2021, Lawrence Livermore National Security, LLC and
+// other Axom Project Developers. See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
 
 #ifndef MMSUBFIELD_H_
 #define MMSUBFIELD_H_
