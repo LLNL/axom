6480bf92fde0bb86223c48b0713cd9660f284629
format-robot <axom-dev@llnl.gov>
2021-05-21 18:13:13 -0700

    Applies clang-format to multimat component

--- src/axom/multimat/mmfield.hpp
+++ src/axom/multimat/mmfield.hpp
@@ -5,21 +5,22 @@
 #include "axom/multimat/multimat.hpp"
 #include "axom/multimat/mmsubfield.hpp"
 
-
-namespace axom {
-namespace multimat {
-
+namespace axom
+{
+namespace multimat
+{
 /**
  * Class for MultiMat 2D Fields
  * Ideally this class would have template specialization for each type of 
  * layout (dense/sparse, mat/cell dom, and maybe more). 
  */
-template<typename DataType, typename BiSet = MultiMat::BivariateSetType>
+template <typename DataType, typename BiSet = MultiMat::BivariateSetType>
 class MMField2D : public MultiMat::BivariateMapTypeStrideOne<DataType, BiSet>
 {
 public:
   using BiVarSetType = BiSet;
-  using BiVarMapType = MultiMat::BivariateMapTypeStrideOne<DataType, BiVarSetType>;
+  using BiVarMapType =
+    MultiMat::BivariateMapTypeStrideOne<DataType, BiVarSetType>;
   using ProductSetType = MultiMat::ProductSetType;
   using RelationSetType = MultiMat::RelationSetType;
 
@@ -38,15 +39,16 @@ public:
   MMField2D() = delete;
   // Constructor with specified layouts
   //MMField2D(MultiMat& mm, DataLayout data_layout, SparsityLayout sparsity_layout,
-  //  const std::string& arr_name = "unnamed", 
+  //  const std::string& arr_name = "unnamed",
   //  const DataType* data_arr = nullptr,
   //  int stride = 1);
   // Constructor given the bivariate set
-  MMField2D(MultiMat& mm, const BiSet*, 
-    const std::string& arr_name = "unnamed", 
-    const DataType* data_arr = nullptr,
-    int stride = 1);
-  
+  MMField2D(MultiMat& mm,
+            const BiSet*,
+            const std::string& arr_name = "unnamed",
+            const DataType* data_arr = nullptr,
+            int stride = 1);
+
   /** Destructor **/
   ~MMField2D() {};
   /** Copy constructor (Deep copy). **/
@@ -54,20 +56,19 @@ public:
   /** Assignment operator **/
   //MMField2D& operator=(const MMField2D&);
 
+  using BiVarMapType::operator();  //why is this needed?
 
-  using BiVarMapType::operator(); //why is this needed?
-
-  //subfield (instead of SubMap) 
+  //subfield (instead of SubMap)
   SubFieldType getSubfield(SetPosition firstIdx)
   {
     return operator()(firstIdx);
   }
-  SubFieldType operator() (SetPosition firstIdx)
+  SubFieldType operator()(SetPosition firstIdx)
   {
     const bool hasInd = this->submapIndicesHaveIndirection();
     return SubFieldType(this, firstIdx, hasInd);
   }
-  const ConstSubFieldType operator() (SetPosition firstIdx) const
+  const ConstSubFieldType operator()(SetPosition firstIdx) const
   {
     const bool hasInd = this->submapIndicesHaveIndirection();
     return ConstSubFieldType(this, firstIdx, hasInd);
@@ -79,21 +80,17 @@ public:
     return BiVarMapType::operator()(firstIdx);
   }
 
-  
-
-  
   std::string getName() { return m_field_name; };
 
-  MultiMat::IndexSet getSubfieldIndexingSet(int idx) {
-    return m_mm->getSubfieldIndexingSet
-    (idx, m_data_layout, m_sparsity_layout);
+  MultiMat::IndexSet getSubfieldIndexingSet(int idx)
+  {
+    return m_mm->getSubfieldIndexingSet(idx, m_data_layout, m_sparsity_layout);
   }
 
   bool isDense() const { return m_sparsity_layout == SparsityLayout::DENSE; }
   bool isSparse() const { return m_sparsity_layout == SparsityLayout::SPARSE; }
   bool isCellDom() const { return m_data_layout == DataLayout::CELL_DOM; }
   bool isMatDom() const { return m_data_layout == DataLayout::MAT_DOM; }
-
 
 private:
   MultiMat* m_mm;
@@ -102,52 +99,59 @@ private:
   SparsityLayout m_sparsity_layout;
 
   std::string m_field_name;
-
 };
 
 //////////////////////// Implementation of MMField2D ////////////////////////////
 
 // Constructor given layouts
 //template<typename DataType, typename BiSet>
-//inline MMField2D<DataType, BiSet>::MMField2D(MultiMat& mm, 
+//inline MMField2D<DataType, BiSet>::MMField2D(MultiMat& mm,
 //    DataLayout data_layout, SparsityLayout sparsity_layout,
 //    const std::string& arr_name, const DataType* data_arr, int stride) :
 //  MMField2D(mm, mm.get_mapped_biSet(data_layout, sparsity_layout),
 //     arr_name, data_arr, stride)
 //{ }
 
-
 // Constructor given the biset
-template<typename DataType, typename BiSet>
-inline MMField2D<DataType, BiSet>::MMField2D(MultiMat& mm, const BiSet *biset,
-    const std::string& arr_name, const DataType* data_arr, int stride) :
-  //call Bivariate map constructor
-  BiVarMapType(biset, DataType(), stride),
-  m_mm(&mm),
-  m_field_name(arr_name)
+template <typename DataType, typename BiSet>
+inline MMField2D<DataType, BiSet>::MMField2D(MultiMat& mm,
+                                             const BiSet* biset,
+                                             const std::string& arr_name,
+                                             const DataType* data_arr,
+                                             int stride)
+  :  //call Bivariate map constructor
+  BiVarMapType(biset, DataType(), stride)
+  , m_mm(&mm)
+  , m_field_name(arr_name)
 {
   SLIC_ASSERT(stride > 0);
 
-  if (data_arr != nullptr)
-    this->copy(data_arr);
+  if(data_arr != nullptr) this->copy(data_arr);
 
-  if (biset == mm.get_mapped_biSet(DataLayout::CELL_DOM, SparsityLayout::DENSE)) {
+  if(biset == mm.get_mapped_biSet(DataLayout::CELL_DOM, SparsityLayout::DENSE))
+  {
     m_data_layout = DataLayout::CELL_DOM;
     m_sparsity_layout = SparsityLayout::DENSE;
   }
-  else if (biset == mm.get_mapped_biSet(DataLayout::CELL_DOM, SparsityLayout::SPARSE)) {
+  else if(biset ==
+          mm.get_mapped_biSet(DataLayout::CELL_DOM, SparsityLayout::SPARSE))
+  {
     m_data_layout = DataLayout::CELL_DOM;
     m_sparsity_layout = SparsityLayout::SPARSE;
   }
-  else if (biset == mm.get_mapped_biSet(DataLayout::MAT_DOM, SparsityLayout::DENSE)) {
+  else if(biset == mm.get_mapped_biSet(DataLayout::MAT_DOM, SparsityLayout::DENSE))
+  {
     m_data_layout = DataLayout::MAT_DOM;
     m_sparsity_layout = SparsityLayout::DENSE;
   }
-  else if (biset == mm.get_mapped_biSet(DataLayout::MAT_DOM, SparsityLayout::SPARSE)) {
+  else if(biset ==
+          mm.get_mapped_biSet(DataLayout::MAT_DOM, SparsityLayout::SPARSE))
+  {
     m_data_layout = DataLayout::MAT_DOM;
     m_sparsity_layout = SparsityLayout::SPARSE;
   }
-  else {
+  else
+  {
     SLIC_ASSERT(false);
   }
 
@@ -155,70 +159,83 @@ inline MMField2D<DataType, BiSet>::MMField2D(MultiMat& mm, const BiSet *biset,
 }
 
 //Copy constructor
-template<typename DataType, typename BiSet>
-inline MMField2D<DataType, BiSet>::MMField2D(const MMField2D & o):
-  MMField2D(*o.m_mm, o.set(), o.m_field_name,
-    o.getMap()->data().data(), o.stride())
-  { }
-
+template <typename DataType, typename BiSet>
+inline MMField2D<DataType, BiSet>::MMField2D(const MMField2D& o)
+  : MMField2D(*o.m_mm,
+              o.set(),
+              o.m_field_name,
+              o.getMap()->data().data(),
+              o.stride())
+{ }
 
 //////////////////////// MMField2D Templated ////////////////////////////
 // Child class of MMField2D, typed with layout (cell/mat dom) and sparsity
 
-
 // basic template
-template<typename DataType, DataLayout DataLayoutT, typename BiSet = MultiMat::BivariateSetType>
+template <typename DataType, DataLayout DataLayoutT, typename BiSet = MultiMat::BivariateSetType>
 class MMField2DTemplated : public MMField2D<DataType, BiSet>
 { };
 
 // A helping struct to map a bivariate set to the corresponding SparsityLayout
-template<typename Biset>
-struct MMBiSet2Sparsity {};
+template <typename Biset>
+struct MMBiSet2Sparsity
+{ };
 
-template<>
-struct MMBiSet2Sparsity<MultiMat::ProductSetType> 
-{ SparsityLayout sparsity = SparsityLayout::DENSE; };
+template <>
+struct MMBiSet2Sparsity<MultiMat::ProductSetType>
+{
+  SparsityLayout sparsity = SparsityLayout::DENSE;
+};
 
-template<>
+template <>
 struct MMBiSet2Sparsity<MultiMat::RelationSetType>
-{ SparsityLayout sparsity = SparsityLayout::SPARSE; };
-
+{
+  SparsityLayout sparsity = SparsityLayout::SPARSE;
+};
 
 // Cell-Dom specialization
-template<typename DataType, typename BiSet>
-class MMField2DTemplated<DataType, DataLayout::CELL_DOM, BiSet> :
-  public MMField2D<DataType, BiSet>
+template <typename DataType, typename BiSet>
+class MMField2DTemplated<DataType, DataLayout::CELL_DOM, BiSet>
+  : public MMField2D<DataType, BiSet>
 {
   using Field2DType = MMField2D<DataType, BiSet>;
-public:
-  MMField2DTemplated(MultiMat& mm, const std::string& arr_name = "unnamed",
-    const DataType* data_arr = nullptr, int stride = 1) :
-    Field2DType(mm, (BiSet*) mm.get_mapped_biSet(DataLayout::CELL_DOM, MMBiSet2Sparsity<BiSet>().sparsity),
-      arr_name, data_arr, stride)
-  {}
 
-};
-
-
-
-// Mat-Dom specialization
-template<typename DataType, typename BiSet>
-class MMField2DTemplated<DataType, DataLayout::MAT_DOM, BiSet> :
-  public MMField2D<DataType, BiSet>
-{
-  using Field2DType = MMField2D<DataType, BiSet>;
 public:
   MMField2DTemplated(MultiMat& mm,
-    const std::string& arr_name = "unnamed",
-    const DataType* data_arr = nullptr,
-    int stride = 1) :
-    Field2DType(mm, (BiSet*) mm.get_mapped_biSet(DataLayout::MAT_DOM, MMBiSet2Sparsity<BiSet>().sparsity),
-      arr_name, data_arr, stride)
-  {}
+                     const std::string& arr_name = "unnamed",
+                     const DataType* data_arr = nullptr,
+                     int stride = 1)
+    : Field2DType(mm,
+                  (BiSet*)mm.get_mapped_biSet(DataLayout::CELL_DOM,
+                                              MMBiSet2Sparsity<BiSet>().sparsity),
+                  arr_name,
+                  data_arr,
+                  stride)
+  { }
 };
 
+// Mat-Dom specialization
+template <typename DataType, typename BiSet>
+class MMField2DTemplated<DataType, DataLayout::MAT_DOM, BiSet>
+  : public MMField2D<DataType, BiSet>
+{
+  using Field2DType = MMField2D<DataType, BiSet>;
 
-} //end namespace multimat
-} //end namespace axom
+public:
+  MMField2DTemplated(MultiMat& mm,
+                     const std::string& arr_name = "unnamed",
+                     const DataType* data_arr = nullptr,
+                     int stride = 1)
+    : Field2DType(mm,
+                  (BiSet*)mm.get_mapped_biSet(DataLayout::MAT_DOM,
+                                              MMBiSet2Sparsity<BiSet>().sparsity),
+                  arr_name,
+                  data_arr,
+                  stride)
+  { }
+};
+
+}  //end namespace multimat
+}  //end namespace axom
 
 #endif
