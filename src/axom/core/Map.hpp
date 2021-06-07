ad7b672e8de3fb39dbf8e86c8d39f97495c59d44
Kae Suarez <suarez29@llnl.gov>
2021-06-07 07:52:56 -0700

    Getters for attributes, fixing a bug.

--- src/axom/core/Map.hpp
+++ src/axom/core/Map.hpp
@@ -46,6 +46,8 @@ namespace axom_map
       list[len-1].next = -1;
       free = 0;
       head = -1;
+      size = 0;
+      capacity = len;
       end.key = Key{0};
       end.value = T{0};
       end.next = -2;
@@ -58,7 +60,9 @@ namespace axom_map
       }
       list[len-1].next = -1;
       free = 0;
-      head = -1;
+      head = -1; 
+      capacity = len;
+      size = 0;
       end.key = Key{0};
       end.value = T{0};
       end.next = -2;
@@ -72,6 +76,7 @@ namespace axom_map
           list[head].next = -1;
           list[head].key = key;
           list[head].value = value;
+          size += 1;
         }
         else{
           IndexType ind = head;
@@ -88,6 +93,7 @@ namespace axom_map
           list[ind].next = -1;
           list[ind].key = key;
           list[ind].value = value;
+          size++;
         }
         return true;
       }
@@ -113,12 +119,14 @@ namespace axom_map
             }
             ind = list[ind].next;
           }
+      
           list[ind].next = free;
           ind = free;
           free = list[free].next;
           list[ind].next = -1;
           list[ind].key = key;
           list[ind].value = value;
+          size++;
         }
         return true;
       }
@@ -181,12 +189,14 @@ namespace axom_map
      }while(ind != -1);
    }   
   
-    
-    
+  int get_capacity(){ return capacity; }
+  int get_size(){ return size; }   
+       
     axom_map::node<Key, T> * list;   
     axom_map::node<Key, T> end;
     IndexType head;
     IndexType free;
+    int capacity, size;
   
   }; 
 }
@@ -209,16 +219,17 @@ template <typename Key, typename T, typename Hash = std::hash<Key> >
 class Map
 {
 public:
-  Map(int buckets, int bucklen ){
-    len = buckets;
-    alloc_map(len, bucklen);    
-   
+  Map(int buckets, int bucklen=10 ){
+    bucket_count = buckets;
+    bucket_len = bucklen;
+    testlist = alloc_map(bucket_count, bucklen);    
+    
    
    insert(27, 236);
    insert(2, 259);
    insert(3, 10);
    insert(35, 2195);
-   std::cout << find(3).value;
+   std::cout << find(3).value << std::endl;
    remove(3);
    if(find(3) == testlist[0].end){
      std::cout << "removed" << std::endl;
@@ -226,37 +237,53 @@ public:
    if(find(4) == testlist[0].end){
      std::cout << "never added" << std::endl;
    }
-   std::cout << find(2).value;
+   std::cout << find(2).value << std::endl;
+   rehash();
+   insert(3,10);
+   std::cout << find(3).value << std::endl;
+   remove(3);
+   if(find(3) == testlist[0].end){
+     std::cout << "removed" << std::endl;
+   }
+   if(find(4) == testlist[0].end){
+     std::cout << "never added" << std::endl;
+   }
+   std::cout << find(2).value << std::endl;
+
+
   }
 
-  
-
-private:
-  
-  bool alloc_map(int bucount, int buclen){
-    testlist = axom::allocate <axom_map::bucket<Key,T> > (len); 
-    for(int i = 0; i < len; i++){
-     testlist[i].init(bucklen);
+  void rehash(int buckets = -1, int factor = -1){
+   
+    int newlen = 0;
+    IndexType ind;
+    std::size_t hashed;
+    if(buckets != -1){
+      newlen = buckets;
+    }
+    else if(factor != -1){
+      newlen = factor*bucket_count;
+    }
+    else{
+      newlen = 2*bucket_count;
     }
 
-    //update when we're testing our returns
-    return true;
-  }
-   
-  axom_map::bucket<Key,T> *testlist;
-  int len;
- 
-  std::size_t get_hash(Key input){
-    std::size_t hashed =  std::hash<Key>{}(input);
-     
-    return hashed;
-  }
+    axom_map::bucket<Key,T> * new_list = alloc_map(newlen, bucket_len);
 
-  axom_map::bucket<Key,T> * get_bucket(std::size_t hash){
-    return &(testlist[hash%len]);
-  }
-
-public:
+    for(int i = 0; i < bucket_count; i++){
+      ind = testlist[i].head;
+      while(ind != -1){
+        hashed = (get_hash(testlist[i].list[ind].key) % newlen);
+        new_list[hashed].insert_noupdate(testlist[i].list[ind].key, testlist[i].list[ind].value);     
+        ind = testlist[i].list[ind].next;
+      }
+      axom::deallocate(testlist[i].list);
+    }
+    axom::deallocate(testlist);
+    testlist = new_list;
+    bucket_count = newlen;
+    return; 
+  } 
 
   bool insert(Key key, T val){
     axom_map::bucket<Key,T> * target = get_bucket(get_hash(key));  
@@ -273,6 +300,39 @@ public:
     axom_map::bucket<Key,T> * target = get_bucket(get_hash(key));
     return target->find(key);
   }
+
+  int get_bucket_size(){ return bucket_len; }
+  int get_bucket_count(){ return bucket_count; }
+  int get_size() { return size; }
+  int get_capacity() { return bucket_len*bucket_count; }
+
+private:
+  
+  axom_map::bucket<Key,T> * alloc_map(int bucount, int bucklen){
+    axom_map::bucket<Key,T> *tmp = axom::allocate <axom_map::bucket<Key,T> > (bucount); 
+    for(int i = 0; i < bucount; i++){
+     tmp[i].init(bucklen);
+    }
+
+    //update when we're testing our returns
+    return tmp;
+  }
+   
+   
+  std::size_t get_hash(Key input){
+    std::size_t hashed =  std::hash<Key>{}(input);
+     
+    return hashed;
+  }
+
+  axom_map::bucket<Key,T> * get_bucket(std::size_t hash){
+    return &(testlist[hash%bucket_count]);
+  }
+
+  axom_map::bucket<Key,T> *testlist;
+  int bucket_count, bucket_len, size, load_factor;
+ 
+
 };
 
 } /* namespace axom */
