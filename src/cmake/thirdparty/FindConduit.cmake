26f8c25fb0cd58f5d1fdffd4423fad00e63215af
Chris White <white238@llnl.gov>
2021-07-19 19:33:06 -0700

    create a single variable that points to conduits config directory

--- src/cmake/thirdparty/FindConduit.cmake
+++ src/cmake/thirdparty/FindConduit.cmake
@@ -17,21 +17,23 @@ if(NOT CONDUIT_DIR)
     MESSAGE(FATAL_ERROR "Could not find Conduit. Conduit requires explicit CONDUIT_DIR.")
 endif()
 
+# This is used in axom-config.cmake.in
+set(AXOM_CONDUIT_CONFIG_DIR "${CONDUIT_DIR}/lib/cmake/conduit" CACHE PATH "")
 
 if(NOT WIN32)
-    set(_conduit_config "${CONDUIT_DIR}/lib/cmake/conduit/ConduitConfig.cmake")
+    set(_conduit_config "${AXOM_CONDUIT_CONFIG_DIR}/ConduitConfig.cmake")
     if(NOT EXISTS ${_conduit_config})
         MESSAGE(FATAL_ERROR "Could not find Conduit cmake include file ${_conduit_config}")
     endif()
 
     find_package(Conduit REQUIRED
                  NO_DEFAULT_PATH
-                 PATHS ${CONDUIT_DIR}/lib/cmake/conduit)
+                 PATHS ${AXOM_CONDUIT_CONFIG_DIR})
 else()
     # Allow for several different configurations of Conduit
     find_package(Conduit CONFIG 
         REQUIRED
-        HINTS ${CONDUIT_DIR}/lib/cmake/conduit
+        HINTS ${AXOM_CONDUIT_CONFIG_DIR}
               ${CONDUIT_DIR}/cmake/conduit
               ${CONDUIT_DIR}/share/cmake/conduit
               ${CONDUIT_DIR}/share/conduit
