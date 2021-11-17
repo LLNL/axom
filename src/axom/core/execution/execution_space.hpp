547e5d522a4de48091f251ad29e514de61d71a1c
Kenneth Weiss <weiss27@llnl.gov>
2021-11-17 13:37:46 -0800

    Adds memory_space trait to axom's execution_space type traits classes

--- src/axom/core/execution/execution_space.hpp
+++ src/axom/core/execution/execution_space.hpp
@@ -79,11 +79,13 @@ struct execution_space
   using atomic_policy = void;
   using sync_policy = void;
 
-  static constexpr bool async() noexcept { return false; };
-  static constexpr bool valid() noexcept { return false; };
-  static constexpr bool onDevice() noexcept { return false; };
-  static constexpr char* name() noexcept { return (char*)"[UNDEFINED]"; };
-  static int allocatorID() noexcept { return axom::INVALID_ALLOCATOR_ID; };
+  static constexpr MemorySpace memory_space = MemorySpace::Dynamic;
+
+  static constexpr bool async() noexcept { return false; }
+  static constexpr bool valid() noexcept { return false; }
+  static constexpr bool onDevice() noexcept { return false; }
+  static constexpr char* name() noexcept { return (char*)"[UNDEFINED]"; }
+  static int allocatorID() noexcept { return axom::INVALID_ALLOCATOR_ID; }
 };
 
 }  // namespace axom
