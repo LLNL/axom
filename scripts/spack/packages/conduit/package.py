fda3b71cdfc8e8a0ed4c96d58559dd356b17776c
Brian Manh Hien Han <han12@llnl.gov>
2021-12-06 13:57:31 -0800

    TOSS4 building +fortran; fortran flag added to conduit, using CMake 3.21; remove cray implicit link directory; add specific flags for cray MPI

--- scripts/spack/packages/conduit/package.py
+++ scripts/spack/packages/conduit/package.py
@@ -369,8 +369,10 @@ class Conduit(CMakePackage):
         if cxxflags:
             cfg.write(cmake_cache_entry("CMAKE_CXX_FLAGS", cxxflags))
         fflags = ' '.join(spec.compiler_flags['fflags'])
-        if self.spec.satisfies('%cce'):
+        # AXOM EDIT START
+        if self.spec.satisfies('%cce') or ("crayftn" in f_compiler):
             fflags += " -ef"
+        # AXOM EDIT END
         if fflags:
             cfg.write(cmake_cache_entry("CMAKE_Fortran_FLAGS", fflags))
 
