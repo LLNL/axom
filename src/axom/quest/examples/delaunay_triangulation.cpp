a486f5b64127053477e70ffaebab6eae41c42bbf
Kenneth Weiss <weiss27@llnl.gov>
2021-12-10 14:23:48 -0800

    Refactors delaunay_triangulation example using cli11 and fmt

--- src/axom/quest/examples/delaunay_triangulation.cpp
+++ src/axom/quest/examples/delaunay_triangulation.cpp
@@ -6,207 +6,166 @@
 /*!
  * \file delaunay_triangulation.cpp
  * \brief This file contains an example that creates Delaunay triangulation
- * sequentially from a set of random points contained within a rectangle
- * centered at the origin.
- * This example can run in 2D with triangles or in 3D with tetrahedrons.
+ * incrementally from a set of random points contained within an axis-aligned bounding box.
+ *
+ * This example can generate a triangle mesh in 3D or a tetrahedral mesh in 3D.
  * It uses SLAM IA, a topological mesh data structure.
  */
 
+#include "axom/core.hpp"
 #include "axom/slic.hpp"
+#include "axom/slam.hpp"
 #include "axom/quest.hpp"
 
-#include <ctime>
-#include <iomanip>
+#include "axom/fmt.hpp"
+#include "axom/CLI11.hpp"
 
-using namespace axom;
-
-//This is the length of the bounding rectangle to contain the random points
-double BOUNDING_BOX_DIM[] = {2.0, 2.0, 2.0};
-
-using IndexType = int;
-using DataType = double;
-using Point2D = axom::primal::Point2D;
-using Point3D = axom::primal::Point3D;
-
-enum InputStatus
-{
-  SUCCESS,
-  SHOWHELP,
-  CANTOPENFILE
-};
-
+/// Struct to parse and contain command line arguments
 struct Input
 {
-  std::string inputFile;
-  std::string outputVTKFile;
-  int numRandPoints;
-  bool outputSteps;
-  int numOutputSteps;
-  int dimension;
+  std::string outputVTKFile {"delaunay_output"};
+  int numRandPoints {20};
+  int numOutputSteps {0};
+  int dimension {2};
+  std::vector<double> boundsMin;
+  std::vector<double> boundsMax;
 
-  InputStatus errorCode;
+private:
+public:
+  bool shouldOutputSteps() const { return numOutputSteps != 0; }
 
-  Input()
-    : outputVTKFile("delaunay_output")
-    , numRandPoints(10)
-    , outputSteps(true)
-    , errorCode(SUCCESS) {};
-
-  Input(int argc, char** argv);
-
-  void showhelp()
+  void parse(int argc, char** argv, axom::CLI::App& app)
   {
-    std::cout << "Argument usage: \n"
-                 "  --help(-h)             Show this help message. \n"
-                 "  --nrandpt(-n) [N]      The number of random points.  "
-                 "Default is N = 20.  \n"
-                 "  --dim(-d) [D]          The dimension of the mesh. 2 = "
-                 "triangles in 2D, 3 = tetrahedrons in 3D.\n"
-                 "  --outfile(-o) [fname]  The VTK output file. Default fname "
-                 "= delaunay_output.\n"
-                 "  --outputsteps(-s) [S]  S number of intermediate steps will "
-                 "be written out as VTK files.\n"
-                 "                         S must be a strictly positive "
-                 "integer, or omit to write one file per step.\n"
-                 "                         By default no internediate VTK file "
-                 "is written.\n"
-              << std::endl;
-  };
+    app.add_option("-n,--nrandpt", numRandPoints)
+      ->description("The number of random points")
+      ->capture_default_str();
+
+    app.add_option("-d,--dim", dimension)
+      ->description(
+        "The dimension of the mesh. 2 for triangle mesh in 2D; 3 for "
+        "tetrahedral mesh in 3D")
+      ->capture_default_str();
+
+    app.add_option("-s,--outputsteps", numOutputSteps)
+      ->description(
+        "Number of intermediate steps to write out as VTK files. "
+        "None by default; Use -1 to write one file per insterted point")
+      ->capture_default_str();
+
+    app.add_option("-o,--outfile", outputVTKFile)
+      ->description("The VTK output file")
+      ->capture_default_str();
+
+    // Optional bounding box for query region
+    auto* minbb = app.add_option("--min", boundsMin)
+                    ->description("Min bounds for query box (x,y[,z])")
+                    ->expected(2, 3);
+    auto* maxbb = app.add_option("--max", boundsMax)
+                    ->description(
+                      "Max bounds for query box (x,y[,z]). "
+                      "Defaults to unit square/cube when not provided")
+                    ->expected(2, 3);
+    minbb->needs(maxbb);
+    maxbb->needs(minbb);
+
+    app.get_formatter()->column_width(45);
+
+    // could throw an exception
+    app.parse(argc, argv);
+
+    // Update number of output steps
+    if(numOutputSteps == -1 || numOutputSteps > numRandPoints)
+    {
+      numOutputSteps = numRandPoints;
+    }
+
+    // If user doesn't provide bounds, default to unit cube
+    if(boundsMin.empty())
+    {
+      boundsMin.clear();
+      boundsMax.clear();
+      for(int i = 0; i < dimension; ++i)
+      {
+        boundsMin.push_back(0.);
+        boundsMax.push_back(1.);
+      }
+    }
+
+    SLIC_INFO(axom::fmt::format(R"(Using parameter values:
+    {{
+      dimension: {}
+      nrandpt: {}
+      bounding box min: {{{}}}
+      bounding box max: {{{}}}
+      outfile = '{}'
+      intermediate output steps: {}
+    }})",
+                                dimension,
+                                numRandPoints,
+                                axom::fmt::join(boundsMin, ", "),
+                                axom::fmt::join(boundsMax, ", "),
+                                outputVTKFile,
+                                numOutputSteps));
+  }
 };
 
-Input::Input(int argc, char** argv)
-  : outputVTKFile("delaunay_output")
-  , numRandPoints(20)
-  , outputSteps(false)
-  , numOutputSteps(-1)
-  , dimension(2)
-  , errorCode(SUCCESS)
+/// Utility function to create Delaunay Triangulation over random points
+template <int DIM>
+void run_delaunay(const Input& params)
 {
-  if(argc >= 2)
-  {
-    std::string help = argv[1];
-    if(help == "--help" || help == "-h")
-    {
-      errorCode = SHOWHELP;
-      return;
-    }
-    for(int i = 1; i < argc; /* increment i in loop */)
-    {
-      std::string arg = argv[i];
-      if(arg == "--nrandpt" || arg == "-n")
-      {
-        numRandPoints = atoi(argv[++i]);
-      }
-      else if(arg == "--outputsteps" || arg == "-s")
-      {
-        outputSteps = true;
-        if(argc > i + 1)
-        {
-          numOutputSteps = atoi(argv[i + 1]);
-          //Check that it is actually a non-zero number
-          if(numOutputSteps != 0 || *argv[i + 1] == '0')
-          {
-            // it's a number
-            i++;
-          }
-          else
-          {
-            numOutputSteps = -1;
-          }
-        }
-      }
-      else if(arg == "--outfile" || arg == "-o")
-      {
-        outputVTKFile = argv[++i];
-      }
-      else if(arg == "--dim" || arg == "-d")
-      {
-        dimension = atoi(argv[++i]);
-      }
-      else
-      {
-        errorCode = SHOWHELP;
-        return;
-      }
-      ++i;
-    }
-  }
+  using axom::utilities::random_real;
+  using axom::utilities::string::removeSuffix;
 
-  // if -s is specified but the number is obmitted, then output one file for
-  // each step.
-  if(outputSteps && (numOutputSteps == -1 || numOutputSteps > numRandPoints))
-  {
-    numOutputSteps = numRandPoints;
-  }
-
-  SLIC_INFO("Using parameter values: "
-            << "\n nrandpt = " << numRandPoints << "\n dimension = " << dimension
-            << "\n outputSteps = " << (numOutputSteps == -1 ? 0 : numOutputSteps)
-            << "\n outfile = " << outputVTKFile << std::endl);
-}
-
-template <unsigned int dimension>
-int run_delaunay(int numPoints, int numOutputVTKsteps, std::string outputVTKFile)
-{
-  // Create Delaunay Triangulation on random points
-  using Delaunay = axom::quest::Delaunay<dimension>;
+  using Delaunay = axom::quest::Delaunay<DIM>;
   using PointType = typename Delaunay::PointType;
-  using BoundingBox = axom::primal::BoundingBox<DataType, dimension>;
+  using BoundingBox = typename Delaunay::BoundingBox;
+
+  const int numPoints = params.numRandPoints;
+  const int numOutputVTKsteps = params.numOutputSteps;
+  const std::string& outputVTKFile = removeSuffix(params.outputVTKFile, ".vtk");
+
+  // Use a slam::ModularInt to help with bookkeeping
+  const int dumpFreq =
+    params.shouldOutputSteps() ? numPoints / numOutputVTKsteps : numPoints;
+  axom::slam::ModularInt<> dumperMod(0, dumpFreq);
+
+  BoundingBox bbox {PointType(params.boundsMin.data()),
+                    PointType(params.boundsMax.data())};
+
+  // Create initial Delaunay triangulation over bounding box
   Delaunay dt;
+  dt.initializeBoundary(bbox);
 
-  int stepOutputCount = 1;
-
-  double bb_min_arr[] = {-BOUNDING_BOX_DIM[0] / 2.0,
-                         -BOUNDING_BOX_DIM[1] / 2.0,
-                         -BOUNDING_BOX_DIM[2] / 2.0};
-
-  double bb_max_arr[] = {BOUNDING_BOX_DIM[0] / 2.0,
-                         BOUNDING_BOX_DIM[1] / 2.0,
-                         BOUNDING_BOX_DIM[2] / 2.0};
-
-  PointType bb_min(bb_min_arr);
-  PointType bb_max(bb_max_arr);
-  BoundingBox bb(bb_min, bb_max);
-  dt.initializeBoundary(bb);
-
-  //Adding a number of random points
-  time_t rseed_val = std::time(NULL);
-  SLIC_INFO("Using " << rseed_val << " as seed for rand()");
-  srand(rseed_val);
-
-  for(int pt_i = 0; pt_i < numPoints; pt_i++)
+  // Incrementally insert random points within bounding box
+  for(int i = 0; i < numPoints; ++i)
   {
-    double pt_coord[dimension];
-    for(unsigned int d = 0; d < dimension; d++)
-      pt_coord[d] = (double)rand() / (double)RAND_MAX * BOUNDING_BOX_DIM[d] -
-        BOUNDING_BOX_DIM[d] / 2.0;
-    PointType new_pt(pt_coord, dimension);
+    PointType new_pt;
+    for(int d = 0; d < DIM; ++d)
+    {
+      new_pt[d] = random_real(bbox.getMin()[d], bbox.getMax()[d]);
+    }
 
+    // Insert the point into the triangulation
     dt.insertPoint(new_pt);
 
-    if(numOutputVTKsteps != -1)
+    // Optionally, dump an intermediate mesh file
+    if(params.shouldOutputSteps() && (dumperMod++ == 0 || i == numPoints - 1))
     {
-      if(stepOutputCount * numPoints / (numOutputVTKsteps + 1) == pt_i)
-      {
-        std::ostringstream stm;
-        stm << outputVTKFile << "_" << std::setfill('0') << std::setw(6) << pt_i
-            << ".vtk";
-        dt.writeToVTKFile(stm.str());
-        stepOutputCount++;
-      }
+      std::string fname = axom::fmt::format("{}_{:06}.vtk", outputVTKFile, i);
+      dt.writeToVTKFile(fname);
     }
   }
 
-  dt.removeBoundary();  //Remove the starting rectangular box
+  //Remove the starting rectangular box
+  dt.removeBoundary();
 
   // Write the final mesh to a vtk file
-  std::ostringstream stm;
-  stm << outputVTKFile << ".vtk";
-  dt.writeToVTKFile(stm.str());
+  {
+    std::string fname = axom::fmt::format("{}.vtk", outputVTKFile);
+    dt.writeToVTKFile(fname);
+  }
 
   SLIC_INFO("Done!");
-
-  return 0;
 }
 
 int main(int argc, char** argv)
@@ -215,40 +174,28 @@ int main(int argc, char** argv)
   axom::slic::SimpleLogger logger;
 
   // Initialize default parameters and update with command line arguments:
-  Input params(argc, argv);
+  Input params;
+  axom::CLI::App app {"Delaunay triangulation of a set of points in 2D or 3D"};
 
-  if(params.errorCode != SUCCESS)
+  try
   {
-    if(params.errorCode == SHOWHELP)
-    {
-      params.showhelp();
-      return EXIT_SUCCESS;
-    }
-    else
-    {
-      std::cerr << "Unknown error " << (int)params.errorCode
-                << " while parsing arguments." << std::endl;
-      return EXIT_FAILURE;
-    }
+    params.parse(argc, argv, app);
+  }
+  catch(const axom::CLI::ParseError& e)
+  {
+    return app.exit(e);
   }
 
-  const int numOutputSteps = params.numOutputSteps;
-  const int numPoints = params.numRandPoints;
-  const std::string outputVTKFile = params.outputVTKFile;
-  const int dimension = params.dimension;
-
-  if(dimension == 2)
+  // Run the delaunay algorithm
+  switch(params.dimension)
   {
-    run_delaunay<2>(numPoints, numOutputSteps, outputVTKFile);
-  }
-  else if(dimension == 3)
-  {
-    run_delaunay<3>(numPoints, numOutputSteps, outputVTKFile);
-  }
-  else
-  {
-    params.showhelp();
+  case 2:
+    run_delaunay<2>(params);
+    break;
+  case 3:
+    run_delaunay<3>(params);
+    break;
   }
 
-  return EXIT_SUCCESS;
+  return 0;
 }
