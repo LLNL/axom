f149588a56feaf037ba18280179c9f3a43c89ff2
Chris White <white238@llnl.gov>
2022-03-07 18:38:36 -0800

    add special cases for rocm and no fortran in host-config names

--- scripts/spack/packages/axom/package.py
+++ scripts/spack/packages/axom/package.py
@@ -169,15 +169,19 @@ class Axom(CachedCMakePackage, CudaPackage):
         if "SYS_TYPE" in env:
             # Are we on a LLNL system then strip node number
             hostname = hostname.rstrip('1234567890')
-        cuda = ""
+        special_case = ""
         if "+cuda" in self.spec:
-            cuda = "_cuda"
+            special_case += "_cuda"
+        if "~fortran" in self.spec:
+            special_case += "_nofortran"
+        if "+rocm" in self.spec:
+            special_case += "_rocm"
         return "{0}-{1}-{2}@{3}{4}.cmake".format(
             hostname,
             self._get_sys_type(self.spec),
             self.spec.compiler.name,
             self.spec.compiler.version,
-            cuda
+            special_case
         )
 
     def initconfig_compiler_entries(self):
