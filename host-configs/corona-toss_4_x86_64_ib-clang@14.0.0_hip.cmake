e11b654d7adfe17c384f8276435acb4523105370
Brian Han <han12@llnl.gov>
2022-05-02 13:50:14 -0700

    Try rebuild corona TPLs

--- host-configs/corona-toss_4_x86_64_ib-clang@14.0.0_hip.cmake
+++ host-configs/corona-toss_4_x86_64_ib-clang@14.0.0_hip.cmake
@@ -7,29 +7,31 @@
 #------------------------------------------------------------------------------
 # Compilers
 #------------------------------------------------------------------------------
-# Compiler Spec: clang@13.0.0
+# Compiler Spec: clang@14.0.0
 #------------------------------------------------------------------------------
 if(DEFINED ENV{SPACK_CC})
 
-  set(CMAKE_C_COMPILER "/usr/WS1/axom/libs/toss_4_x86_64_ib/2022_03_14_16_36_51/spack/lib/spack/env/clang/clang" CACHE PATH "")
+  set(CMAKE_C_COMPILER "/usr/WS1/axom/libs/toss_4_x86_64_ib/2022_05_02_12_24_23/spack/lib/spack/env/clang/clang" CACHE PATH "")
 
-  set(CMAKE_CXX_COMPILER "/usr/WS1/axom/libs/toss_4_x86_64_ib/2022_03_14_16_36_51/spack/lib/spack/env/clang/clang++" CACHE PATH "")
+  set(CMAKE_CXX_COMPILER "/usr/WS1/axom/libs/toss_4_x86_64_ib/2022_05_02_12_24_23/spack/lib/spack/env/clang/clang++" CACHE PATH "")
 
-  set(CMAKE_Fortran_COMPILER "/usr/WS1/axom/libs/toss_4_x86_64_ib/2022_03_14_16_36_51/spack/lib/spack/env/clang/flang" CACHE PATH "")
+  set(CMAKE_Fortran_COMPILER "/usr/WS1/axom/libs/toss_4_x86_64_ib/2022_05_02_12_24_23/spack/lib/spack/env/clang/flang" CACHE PATH "")
 
 else()
 
-  set(CMAKE_C_COMPILER "/usr/tce/packages/rocmcc-tce/rocmcc-4.5.2/bin/amdclang" CACHE PATH "")
+  set(CMAKE_C_COMPILER "/opt/rocm-5.1.0/llvm/bin/amdclang" CACHE PATH "")
 
-  set(CMAKE_CXX_COMPILER "/usr/tce/packages/rocmcc-tce/rocmcc-4.5.2/bin/hipcc" CACHE PATH "")
+  set(CMAKE_CXX_COMPILER "/opt/rocm-5.1.0/llvm/bin/amdclang++" CACHE PATH "")
 
-  set(CMAKE_Fortran_COMPILER "/usr/tce/packages/rocmcc-tce/rocmcc-4.5.2/bin/amdflang" CACHE PATH "")
+  set(CMAKE_Fortran_COMPILER "/opt/rocm-5.1.0/llvm/bin/amdflang" CACHE PATH "")
 
 endif()
 
 set(CMAKE_Fortran_FLAGS "-Mfreeform" CACHE STRING "")
 
 set(ENABLE_FORTRAN ON CACHE BOOL "")
+
+set(BLT_CXX_STD "c++14" CACHE STRING "")
 
 #------------------------------------------------------------------------------
 # MPI
@@ -60,13 +62,13 @@ set(ENABLE_MPI ON CACHE BOOL "")
 
 set(ENABLE_HIP ON CACHE BOOL "")
 
-set(HIP_ROOT_DIR "/opt/rocm-4.5.2/hip" CACHE STRING "")
+set(HIP_ROOT_DIR "/opt/rocm-5.1.0/hip" CACHE STRING "")
 
-set(HIP_CLANG_PATH "/opt/rocm-4.5.2/hip/../llvm/bin" CACHE STRING "")
+set(HIP_CLANG_PATH "/opt/rocm-5.1.0/hip/../llvm/bin" CACHE STRING "")
 
 set(CMAKE_HIP_ARCHITECTURES "gfx906" CACHE STRING "")
 
-set(CMAKE_EXE_LINKER_FLAGS "-Wl,--disable-new-dtags -L/opt/rocm-4.5.2/hip/../llvm/lib -L/opt/rocm-4.5.2/hip/lib -Wl,-rpath,/opt/rocm-4.5.2/hip/../llvm/lib:/opt/rocm-4.5.2/hip/lib -lpgmath -lflang -lflangrti -lompstub -lamdhip64 -L/opt/rocm-4.5.2/hip/../lib64 -Wl,-rpath,/opt/rocm-4.5.2/hip/../lib64 -lhsakmt -lamd_comgr" CACHE STRING "")
+set(CMAKE_EXE_LINKER_FLAGS "-Wl,--disable-new-dtags -L/opt/rocm-5.1.0/hip/../llvm/lib -L/opt/rocm-5.1.0/hip/lib -Wl,-rpath,/opt/rocm-5.1.0/hip/../llvm/lib:/opt/rocm-5.1.0/hip/lib -lpgmath -lflang -lflangrti -lompstub -lamdhip64 -L/opt/rocm-5.1.0/hip/../lib64 -Wl,-rpath,/opt/rocm-5.1.0/hip/../lib64 -lhsakmt -lamd_comgr" CACHE STRING "")
 
 #------------------------------------------------
 # Hardware Specifics
@@ -82,9 +84,9 @@ set(ENABLE_GTEST_DEATH_TESTS ON CACHE BOOL "")
 
 # Root directory for generated TPLs
 
-set(TPL_ROOT "/usr/WS1/axom/libs/toss_4_x86_64_ib/2022_03_14_16_36_51/clang-13.0.0" CACHE PATH "")
+set(TPL_ROOT "/usr/WS1/axom/libs/toss_4_x86_64_ib/2022_05_02_12_24_23/clang-14.0.0" CACHE PATH "")
 
-set(CONDUIT_DIR "${TPL_ROOT}/conduit-0.7.2axom" CACHE PATH "")
+set(CONDUIT_DIR "${TPL_ROOT}/conduit-0.8.3" CACHE PATH "")
 
 set(C2C_DIR "${TPL_ROOT}/c2c-1.3.0" CACHE PATH "")
 
@@ -94,9 +96,11 @@ set(HDF5_DIR "${TPL_ROOT}/hdf5-1.8.22" CACHE PATH "")
 
 set(LUA_DIR "${TPL_ROOT}/lua-5.3.5" CACHE PATH "")
 
-set(RAJA_DIR "${TPL_ROOT}/raja-0.14.0" CACHE PATH "")
+set(RAJA_DIR "${TPL_ROOT}/raja-2022.03.0" CACHE PATH "")
 
-set(UMPIRE_DIR "${TPL_ROOT}/umpire-6.0.0axom" CACHE PATH "")
+set(UMPIRE_DIR "${TPL_ROOT}/umpire-2022.03.1" CACHE PATH "")
+
+set(CAMP_DIR "${TPL_ROOT}/camp-2022.03.0" CACHE PATH "")
 
 # scr not built
 
