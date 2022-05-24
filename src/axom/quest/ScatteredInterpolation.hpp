d7cd21beadbdb242934f8397262ac05d9be81a7c
Kenneth Weiss <weiss27@llnl.gov>
2022-05-23 20:56:42 -0700

    Adds `interpolateField()` function to quest's `ScatteredInterpolation` class
    This function linearly interpolates a scalar field from one point mesh to another
    using the generated Delaunay triangulation.

--- src/axom/quest/ScatteredInterpolation.hpp
+++ src/axom/quest/ScatteredInterpolation.hpp
@@ -17,11 +17,8 @@
 
 #include "conduit_blueprint.hpp"
 
-#include <list>
-#include <vector>
-#include <set>
-#include <cstdlib>
 #include <cmath>
+#include <limits>
 
 namespace
 {
@@ -116,15 +113,8 @@ public:
     // Perform some simple error checking
     SLIC_ASSERT(this->isValidBlueprint(mesh_node));
 
-    auto valuesPath = fmt::format("coordsets/{}/values", coordset);
-    SLIC_ASSERT(mesh_node.has_path(valuesPath));
-    auto& values = mesh_node[valuesPath];
-
-    SLIC_ASSERT(::extractDimension(values) == DIM);
-    const int npts = ::extractSize(values);
-
-    // Assume initially that coords are interleaved
-    auto coords = ::ArrayView_from_Node<PointType>(values["x"], npts);
+    // Extract coordinates as ArrayView of PointType
+    auto coords = getCoordinatesArrayView(mesh_node, coordset);
 
     // Compute the bounding box
     BoundingBoxType bb;
@@ -149,11 +139,11 @@ public:
   /**
    * \brief Locates cell from Delaunay complex containing each point in \a query_mesh
    *
-   * \param [inout] query_node Conduit node for the query points in mesh Blueprint format; 
+   * \param [inout] query_node Conduit node for the query points in mesh Blueprint format;
    * results will be placed into the `cell_idx` field
    * \param [in] coordset The name of the coordinate set for the query mesh
    *
-   * \pre query_mesh is the root of a valid mesh blueprint with an unstructured 
+   * \pre query_mesh is the root of a valid mesh blueprint with an unstructured
    * coordinate set \a coordset and a scalar field named `cell_idx` to store the results
    * \note Uses `Delaunay::INVALID_INDEX` for points that cannot be located within the mesh
    */
@@ -162,15 +152,8 @@ public:
     // Perform some simple error checking
     SLIC_ASSERT(this->isValidBlueprint(query_mesh));
 
-    auto valuesPath = fmt::format("coordsets/{}/values", coordset);
-    SLIC_ASSERT(query_mesh.has_path(valuesPath));
-    auto& values = query_mesh[valuesPath];
-
-    SLIC_ASSERT(::extractDimension(values) == DIM);
-    const int npts = ::extractSize(values);
-
-    // Assume initially that coords are interleaved
-    auto coords = ::ArrayView_from_Node<PointType>(values["x"], npts);
+    auto coords = getCoordinatesArrayView(query_mesh, coordset);
+    const int npts = coords.size();
 
     SLIC_ERROR_IF(!query_mesh.has_path("fields/cell_idx/values"),
                   "Query mesh for ScatteredInterpolation::locatePoints() is "
@@ -189,6 +172,69 @@ public:
     }
   }
 
+  /**
+   * \brief Interpolates a field from an \a input_mesh to one on a \a query_mesh
+   *
+   * \param [inout] query_mesh Root node of mesh (in blueprint format) containing field to generate
+   * \param [in] coordset The name of the coords for query points in \a query_mesh
+   * \param [in] input_mesh Root node of mesh (in blueprint format) containing the field to interpolate
+   * \param [in] input_field_name Name of field on \a input_mesh
+   * \param [in] output_field_name Name of field on \a query_mesh
+   * \param [in] INVALID_VALUE Value to use for points that are not in the \a input_mesh
+   *
+   * \pre \a input_mesh and \a query_mesh must conform to the mesh blueprint schema for point meshes
+   * \pre \a input_mesh must contain a nodal scalar field named \a input_field_name
+   * \pre \a query_mesh must contain a nodal scalar field named \a output_field_name
+   * \pre \a query_mesh must contain a nodal scalar field named \a cell_idx whose values
+   * are the index of the cell from the Delaunay triangulation containing the query points.
+   * These can be computed in the \a locatePoints() function
+   * \post Scalar field \a output_field_name on \a query_mesh will contain the interpolated
+   * values of \a input_field_name from \a input_mesh for all query points that have a valid
+   * \a cell_idx to a cell in the \a input_mesh.
+   */
+  void interpolateField(
+    conduit::Node& query_mesh,
+    const std::string& coordset,
+    conduit::Node& input_mesh,
+    const std::string& input_field_name,
+    const std::string& output_field_name,
+    const double INVALID_VALUE = std::numeric_limits<double>::quiet_NaN())
+  {
+    constexpr auto INVALID_INDEX = DelaunayTriangulation::INVALID_INDEX;
+
+    SLIC_ASSERT(this->isValidBlueprint(query_mesh));
+    SLIC_ASSERT(this->isValidBlueprint(input_mesh));
+
+    // Extract the required fields from the input and query meshes
+    auto in_fld = getField<double>(input_mesh, input_field_name);
+    auto out_fld = getField<double>(query_mesh, output_field_name);
+    auto containing_cell = getField<axom::IndexType>(query_mesh, "cell_idx");
+    auto coords = getCoordinatesArrayView(query_mesh, coordset);
+
+    // Interpolate field at query points
+    const int npts = coords.size();
+    for(int idx = 0; idx < npts; ++idx)
+    {
+      const auto cell_id = containing_cell[idx];
+      if(cell_id == INVALID_INDEX)
+      {
+        out_fld[idx] = INVALID_VALUE;
+      }
+      else
+      {
+        double res = 0.;
+        const auto baryCoords = m_delaunay.getBaryCoords(cell_id, coords[idx]);
+        const auto verts = m_delaunay.getMeshData()->boundaryVertices(cell_id);
+        for(auto it = verts.begin(); it < verts.end(); ++it)
+        {
+          res += in_fld[*it] * baryCoords[it.index()];
+        }
+
+        out_fld[idx] = res;
+      }
+    }
+  }
+
 private:
   /// Check validity of blueprint group
   bool isValidBlueprint(const conduit::Node& mesh_node) const
@@ -202,6 +248,54 @@ private:
     }
 
     return success;
+  }
+
+  /**
+   * \brief Returns and ArrayView to the desired field from a mesh blueprint, which must be present
+   *
+   * \param [in] mesh_node The root conduit node of a valid mesh blueprint
+   * \param [in] field_name The name of the field to return; Can either be either the name of the field,
+   *  or the path to the field, e.g. fields/<field_name>/values
+   *
+   * \pre The field named \a field_name must be present in \a mesh_node or code will error out
+   * \note Only currently supports scalar fields
+   */
+  template <typename T>
+  ArrayView<T> getField(conduit::Node& mesh_node, const std::string& field_name)
+  {
+    using axom::utilities::string::startsWith;
+
+    const std::string field_path = startsWith(field_name, "field")
+      ? field_name
+      : fmt::format("fields/{}/values", field_name);
+    SLIC_ERROR_IF(
+      !mesh_node.has_path(field_path),
+      fmt::format("Mesh blueprint is missing required field '{}'", field_name));
+
+    auto& values = mesh_node[field_path];
+    const auto sz = values.dtype().number_of_elements();
+
+    return ::ArrayView_from_Node<T>(values, sz);
+  }
+
+  /**
+   * \brief Returns an \a ArrayView<PointType> of the coordinates of the given \a mesh_node
+   *
+   * \param [in] mesh_node The root conduit node of a valid mesh blueprint
+   * \param [in] coordset The name of the coordinate set within \a mesh_node that we're returning
+   */
+  axom::ArrayView<PointType> getCoordinatesArrayView(conduit::Node& mesh_node,
+                                                     const std::string& coordset)
+  {
+    const auto valuesPath = fmt::format("coordsets/{}/values", coordset);
+    SLIC_ASSERT(mesh_node.has_path(valuesPath));
+    auto& values = mesh_node[valuesPath];
+
+    SLIC_ASSERT(::extractDimension(values) == DIM);
+    const int npts = ::extractSize(values);
+
+    // Assume initially that coords are interleaved
+    return ::ArrayView_from_Node<PointType>(values["x"], npts);
   }
 
 private:
