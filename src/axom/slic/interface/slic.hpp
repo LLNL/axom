827962d65577fea1eb046f461e71126d0f51129f
Chris White <white238@llnl.gov>
2022-05-25 14:52:23 -0700

    add space

--- src/axom/slic/interface/slic.hpp
+++ src/axom/slic/interface/slic.hpp
@@ -211,7 +211,7 @@ void addStreamToAllMsgLevels(GenericOutputStream* ls);
 
 /*!
  * \brief Aborts and flushes on warning or error if corresponding AbortOnError
- * or AbortOnWarningis set to true
+ * or AbortOnWarning is set to true
  * \collective
  * \param [in] level the logging level.
  */
