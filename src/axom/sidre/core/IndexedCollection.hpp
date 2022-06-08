3dd59777d9b1e5013a5f8e6cf7b04627771c6665
Kenneth Weiss <weiss27@llnl.gov>
2022-06-07 22:32:50 -0700

    Adds IndexedCollection to sidre
    An IndexedCollection is a collection of pointers to items of a given type
    where each item has an associated index.

---
+++ src/axom/sidre/core/IndexedCollection.hpp
@@ -0,0 +1,208 @@
+// Copyright (c) 2017-2022, Lawrence Livermore National Security, LLC and
+// other Axom Project Developers. See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
+#ifndef SIDRE_INDEXED_COLLECTION_HPP_
+#define SIDRE_INDEXED_COLLECTION_HPP_
+
+// Standard C++ headers
+#include <map>
+#include <stack>
+#include <string>
+#include <vector>
+
+// Other axom headers
+#include "axom/config.hpp"
+#include "axom/core/Types.hpp"
+#include "axom/core/Macros.hpp"
+
+// Sidre project headers
+#include "SidreTypes.hpp"
+#include "ItemCollection.hpp"
+
+namespace axom
+{
+namespace sidre
+{
+/*!
+ *************************************************************************
+ *
+ * \class IndexedCollection
+ *
+ * \brief IndexedCollection is a container for a collection of pointers
+ * to items of template parameter type T, each with a corresponding index
+ *************************************************************************
+ */
+template <typename T>
+class IndexedCollection : public ItemCollection<T>
+{
+public:
+  using iterator = typename ItemCollection<T>::iterator;
+  using const_iterator = typename ItemCollection<T>::const_iterator;
+
+public:
+  //
+  // Default compiler-generated ctor, dtor, copy ctor, and copy assignment
+  // operator suffice for this class.
+  //
+
+  /// Gets the number of items stored in the collection
+  size_t getNumItems() const { return m_items.size() - m_free_ids.size(); }
+
+  /// Returns the index of the first valid item or InvalidIndex if there are none
+  IndexType getFirstValidIndex() const;
+
+  /// Returns the next valid index after \idx or InvalidIndex if there are none
+  IndexType getNextValidIndex(IndexType idx) const;
+
+  /// Return true if there is an item at index \a idx
+  bool hasItem(IndexType idx) const
+  {
+    return isInRange(idx) && m_items[static_cast<unsigned>(idx)] != nullptr;
+  }
+
+  /// Return the \a item at index \idx or nullptr if that index is empty
+  T* getItem(IndexType idx)
+  {
+    return (hasItem(idx) ? m_items[static_cast<unsigned>(idx)] : nullptr);
+  }
+
+  /// Return the \a item at index \idx or nullptr if that index is empty
+  T const* getItem(IndexType idx) const
+  {
+    return (hasItem(idx) ? m_items[static_cast<unsigned>(idx)] : nullptr);
+  }
+
+  /// Insert \a item into the next available free index
+  IndexType insertItem(T* item)
+  {
+    const IndexType newIndex = getValidEmptyIndex();
+    m_items[newIndex] = item;
+    return newIndex;
+  }
+
+  /*!
+   * \brief Insert \a item into the next available free index
+   *
+   * \note The second parameter is unused and only present to conform to
+   * the ItemCollection interface
+   */
+  IndexType insertItem(T* item, const std::string& AXOM_UNUSED_PARAM(name))
+  {
+    return insertItem(item);
+  }
+
+  /// Insert \a item at index \a idx
+  IndexType insertItem(T* item, IndexType idx)
+  {
+    SLIC_ASSERT_MSG(!hasItem(idx),
+                    "Attempting to insert item into non-empty index " << idx);
+    SLIC_ASSERT_MSG(isInRange(idx),
+                    "Index " << idx << " is out of range. Max allowed index is "
+                             << m_items.size());
+
+    m_items[idx] = item;
+    return idx;
+  }
+
+  /// Removes the item from index \a idx but does not destroy it
+  T* removeItem(IndexType idx);
+
+  /// Removes all items from the collection, but does not destroy them
+  void removeAllItems()
+  {
+    m_items.clear();
+    while(!m_free_ids.empty())
+    {
+      m_free_ids.pop();
+    }
+  }
+
+  /// Return the index of a valid empty slot in the collection
+  IndexType getValidEmptyIndex()
+  {
+    IndexType newIndex;
+    if(m_free_ids.empty())
+    {
+      newIndex = m_items.size();
+      m_items.push_back(nullptr);
+    }
+    else
+    {
+      newIndex = m_free_ids.top();
+      m_free_ids.pop();
+    }
+
+    return newIndex;
+  }
+
+  iterator begin() { return iterator(this, true); }
+  iterator end() { return iterator(this, false); }
+
+  const_iterator cbegin() const { return const_iterator(this, true); }
+  const_iterator cend() const { return const_iterator(this, false); }
+
+  const_iterator begin() const { return const_iterator(this, true); }
+  const_iterator end() const { return const_iterator(this, false); }
+
+private:
+  bool isInRange(IndexType idx) const
+  {
+    return idx >= 0 && static_cast<unsigned>(idx) < m_items.size();
+  }
+
+private:
+  std::vector<T*> m_items;
+  std::stack<IndexType> m_free_ids;
+};
+
+// -----------------------------------------------------------------------------
+
+template <typename T>
+IndexType IndexedCollection<T>::getFirstValidIndex() const
+{
+  IndexType idx = 0;
+  while(static_cast<unsigned>(idx) < m_items.size() &&
+        m_items[static_cast<unsigned>(idx)] == nullptr)
+  {
+    ++idx;
+  }
+  return ((static_cast<unsigned>(idx) < m_items.size()) ? idx : InvalidIndex);
+}
+
+template <typename T>
+IndexType IndexedCollection<T>::getNextValidIndex(IndexType idx) const
+{
+  if(idx == InvalidIndex)
+  {
+    return InvalidIndex;
+  }
+
+  idx++;
+  while(static_cast<unsigned>(idx) < m_items.size() &&
+        m_items[static_cast<unsigned>(idx)] == nullptr)
+  {
+    idx++;
+  }
+  return ((static_cast<unsigned>(idx) < m_items.size()) ? idx : InvalidIndex);
+}
+
+template <typename T>
+T* IndexedCollection<T>::removeItem(IndexType idx)
+{
+  if(hasItem(idx))
+  {
+    T* item = m_items[idx];
+    m_items[idx] = nullptr;
+    m_free_ids.push(idx);
+
+    return item;
+  }
+  return nullptr;
+}
+
+}  // end namespace sidre
+}  // end namespace axom
+
+#endif  // SIDRE_INDEXED_COLLECTION_HPP_
