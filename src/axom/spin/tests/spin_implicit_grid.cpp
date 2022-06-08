c7ee2eae0e293ff838725daaa0c939b9865d0443
Brian Manh Hien Han <han12@llnl.gov>
2022-06-08 15:58:53 -0700

    spin_implicit_grid

--- src/axom/spin/tests/spin_implicit_grid.cpp
+++ src/axom/spin/tests/spin_implicit_grid.cpp
@@ -73,6 +73,11 @@ using MyTypesExec = ::testing::Types<
   axom::spin::ImplicitGrid<2, axom::CUDA_EXEC<256>>,
   axom::spin::ImplicitGrid<3, axom::CUDA_EXEC<256>>,
 #endif
+#ifdef AXOM_USE_HIP
+  axom::spin::ImplicitGrid<1, axom::HIP_EXEC<256>>,
+  axom::spin::ImplicitGrid<2, axom::HIP_EXEC<256>>,
+  axom::spin::ImplicitGrid<3, axom::HIP_EXEC<256>>,
+#endif
   axom::spin::ImplicitGrid<1, axom::SEQ_EXEC>,
   axom::spin::ImplicitGrid<2, axom::SEQ_EXEC>,
   axom::spin::ImplicitGrid<3, axom::SEQ_EXEC>>;
@@ -476,26 +481,28 @@ TYPED_TEST(ImplicitGridExecTest, get_candidates_pt_vectorized)
 
   GridT grid(bbox, &res, maxElts);
 
-  BBox objBox1(SpacePt {.15, .25, .05}, SpacePt {.45, .25, .35});
-  grid.insert(objBox1, 1);
+  BBox* objBox1 = axom::allocate<BBox>(1, kernelAllocID);
+  objBox1[0] = BBox {SpacePt {.15, .25, .05}, SpacePt {.45, .25, .35}};
+
+  grid.insert(objBox1[0], 1);
 
   {
-    SpacePt queryPts[] = {
-      // First three query points inside only obj1
-      objBox1.getMin(),
-      objBox1.getMax(),
-      objBox1.getCentroid(),
+    SpacePt* queryPts = axom::allocate<SpacePt>(9, kernelAllocID);
 
-      // Next two points are not in obj1, but in same grid cells
-      SpacePt {0.11, 0.21, 0.01},
-      SpacePt {0.49, 0.29, 0.39},
+    // First three query points inside only obj1
+    queryPts[0] = objBox1[0].getMin();
+    queryPts[1] = objBox1[0].getMax();
+    queryPts[2] = objBox1[0].getCentroid();
 
-      // Next four points are not in obj1 or any of same cells
-      SpacePt(0.55),
-      SpacePt {.99, 0.25, 0.25},  // outside coord 0
-      SpacePt {.35, 0.99, 0.25},  // outside coord 1
-      SpacePt {.35, 0.25, 0.99},  // outside coord 2
-    };
+    // Next two points are not in obj1, but in same grid cells
+    queryPts[3] = SpacePt {0.11, 0.21, 0.01};
+    queryPts[4] = SpacePt {0.49, 0.29, 0.39};
+
+    // Next four points are not in obj1 or any of same cells
+    queryPts[5] = SpacePt(0.55);
+    queryPts[6] = SpacePt {.99, 0.25, 0.25};  // outside coord 0
+    queryPts[7] = SpacePt {.35, 0.99, 0.25};  // outside coord 1
+    queryPts[8] = SpacePt {.35, 0.25, 0.99};  // outside coord 2
 
     axom::Array<int> count, offset, candidates;
     {
@@ -535,21 +542,27 @@ TYPED_TEST(ImplicitGridExecTest, get_candidates_pt_vectorized)
     {
       EXPECT_EQ(0, count[i]);
     }
+
+    axom::deallocate(queryPts);
   }
 
-  BBox objBox2(SpacePt {.75, .85, .85}, SpacePt(.85));
-  grid.insert(objBox2, 2);
+  BBox* objBox2 = axom::allocate<BBox>(1, kernelAllocID);
+  objBox2[0] = BBox {SpacePt {.75, .85, .85}, SpacePt(.85)};
+  grid.insert(objBox2[0], 2);
 
-  BBox objBox3(SpacePt {.85, .85, .75}, SpacePt(.95));
-  grid.insert(objBox3, 3);
+  BBox* objBox3 = axom::allocate<BBox>(1, kernelAllocID);
+  objBox3[0] = BBox {SpacePt {.85, .85, .75}, SpacePt(.95)};
+  grid.insert(objBox3[0], 3);
 
   {
-    SpacePt queryPts[] = {// Should only be inside obj2
-                          SpacePt {.75, .85, .85},
-                          // Should only be inside obj3
-                          SpacePt(.91),
-                          // Should be inside obj2 and obj3, but not obj1
-                          SpacePt {.85, .85, .85}};
+    SpacePt* queryPts = axom::allocate<SpacePt>(3, kernelAllocID);
+
+    // Should only be inside obj2
+    queryPts[0] = SpacePt {.75, .85, .85};
+    // Should only be inside obj3
+    queryPts[1] = SpacePt(.91);
+    // Should be inside obj2 and obj3, but not obj1
+    queryPts[2] = SpacePt {.85, .85, .85};
 
     axom::Array<int> count, offset, candidates;
     {
@@ -580,7 +593,13 @@ TYPED_TEST(ImplicitGridExecTest, get_candidates_pt_vectorized)
                                      candidates.data() + offset[i] + count[i]);
       EXPECT_EQ(expected[i], actual);
     }
+
+    axom::deallocate(queryPts);
   }
+
+  axom::deallocate(objBox1);
+  axom::deallocate(objBox2);
+  axom::deallocate(objBox3);
 }
 
 TYPED_TEST(ImplicitGridTest, get_candidates_box)
@@ -790,6 +809,9 @@ TYPED_TEST(ImplicitGridExecTest, get_candidates_box_vectorized)
   const int j_max = DIM >= 2 ? grid.gridResolution()[1] : 1;
   const int k_max = DIM >= 3 ? grid.gridResolution()[2] : 1;
 
+  BBox* BBoxes = axom::allocate<BBox>(maxElts, kernelAllocID);
+  SpacePt* queryPt = axom::allocate<SpacePt>(1, kernelAllocID);
+
   // Add some boxes to the spatial index
   {
     // Assumes unit cube
@@ -801,18 +823,18 @@ TYPED_TEST(ImplicitGridExecTest, get_candidates_box_vectorized)
 
     for(int i = 0; i < res[0]; ++i)
     {
-      grid.insert(BBox(SpacePt {0.025 + 0.1 * i, .05, .05},
-                       SpacePt {0.075 + 0.1 * i, .95, .95}),
-                  i);
+      BBoxes[i] = BBox {SpacePt {0.025 + 0.1 * i, .05, .05},
+                        SpacePt {0.075 + 0.1 * i, .95, .95}};
+      grid.insert(BBoxes[i], i);
     }
 
     if(DIM >= 2)
     {
       for(int i = 0; i < res[1]; ++i)
       {
-        grid.insert(BBox(SpacePt {0.05, 0.025 + 0.1 * i, .05},
-                         SpacePt {0.95, 0.075 + 0.1 * i, .95}),
-                    10 + i);
+        BBoxes[10 + i] = BBox {SpacePt {0.05, 0.025 + 0.1 * i, .05},
+                               SpacePt {0.95, 0.075 + 0.1 * i, .95}};
+        grid.insert(BBoxes[10 + i], 10 + i);
       }
     }
 
@@ -820,9 +842,9 @@ TYPED_TEST(ImplicitGridExecTest, get_candidates_box_vectorized)
     {
       for(int i = 0; i < res[2]; ++i)
       {
-        grid.insert(BBox(SpacePt {0.05, 0.05, 0.025 + 0.1 * i},
-                         SpacePt {0.95, 0.95, 0.075 + 0.1 * i}),
-                    20 + i);
+        BBoxes[20 + i] = BBox {SpacePt {0.05, 0.05, 0.025 + 0.1 * i},
+                               SpacePt {0.95, 0.95, 0.075 + 0.1 * i}};
+        grid.insert(BBoxes[20 + i], 20 + i);
       }
     }
 
@@ -833,32 +855,36 @@ TYPED_TEST(ImplicitGridExecTest, get_candidates_box_vectorized)
       {
         for(int k = 0; k < k_max; ++k)
         {
-          SpacePt queryPt {i * .1 + .05, j * .1 + .05, k * .1 + .05};
-          EXPECT_EQ(DIM, grid.getCandidates(queryPt).count());
+          queryPt[0] = SpacePt {i * .1 + .05, j * .1 + .05, k * .1 + .05};
+          EXPECT_EQ(DIM, grid.getCandidates(queryPt[0]).count());
         }
       }
     }
   }
 
-  //// Run some queries
-  BBox queryBoxes[] = {// Empty box -- covers no objects
-                       BBox {},
-                       // Box covers entire domain -- covers all objects
-                       bbox,
-                       // Box is larger than domain -- covers all objects
-                       BBox {SpacePt(-1.), SpacePt(2.)},
-                       // Box only covers first quadrant/octant of domain
-                       BBox {SpacePt(-1.), SpacePt(0.45)},
-                       // Box covers a single cell
-                       BBox {SpacePt(0.525), SpacePt(0.575)},
-                       // Box only covers last quadrant/octant of domain
-                       BBox {SpacePt(0.55), SpacePt(2.)},
-                       // Box covers middle of domain
-                       BBox {SpacePt(0.25), SpacePt(0.75)},
-                       // Box is inverted -- BoundingBox constructor fixes this
-                       BBox {SpacePt(2.), SpacePt(-1)}};
+  axom::deallocate(BBoxes);
+  axom::deallocate(queryPt);
 
+  //// Run some queries
   constexpr int N_QUERIES = 8;
+  BBox* queryBoxes = axom::allocate<BBox>(N_QUERIES, kernelAllocID);
+
+  // Empty box -- covers no objects
+  queryBoxes[0] = BBox {};
+  // Box covers entire domain -- covers all objects
+  queryBoxes[1] = bbox;
+  // Box is larger than domain -- covers all objects
+  queryBoxes[2] = BBox {SpacePt(-1.), SpacePt(2.)};
+  // Box only covers first quadrant/octant of domain
+  queryBoxes[3] = BBox {SpacePt(-1.), SpacePt(0.45)};
+  // Box covers a single cell
+  queryBoxes[4] = BBox {SpacePt(0.525), SpacePt(0.575)};
+  // Box only covers last quadrant/octant of domain
+  queryBoxes[5] = BBox {SpacePt(0.55), SpacePt(2.)};
+  // Box covers middle of domain
+  queryBoxes[6] = BBox {SpacePt(0.25), SpacePt(0.75)};
+  // Box is inverted -- BoundingBox constructor fixes this
+  queryBoxes[7] = BBox {SpacePt(2.), SpacePt(-1)};
 
   std::vector<int> expectedVisits {0,
                                    DIM * 10,
@@ -904,6 +930,8 @@ TYPED_TEST(ImplicitGridExecTest, get_candidates_box_vectorized)
   EXPECT_EQ(DIM >= 1, query_4.count(5) == 1);
   EXPECT_EQ(DIM >= 2, query_4.count(15) == 1);
   EXPECT_EQ(DIM >= 3, query_4.count(25) == 1);
+
+  axom::deallocate(queryBoxes);
 }
 
 //----------------------------------------------------------------------
