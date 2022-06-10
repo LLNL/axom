60f615f998a14e21ac815ec7ef0268d167c7aa71
Jacob Spainhour <Jacob.Spainhour@colorado.edu>
2022-06-09 18:05:06 -0700

    Updated config-build for MSVC 2022

--- config-build.py
+++ config-build.py
@@ -92,10 +92,14 @@ def parse_arguments():
     msvcversions = {'2017': 'Visual Studio 15 2017',
                     '201764': 'Visual Studio 15 2017 Win64',
                     '2019': 'Visual Studio 16 2019',
-                    '201964': 'Visual Studio 16 2019'}
+                    '201964': 'Visual Studio 16 2019',
+                    '2022': 'Visual Studio 17 2022',
+                    '202264': 'Visual Studio 17 2022'}
     # Newer versions  of MSVC might supply an architecture flag
     generator_archs = {'2019': 'Win32',
-                       '201964': 'x64'}
+                       '201964': 'x64',
+                       '2022': 'Win32',
+                       '202264': 'x64'}
     parser.add_argument(
         "--msvc",
         type=str,
@@ -305,6 +309,7 @@ def run_cmake(buildpath, cmakeline):
     os.chdir(buildpath)
     print("Executing CMake line: '%s'" % cmakeline)
     print("")
+
     returncode = subprocess.call(cmakeline, shell=True)
     if not returncode == 0:
         print(
