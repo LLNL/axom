408d572bdb29a24e45ac6f385a28bbce73e865dd
Jacob Spainhour <Jacob.Spainhour@colorado.edu>
2022-06-27 12:56:55 -0700

    Edited examples

--- src/axom/primal/examples/winding_numbers.cpp
+++ src/axom/primal/examples/winding_numbers.cpp
@@ -24,6 +24,7 @@ void parabola_test();
 void split_parabola_test();
 void square_test();
 void big_square_test();
+void segment_test();
 std::function<Vector2D(Point2D)> get_winding_func(Point2D p);
 
 int main(int argc, char** argv)
@@ -32,12 +33,52 @@ int main(int argc, char** argv)
   AXOM_UNUSED_VAR(argv);
 
   axom::slic::SimpleLogger logger;
-  square_test();
-  parabola_test();
-  split_parabola_test();
+  //square_test();
+  //parabola_test();
+  //split_parabola_test();
+  segment_test();
 
   return 0;
 }
+
+void segment_test()
+{
+  Point2D segnodes[] = {Point2D {1.0, -1.0}, Point2D {1.0, 1.0}};
+  Bezier segment(segnodes, 1);
+
+  int npts = 4;
+  for(int i = 0; i < 5; i++)
+  {
+    Point2D qpoint({1 - std::pow(10, -i), 0.0});
+
+    auto winding_func = get_winding_func(qpoint);
+    double winding_num =
+      evaluate_line_integral(segment, winding_func, npts);
+
+    std::cout << qpoint << ": " << winding_num << std::endl;
+  }
+  
+  Point2D badpoint({1.0, 0.0});
+
+  auto bad_winding_func = get_winding_func(badpoint);
+  double bad_winding_num =
+    evaluate_line_integral(segment, bad_winding_func, npts);
+
+  std::cout << badpoint << ": " << bad_winding_num << std::endl;
+
+  for(int i = 4; i > 0; i--)
+  {
+    Point2D qpoint({1 + std::pow(10, -i), 0.0});
+
+    auto winding_func = get_winding_func(qpoint);
+    double winding_num =
+      evaluate_line_integral(segment, winding_func, npts);
+
+    std::cout << qpoint << ": " << winding_num << std::endl;
+  }
+}
+
+
 
 void parabola_test()
 {
@@ -54,11 +95,11 @@ void parabola_test()
 
   Bezier pedges[2] = {para1, para2};
   CPolygon parabola_polygon(pedges, 2);
-  int npts = 15;
+  int npts = 5;
 
   for(int i = 0; i < 5; i++)
   {
-    Point2D qpoint({1 + std::pow(10, -i), 0.0});
+    Point2D qpoint({0.0, 1 - std::pow(10, -i)});
     
     auto winding_func = get_winding_func(qpoint);
     double winding_num = evaluate_line_integral(parabola_polygon, winding_func, npts);
@@ -66,6 +107,16 @@ void parabola_test()
     std::cout << qpoint << ": " << winding_num << std::endl;
   }
 
+  for(int i = 4; i > 0; i--)
+  {
+    Point2D qpoint({0.0, 1 + std::pow(10, -i)});
+
+    auto winding_func = get_winding_func(qpoint);
+    double winding_num =
+      evaluate_line_integral(parabola_polygon, winding_func, npts);
+
+    std::cout << qpoint << ": " << winding_num << std::endl;
+  }
 }
 
 void split_parabola_test()
@@ -183,6 +234,8 @@ std::function<Vector2D(Point2D)> get_winding_func(Point2D p)
   return [p](Point2D x) -> Vector2D {
     double denom =
       2 * M_PI * ((x[0] - p[0]) * (x[0] - p[0]) + (x[1] - p[1]) * (x[1] - p[1]));
+    //std::cout << x << std::endl;
+    //std::cout << denom << std::endl;
     return Vector2D({-(x[1] - p[1]) / denom, (x[0] - p[0]) / denom});
   };
 }
