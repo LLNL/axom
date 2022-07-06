8701b0803f3998bf30f38981a40110b20c79a078
Brian Han <han12@llnl.gov>
2022-07-06 09:04:12 -0700

    Updates tioga installation to shared directory

--- host-configs/tioga-toss_4_x86_64_ib_cray-clang@14.0.0_hip.cmake
+++ host-configs/tioga-toss_4_x86_64_ib_cray-clang@14.0.0_hip.cmake
@@ -11,11 +11,11 @@
 #------------------------------------------------------------------------------
 if(DEFINED ENV{SPACK_CC})
 
-  set(CMAKE_C_COMPILER "/usr/WS1/weiss27/axom/tioga_tpls/toss_4_x86_64_ib_cray/2022_06_23_22_08_18/spack/lib/spack/env/clang/clang" CACHE PATH "")
+  set(CMAKE_C_COMPILER "/usr/WS1/axom/libs/toss_4_x86_64_ib_cray/2022_07_06_08_32_51/spack/lib/spack/env/clang/clang" CACHE PATH "")
 
-  set(CMAKE_CXX_COMPILER "/usr/WS1/weiss27/axom/tioga_tpls/toss_4_x86_64_ib_cray/2022_06_23_22_08_18/spack/lib/spack/env/clang/clang++" CACHE PATH "")
+  set(CMAKE_CXX_COMPILER "/usr/WS1/axom/libs/toss_4_x86_64_ib_cray/2022_07_06_08_32_51/spack/lib/spack/env/clang/clang++" CACHE PATH "")
 
-  set(CMAKE_Fortran_COMPILER "/usr/WS1/weiss27/axom/tioga_tpls/toss_4_x86_64_ib_cray/2022_06_23_22_08_18/spack/lib/spack/env/clang/flang" CACHE PATH "")
+  set(CMAKE_Fortran_COMPILER "/usr/WS1/axom/libs/toss_4_x86_64_ib_cray/2022_07_06_08_32_51/spack/lib/spack/env/clang/flang" CACHE PATH "")
 
 else()
 
@@ -82,7 +82,7 @@ set(ENABLE_GTEST_DEATH_TESTS ON CACHE BOOL "")
 
 # Root directory for generated TPLs
 
-set(TPL_ROOT "/usr/WS1/weiss27/axom/tioga_tpls/toss_4_x86_64_ib_cray/2022_06_23_22_08_18/clang-14.0.0" CACHE PATH "")
+set(TPL_ROOT "/usr/WS1/axom/libs/toss_4_x86_64_ib_cray/2022_07_06_08_32_51/clang-14.0.0" CACHE PATH "")
 
 set(CONDUIT_DIR "${TPL_ROOT}/conduit-0.8.3" CACHE PATH "")
 
