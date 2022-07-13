800cd78a0b9b04ca190e7fbd452d89cad4733bae
Jacob Spainhour <Jacob.Spainhour@colorado.edu>
2022-07-13 10:18:39 -0700

    Improved interface, added tests

--- src/axom/primal/operators/in_curved_polygon.hpp
+++ src/axom/primal/operators/in_curved_polygon.hpp
@@ -57,7 +57,7 @@ inline bool in_curved_polygon(const Point<T, 2>& query,
     max_depth = std::max(max_depth, this_depth);
   }
 
-  return (std::round(ret_val) == 0);
+  return !(std::round(ret_val) == 0);
 }
 
 // Base winding number function
@@ -65,20 +65,29 @@ template <typename T>
 double winding_number(const CurvedPolygon<T, 2>& cpoly,
                       const Point2D& q,
                       int qnodes,
-                      int& max_depth,
-                      double atol = 1e-5)
+                      double int_tol = 1e-5,
+                      double linear_tol = 1e-8)
 {
   double ret_val = 0.0;
-  int this_depth = 0;
-
   for(int i = 0; i < cpoly.numEdges(); i++)
   {
-    ret_val +=
-      detail::adaptive_winding_number(cpoly[i], q, qnodes, this_depth, atol);
-    max_depth = std::max(max_depth, this_depth);
+    double this_val =
+      detail::adaptive_winding_number(cpoly[i], q, qnodes, int_tol, linear_tol);
+    ret_val += this_val;
   }
 
   return ret_val;
+}
+
+// Overload for single bezier curve
+template <typename T>
+double winding_number(const BezierCurve<T, 2>& c,
+                      const Point2D& q,
+                      int qnodes,
+                      double int_tol = 1e-5,
+                      double linear_tol = 1e-8)
+{
+  return detail::adaptive_winding_number(c, q, qnodes, int_tol, linear_tol);
 }
 
 }  // namespace primal
---
+++ src/axom/primal/tests/primal_winding_number.cpp
@@ -0,0 +1,187 @@
+// Copyright (c) 2017-2022, Lawrence Livermore National Security, LLC and
+// other Axom Project Developers. See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
+#include "axom/config.hpp"
+
+#include "axom/primal.hpp"
+#include "axom/core.hpp"
+#include "axom/slic.hpp"
+#include "axom/fmt.hpp"
+
+#include "gtest/gtest.h"
+
+// C++ headers
+#include <cmath>
+#include <iostream>
+#include <fstream>
+
+namespace primal = axom::primal;
+
+TEST(primal_winding_number, simple_cases)
+{
+  using Point2D = primal::Point<double, 2>;
+  using Bezier = primal::BezierCurve<double, 2>;
+  using CPolygon = primal::CurvedPolygon<double, 2>;
+
+  double abs_tol = 1e-10;
+  double int_tol = 1e-5;
+  double lin_tol = 1e-8;
+
+  int qnodes = 15;
+
+  // Simple cubic shape for testing intersecting cubic
+  Point2D top_nodes[] = {Point2D {0.0, 0.0},
+                         Point2D {0.0, 1.0},
+                         Point2D {-1.0, 1.0},
+                         Point2D {-1.0, 0.0}};
+  Bezier top_curve(top_nodes, 3);
+
+  Point2D bot_nodes[] = {Point2D {-1.0, 0.0},
+                         Point2D {-1.0, -1.0},
+                         Point2D {0.0, -1.0},
+                         Point2D {0.0, 0.0}};
+  Bezier bot_curve(bot_nodes, 3);
+  Bezier simple_shape_edges[] = {top_curve, bot_curve};
+  CPolygon simple_shape(simple_shape_edges, 2);
+
+  // Check interior points
+  for(int i = 1; i < 9; i++)
+  {
+    double offset = std::pow(10, -i);
+    Point2D q_vert({-0.352, 0.72 - offset});
+    Point2D q_horz({-0.352 - offset, 0.72});
+
+    EXPECT_NEAR(winding_number(simple_shape, q_vert, qnodes, int_tol, lin_tol),
+                1.0,
+                abs_tol);
+    EXPECT_NEAR(winding_number(simple_shape, q_horz, qnodes, int_tol, lin_tol),
+                1.0,
+                abs_tol);
+  }
+
+  // Check exterior points
+  for(int i = 1; i < 9; i++)
+  {
+    double offset = std::pow(10, -i);
+    Point2D q_vert({-0.352, 0.72 + offset});
+    Point2D q_horz({-0.352 + offset, 0.72});
+
+    EXPECT_NEAR(winding_number(simple_shape, q_vert, qnodes, int_tol, lin_tol),
+                0.0,
+                abs_tol);
+    EXPECT_NEAR(winding_number(simple_shape, q_horz, qnodes, int_tol, lin_tol),
+                0.0,
+                abs_tol);
+  }
+}
+
+TEST(primal_winding_number, edge_cases)
+{
+  using Point2D = primal::Point<double, 2>;
+  using Bezier = primal::BezierCurve<double, 2>;
+
+  double abs_tol = 1e-10;
+  double int_tol = 1e-5;
+  double lin_tol = 1e-8;
+
+  int qnodes = 15;
+
+  // Line segment
+  Point2D nodes1[] = {Point2D {0.0, 0.0}, Point2D {1.0, 1.0}};
+  Bezier linear(nodes1, 1);
+
+  // Cubic curve
+  Point2D nodes2[] = {Point2D {0.0, 0.0},
+                      Point2D {0.0, 1.0},
+                      Point2D {-1.0, 1.0},
+                      Point2D {-1.0, 0.0}};
+  Bezier cubic(nodes2, 3);
+
+  // If query is outside the range of the two endpoints, return 0
+  EXPECT_NEAR(
+    winding_number(cubic, Point2D({-2.5, 0.0}), qnodes, int_tol, lin_tol),
+    0.0,
+    abs_tol);
+
+  EXPECT_NEAR(
+    winding_number(linear, Point2D({-0.45, -0.45}), qnodes, int_tol, lin_tol),
+    0.0,
+    abs_tol);
+
+  // If query point between the endpoints, return +0.5/-0.5
+  EXPECT_NEAR(
+    winding_number(cubic, Point2D({-0.5, 0.0}), qnodes, int_tol, lin_tol),
+    0.5,
+    abs_tol);
+
+  cubic.reverseOrientation();
+  EXPECT_NEAR(
+    winding_number(cubic, Point2D({-0.5, 0.0}), qnodes, int_tol, lin_tol),
+    -0.5,
+    abs_tol);
+  cubic.reverseOrientation();
+}
+
+TEST(primal_winding_number, degenerate_cases)
+{
+  using Point2D = primal::Point<double, 2>;
+  using Bezier = primal::BezierCurve<double, 2>;
+
+  double abs_tol = 1e-10;
+  double int_tol = 1e-5;
+  double lin_tol = 1e-8;
+
+  int qnodes = 15;
+
+  // Line segment
+  Point2D nodes1[] = {Point2D {0.0, 0.0}, Point2D {1.0, 1.0}};
+  Bezier linear(nodes1, 1);
+
+  // Cubic curve
+  Point2D nodes2[] = {Point2D {0.0, 0.0},
+                      Point2D {0.0, 1.0},
+                      Point2D {-1.0, 1.0},
+                      Point2D {-1.0, 0.0}};
+  Bezier cubic(nodes2, 3);
+
+  // Edge cases with query points somewhere on the curve
+  EXPECT_NEAR(  // Query on linear curve
+    winding_number(linear, Point2D({0.45, 0.45}), qnodes, int_tol, lin_tol),
+    0.5,
+    abs_tol);
+  EXPECT_NEAR(  // Query on linear curve, also on quadrature node
+    winding_number(linear, Point2D({0.5, 0.5}), 0.5, int_tol, lin_tol),
+    0.5,
+    abs_tol);
+  EXPECT_NEAR(  // Query on cubic curve, not to machine precision
+    winding_number(cubic, Point2D({-0.352, 0.72}), qnodes, int_tol, lin_tol),
+    0.5,
+    abs_tol);
+  EXPECT_NEAR(  // Query on endpoint of linear
+    winding_number(linear, Point2D({0.0, 0.0}), qnodes, int_tol, lin_tol),
+    0.5,
+    abs_tol);
+  EXPECT_NEAR(  // Query on endpoint of cubic
+    winding_number(cubic, Point2D({-1.0, 0.0}), qnodes, int_tol, lin_tol),
+    0.5,
+    abs_tol);
+  cubic.reverseOrientation();
+  EXPECT_NEAR(  // Query on endpoint of reverse oriented cubic
+    winding_number(cubic, Point2D({-1.0, 0.0}), qnodes, int_tol, lin_tol),
+    0.5,
+    abs_tol);
+  cubic.reverseOrientation();
+}
+
+int main(int argc, char** argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+
+  axom::slic::SimpleLogger logger;
+
+  int result = RUN_ALL_TESTS();
+
+  return result;
+}
