5d56152ad9c6ff6f1288c49b8ad433b618f21da8
Jacob Spainhour <Jacob.Spainhour@colorado.edu>
2022-07-20 15:32:04 -0700

    Adjust formula to easily recognizable as shoelace theorem

--- src/axom/primal/geometry/Triangle.hpp
+++ src/axom/primal/geometry/Triangle.hpp
@@ -138,8 +138,8 @@ public:
     const PointType& C = m_points[2];
 
     // clang-format off
-    return 0.5 * determinant(B[0]-A[0], B[1]-A[1],
-                             C[0]-A[0], C[1]-A[1]);
+    return 0.5 * determinant(B[0]-A[0], C[0]-A[0],
+                             B[1]-A[1], C[1]-A[1]);
     // clang-format on
   }
 
