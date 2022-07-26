f215d65a37568b6c11faa3e507ec350b2c922d09
Jacob Spainhour <Jacob.Spainhour@colorado.edu>
2022-07-26 09:44:44 -0700

    Additional tests for edge/corner cases

--- src/axom/primal/tests/primal_winding_number.cpp
+++ src/axom/primal/tests/primal_winding_number.cpp
@@ -25,8 +25,9 @@ TEST(primal_winding_number, simple_cases)
   using Bezier = primal::BezierCurve<double, 2>;
   using CPolygon = primal::CurvedPolygon<double, 2>;
 
-  double abs_tol = 1e-10;
-  double lin_tol = 1e-16;
+  double abs_tol = 1e-8;
+  double lin_tol = 0;
+  double edge_tol = 0;
 
   // Simple cubic shape for testing intersecting cubic
   Point2D top_nodes[] = {Point2D {0.0, 0.0},
@@ -50,8 +51,12 @@ TEST(primal_winding_number, simple_cases)
     Point2D q_vert({-0.352, 0.72 - offset});
     Point2D q_horz({-0.352 - offset, 0.72});
 
-    EXPECT_NEAR(winding_number(q_vert, simple_shape, lin_tol), 1.0, abs_tol);
-    EXPECT_NEAR(winding_number(q_horz, simple_shape, lin_tol), 1.0, abs_tol);
+    EXPECT_NEAR(winding_number(q_vert, simple_shape, lin_tol, edge_tol),
+                1.0,
+                abs_tol);
+    EXPECT_NEAR(winding_number(q_horz, simple_shape, lin_tol, edge_tol),
+                1.0,
+                abs_tol);
   }
 
   // Check exterior points
@@ -61,9 +66,24 @@ TEST(primal_winding_number, simple_cases)
     Point2D q_vert({-0.352, 0.72 + offset});
     Point2D q_horz({-0.352 + offset, 0.72});
 
-    EXPECT_NEAR(winding_number(q_vert, simple_shape, lin_tol), 0.0, abs_tol);
-    EXPECT_NEAR(winding_number(q_horz, simple_shape, lin_tol), 0.0, abs_tol);
+    EXPECT_NEAR(winding_number(q_vert, simple_shape, lin_tol, edge_tol),
+                0.0,
+                abs_tol);
+    EXPECT_NEAR(winding_number(q_horz, simple_shape, lin_tol, edge_tol),
+                0.0,
+                abs_tol);
   }
+
+  // Test that points on either side of cubic are offset by 1
+  EXPECT_NEAR(winding_number(Point2D({-0.352, 0.72 - 1e-16}), top_curve, 0, 0) -
+                winding_number(Point2D({-0.352, 0.72 + 1e-16}), top_curve, 0, 0),
+              1,
+              abs_tol);
+  top_curve.reverseOrientation();
+  EXPECT_NEAR(winding_number(Point2D({-0.352, 0.72 + 1e-16}), top_curve, 0, 0) -
+                winding_number(Point2D({-0.352, 0.72 - 1e-16}), top_curve, 0, 0),
+              1,
+              abs_tol);
 }
 
 TEST(primal_winding_number, edge_cases)
@@ -74,6 +94,7 @@ TEST(primal_winding_number, edge_cases)
 
   double abs_tol = 1e-10;
   double lin_tol = 1e-16;
+  double edge_tol = 1e-16;
 
   // Line segment
   Point2D nodes1[] = {Point2D {0.0, 0.0}, Point2D {1.0, 1.0}};
@@ -86,28 +107,57 @@ TEST(primal_winding_number, edge_cases)
                       Point2D {-1.0, 0.0}};
   Bezier cubic(nodes2, 3);
 
-  // If query is outside the range of the two endpoints, return 0
-  EXPECT_NEAR(winding_number(Point2D({-2.5, 0.0}), cubic, lin_tol), 0.0, abs_tol);
+  // Flipped Cubic curve
+  Point2D fnodes2[] = {Point2D {0.0, 0.0},
+                       Point2D {0.0, -1.0},
+                       Point2D {-1.0, -1.0},
+                       Point2D {-1.0, 0.0}};
+  Bezier fcubic(fnodes2, 3);
 
-  EXPECT_NEAR(winding_number(Point2D({-0.45, -0.45}), linear, lin_tol),
+  // Test on linear cases
+  EXPECT_NEAR(winding_number(Point2D({-0.45, -0.45}), linear, lin_tol, edge_tol),
+              0.0,
+              abs_tol);
+  EXPECT_NEAR(winding_number(Point2D({1.45, 1.45}), linear, lin_tol, edge_tol),
+              0.0,
+              abs_tol);
+
+  // Test on cubic. If query is outside the range of the two endpoints, return 0
+  EXPECT_NEAR(winding_number(Point2D({-2.5, 0.0}), cubic, lin_tol, edge_tol),
+              0.0,
+              abs_tol);
+  EXPECT_NEAR(winding_number(Point2D({-2.5, 0.0}), fcubic, lin_tol, edge_tol),
               0.0,
               abs_tol);
 
   // If query point between the endpoints, return +0.5/-0.5
-  EXPECT_NEAR(winding_number(Point2D({-0.5, 0.0}), cubic, lin_tol), 0.5, abs_tol);
+  EXPECT_NEAR(winding_number(Point2D({-0.5, 0.0}), cubic, lin_tol, edge_tol),
+              0.5,
+              abs_tol);
+  EXPECT_NEAR(winding_number(Point2D({-0.5, 0.0}), fcubic, lin_tol, edge_tol),
+              -0.5,
+              abs_tol);
 
   cubic.reverseOrientation();
-  EXPECT_NEAR(winding_number(Point2D({-0.5, 0.0}), cubic, lin_tol), -0.5, abs_tol);
-  cubic.reverseOrientation();
+  fcubic.reverseOrientation();
+  EXPECT_NEAR(winding_number(Point2D({-0.5, 0.0}), cubic, lin_tol, edge_tol),
+              -0.5,
+              abs_tol);
+  EXPECT_NEAR(winding_number(Point2D({-0.5, 0.0}), fcubic, lin_tol, edge_tol),
+              0.5,
+              abs_tol);
+
+  return;
 }
 
-TEST(primal_winding_number, degenerate_cases)
+TEST(primal_winding_number, corner_cases)
 {
   using Point2D = primal::Point<double, 2>;
   using Bezier = primal::BezierCurve<double, 2>;
 
   double abs_tol = 1e-8;
   double lin_tol = 1e-8;
+  double edge_tol = 1e-8;
 
   // Line segment
   Point2D nodes1[] = {Point2D {0.0, 0.0}, Point2D {1.0, 1.0}};
@@ -120,60 +170,42 @@ TEST(primal_winding_number, degenerate_cases)
                       Point2D {-1.0, 0.0}};
   Bezier cubic(nodes2, 3);
 
-  // Test edge cases with tolerance 0 to avoid line thickness issues
-  // Current behavior is that points exactly on the curve will return 
-  //  value as if they were inside the osculating circle
+  // Query points exactly on the curve will return a value 
+  // between 0 and 1 that depends on the incident angle of the
+  // tangent lines
 
-  // Special case for lines, returns 1/2 directly
+  // At any point on a line, returns 0
   EXPECT_NEAR(  // Query on linear curve
-    winding_number(Point2D({0.45, 0.45}), linear, 0),
-    0.5,
+    winding_number(Point2D({0.45, 0.45}), linear, 0, edge_tol),
+    0.0,
     abs_tol);
-  linear.reverseOrientation();
-  EXPECT_NEAR(  // Query on endpoint of reverse oriented linear curve
-    winding_number(Point2D({0.45, 0.45}), linear, 0),
-    0.5,
-    abs_tol);
-  linear.reverseOrientation();
-
-  // Test on cubic 
-  EXPECT_NEAR(winding_number(Point2D({-0.352, 0.72}), cubic, 0),
-              winding_number(Point2D({-0.352, 0.72-1e-16}), cubic, 0),
-              abs_tol);
-  cubic.reverseOrientation();
-  EXPECT_NEAR(  
-    winding_number(Point2D({-0.352, 0.72}), cubic, 0),
-    winding_number(Point2D({-0.352, 0.72 - 1e-16}), cubic, 0),
-    abs_tol);
-  cubic.reverseOrientation();
-
-  return;
-  // UNTESTED BEHAVIOR: Query point on endpoint of curve
-
-  // Check asymptotic behavior as you approach the curve
-  EXPECT_NEAR(winding_number(Point2D({-0.5, 0.75 - 1e-16}), cubic, 0) -
-                winding_number(Point2D({-0.5, 0.75 + 1e-16}), cubic, 0),
-              1.0,
-              abs_tol);
-
   EXPECT_NEAR(  // Query on endpoint of linear
-    winding_number(Point2D({0.0, 0.0}), linear, lin_tol),
-    0.5,
+    winding_number(Point2D({0.0, 0.0}), linear, 0, edge_tol),
+    0.0,
     abs_tol);
-  EXPECT_NEAR(  // Query on endpoint of cubic
-    winding_number(Point2D({-1.0, 0.0}), cubic, lin_tol),
-    0.5,
+  EXPECT_NEAR(  // Query on endpoint of linear
+    winding_number(Point2D({1.0, 1.0}), linear, 0, edge_tol),
+    0.0,
     abs_tol);
-  cubic.reverseOrientation();
-  EXPECT_NEAR(  // Query on endpoint of reverse oriented cubic
-    winding_number(Point2D({-1.0, 0.0}), cubic, lin_tol),
-    0.5,
+  
+
+  EXPECT_NEAR(  // Query on initial endpoint of cubic
+    winding_number(Point2D({-1.0, 0.0}), cubic, 0, edge_tol),
+    0.25,
     abs_tol);
-  cubic.reverseOrientation();
+  EXPECT_NEAR(  // Query on terminal endpoint of cubic
+    winding_number(Point2D({-1.0, 0.0}), cubic, 0, edge_tol),
+    0.25,
+    abs_tol);
 
   // The query is on the endpoint after one bisection
-  EXPECT_NEAR(winding_number(Point2D({-0.5, 0.75}), cubic, 0),
-              winding_number(Point2D({-0.5, 0.75 - 1e-16}), cubic, 0),
+  EXPECT_NEAR(winding_number(Point2D({-0.5, 0.75}), cubic, 0, edge_tol),
+              0.312832962673,
+              abs_tol);
+
+  // The query is not on an endpoint after any number of bisections
+  EXPECT_NEAR(winding_number(cubic.evaluate(0.4), cubic, 0, edge_tol),
+              0.310998027957,
               abs_tol);
 }
 
