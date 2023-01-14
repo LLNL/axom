dcaaf795a57c85bce32733b5ab0db914986911a6
Kenneth Weiss <weiss27@llnl.gov>
2023-01-13 17:02:29 -0800

    Adds a smoke test for adiak

---
+++ src/thirdparty/tests/adiak_smoke.cpp
@@ -0,0 +1,44 @@
+// Copyright (c) 2017-2022, Lawrence Livermore National Security, LLC and
+// other Axom Project Developers. See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
+#include "gtest/gtest.h"
+
+#include "adiak.hpp"
+
+#include <iostream>
+
+//------------------------------------------------------------------------------
+// UNIT TESTS
+//------------------------------------------------------------------------------
+TEST(adiak_smoke, check_version)
+{
+  std::cout << "Using adiak version: "     //
+            << ADIAK_VERSION << "."        //
+            << ADIAK_MINOR_VERSION << "."  //
+            << ADIAK_POINT_VERSION << std::endl;
+
+  EXPECT_TRUE(ADIAK_VERSION >= 0);
+  EXPECT_TRUE(ADIAK_MINOR_VERSION >= 0);
+  EXPECT_TRUE(ADIAK_POINT_VERSION >= 0);
+}
+
+TEST(adiak_smoke, basic)
+{
+  adiak::init(nullptr);
+
+  // use a built-in value
+  {
+    bool ret = adiak::user();
+    EXPECT_TRUE(ret);
+  }
+
+  // set a custom value
+  {
+    bool ret = adiak::value("test_type", "smoke");
+    EXPECT_TRUE(ret);
+  }
+
+  adiak::fini();
+}
