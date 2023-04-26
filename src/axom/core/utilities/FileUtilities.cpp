33c7ec08aa9deaacf48bf4495ea4255b2d8dd2ed
Brad Whitlock <whitlock2@llnl.gov>
2023-04-26 15:33:57 -0700

    Added removeFile function.

--- src/axom/core/utilities/FileUtilities.cpp
+++ src/axom/core/utilities/FileUtilities.cpp
@@ -19,6 +19,7 @@
   #define GetCurrentDir _getcwd
   #define ChangeCurrentDir _chdir
   #define Stat _stat
+  #define Unlink _unlink
 #else
   #include <unistd.h>    // for getcwd
   #include <sys/stat.h>  // for stat
@@ -26,6 +27,7 @@
   #define GetCurrentDir getcwd
   #define ChangeCurrentDir chdir
   #define Stat stat
+  #define Unlink unlink
 #endif
 
 namespace axom
@@ -118,6 +120,12 @@ void getDirName(std::string& dir, const std::string& path)
   {
     dir = "";
   }
+}
+
+//-----------------------------------------------------------------------------
+void removeFile(const std::string& filename)
+{
+  Unlink(filename.c_str());
 }
 
 }  // end namespace filesystem
