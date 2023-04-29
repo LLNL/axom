883db4ba558649740c27540126a07f4a342bbd02
Chris White <white238@llnl.gov>
2023-04-28 21:34:16 -0700

    make spin and primal header only libraries

--- src/axom/spin/dummy.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2017-2023, Lawrence Livermore National Security, LLC and
-// other Axom Project Developers. See the top-level COPYRIGHT file for details.
-//
-// SPDX-License-Identifier: (BSD-3-Clause)
-
-// Dummy source for spin
