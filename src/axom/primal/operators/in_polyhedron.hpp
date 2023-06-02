0a97b2ba59353d2010950b3f784a3fa563454590
Jacob Spainhour <Jacob.Spainhour@colorado.edu>
2023-06-02 10:20:18 -0700

    Initial commit of in_polyhedron

---
+++ src/axom/primal/operators/in_polyhedron.hpp
@@ -0,0 +1,67 @@
+// Copyright (c) 2017-2023, Lawrence Livermore National Security, LLC and
+// other Axom Project Developers. See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
+/*!
+ * \file in_polyhedron.hpp
+ *
+ * \brief Consists of methods that test whether a given query point is
+ * inside a given polyhedron.
+ *
+ * Uses a winding number algorithm
+ */
+
+#ifndef AXOM_PRIMAL_IN_POLYHEDRON_HPP_
+#define AXOM_PRIMAL_IN_POLYHEDRON_HPP_
+
+// Axom includes
+#include "axom/config.hpp"
+
+#include "axom/primal/geometry/Point.hpp"
+#include "axom/primal/geometry/Polygon.hpp"
+#include "axom/primal/geometry/Polyhedron.hpp"
+
+// C++ includes
+#include <cmath>
+
+namespace axom
+{
+namespace primal
+{
+/*!
+ * \brief Determines containment for a point in a polygon
+ *
+ * \param [in] query The query point to test
+ * \param [in] poly The Polygon object to test for containment
+ * \param [in] useNonzeroRule If false, use even/odd protocol for inclusion
+ * \param [in] useStrictInclusion If true, points on the boundary are considered exterior.
+ * \param [in] EPS The tolerance level for collinearity
+ * 
+ * Determines containment using the winding number with respect to the 
+ * given polygon. 
+ * Different protocols determine containment from the winding number differently.
+ *   Nonzero Rule:  If the winding number is nonzero, the point is interior.
+ *   Even/Odd rule: If the winding number is odd, it is interior. Exterior otherwise.
+ *
+ * \return boolean value indicating containment.
+ */
+template <typename T>
+bool in_polyhedron(const Point<T, 2>& query,
+                   const Polyhedron<T, 2>& poly,
+                   bool useNonzeroRule = true,
+                   double edge_tol = 1e-8,
+                   double EPS = 1e-8)
+{
+  SLIC_ASSERT(poly.hasNeighbors());
+
+  // For each face
+  // winding_number += winding_number( query, poly, edge_tol, EPS );
+  // return useNonzeroRule ? std::lround( winding_number ) != 0
+  //                      : (std::lround( winding_number ) % 2) == 1;
+}
+
+}  // namespace primal
+}  // namespace axom
+
+#endif  // AXOM_PRIMAL_IN_CURVED_POLYGON_H_
