b6ce8dc15d1be8fd48065cca48a3f0ae21b449b8
Max Yang <yang39@llnl.gov>
2023-06-08 12:53:23 -0700

    ArrayBase: add size+stride constructor for DIM=1 specialization

--- src/axom/core/ArrayBase.hpp
+++ src/axom/core/ArrayBase.hpp
@@ -560,6 +560,11 @@ public:
     : m_stride(spacing)
   { }
 
+  AXOM_HOST_DEVICE ArrayBase(const StackArray<IndexType, 1>&,
+                             const StackArray<IndexType, 1>& stride)
+    : m_stride(stride[0])
+  { }
+
   // Empty implementation because no member data
   template <typename OtherArrayType>
   ArrayBase(const ArrayBase<typename std::remove_const<T>::type, 1, OtherArrayType>&)
