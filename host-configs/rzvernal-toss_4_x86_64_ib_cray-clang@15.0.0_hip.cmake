07c6047ba5d487e27278a554084eb66c256bde73
Brian Manh Hien Han <han12@llnl.gov>
2023-06-21 13:18:04 -0700

    Test installs of rocm 5.4.3 and 5.5.1

---
+++ host-configs/rzvernal-toss_4_x86_64_ib_cray-clang@15.0.0_hip.cmake
@@ -0,0 +1,117 @@
+#------------------------------------------------------------------------------
+# !!!! This is a generated file, edit at own risk !!!!
+#------------------------------------------------------------------------------
+# CMake executable path: /usr/tce/packages/cmake/cmake-3.21.1/bin/cmake
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+# Compilers
+#------------------------------------------------------------------------------
+# Compiler Spec: clang@=15.0.0
+#------------------------------------------------------------------------------
+if(DEFINED ENV{SPACK_CC})
+
+  set(CMAKE_C_COMPILER "/usr/WS1/axom/libs/toss_4_x86_64_ib_cray/2023_06_21_11_54_21/spack/lib/spack/env/clang/clang" CACHE PATH "")
+
+  set(CMAKE_CXX_COMPILER "/usr/WS1/axom/libs/toss_4_x86_64_ib_cray/2023_06_21_11_54_21/spack/lib/spack/env/clang/clang++" CACHE PATH "")
+
+  set(CMAKE_Fortran_COMPILER "/usr/WS1/axom/libs/toss_4_x86_64_ib_cray/2023_06_21_11_54_21/spack/lib/spack/env/clang/flang" CACHE PATH "")
+
+else()
+
+  set(CMAKE_C_COMPILER "/opt/rocm-5.4.3/llvm/bin/amdclang" CACHE PATH "")
+
+  set(CMAKE_CXX_COMPILER "/opt/rocm-5.4.3/llvm/bin/amdclang++" CACHE PATH "")
+
+  set(CMAKE_Fortran_COMPILER "/opt/rocm-5.4.3/llvm/bin/amdflang" CACHE PATH "")
+
+endif()
+
+set(CMAKE_C_FLAGS "-Wno-int-conversion" CACHE STRING "")
+
+set(CMAKE_Fortran_FLAGS "-Mfreeform" CACHE STRING "")
+
+set(ENABLE_FORTRAN ON CACHE BOOL "")
+
+#------------------------------------------------------------------------------
+# MPI
+#------------------------------------------------------------------------------
+
+set(MPI_C_COMPILER "/usr/tce/packages/cray-mpich-tce/cray-mpich-8.1.25-rocmcc-5.4.3/bin/mpicc" CACHE PATH "")
+
+set(MPI_CXX_COMPILER "/usr/tce/packages/cray-mpich-tce/cray-mpich-8.1.25-rocmcc-5.4.3/bin/mpicxx" CACHE PATH "")
+
+set(MPI_Fortran_COMPILER "/usr/tce/packages/cray-mpich-tce/cray-mpich-8.1.25-rocmcc-5.4.3/bin/mpif90" CACHE PATH "")
+
+set(MPIEXEC_EXECUTABLE "/usr/bin/srun" CACHE PATH "")
+
+set(MPIEXEC_NUMPROC_FLAG "-n" CACHE STRING "")
+
+set(ENABLE_MPI ON CACHE BOOL "")
+
+#------------------------------------------------------------------------------
+# Hardware
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+
+# HIP
+
+#------------------------------------------------------------------------------
+
+
+set(ENABLE_HIP ON CACHE BOOL "")
+
+set(HIP_ROOT_DIR "/opt/rocm-5.4.3/hip" CACHE STRING "")
+
+set(HIP_CLANG_INCLUDE_PATH "/opt/rocm-5.4.3/hip/../llvm/lib/clang/15.0.0/include" CACHE PATH "")
+
+set(CMAKE_CXX_FLAGS "--std=c++14" CACHE STRING "")
+
+set(CMAKE_HIP_ARCHITECTURES "gfx90a" CACHE STRING "")
+
+set(CMAKE_EXE_LINKER_FLAGS "-Wl,--disable-new-dtags -L/opt/rocm-5.4.3/hip/../llvm/lib -L/opt/rocm-5.4.3/hip/lib -Wl,-rpath,/opt/rocm-5.4.3/hip/../llvm/lib:/opt/rocm-5.4.3/hip/lib -lpgmath -lflang -lflangrti -lompstub -lamdhip64  -L/opt/rocm-5.4.3/hip/../lib64 -Wl,-rpath,/opt/rocm-5.4.3/hip/../lib64 " CACHE STRING "")
+
+#------------------------------------------------
+# Hardware Specifics
+#------------------------------------------------
+
+set(ENABLE_OPENMP OFF CACHE BOOL "")
+
+set(ENABLE_GTEST_DEATH_TESTS ON CACHE BOOL "")
+
+#------------------------------------------------------------------------------
+# TPLs
+#------------------------------------------------------------------------------
+
+set(TPL_ROOT "/usr/WS1/axom/libs/toss_4_x86_64_ib_cray/2023_06_21_11_54_21/clang-15.0.0" CACHE PATH "")
+
+set(CONDUIT_DIR "${TPL_ROOT}/conduit-0.8.6-rfxxtopd2mfha66e2amph7xesjljl62r" CACHE PATH "")
+
+set(C2C_DIR "${TPL_ROOT}/c2c-1.8.0-w3miiiztg3zxr2z23dxpibcv3655lape" CACHE PATH "")
+
+set(MFEM_DIR "${TPL_ROOT}/mfem-4.5.0-nym2vsyl2mxkvczbabfd4turwnvbjadl" CACHE PATH "")
+
+set(HDF5_DIR "${TPL_ROOT}/hdf5-1.8.22-adojxdc5htttyo4ifmvz2ykk2bnc2q3e" CACHE PATH "")
+
+set(LUA_DIR "${TPL_ROOT}/lua-5.4.4-p6ucuzptyhvlaolwlxliaj4kwhihgwzh" CACHE PATH "")
+
+set(RAJA_DIR "${TPL_ROOT}/raja-2022.03.0-bca5ndzdpx657uzu2m66ldlhiympirdy" CACHE PATH "")
+
+set(UMPIRE_DIR "${TPL_ROOT}/umpire-2022.03.1-wy3mjawp5zo3xoyhmpv6vfpps3ptwbqj" CACHE PATH "")
+
+set(CAMP_DIR "${TPL_ROOT}/camp-2022.10.1-cfxcf5sr4nzeacjxf7hcsxfrljs4klyk" CACHE PATH "")
+
+# scr not built
+
+#------------------------------------------------------------------------------
+# Devtools
+#------------------------------------------------------------------------------
+
+# ClangFormat disabled due to llvm and devtools not in spec
+
+set(ENABLE_CLANGFORMAT OFF CACHE BOOL "")
+
+set(ENABLE_DOCS OFF CACHE BOOL "")
+
+
