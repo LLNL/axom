69e46152141e58f5fdb9b7262c46b202db20a744
Brian Han <han12@llnl.gov>
2023-07-06 15:07:47 -0700

    Workaround for rocm@5.6.0beta error; add missing hip library link flags

--- scripts/spack/packages/axom/package.py
+++ scripts/spack/packages/axom/package.py
@@ -310,6 +310,7 @@ class Axom(CachedCMakePackage, CudaPackage, ROCmPackage):
             # Additional libraries for TOSS4
             hip_link_flags += " -L{0}/../lib64 -Wl,-rpath,{0}/../lib64 ".format(hip_root)
             hip_link_flags += " -L{0}/../lib -Wl,-rpath,{0}/../lib ".format(hip_root)
+            hip_link_flags += "-lamd_comgr -lhsa-runtime64 "
 
             entries.append(cmake_cache_string("CMAKE_EXE_LINKER_FLAGS", hip_link_flags))
 
