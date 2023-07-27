e2fd828e52057b99bf7620ecdad1176b6ae3a010
Jacob Spainhour <Jacob.Spainhour@colorado.edu>
2023-07-27 16:45:24 -0700

    bugfix + Ma algorithms for 3D

--- src/axom/primal/operators/is_convex.hpp
+++ src/axom/primal/operators/is_convex.hpp
@@ -19,9 +19,10 @@ namespace primal
  * 
  * \param [in] poly The polygon
  * 
- * Uses dot products to detect whether vertices extend in the "convex" direction.
- * Uses the edge P[0]P[N] as a reference, meaning its adjacent edges are
- * always considered to be oriented correctly.
+ * A 2D polygon is convex when every line that does not contain an edge
+ * intersects the shape at most twice.
+ * Checks whether for each pair of vertices P[i-1]P[i+1], the point
+ * P[i] and (P[0] or P[n]) lie on the same side of the line connecting them.
  * 
  * Algorithm adapted from:
  * Y. L. Ma, W.T. Hewitt. "Point inversion and projection for NURBS curve 
@@ -54,7 +55,101 @@ bool is_convex(const Polygon<T, 2>& poly, double EPS = 1e-8)
     }
 
     // Ensure other point to check against isn't adjacent
-    if(res1 == orientation(poly[(i < n / 2) ? n : 0], seg, EPS))
+    if(res1 == orientation(poly[(i <= n / 2) ? n : 0], seg, EPS))
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+/*!
+ * \brief Determines if a 3D polygon defined by ordered vertices is convex
+ * 
+ * \param [in] poly The polygon
+ * 
+ * Uses dot products to detect whether vertices extend in the "convex" direction.
+ * Uses the edge P[0]P[N] as a reference, meaning its adjacent edges are
+ * always considered to be oriented correctly.
+ * 
+ * Algorithm adapted from:
+ * Y. L. Ma, W.T. Hewitt. "Point inversion and projection for NURBS curve 
+ * and surface: Control polygon approach"
+ * Computer Aided Geometric Design 20(2):79-99, May 2003.
+ * \note Only defined in 2D
+ * 
+ * \return A boolean value indicating convexity
+ */
+template <typename T>
+bool is_convex(const Polygon<T, 3>& poly, double EPS = 1e-8)
+{
+  int n = poly.numVertices() - 1;
+  if(n + 1 < 3)
+  {
+    return true;  // Triangles and lines are convex
+  }
+
+  for(int i = 1; i < n; i++)
+  {
+    // For each non-endpoint, check if that point and one of the endpoints
+    //  are on the same side as the segment connecting the adjacent nodes
+    Vector<T, 3> v0(poly[i - 1], poly[i]);
+    Vector<T, 3> v1(poly[i - 1], poly[i + 1]);
+    Vector<T, 3> v2(poly[i - 1], poly[(i <= n / 2) ? n : 0]);
+
+    // Equivalent to (v0 x v1) dot (v0 x v2) > 0
+    if(v0.squared_norm() * v1.dot(v2) > v0.dot(v2) * v0.dot(v1))
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+/* 
+ * Check if a polygon is "shallow," which we define as every line
+ * that is normal to an edge of the polygon intersects only that edge
+ * and the edge P[n]P[0].
+ * 
+ * This is true when ang(P[n]P[0]P[1]) + ang(P[n-1]P[n]P[0]) < 90,
+ * and every interior angle is greater than 90 degrees
+ */
+template <typename T, int NDIMS>
+bool is_shallow(const Polygon<T, NDIMS>& poly, double EPS = 1e-8)
+{
+  // Get max index of polygon vertices
+  const int n = poly.numVertices() - 1;
+
+  if(n + 1 < 2)
+  {
+    return true;  // Triangles and points are shallow
+  }
+
+  // Check two edges connected to P[n]P[0].
+  auto compute_angle = [](const Point<T, NDIMS>& a,
+                          const Point<T, NDIMS>& b,
+                          const Point<T, NDIMS>& c) -> double {
+    Vector<T, NDIMS> v1, v2;
+    v1 = Vector<T, NDIMS>(b, a).unitVector();
+    v2 = Vector<T, NDIMS>(b, c).unitVector();
+
+    return acos(axom::utilities::clampVal(v1.dot(v2), -1.0, 1.0));
+  };
+
+  // Check endpoints
+  if(compute_angle(poly[n], poly[0], poly[1]) +
+       compute_angle(poly[n - 1], poly[n], poly[0]) >=
+     0.5 * M_PI)
+  {
+    return false;
+  }
+
+  // Iterate over the middle vertices
+  for(int i = 1; i < n; ++i)
+  {
+    if(compute_angle(poly[i - 1], poly[i], poly[i + 1]) < 0.5 * M_PI)
     {
       return false;
     }
