e04898665daef94f2c23938ed7b4f0367941045c
Jacob Spainhour <Jacob.Spainhour@colorado.edu>
2023-07-27 16:45:42 -0700

    First attempt at universal splitting

--- src/axom/primal/operators/split.hpp
+++ src/axom/primal/operators/split.hpp
@@ -16,6 +16,7 @@
 #include "axom/core/Array.hpp"
 #include "axom/primal/geometry/Octahedron.hpp"
 #include "axom/primal/geometry/Tetrahedron.hpp"
+#include "axom/primal/geometry/BezierPatch.hpp"
 
 #include "axom/slic.hpp"
 
@@ -77,6 +78,69 @@ void split(const Octahedron<Tp, NDIMS>& oct,
   out.push_back(Tet(oct[P], oct[T], oct[R], C));
   out.push_back(Tet(oct[Q], oct[S], oct[U], C));
 };
+
+/*!
+ * \brief Splits a BezierPatch object into SuperConvex subpatches
+ *
+ * \param [in] bPatch BezierPatch to split
+ * \param [out] out The \a axom::Array of BezierPatch objects; a set of SuperConvex
+ *              components of c are appended to out.
+ *
+ *
+ * Uses a bisection method, splitting the patch recursively until each section
+ * is SuperConvex (convex + shallow).
+ * 
+ */
+template <typename Tp>
+void split_to_valid(const BezierPatch<Tp>& bPatch,
+                    axom::Array<BezierPatch<Tp>>& out)
+{
+  using Poly = Polygon<Tp, NDIMS>;
+  using Patch = BezierPatch<Tp>;
+
+  const int ord_u = bPatch.getOrder_u();
+  const int ord_v = bPatch.getOrder_v();
+  Poly control_slice();
+
+  bool is_valid = true;
+
+  // Check the slices that are fixed in u
+  for(int p = 0; is_valid && p <= ord_u; ++p)
+  {
+    control_slice.clear();
+    for(int q = 0; q <= ord_v; ++q)
+    {
+      control_slice.addVertex(bPatch(p, q));
+    }
+
+    is_valid = is_convex(control_slice) && is_shallow(control_slice);
+  }
+
+  // Check the slices that are fixed in v
+  for(int q = 0; is_valid && q <= ord_v; ++q)
+  {
+    control_slice.clear();
+    for(int p = 0; p <= ord_u; ++p)
+    {
+      control_slice.addVertex(bPatch(p, q));
+    }
+    is_valid = is_convex(control_slice) && is_shallow(control_slice);
+  }
+
+  if(is_valid)
+  {
+    out.push_back(Patch(bPatch));
+  }
+  else
+  {
+    Patch p1, p2, p3, p4;
+    bPatch.split(0.5, p1, p2, p3, p4);
+    split_to_valid(p1, out);
+    split_to_valid(p2, out);
+    split_to_valid(p3, out);
+    split_to_valid(p4, out);
+  }
+}
 
 }  // namespace primal
 }  // namespace axom
