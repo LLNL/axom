4a2839bc7150d3e26d1fd9c7fc221cba0c76b32d
Kenneth Weiss <weiss27@llnl.gov>
2023-08-18 13:52:17 -0700

    Bootstrap tutorial starting from axom's using-with-blt example

---
+++ src/examples/radiuss_tutorial/example.cpp
@@ -0,0 +1,30 @@
+// Copyright (c) 2017-2023, Lawrence Livermore National Security, LLC and
+// other Axom Project Developers. See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
+
+//-----------------------------------------------------------------------------
+///
+/// file: example.cpp
+///
+//-----------------------------------------------------------------------------
+
+#include "axom/core.hpp"
+#include "axom/fmt.hpp"
+
+#include <iostream>
+
+int main()
+{
+   // Using fmt library exported by axom
+   std::cout << axom::fmt::format(
+        "Example of using an installed version of Axom {}",
+        axom::getVersion()) << std::endl << std::endl;
+
+   // Uses installed axom library
+   axom::about();
+
+   return 0;
+}
+
