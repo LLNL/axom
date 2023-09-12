b36e7e93abdea0936658882bac46a4283d0d88fd
Alan Dayton <dayton8@llnl.gov>
2023-09-12 16:06:34 -0700

    Add tests for quadrilateral constructors

--- src/axom/primal/tests/primal_quadrilateral.cpp
+++ src/axom/primal/tests/primal_quadrilateral.cpp
@@ -11,6 +11,47 @@
 
 namespace primal = axom::primal;
 
+TEST(primal_quadrilateral, construct_from_points)
+{
+  constexpr int DIM = 2;
+  using CoordinateType = double;
+  using PointType = primal::Point<CoordinateType, DIM>;
+  using QuadrilateralType = primal::Quadrilateral<CoordinateType, DIM>;
+
+  PointType A {-1.0, 0.1};
+  PointType B {-0.1, 0.5};
+  PointType C {2.0, 0.0};
+  PointType D {0.0, 1.0};
+
+  QuadrilateralType quad {A, B, C, D};
+
+  EXPECT_EQUAL(A, quad[0]);
+  EXPECT_EQUAL(B, quad[1]);
+  EXPECT_EQUAL(C, quad[2]);
+  EXPECT_EQUAL(D, quad[3]);
+}
+
+TEST(primal_quadrilateral, construct_from_array_view)
+{
+  constexpr int DIM = 3;
+  constexpr double EPS = 1e-12;
+  using CoordinateType = double;
+  using PointType = primal::Point<CoordinateType, DIM>;
+  using QuadrilateralType = primal::Quadrilateral<CoordinateType, DIM>;
+
+  // This is a concave quadrilateral
+  PointType quadPoints[QuadrilateralType::NUM_QUAD_VERTS] {PointType {-1.0, 0.1, 0.0},
+                                                           PointType {-0.1, 0.5, 0.0},
+                                                           PointType {2.0, 0.0, 1.0},
+                                                           PointType {0.0, 1.0, 1.0}};
+
+  QuadrilateralType quad {axom::ArrayView<PointType>{quadPoints, QuadrilateralType::NUM_QUAD_VERTS}};
+
+  for (int i = 0; i < QuadrilateralType::NUM_QUAD_VERTS; ++i) {
+    EXPECT_EQUAL(quadPoints[i], quad[i]);
+  }
+}
+
 TEST(primal_quadrilateral, area_2D)
 {
   constexpr int DIM = 2;
