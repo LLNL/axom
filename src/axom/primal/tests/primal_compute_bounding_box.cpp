7e6bcd4689bd69f5c6fe439c0a4b94b49d9de528
Alan Dayton <dayton8@llnl.gov>
2023-09-12 15:53:09 -0700

    Add compute_bounding_box test for 3D quad

--- src/axom/primal/tests/primal_compute_bounding_box.cpp
+++ src/axom/primal/tests/primal_compute_bounding_box.cpp
@@ -153,6 +153,30 @@ TEST(primal_compute_bounding_box, compute_quad_2d_box_test)
   EXPECT_EQ(box.getMax(), (PointType {2.0, 1.0}));
 }
 
+TEST(primal_compute_bounding_box, compute_quad_3d_box_test)
+{
+  constexpr int DIM = 3;
+  using CoordinateType = double;
+  using PointType = primal::Point<CoordinateType, DIM>;
+  using BoundingBoxType = primal::BoundingBox<CoordinateType, DIM>;
+  using QuadrilateralType = primal::Octahedron<CoordinateType, DIM>;
+
+  PointType A {-1.0, 0.1, 0.0};
+  PointType B {-0.1, 0.5, 0.0};
+  PointType C {2.0, 0.0, 1.0};
+  PointType D {0.0, 1.0, 1.0};
+
+  QuadrilateralType quad {A, B, C, D};
+  BoundingBoxType box = primal::compute_bounding_box<CoordinateType, DIM>(quad);
+
+  EXPECT_TRUE(box.contains(A));
+  EXPECT_TRUE(box.contains(B));
+  EXPECT_TRUE(box.contains(C));
+  EXPECT_TRUE(box.contains(D));
+  EXPECT_EQ(box.getMin(), (PointType {-1.0, 0.0, 0.0}));
+  EXPECT_EQ(box.getMax(), (PointType {2.0, 1.0, 1.0}));
+}
+
 TEST(primal_compute_bounding_box, compute_oct_box_test)
 {
   static const int DIM = 3;
