79b10408a51db0f32fd560350186750284bcc7dc
Brian T.N. Gunney <gunney1@llnl.gov>
2023-11-10 15:13:44 -0800

    Reformat.

--- src/axom/quest/tests/quest_array_indexer.cpp
+++ src/axom/quest/tests/quest_array_indexer.cpp
@@ -14,42 +14,42 @@
 // Test strides and permutations.
 TEST(quest_array_indexer, quest_strides_and_permutatations)
 {
-  axom::StackArray<axom::IndexType, 3> lengths{5,3,2};
+  axom::StackArray<axom::IndexType, 3> lengths {5, 3, 2};
 
   axom::ArrayIndexer<axom::IndexType, 3> colIndexer(lengths, 'c');
-  axom::StackArray<std::uint16_t, 3> colSlowestDirs{0,1,2};
-  axom::StackArray<axom::IndexType, 3> colStrides{6,2,1};
-  for(int d=0; d<3; ++d)
+  axom::StackArray<std::uint16_t, 3> colSlowestDirs {0, 1, 2};
+  axom::StackArray<axom::IndexType, 3> colStrides {6, 2, 1};
+  for(int d = 0; d < 3; ++d)
   {
-    EXPECT_EQ( colIndexer.slowestDirs()[d], colSlowestDirs[d] );
-    EXPECT_EQ( colIndexer.strides()[d], colStrides[d] );
+    EXPECT_EQ(colIndexer.slowestDirs()[d], colSlowestDirs[d]);
+    EXPECT_EQ(colIndexer.strides()[d], colStrides[d]);
   }
 
   axom::ArrayIndexer<axom::IndexType, 3> rowIndexer(lengths, 'r');
-  axom::StackArray<std::uint16_t, 3> rowSlowestDirs{2,1,0};
-  axom::StackArray<axom::IndexType, 3> rowStrides{1,5,15};
-  for(int d=0; d<3; ++d)
+  axom::StackArray<std::uint16_t, 3> rowSlowestDirs {2, 1, 0};
+  axom::StackArray<axom::IndexType, 3> rowStrides {1, 5, 15};
+  for(int d = 0; d < 3; ++d)
   {
-    EXPECT_EQ( rowIndexer.slowestDirs()[d], rowSlowestDirs[d] );
-    EXPECT_EQ( rowIndexer.strides()[d], rowStrides[d] );
+    EXPECT_EQ(rowIndexer.slowestDirs()[d], rowSlowestDirs[d]);
+    EXPECT_EQ(rowIndexer.strides()[d], rowStrides[d]);
   }
 }
 
 // Test column-major offsets.
 TEST(quest_array_indexer, quest_col_major_offset)
 {
-  axom::StackArray<axom::IndexType, 3> lengths{5,3,2};
+  axom::StackArray<axom::IndexType, 3> lengths {5, 3, 2};
   axom::ArrayIndexer<axom::IndexType, 3> ai(lengths, 'c');
   axom::IndexType offset = 0;
-  for(int i=0; i<lengths[0]; ++i)
+  for(int i = 0; i < lengths[0]; ++i)
   {
-    for(int j=0; j<lengths[1]; ++j)
+    for(int j = 0; j < lengths[1]; ++j)
     {
-      for(int k=0; k<lengths[2]; ++k)
+      for(int k = 0; k < lengths[2]; ++k)
       {
-        axom::StackArray<axom::IndexType, 3> indices{i,j,k};
-        EXPECT_EQ( ai.toFlatIndex(indices), offset );
-        EXPECT_EQ( ai.toMultiIndex(offset), indices );
+        axom::StackArray<axom::IndexType, 3> indices {i, j, k};
+        EXPECT_EQ(ai.toFlatIndex(indices), offset);
+        EXPECT_EQ(ai.toMultiIndex(offset), indices);
         ++offset;
       }
     }
@@ -59,34 +59,35 @@ TEST(quest_array_indexer, quest_col_major_offset)
 // Test row-major offsets.
 TEST(quest_array_indexer, quest_row_major_offset)
 {
-  axom::StackArray<axom::IndexType, 3> lengths{5,3,2};
+  axom::StackArray<axom::IndexType, 3> lengths {5, 3, 2};
   axom::ArrayIndexer<axom::IndexType, 3> ai(lengths, 'r');
   axom::IndexType offset = 0;
-  for(int k=0; k<lengths[2]; ++k)
+  for(int k = 0; k < lengths[2]; ++k)
   {
-    for(int j=0; j<lengths[1]; ++j)
+    for(int j = 0; j < lengths[1]; ++j)
     {
-      for(int i=0; i<lengths[0]; ++i)
+      for(int i = 0; i < lengths[0]; ++i)
       {
-        axom::StackArray<axom::IndexType, 3> indices{i,j,k};
-        EXPECT_EQ( ai.toFlatIndex(indices), offset );
-        EXPECT_EQ( ai.toMultiIndex(offset), indices );
+        axom::StackArray<axom::IndexType, 3> indices {i, j, k};
+        EXPECT_EQ(ai.toFlatIndex(indices), offset);
+        EXPECT_EQ(ai.toMultiIndex(offset), indices);
         ++offset;
       }
     }
   }
 }
 
-void check_arbitrary_strides(
-  const axom::StackArray<axom::IndexType, 3>& lengths,
-  const axom::StackArray<axom::IndexType, 3>& fastestDirs)
+void check_arbitrary_strides(const axom::StackArray<axom::IndexType, 3>& lengths,
+                             const axom::StackArray<axom::IndexType, 3>& fastestDirs)
 {
   // fastestDirs should be a permutation.
-  SLIC_INFO(axom::fmt::format("Testing lengths {} with fastestDirs {}", lengths, fastestDirs));
+  SLIC_INFO(axom::fmt::format("Testing lengths {} with fastestDirs {}",
+                              lengths,
+                              fastestDirs));
 
   axom::StackArray<axom::IndexType, 3> strides;
   axom::IndexType currentStride = 1;
-  for(int d=0; d<3; ++d)
+  for(int d = 0; d < 3; ++d)
   {
     axom::IndexType currentDir = fastestDirs[d];
     strides[currentDir] = currentStride;
@@ -96,25 +97,25 @@ void check_arbitrary_strides(
   axom::ArrayIndexer<axom::IndexType, 3> ai(strides);
 
   const auto slowestDirs = ai.slowestDirs();
-  for(int d=0; d<3; ++d)
+  for(int d = 0; d < 3; ++d)
   {
-     EXPECT_EQ( slowestDirs[d], fastestDirs[3-d-1] );
+    EXPECT_EQ(slowestDirs[d], fastestDirs[3 - d - 1]);
   }
 
   axom::IndexType offset = 0;
-  axom::StackArray<axom::IndexType, 3> ijk; // Natural indices.
-  auto& l = ijk[fastestDirs[2]]; // Slowest dir
+  axom::StackArray<axom::IndexType, 3> ijk;  // Natural indices.
+  auto& l = ijk[fastestDirs[2]];             // Slowest dir
   auto& m = ijk[fastestDirs[1]];
-  auto& n = ijk[fastestDirs[0]]; // Fastest dir
-  for(l=0; l<lengths[fastestDirs[2]]; ++l)
+  auto& n = ijk[fastestDirs[0]];  // Fastest dir
+  for(l = 0; l < lengths[fastestDirs[2]]; ++l)
   {
-    for(m=0; m<lengths[fastestDirs[1]]; ++m)
+    for(m = 0; m < lengths[fastestDirs[1]]; ++m)
     {
-      for(n=0; n<lengths[fastestDirs[0]]; ++n)
+      for(n = 0; n < lengths[fastestDirs[0]]; ++n)
       {
         SLIC_INFO(axom::fmt::format("offset {}   ijk {}", offset, ijk));
-        EXPECT_EQ( ai.toMultiIndex(offset), ijk );
-        EXPECT_EQ( ai.toFlatIndex(ijk), offset );
+        EXPECT_EQ(ai.toMultiIndex(offset), ijk);
+        EXPECT_EQ(ai.toFlatIndex(ijk), offset);
         ++offset;
       }
     }
@@ -124,41 +125,41 @@ void check_arbitrary_strides(
 // Test arbitrary strides.
 TEST(quest_array_indexer, quest_arbitrary_strides)
 {
-  axom::StackArray<axom::IndexType, 3> lengths{5,3,2};
+  axom::StackArray<axom::IndexType, 3> lengths {5, 3, 2};
 
   // Row-major ordering.
-  axom::StackArray<axom::IndexType, 3> rowFastestDirs{0,1,2};
+  axom::StackArray<axom::IndexType, 3> rowFastestDirs {0, 1, 2};
   check_arbitrary_strides(lengths, rowFastestDirs);
 
   // Col-major ordering.
-  axom::StackArray<axom::IndexType, 3> colFastestDirs{2,1,0};
+  axom::StackArray<axom::IndexType, 3> colFastestDirs {2, 1, 0};
   check_arbitrary_strides(lengths, colFastestDirs);
 
   // A few random orderings.
-  axom::StackArray<axom::IndexType, 3> rand1FastestDirs{0,2,1};
+  axom::StackArray<axom::IndexType, 3> rand1FastestDirs {0, 2, 1};
   check_arbitrary_strides(lengths, rand1FastestDirs);
-  axom::StackArray<axom::IndexType, 3> rand2FastestDirs{2,0,1};
+  axom::StackArray<axom::IndexType, 3> rand2FastestDirs {2, 0, 1};
   check_arbitrary_strides(lengths, rand2FastestDirs);
-  axom::StackArray<axom::IndexType, 3> rand3FastestDirs{1,0,2};
+  axom::StackArray<axom::IndexType, 3> rand3FastestDirs {1, 0, 2};
   check_arbitrary_strides(lengths, rand3FastestDirs);
-  axom::StackArray<axom::IndexType, 3> rand4FastestDirs{1,2,0};
+  axom::StackArray<axom::IndexType, 3> rand4FastestDirs {1, 2, 0};
   check_arbitrary_strides(lengths, rand4FastestDirs);
 }
 
 // Test column-major element offsets with Array's.
 TEST(quest_array_indexer, quest_array_match)
 {
-  axom::StackArray<axom::IndexType, 3> lengths{5,3,2};
+  axom::StackArray<axom::IndexType, 3> lengths {5, 3, 2};
   axom::Array<int, 3> a(lengths);
   axom::ArrayIndexer<axom::IndexType, 3> ai(lengths, 'c');
-  for(axom::IndexType i=0; i<lengths[0]; ++i)
+  for(axom::IndexType i = 0; i < lengths[0]; ++i)
   {
-    for(axom::IndexType j=0; j<lengths[1]; ++j)
+    for(axom::IndexType j = 0; j < lengths[1]; ++j)
     {
-      for(axom::IndexType k=0; j<lengths[2]; ++j)
+      for(axom::IndexType k = 0; j < lengths[2]; ++j)
       {
-        axom::StackArray<axom::IndexType, 3> indices{i,j,k};
-        EXPECT_EQ( &a(i,j,k), &a.flatIndex(ai.toFlatIndex(indices)) );
+        axom::StackArray<axom::IndexType, 3> indices {i, j, k};
+        EXPECT_EQ(&a(i, j, k), &a.flatIndex(ai.toFlatIndex(indices)));
       }
     }
   }
