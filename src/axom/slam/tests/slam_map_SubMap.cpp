36be40969f199059a906fd9dfb50952f471a13ca
Max Yang <yang39@llnl.gov>
2024-01-29 17:34:08 -0800

    Add a test for SubMap range iterator

--- src/axom/slam/tests/slam_map_SubMap.cpp
+++ src/axom/slam/tests/slam_map_SubMap.cpp
@@ -159,6 +159,20 @@ bool constructAndTestSubMap()
       EXPECT_EQ(ssm.value(idx), getValue<T>(subset_indices[idx]));
       EXPECT_EQ(ssm.index(idx), s[subset_indices[idx]]);
     }
+
+    SLIC_INFO("Checking the elements using SubMap range iterator.");
+    {
+      int cnt = 0;
+      for(auto it = ssm.set_begin(); it != ssm.set_end(); ++it, ++cnt)
+      {
+        auto expectedValue = getValue<T>(subset_indices[cnt]);
+        EXPECT_EQ(expectedValue, (*it)[0]);
+        EXPECT_EQ(expectedValue, it(0));
+        EXPECT_EQ(expectedValue, it.value(0));
+        EXPECT_EQ(it.index(), s[subset_indices[cnt]]);
+      }
+      EXPECT_EQ(cnt, subset_indices.size());
+    }
   }
   return true;
 }
