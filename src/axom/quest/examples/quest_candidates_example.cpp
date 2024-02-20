6b28dc3e9ed991e369faad008c5b925031f94726
Brian Manh Hien Han <han12@llnl.gov>
2024-02-20 10:53:19 -0800

    Differentiate between initialize candidate pairs on device and copying to host for output

--- src/axom/quest/examples/quest_candidates_example.cpp
+++ src/axom/quest/examples/quest_candidates_example.cpp
@@ -440,27 +440,56 @@ axom::Array<IndexPair> findCandidatesBVH(const HexMesh& insertMesh,
   SLIC_INFO(axom::fmt::format(
     "1: Querying candidate bounding boxes took {:4.3} seconds.",
     timer.elapsedTimeInSec()));
-
-  // Initialize candidatePairs to return
   timer.start();
 
-  IndexArray offsets_h(offsets_d, host_allocator);
-  IndexArray counts_h(counts_d, host_allocator);
-  IndexArray candidates_h(candidates_d, host_allocator);
+  // Initialize candidate pairs on device.
+  auto candidates_v = candidates_d.view();
+  IndexArray firstPair_d(candidates_v.size(),
+                         candidates_v.size(),
+                         kernel_allocator);
+  IndexArray secondPair_d(candidates_v.size(),
+                          candidates_v.size(),
+                          kernel_allocator);
+  auto first_pair_v = firstPair_d.view();
+  auto second_pair_v = secondPair_d.view();
 
-  for(int i = 0; i < queryMesh.numHexes(); i++)
-  {
-    for(int j = 0; j < counts_h[i]; j++)
-    {
-      candidatePairs.emplace_back(
-        std::make_pair(i, candidates_h[offsets_h[i] + j]));
-    }
-  }
+  axom::for_all<ExecSpace>(
+    query_bbox_v.size(),
+    AXOM_LAMBDA(axom::IndexType icell) {
+      axom::IndexType offset = offsets_v[icell];
+
+      for(int j = 0; j < counts_v[icell]; j++)
+      {
+        int pair_index = offset + j;
+        first_pair_v[pair_index] = icell;
+        second_pair_v[pair_index] = candidates_v[pair_index];
+      }
+    });
+
   timer.stop();
-
   SLIC_INFO(axom::fmt::format(
-    "2: Initializing candidate pairs on host took {:4.3} seconds.",
+    "2: Initializing candidate pairs (on device) took {:4.3} seconds.",
     timer.elapsedTimeInSec()));
+  timer.start();
+
+  // copy pairs back to host and into return array
+  IndexArray candidates_h[2] = {
+    on_device ? IndexArray(firstPair_d, host_allocator) : IndexArray(),
+    on_device ? IndexArray(secondPair_d, host_allocator) : IndexArray()};
+
+  auto candidate1_h_v = on_device ? candidates_h[0].view() : firstPair_d.view();
+  auto candidate2_h_v = on_device ? candidates_h[1].view() : secondPair_d.view();
+
+  for(int idx = 0; idx < candidates_v.size(); ++idx)
+  {
+    candidatePairs.emplace_back(
+      std::make_pair(candidate1_h_v[idx], candidate2_h_v[idx]));
+  }
+
+  timer.stop();
+  SLIC_INFO(
+    axom::fmt::format("3: Moving candidate pairs to host took {:4.3} seconds.",
+                      timer.elapsedTimeInSec()));
 
   SLIC_INFO(axom::fmt::format(axom::utilities::locale(),
                               R"(Stats for query
@@ -472,7 +501,7 @@ axom::Array<IndexPair> findCandidatesBVH(const HexMesh& insertMesh,
                               insertMesh.numHexes(),
                               queryMesh.numHexes(),
                               1.0 * insertMesh.numHexes() * queryMesh.numHexes(),
-                              candidates_h.size()));
+                              candidatePairs.size()));
 
   return candidatePairs;
 }  // end of findCandidatesBVH for Blueprint Meshes
@@ -589,6 +618,12 @@ axom::Array<IndexPair> findCandidatesImplicit(const HexMesh& insertMesh,
       totalCandidatePairs += count;
     });
 
+  timer.stop();
+  SLIC_INFO(axom::fmt::format(
+    "1: Querying candidate bounding boxes took {:4.3} seconds.",
+    timer.elapsedTimeInSec()));
+  timer.start();
+
 // Generate offsets from the counts
 // (Note: exclusive scan for offsets in candidate array
 // Intel oneAPI compiler segfaults with OpenMP RAJA scan)
@@ -636,7 +671,7 @@ axom::Array<IndexPair> findCandidatesImplicit(const HexMesh& insertMesh,
   timer.stop();
 
   SLIC_INFO(axom::fmt::format(
-    "1: Querying candidate bounding boxes took {:4.3} seconds.",
+    "2: Initializing candidate pairs (on device) took {:4.3} seconds.",
     timer.elapsedTimeInSec()));
 
   // copy results back to host and into return vector
@@ -657,9 +692,9 @@ axom::Array<IndexPair> findCandidatesImplicit(const HexMesh& insertMesh,
 
   timer.stop();
 
-  SLIC_INFO(axom::fmt::format(
-    "2: Initializing candidate pairs on host took {:4.3} seconds.",
-    timer.elapsedTimeInSec()));
+  SLIC_INFO(
+    axom::fmt::format("3: Moving candidate pairs to host took {:4.3} seconds.",
+                      timer.elapsedTimeInSec()));
 
   SLIC_INFO(axom::fmt::format(axom::utilities::locale(),
                               R"(Stats for query
