845685efc342de2147e9f49d9c29471aac356de9
Brad Whitlock <whitlock2@llnl.gov>
2024-07-01 16:48:19 -0700

    Started adding test

---
+++ src/axom/mir/tests/mir_views_indexing.cpp
@@ -0,0 +1,53 @@
+// Copyright (c) 2017-2024, Lawrence Livermore National Security, LLC and
+// other Axom Project Developers. See the top-level COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
+#include "gtest/gtest.h"
+
+#include "axom/slic.hpp"
+#include "axom/mir.hpp"
+
+//----------------------------------------------------------------------
+
+TEST(mir_views_indexing, strided_structured_indexing_2d)
+{
+  using Indexing2D = views::StridedStructuredIndexing<int, 2>;
+  using LogicalIndex = typename views::StridedStructuredIndexing<int, 2>::LogicalIndex;
+  LogicalIndex dims{6, 5};
+  LogicalIndex origin{2, 2};
+  LogicalIndex stride{1, 6};
+  Indexing2D indexing(dims, origin, stride);
+
+  EXPECT_EQ(indexing.dimensions(), 2);
+  EXPECT_EQ(indexing.size(), dims[0] * dims[1]);
+
+  const auto index2_2 = indexing.LogicalIndexToIndex(LogicalIndex{2, 2});
+  EXPECT_EQ(index2_2, 14);
+  LogicalIndex logical = indexing.IndexToLogicalIndex(index2_2);
+  EXPECT_EQ(logical, LogicalIndex{2,2});
+
+  for(int j = 0; j < 3; j++)
+  {
+     for(int i = 0; i < 4; i++)
+     {
+        LogicalIndex logical{i, j};
+        const auto flat = indexing.LogicalIndexToIndex(logical);
+        const auto logical2 = indexing.IndexToLogicalIndex(flat);
+        EXPECT_EQ(logical, logical2);
+     }
+  } 
+}
+
+//----------------------------------------------------------------------
+
+int main(int argc, char* argv[])
+{
+  int result = 0;
+  ::testing::InitGoogleTest(&argc, argv);
+
+  axom::slic::SimpleLogger logger;  // create & initialize test logger,
+
+  result = RUN_ALL_TESTS();
+  return result;
+}
