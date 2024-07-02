e0164e7482270b0ea728e3a2ef94957a969b69b4
Brad Whitlock <whitlock2@llnl.gov>
2024-07-02 11:48:11 -0700

    Made more general

--- src/axom/mir/ElementFieldToVertexField.hpp
+++ src/axom/mir/ElementFieldToVertexField.hpp
@@ -3,114 +3,119 @@
 //
 // SPDX-License-Identifier: (BSD-3-Clause)
 
-#ifndef AXOM_MIR_ELEMENT_FIELD_TO_VERTEX_FIELD_HPP_
-#define AXOM_MIR_ELEMENT_FIELD_TO_VERTEX_FIELD_HPP_
+#ifndef AXOM_MIR_RECENTER_FIELD_HPP_
+#define AXOM_MIR_RECENTER_FIELD_HPP_
 
 #include "axom/core.hpp"
-#include "axom/mir/utilities.hpp"
+#include "axom/mir/views/NodeToArrayView.hpp"
 
 #include <conduit/conduit.hpp>
-
-#include <RAJA/RAJA.hpp>
+#include <conduit/conduit_blueprint.hpp>
 
 namespace axom
 {
 namespace mir
 {
-namespace utilities
-{
 
 /**
- * \brief Convert an element field to a vertex field.
+ * \brief This struct contains the type that should be used to accumulate values of type T.
+ */
+template <typename T>
+struct accumulate_traits
+{
+  using value_type = double;
+};
+
+template <>
+struct accumulate_traits<float>
+{
+  using value_type = float;
+};
+
+/**
+ * \brief Convert a field with one association type to a field of another association type using an o2mrelation.
  */
 template <typename ExecSpace>
-class ElementFieldToVertexField
+class RecenterField
 {
 public:
 
   /**
-   * \brief Convert an element field to a vertex field, storing the new field in a new Conduit node.
+   * \brief Convert the input field to a different association type using the o2mrelation and store the new field in the output field.
    *
    * \param field       The input field.
-   * \param relation    The node that contains an o2m relation with nodes to zones.
+   * \param relation    The node that contains an o2mrelation with nodes to zones.
    * \param outField[out] The node that will contain the new field.
    */
-  void execute(const conduit::Node &field, const conduit::Node &relation, conduit::Node &outField);
+  static void execute(const conduit::Node &field, const conduit::Node &relation, conduit::Node &outField);
 };
 
 template <typename ExecSpace>
 void
-ElementFieldToVertexField<ExecSpace>::execute(const conduit::Node &field, const conduit::Node &relation, conduit::Node &outField)
+RecenterField<ExecSpace>::execute(const conduit::Node &field, const conduit::Node &relation, conduit::Node &outField)
 {
-  using loop_policy = axom::execution_space<ExecSpace>::loop_policy;
-  const std::string association = field.fetch_existing("association").as_string();
-  const auto allocatorID = axom::execution_space<ExecSpace>::allocatorID();
-
-  outField["association"] = "vertex";
-  outField["topology"] = field["topology"];
-
   auto handleComponent = [](const conduit::Node &relation, const conduit::Node &n_comp, conduit::Node &n_out, int allocatorID)
   {
-    n_out.set_allocator(allocatorID);
-    n_out.set(n_comp.dtype().id(), n_comp.dtype().number_of_elements());
+    // Get the data field for the o2m relation.
+    const auto data_paths = conduit::blueprint::o2mrelation::data_paths(relation);
 
-    const conduit::Node &n_zones = relation["zones"];
+    // Use the o2mrelation to average data from n_comp to the n_out.
+    const conduit::Node &n_relvalues = relation[data_paths[0]];
     const conduit::Node &n_sizes = relation["sizes"];
     const conduit::Node &n_offsets = relation["offsets"];
-    views::IndexNode_to_ArrayView_same(n_zones, n_sizes, n_offsets, [&](auto zonesView, auto sizesView, auto offsetsView)
+    views::IndexNode_to_ArrayView_same(n_relvalues, n_sizes, n_offsets, [&](auto relView, auto sizesView, auto offsetsView)
     {
+      const auto relSize = sizesView.size();
+
+      // Allocate data for n_out (same type as n_comp).
+      n_out.set_allocator(allocatorID);
+      n_out.set(n_comp.dtype().id(), relSize);
+
       views::Node_to_ArrayView_same(n_comp, n_out, [&](auto compView, auto outView)
       {
-        using Precision = typename compView::value_type;
-        const auto nnodes = sizesView.size();
-        axom::for_all<ExecSpace>(nnodes, AXOM_LAMBDA(int nodeIndex)
+        using Precision = typename decltype(compView)::value_type;
+        using AccumType = accumulate_traits<Precision>::value_type;
+        axom::for_all<ExecSpace>(relSize, AXOM_LAMBDA(int relIndex)
         {
-          const auto n = sizesView[nodeIndex];
-          const auto offset = offsetsView[nodeIndex];
+          const auto n = sizesView[relIndex];
+          const auto offset = offsetsView[relIndex];
 
-          double sum = 0.;
+          AccumType sum = 0;
           for(int i = 0; i < n; i++)
           {
-            const auto zid = zonesView[offset + i];
-            sum += compView[zid];
+            const auto id = relView[offset + i];
+            sum += static_cast<AccumType>(compView[id]);
           }
 
-          outView[nodeIndex] = static_cast<Precision>(sum / n);
+          outView[relIndex] = static_cast<Precision>(sum / n);
         });
       });
     });
   };
 
-  if(association == "element")
+  const std::string association = field.fetch_existing("association").as_string();
+  const auto allocatorID = axom::execution_space<ExecSpace>::allocatorID();
+
+  // Assume that we're flipping the association.
+  outField["association"] = (association == "element") ? "vertex" : "element";
+  outField["topology"] = field["topology"];
+
+  // Make output values.
+  const conduit::Node &n_values = field["values"];
+  if(n_values.number_of_children() > 0)
   {
-    const conduit::Node &n_zones = relation["zones"];
-    const conduit::Node &n_sizes = relation["sizes"];
-    const conduit::Node &n_offsets = relation["offsets"];
-    views::IndexNode_to_ArrayView_same(n_zones, n_sizes, n_offsets, [&](auto zonesView, auto sizesView, auto offsetsView)
+    for(conduit::index_t c = 0; c < n_values.number_of_children(); c++)
     {
-      const auto nnodes = sizesView.size();
-      const conduit::Node &n_values = field["values"];
-      if(n_values.number_of_children() > 0)
-      {
-        for(conduit::index_t c = 0; c < n_values.number_of_children(); c++)
-        {
-          const conduit::Node &n_comp = n_values[c];
-          handleComponent(relation, n_comp, outField["values"][n_comp.name()], allocatorID);
-        }
-      }
-      else
-      {
-        handleComponent(relation, n_values, outField["values"], allocatorID);
-      }
-    });
+      const conduit::Node &n_comp = n_values[c];
+      handleComponent(relation, n_comp, outField["values"][n_comp.name()], allocatorID);
+    }
   }
-  else if(association == "vertex")
+  else
   {
-    outField["values"].set_external(field["values"]);
+    handleComponent(relation, n_values, outField["values"], allocatorID);
   }
 }
 
-} // end namespace utilities
 } // end namespace mir
 } // end namespace axom
 
