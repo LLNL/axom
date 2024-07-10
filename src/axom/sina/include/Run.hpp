5c181c7d2601b5d92f2cea3a6163f437a0fb3b1d
Brian Gunnarson <brianfunnarson14@gmail.com>
2024-07-10 13:32:35 -0700

    add copyright header to sina files

--- src/axom/sina/include/Run.hpp
+++ src/axom/sina/include/Run.hpp
@@ -1,3 +1,9 @@
+// Copyright (c) 2017-2024, Lawrence Livermore National Security, LLC and
+// other Axom Project Developers. See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
+
 #ifndef SINA_RUN_HPP
 #define SINA_RUN_HPP
 
