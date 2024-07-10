968a595b2dfcc5795dd6068e51c15b892f298d2d
Chris White <white238@llnl.gov>
2024-07-10 14:59:20 -0700

    add host-configs and use new docker images

---
+++ host-configs/docker/clang@14.0.0.cmake
@@ -0,0 +1,114 @@
+#------------------------------------------------------------------------------
+# !!!! This is a generated file, edit at own risk !!!!
+#------------------------------------------------------------------------------
+# CMake executable path: /usr/local/bin/cmake
+#------------------------------------------------------------------------------
+
+set(CMAKE_PREFIX_PATH "/home/axom/axom_tpls/clang-14.0.0/umpire-2024.02.0-3w43hp32my2mdmd2juhocmkjdqnzqq2g;/home/axom/axom_tpls/clang-14.0.0/raja-2024.02.0-e5cxi4k7suylwq3jenni4lbygbncvmio;/home/axom/axom_tpls/clang-14.0.0/mfem-4.6.0-3tnuhhoq334focivaxxoc6tgeukkedsp;/home/axom/axom_tpls/clang-14.0.0/hypre-2.24.0-3p44dyaeytjno7rlzom4oa6dw5xlaic3;/home/axom/axom_tpls/clang-14.0.0/conduit-0.9.1-lfxh5l5ngtswc7qkxpwejmxmj75kbucv;/home/axom/axom_tpls/clang-14.0.0/parmetis-4.0.3-2oiwd6fzdvamz7crvqrjd23an466qwya;/home/axom/axom_tpls/clang-14.0.0/hdf5-1.8.23-ff7tq76ofskjc4fr3mehe7dzoovvoi3p;/home/axom/axom_tpls/clang-14.0.0/caliper-2.10.0-wj5rtnyg6zaf7bksoqfqqfsfbgptc6ef;/home/axom/axom_tpls/clang-14.0.0/elfutils-0.190-p7yphf7bv5765wcur3vcdhlz4jbuklc4;/home/axom/axom_tpls/clang-14.0.0/blt-0.6.2-rfu7gfz6sidwkq77uvuzolal2ho5orqc;/home/axom/axom_tpls/clang-14.0.0/adiak-0.4.0-d7z6dbud2wo5redc54jpxpca6zfch63n;/home/axom/axom_tpls/clang-14.0.0/fmt-10.2.1-xqv65krexlvp5fmkrqa7uri2xy3m5ymb;/home/axom/axom_tpls/clang-14.0.0/camp-2024.02.0-ew3nu42cjjz6mlj7nz3rlykzjr2xjmtz;/home/axom/axom_tpls/clang-14.0.0/zlib-ng-2.1.6-t3ijs4ecfcyv4dyr2v4y555lnxjkubnr;/home/axom/axom_tpls/clang-14.0.0/metis-5.1.0-cwicizetbw2saiqeohuoawepe5yzyzwr;/home/axom/axom_tpls/clang-14.0.0/gmake-4.4.1-6g3tmsuzby4oldevfkjivdd3vj3y7yec;/home/axom/axom_tpls/clang-14.0.0/pkgconf-1.9.5-3z4u4krn4kjq3374f37nqcsqj2mulfxl;/home/axom/axom_tpls/clang-14.0.0/libunwind-1.6.2-scystt7y72uftlw5v2o4tavrsktfd2sg;/home/axom/axom_tpls/clang-14.0.0/zstd-1.5.5-7r7dy2rjd4gfgdnywn2d3lvxo6r4y766;/home/axom/axom_tpls/clang-14.0.0/xz-5.4.6-ifk7aohqs6fz4dhytn6w4iya6gcz3avu;/home/axom/axom_tpls/clang-14.0.0/libiconv-1.17-ha3xngugbv2o6azu4bp4dhcbvct74rgj" CACHE STRING "")
+
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH "ON" CACHE STRING "")
+
+set(CMAKE_BUILD_RPATH "/home/axom/axom_tpls/clang-14.0.0/axom-develop-lgiwl2b7n52eg6c425uxbn5g4hecnoh4/lib;/home/axom/axom_tpls/clang-14.0.0/axom-develop-lgiwl2b7n52eg6c425uxbn5g4hecnoh4/lib64;/home/axom/axom_tpls/clang-14.0.0/adiak-0.4.0-d7z6dbud2wo5redc54jpxpca6zfch63n/lib;/home/axom/axom_tpls/clang-14.0.0/caliper-2.10.0-wj5rtnyg6zaf7bksoqfqqfsfbgptc6ef/lib;/home/axom/axom_tpls/clang-14.0.0/elfutils-0.190-p7yphf7bv5765wcur3vcdhlz4jbuklc4/lib;/home/axom/axom_tpls/clang-14.0.0/libiconv-1.17-ha3xngugbv2o6azu4bp4dhcbvct74rgj/lib;/home/axom/axom_tpls/clang-14.0.0/pkgconf-1.9.5-3z4u4krn4kjq3374f37nqcsqj2mulfxl/lib;/home/axom/axom_tpls/clang-14.0.0/xz-5.4.6-ifk7aohqs6fz4dhytn6w4iya6gcz3avu/lib;/home/axom/axom_tpls/clang-14.0.0/zlib-ng-2.1.6-t3ijs4ecfcyv4dyr2v4y555lnxjkubnr/lib;/home/axom/axom_tpls/clang-14.0.0/zstd-1.5.5-7r7dy2rjd4gfgdnywn2d3lvxo6r4y766/lib;/home/axom/axom_tpls/clang-14.0.0/libunwind-1.6.2-scystt7y72uftlw5v2o4tavrsktfd2sg/lib;/home/axom/axom_tpls/clang-14.0.0/conduit-0.9.1-lfxh5l5ngtswc7qkxpwejmxmj75kbucv/lib;/home/axom/axom_tpls/clang-14.0.0/hdf5-1.8.23-ff7tq76ofskjc4fr3mehe7dzoovvoi3p/lib;/home/axom/axom_tpls/clang-14.0.0/metis-5.1.0-cwicizetbw2saiqeohuoawepe5yzyzwr/lib;/home/axom/axom_tpls/clang-14.0.0/parmetis-4.0.3-2oiwd6fzdvamz7crvqrjd23an466qwya/lib;/home/axom/axom_tpls/clang-14.0.0/mfem-4.6.0-3tnuhhoq334focivaxxoc6tgeukkedsp/lib;/home/axom/axom_tpls/clang-14.0.0/hypre-2.24.0-3p44dyaeytjno7rlzom4oa6dw5xlaic3/lib;/home/axom/axom_tpls/clang-14.0.0/raja-2024.02.0-e5cxi4k7suylwq3jenni4lbygbncvmio/lib;/home/axom/axom_tpls/clang-14.0.0/camp-2024.02.0-ew3nu42cjjz6mlj7nz3rlykzjr2xjmtz/lib;/home/axom/axom_tpls/clang-14.0.0/umpire-2024.02.0-3w43hp32my2mdmd2juhocmkjdqnzqq2g/lib;/home/axom/axom_tpls/clang-14.0.0/fmt-10.2.1-xqv65krexlvp5fmkrqa7uri2xy3m5ymb/lib" CACHE STRING "")
+
+set(CMAKE_INSTALL_RPATH "/home/axom/axom_tpls/clang-14.0.0/axom-develop-lgiwl2b7n52eg6c425uxbn5g4hecnoh4/lib;/home/axom/axom_tpls/clang-14.0.0/axom-develop-lgiwl2b7n52eg6c425uxbn5g4hecnoh4/lib64;/home/axom/axom_tpls/clang-14.0.0/adiak-0.4.0-d7z6dbud2wo5redc54jpxpca6zfch63n/lib;/home/axom/axom_tpls/clang-14.0.0/caliper-2.10.0-wj5rtnyg6zaf7bksoqfqqfsfbgptc6ef/lib;/home/axom/axom_tpls/clang-14.0.0/elfutils-0.190-p7yphf7bv5765wcur3vcdhlz4jbuklc4/lib;/home/axom/axom_tpls/clang-14.0.0/libiconv-1.17-ha3xngugbv2o6azu4bp4dhcbvct74rgj/lib;/home/axom/axom_tpls/clang-14.0.0/pkgconf-1.9.5-3z4u4krn4kjq3374f37nqcsqj2mulfxl/lib;/home/axom/axom_tpls/clang-14.0.0/xz-5.4.6-ifk7aohqs6fz4dhytn6w4iya6gcz3avu/lib;/home/axom/axom_tpls/clang-14.0.0/zlib-ng-2.1.6-t3ijs4ecfcyv4dyr2v4y555lnxjkubnr/lib;/home/axom/axom_tpls/clang-14.0.0/zstd-1.5.5-7r7dy2rjd4gfgdnywn2d3lvxo6r4y766/lib;/home/axom/axom_tpls/clang-14.0.0/libunwind-1.6.2-scystt7y72uftlw5v2o4tavrsktfd2sg/lib;/home/axom/axom_tpls/clang-14.0.0/conduit-0.9.1-lfxh5l5ngtswc7qkxpwejmxmj75kbucv/lib;/home/axom/axom_tpls/clang-14.0.0/hdf5-1.8.23-ff7tq76ofskjc4fr3mehe7dzoovvoi3p/lib;/home/axom/axom_tpls/clang-14.0.0/metis-5.1.0-cwicizetbw2saiqeohuoawepe5yzyzwr/lib;/home/axom/axom_tpls/clang-14.0.0/parmetis-4.0.3-2oiwd6fzdvamz7crvqrjd23an466qwya/lib;/home/axom/axom_tpls/clang-14.0.0/mfem-4.6.0-3tnuhhoq334focivaxxoc6tgeukkedsp/lib;/home/axom/axom_tpls/clang-14.0.0/hypre-2.24.0-3p44dyaeytjno7rlzom4oa6dw5xlaic3/lib;/home/axom/axom_tpls/clang-14.0.0/raja-2024.02.0-e5cxi4k7suylwq3jenni4lbygbncvmio/lib;/home/axom/axom_tpls/clang-14.0.0/camp-2024.02.0-ew3nu42cjjz6mlj7nz3rlykzjr2xjmtz/lib;/home/axom/axom_tpls/clang-14.0.0/umpire-2024.02.0-3w43hp32my2mdmd2juhocmkjdqnzqq2g/lib;/home/axom/axom_tpls/clang-14.0.0/fmt-10.2.1-xqv65krexlvp5fmkrqa7uri2xy3m5ymb/lib" CACHE STRING "")
+
+set(CMAKE_BUILD_TYPE "Release" CACHE STRING "")
+
+#------------------------------------------------------------------------------
+# Compilers
+#------------------------------------------------------------------------------
+# Compiler Spec: clang@=14.0.0
+#------------------------------------------------------------------------------
+if(DEFINED ENV{SPACK_CC})
+
+  set(CMAKE_C_COMPILER "/home/axom/axom_tpls/spack/lib/spack/env/clang/clang" CACHE PATH "")
+
+  set(CMAKE_CXX_COMPILER "/home/axom/axom_tpls/spack/lib/spack/env/clang/clang++" CACHE PATH "")
+
+  set(CMAKE_Fortran_COMPILER "/home/axom/axom_tpls/spack/lib/spack/env/clang/gfortran" CACHE PATH "")
+
+else()
+
+  set(CMAKE_C_COMPILER "/usr/bin/clang" CACHE PATH "")
+
+  set(CMAKE_CXX_COMPILER "/usr/bin/clang++" CACHE PATH "")
+
+  set(CMAKE_Fortran_COMPILER "/usr/bin/gfortran-11" CACHE PATH "")
+
+endif()
+
+set(CMAKE_C_FLAGS "-fPIC -pthread" CACHE STRING "")
+
+set(CMAKE_CXX_FLAGS "-fPIC -pthread" CACHE STRING "")
+
+set(ENABLE_FORTRAN ON CACHE BOOL "")
+
+set(BLT_EXE_LINKER_FLAGS " -Wl,-rpath,/usr/lib -Wl,-rpath,/usr/lib64" CACHE STRING "Adds a missing libstdc++ rpath")
+
+#------------------------------------------------------------------------------
+# MPI
+#------------------------------------------------------------------------------
+
+set(MPI_C_COMPILER "/usr/bin/mpicc" CACHE PATH "")
+
+set(MPI_CXX_COMPILER "/usr/bin/mpic++" CACHE PATH "")
+
+set(MPI_Fortran_COMPILER "/usr/bin/mpif90" CACHE PATH "")
+
+set(MPIEXEC_EXECUTABLE "/usr/bin/mpirun" CACHE PATH "")
+
+set(MPIEXEC_NUMPROC_FLAG "-np" CACHE STRING "")
+
+set(ENABLE_MPI ON CACHE BOOL "")
+
+#------------------------------------------------------------------------------
+# Hardware
+#------------------------------------------------------------------------------
+
+#------------------------------------------------
+# Hardware Specifics
+#------------------------------------------------
+
+set(ENABLE_OPENMP ON CACHE BOOL "")
+
+set(ENABLE_GTEST_DEATH_TESTS ON CACHE BOOL "")
+
+#------------------------------------------------------------------------------
+# TPLs
+#------------------------------------------------------------------------------
+
+set(TPL_ROOT "/home/axom/axom_tpls/clang-14.0.0" CACHE PATH "")
+
+set(CONDUIT_DIR "${TPL_ROOT}/conduit-0.9.1-lfxh5l5ngtswc7qkxpwejmxmj75kbucv" CACHE PATH "")
+
+# C2C not built
+
+set(MFEM_DIR "${TPL_ROOT}/mfem-4.6.0-3tnuhhoq334focivaxxoc6tgeukkedsp" CACHE PATH "")
+
+set(HDF5_DIR "${TPL_ROOT}/hdf5-1.8.23-ff7tq76ofskjc4fr3mehe7dzoovvoi3p" CACHE PATH "")
+
+set(LUA_DIR "/usr" CACHE PATH "")
+
+set(RAJA_DIR "${TPL_ROOT}/raja-2024.02.0-e5cxi4k7suylwq3jenni4lbygbncvmio" CACHE PATH "")
+
+set(UMPIRE_DIR "${TPL_ROOT}/umpire-2024.02.0-3w43hp32my2mdmd2juhocmkjdqnzqq2g" CACHE PATH "")
+
+set(ADIAK_DIR "${TPL_ROOT}/adiak-0.4.0-d7z6dbud2wo5redc54jpxpca6zfch63n" CACHE PATH "")
+
+set(CALIPER_DIR "${TPL_ROOT}/caliper-2.10.0-wj5rtnyg6zaf7bksoqfqqfsfbgptc6ef" CACHE PATH "")
+
+set(CAMP_DIR "${TPL_ROOT}/camp-2024.02.0-ew3nu42cjjz6mlj7nz3rlykzjr2xjmtz" CACHE PATH "")
+
+# scr not built
+
+#------------------------------------------------------------------------------
+# Devtools
+#------------------------------------------------------------------------------
+
+# ClangFormat disabled due to llvm and devtools not in spec
+
+set(ENABLE_CLANGFORMAT OFF CACHE BOOL "")
+
+set(ENABLE_DOCS OFF CACHE BOOL "")
+
+
