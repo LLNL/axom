9335ff3bae66a539e19684609f58febcfce202e0
Chris White <white238@llnl.gov>
2024-07-17 13:42:30 -0700

    fix extensions

--- src/cmake/AxomMacros.cmake
+++ src/cmake/AxomMacros.cmake
@@ -30,9 +30,11 @@ macro(axom_add_code_checks)
     # another project
     if ("${PROJECT_SOURCE_DIR}" STREQUAL "${CMAKE_SOURCE_DIR}")
         # Create file globbing expressions that only include directories that contain source
-        set(_base_dirs "axom" "examples" "thirdparty/tests/" "tools")
-        # Note: any extensions added here should also be added to BLT's lists in CMakeLists.txt
-        set(_ext_expressions "*.cpp" "*.hpp" "*.inl" "*.cuh" "*.cu" "*.cpp.in" "*.hpp.in"
+        set(_base_dirs "axom" "examples" "thirdparty/tests" "tools")
+
+        # Note: any extensions added here should also be added to BLT's lists
+        # in src/CMakeLists.txt
+        set(_ext_expressions "*.cpp" "*.hpp" "*.inl" "*.cpp.in" "*.hpp.in"
                              "*.cxx" "*.hxx" "*.cc" "*.c" "*.h" "*.hh"
                              "*.F" "*.f" "*.f90" "*.F90")
 
