e03b4814b5566dc838b077582d75af76075ff869
Brad Whitlock <whitlock2@llnl.gov>
2024-10-02 19:16:30 -0700

    fix

--- src/axom/mir/examples/concentric_circles/runMIR_hip.cpp
+++ src/axom/mir/examples/concentric_circles/runMIR_hip.cpp
@@ -4,14 +4,14 @@
 // SPDX-License-Identifier: (BSD-3-Clause)
 #include "runMIR.hpp"
 
-#if defined(AXOM_USE_RAJA) && defined(AXOM_USE_UMPIRE) && defined(AXOM_USE_CUDA)
+#if defined(AXOM_USE_RAJA) && defined(AXOM_USE_UMPIRE) && defined(AXOM_USE_HIP)
 int runMIR_hip(const conduit::Node &mesh,
                const conduit::Node &options,
                conduit::Node &result)
 {
   constexpr int HIP_BLOCK_SIZE = 64;
   using hip_exec = axom::HIP_EXEC<HIP_BLOCK_SIZE>;
-  retval = runMIR<hip_exec>(mesh, options, result);
+  return runMIR<hip_exec>(mesh, options, result);
 }
 #else
 int runMIR_hip(const conduit::Node &AXOM_UNUSED_PARAM(mesh),
