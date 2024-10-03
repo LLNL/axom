a9f5abfb1aa25128023248f15441494b5da71512
Kenneth Weiss <weiss27@llnl.gov>
2024-10-02 18:12:10 -0700

    Updates and fixes benchmark tests for slic macros/asserts

--- src/axom/slic/tests/slic_benchmark_asserts.cpp
+++ src/axom/slic/tests/slic_benchmark_asserts.cpp
@@ -3,41 +3,43 @@
 //
 // SPDX-License-Identifier: (BSD-3-Clause)
 
-#include "benchmark/benchmark_api.h"
+#include "benchmark/benchmark.h"
 #include "axom/slic/interface/slic.hpp"
 #include "axom/slic/core/SimpleLogger.hpp"
 
 /*!
  * \file
  *
- * A series of tests of SLIC logging macros (SLIC_WARNING to avoid killing the
- * program) in the TPL benchmark framework.
+ * A series of tests of SLIC logging macros (using SLIC_WARNING to avoid 
+ * killing the program) in the TPL benchmark framework.
  *
  * These tests were written to see why SLIC macros was not being logged
  * when called from a benchmark fixture's destructor.
  * It turns out that the fixtures are destructed after main exits,
  * so SLIC is already finalized.
- *
  */
 
 namespace
 {
-static const bool IS_VALID = true;
-static const bool SHOULD_PRINT = false;
+static constexpr bool IS_VALID = true;
+static constexpr bool SHOULD_PRINT = false;
 
 void printMsg(std::string const& str, bool override = false)
 {
-  if(SHOULD_PRINT || override) std::cout << str << std::endl;
+  if(SHOULD_PRINT || override)
+  {
+    std::cout << str << std::endl;
+  }
 }
 
 struct AssertCtor
 {
-  AssertCtor() { SLIC_WARNING_IF(IS_VALID, "Testing warning in .ctor"); }
-};
+  AssertCtor() { SLIC_WARNING_IF(IS_VALID, "Testing warning in .ctor"); };
 
-struct AssertMethod
-{
-  void foo() { SLIC_WARNING_IF(IS_VALID, "Testing warning in class method"); }
+  struct AssertMethod
+  {
+    void foo() { SLIC_WARNING_IF(IS_VALID, "Testing warning in class method"); }
+  };
 };
 
 struct AssertDtor
@@ -50,24 +52,36 @@ class SetFixtureC : public ::benchmark::Fixture
 public:
   SetFixtureC()
   {
-    SLIC_WARNING_IF(IS_VALID, "Testing warning in .ctor");
-    printMsg(
-      "*** Note: the slic log is not printed for SetFixtureC/assertCtor since "
-      "it is called before slic::initialize() ***",
-      true);
+    static bool override_once = true;
+    if(override_once)
+    {
+      std::cout
+        << "*** Note: cannot use slic macros in fixture constructors since "
+           "they're called before main (i.e. before slic::initialize()) ***"
+        << std::endl;
+      override_once = false;
+    }
+
+    //SLIC_WARNING_IF(IS_VALID, "Testing warning in .ctor");
   }
 };
 
 class SetFixtureS : public ::benchmark::Fixture
 {
 public:
-  void SetUp() { SLIC_WARNING_IF(IS_VALID, "Testing warning in setup"); }
+  void SetUp(const ::benchmark::State& /*state*/) override
+  {
+    SLIC_WARNING_IF(IS_VALID, "Testing warning in setup");
+  }
 };
 
 class SetFixtureT : public ::benchmark::Fixture
 {
 public:
-  void TearDown() { SLIC_WARNING_IF(IS_VALID, "Testing warning in teardown"); }
+  void TearDown(const ::benchmark::State& /*state*/) override
+  {
+    SLIC_WARNING_IF(IS_VALID, "Testing warning in teardown");
+  }
 };
 
 class SetFixtureD : public ::benchmark::Fixture
@@ -75,50 +89,81 @@ class SetFixtureD : public ::benchmark::Fixture
 public:
   ~SetFixtureD()
   {
-    SLIC_WARNING_IF(IS_VALID, "Testing warning in .dtor");
-    printMsg(
-      "*** Note: the slic log is not printed for SetFixtureD/assertDtor since "
-      "it is called after slic::finalize() ***",
-      true);
+    static bool override_once = true;
+    if(override_once)
+    {
+      std::cout
+        << "*** Note: cannot use slic macros in fixture destructors since "
+           "they're called after main (i.e. after slic::finalize()) ***"
+        << std::endl;
+      override_once = false;
+    }
+
+    //SLIC_WARNING_IF(IS_VALID, "Testing warning in .dtor");
   }
 };
 
 class SetFixtureOutput : public ::benchmark::Fixture
 {
 public:
-  SetFixtureOutput() { printMsg("  fixure .ctor"); }
-  void SetUp() { printMsg("  fixure SetUp"); }
-  void TearDown() { printMsg("  fixure TearDown"); }
-  ~SetFixtureOutput() { printMsg("  fixure ~.dtor"); }
+  SetFixtureOutput() { printMsg("  fixure .ctor (called before main)"); }
+  void SetUp(const ::benchmark::State& /*state*/) override
+  {
+    printMsg("  fixure SetUp");
+  }
+  void TearDown(const ::benchmark::State& /*state*/) override
+  {
+    printMsg("  fixure TearDown");
+  }
+  ~SetFixtureOutput() { printMsg("  fixure ~.dtor (called after main)"); }
 };
 
 static void BM_AssertBefore(benchmark::State& state)
 {
   SLIC_WARNING_IF(IS_VALID, "Testing warning before running benchmark");
-  while(state.KeepRunning()) benchmark::DoNotOptimize(0);
+  while(state.KeepRunning())
+  {
+    std::int64_t x = 0;
+    benchmark::DoNotOptimize(x);
+  }
 }
 
 static void BM_AssertAfter(benchmark::State& state)
 {
-  while(state.KeepRunning()) benchmark::DoNotOptimize(0);
+  while(state.KeepRunning())
+  {
+    std::int64_t x = 0;
+    benchmark::DoNotOptimize(x);
+  }
   SLIC_WARNING_IF(IS_VALID, "Testing warning after running benchmark");
 }
 
 static void BM_CallAssertCtorBefore(benchmark::State& state)
 {
   AssertCtor();
-  while(state.KeepRunning()) benchmark::DoNotOptimize(0);
+  while(state.KeepRunning())
+  {
+    std::int64_t x = 0;
+    benchmark::DoNotOptimize(x);
+  }
 }
 
 static void BM_CallAssertDtorBefore(benchmark::State& state)
 {
   AssertDtor();
-  while(state.KeepRunning()) benchmark::DoNotOptimize(0);
+  while(state.KeepRunning())
+  {
+    std::int64_t x = 0;
+    benchmark::DoNotOptimize(x);
+  }
 }
 
 static void BM_CallAssertDtorDuring(benchmark::State& state)
 {
-  while(state.KeepRunning()) AssertDtor();
+  while(state.KeepRunning())
+  {
+    AssertDtor();
+  }
 }
 
 }  // namespace
@@ -133,41 +178,45 @@ BENCHMARK_F(SetFixtureC, assertCtor)(benchmark::State& state)
 {
   while(state.KeepRunning())
   {
-    benchmark::DoNotOptimize(0);
+    std::int64_t x = 0;
+    benchmark::DoNotOptimize(x);
   }
 }
 BENCHMARK_F(SetFixtureS, assertSetup)(benchmark::State& state)
 {
   while(state.KeepRunning())
   {
-    benchmark::DoNotOptimize(0);
+    std::int64_t x = 0;
+    benchmark::DoNotOptimize(x);
   }
 }
 BENCHMARK_F(SetFixtureT, assertTeardown)(benchmark::State& state)
 {
   while(state.KeepRunning())
   {
-    benchmark::DoNotOptimize(0);
+    std::int64_t x = 0;
+    benchmark::DoNotOptimize(x);
   }
 }
 BENCHMARK_F(SetFixtureD, assertDtor)(benchmark::State& state)
 {
   while(state.KeepRunning())
   {
-    benchmark::DoNotOptimize(0);
+    std::int64_t x = 0;
+    benchmark::DoNotOptimize(x);
   }
 }
 
 // The following two benchmarks are here to show the order of construction,
 // setup, teardown and destruction of benchmarks fixtures.
-// Note: Need to enable printing (by setting SHOULD_PRINT above to true) to see
-// this.
+// Note: Need to enable printing (by setting SHOULD_PRINT above to true) to see this.
 BENCHMARK_F(SetFixtureOutput, process1)(benchmark::State& state)
 {
   printMsg("   P1 (before)");
   while(state.KeepRunning())
   {
-    benchmark::DoNotOptimize(0);
+    std::int64_t x = 0;
+    benchmark::DoNotOptimize(x);
   }
   printMsg("   P1 (after)");
 }
@@ -176,7 +225,8 @@ BENCHMARK_F(SetFixtureOutput, process2)(benchmark::State& state)
   printMsg("   P2 (before)");
   while(state.KeepRunning())
   {
-    benchmark::DoNotOptimize(0);
+    std::int64_t x = 0;
+    benchmark::DoNotOptimize(x);
   }
   printMsg("   P2 (after)");
 }
@@ -190,7 +240,6 @@ int main(int argc, char* argv[])
   printMsg(" After init logger");
 
   printMsg(" Before init benchmark");
-
   ::benchmark::Initialize(&argc, argv);
   printMsg(" after init benchmark");
 
