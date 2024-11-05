1bce8b34156b1d7fd6bd168ac7add25eae3736e0
Brian Gunnarson <brianfunnarson14@gmail.com>
2024-11-05 12:56:49 -0800

    move sina version into API

--- src/axom/sina/core/Document.hpp
+++ src/axom/sina/core/Document.hpp
@@ -24,6 +24,9 @@
 #include "axom/sina/core/Record.hpp"
 #include "axom/sina/core/Relationship.hpp"
 
+#define SINA_FILE_FORMAT_VERSION_MAJOR 1
+#define SINA_FILE_FORMAT_VERSION_MINOR 0
+
 namespace axom
 {
 namespace sina
@@ -71,6 +74,11 @@ namespace sina
  * You can also export your Document to file:
  * \code
  *   axom::sina::saveDocument(myDocument, "path/to/outfile.json")
+ * \endcode
+ *
+ * Check the Sina file format version with:
+ * \code
+ *   axom::sina::getSinaFileFormatVersion();
  * \endcode
  *
  */
@@ -198,6 +206,16 @@ private:
  * \throws std::ios::failure if there are any IO errors
  */
 void saveDocument(Document const &document, std::string const &fileName);
+
+/**
+ * \brief Get the current file format version.
+ *
+ * \return A string representing the file format version.
+ */
+inline std::string getSinaFileFormatVersion() {
+  return std::to_string(SINA_FILE_FORMAT_VERSION_MAJOR) + "." +
+         std::to_string(SINA_FILE_FORMAT_VERSION_MINOR);
+}
 
 /**
  * \brief Load a document from the given path. Only records which this library
