37ccb0070c86d7fea16f477b12823c1e56fe9d55
Kenneth Weiss <weiss27@llnl.gov>
2024-11-11 14:21:39 -0800

    Adds command line args to array benchmarks
    Allows users to control which array features to test as well as the sizes of the arrays.

--- src/axom/core/tests/core_benchmark_array.cpp
+++ src/axom/core/tests/core_benchmark_array.cpp
@@ -7,12 +7,94 @@
 #include "axom/config.hpp"
 #include "axom/core.hpp"
 #include "axom/slic.hpp"
+
 #include "axom/fmt.hpp"
+#include "axom/CLI11.hpp"
 
 #include <tuple>
 #include <vector>
 #include <type_traits>
 #include <cstdint>
+
+namespace
+{
+
+enum class ArrayFeatureBenchmarks
+{
+  None = 0,
+  Constructors = 1 << 0,
+  Insertion = 1 << 1,
+  Iterators = 1 << 2,
+
+  All = Constructors | Insertion | Iterators
+};
+
+inline ArrayFeatureBenchmarks operator|(ArrayFeatureBenchmarks lhs,
+                                        ArrayFeatureBenchmarks rhs)
+{
+  using T = std::underlying_type_t<ArrayFeatureBenchmarks>;
+  return static_cast<ArrayFeatureBenchmarks>(static_cast<T>(lhs) |
+                                             static_cast<T>(rhs));
+}
+
+inline ArrayFeatureBenchmarks& operator|=(ArrayFeatureBenchmarks& lhs,
+                                          ArrayFeatureBenchmarks rhs)
+{
+  lhs = lhs | rhs;
+  return lhs;
+}
+
+inline ArrayFeatureBenchmarks operator&(ArrayFeatureBenchmarks lhs,
+                                        ArrayFeatureBenchmarks rhs)
+{
+  using T = std::underlying_type_t<ArrayFeatureBenchmarks>;
+  return static_cast<ArrayFeatureBenchmarks>(static_cast<T>(lhs) &
+                                             static_cast<T>(rhs));
+}
+
+std::vector<int> args_benchmark_sizes;
+ArrayFeatureBenchmarks args_benchmark_features {ArrayFeatureBenchmarks::None};
+}  // namespace
+
+// Custom fmt formatter for ArrayFeatureBenchmarks
+template <>
+struct axom::fmt::formatter<ArrayFeatureBenchmarks>
+{
+  template <typename ParseContext>
+  constexpr auto parse(ParseContext& ctx)
+  {
+    return ctx.begin();
+  }
+
+  template <typename FormatContext>
+  auto format(ArrayFeatureBenchmarks feature, FormatContext& ctx)
+  {
+    static const std::map<ArrayFeatureBenchmarks, std::string> feature_map = {
+      {ArrayFeatureBenchmarks::Constructors, "Constructors"},
+      {ArrayFeatureBenchmarks::Insertion, "Insertion"},
+      {ArrayFeatureBenchmarks::Iterators, "Iterators"}};
+
+    if(feature == ArrayFeatureBenchmarks::None)
+    {
+      return axom::fmt::format_to(ctx.out(), "None");
+    }
+    else if(feature == ArrayFeatureBenchmarks::All)
+    {
+      return axom::fmt::format_to(ctx.out(), "All");
+    }
+
+    std::string name;
+    for(const auto& kv : feature_map)
+    {
+      if((feature & kv.first) != ArrayFeatureBenchmarks::None)
+      {
+        name += name.empty() ? kv.second : "|" + kv.second;
+      }
+    }
+
+    return axom::fmt::format_to(ctx.out(), "{}", name);
+  }
+};
 
 //-----------------------------------------------------------------------------
 // Arguments and types to test
@@ -20,10 +102,10 @@
 
 void CustomArgs(benchmark::internal::Benchmark* b)
 {
-  // b->Arg(1 << 3);   // small
-  b->Arg(1 << 16);  // larger than  32K L1 cache
-  // b->Arg(1 << 19);  // larger than 256K L2 cache
-  // b->Arg(1 << 25);  // larger than  25M L3 cache
+  for(int sz : ::args_benchmark_sizes)
+  {
+    b->Arg(sz);
+  }
 }
 
 template <typename T>
@@ -304,23 +386,32 @@ void RegisterBenchmark()
   };
 
   // clang-format off
-  benchmark::RegisterBenchmark(tname("Array::ctor"), &ctor<axom::Array<T>>)->Apply(CustomArgs);
-  benchmark::RegisterBenchmark(tname("vector::ctor"), &ctor<std::vector<T>>)->Apply(CustomArgs);
+  if((args_benchmark_features & ArrayFeatureBenchmarks::Constructors) != ArrayFeatureBenchmarks::None)
+  {
+    benchmark::RegisterBenchmark(tname("Array::ctor"), &ctor<axom::Array<T>>)->Apply(CustomArgs);
+    benchmark::RegisterBenchmark(tname("vector::ctor"), &ctor<std::vector<T>>)->Apply(CustomArgs);
+  }
 
-  benchmark::RegisterBenchmark(tname("Array::push_back_startEmpty"), &push_back_startEmpty<axom::Array<T>>)->Apply(CustomArgs);
-  benchmark::RegisterBenchmark(tname("Array::emplace_back_startEmpty"), &emplace_back_startEmpty<axom::Array<T>>)->Apply(CustomArgs);
-  benchmark::RegisterBenchmark(tname("Array::push_back_initialReserve"), &push_back_initialReserve<axom::Array<T>>)->Apply(CustomArgs);
-  benchmark::RegisterBenchmark(tname("Array::emplace_back_initialReserve"), &emplace_back_initialReserve<axom::Array<T>>)->Apply(CustomArgs);
+  if((args_benchmark_features & ArrayFeatureBenchmarks::Insertion) != ArrayFeatureBenchmarks::None)
+  {
+    benchmark::RegisterBenchmark(tname("Array::push_back_startEmpty"), &push_back_startEmpty<axom::Array<T>>)->Apply(CustomArgs);
+    benchmark::RegisterBenchmark(tname("Array::emplace_back_startEmpty"), &emplace_back_startEmpty<axom::Array<T>>)->Apply(CustomArgs);
+    benchmark::RegisterBenchmark(tname("Array::push_back_initialReserve"), & push_back_initialReserve<axom::Array<T>>)->Apply(CustomArgs);
+    benchmark::RegisterBenchmark(tname("Array::emplace_back_initialReserve"), &emplace_back_initialReserve<axom::Array<T>>)->Apply(CustomArgs);
 
-  benchmark::RegisterBenchmark(tname("vector::push_back_startEmpty"), &push_back_startEmpty<std::vector<T>>)->Apply(CustomArgs);
-  benchmark::RegisterBenchmark(tname("vector::emplace_back_startEmpty"), &emplace_back_startEmpty<std::vector<T>>)->Apply(CustomArgs);
-  benchmark::RegisterBenchmark(tname("vector::push_back_initialReserve"), &push_back_initialReserve<std::vector<T>>)->Apply(CustomArgs);
-  benchmark::RegisterBenchmark(tname("vector::emplace_back_initialReserve"), &emplace_back_initialReserve<std::vector<T>>)->Apply(CustomArgs);
+    benchmark::RegisterBenchmark(tname("vector::push_back_startEmpty"), &push_back_startEmpty<std::vector<T>>)->Apply(CustomArgs);
+    benchmark::RegisterBenchmark(tname("vector::emplace_back_startEmpty"), &emplace_back_startEmpty<std::vector<T>>)->Apply(CustomArgs);
+    benchmark::RegisterBenchmark(tname("vector::push_back_initialReserve"), &push_back_initialReserve<std::vector<T>>)->Apply(CustomArgs);
+    benchmark::RegisterBenchmark(tname("vector::emplace_back_initialReserve"), &emplace_back_initialReserve<std::vector<T>>)->Apply(CustomArgs);
+  }
 
-  benchmark::RegisterBenchmark(tname("Array::iterate_range"), &iterate_range<axom::Array<T>>)->Apply(CustomArgs);
-  benchmark::RegisterBenchmark(tname("Array::iterate_direct"), &iterate_direct<axom::Array<T>>)->Apply(CustomArgs);
-  benchmark::RegisterBenchmark(tname("vector::iterate_range"), &iterate_range<std::vector<T>>)->Apply(CustomArgs);
-  benchmark::RegisterBenchmark(tname("vector::iterate_direct"), &iterate_direct<std::vector<T>>)->Apply(CustomArgs);
+  if((args_benchmark_features & ArrayFeatureBenchmarks::Iterators) != ArrayFeatureBenchmarks::None)
+  {
+    benchmark::RegisterBenchmark(tname("Array::iterate_range"), &iterate_range<axom::Array<T>>)->Apply(CustomArgs);
+    benchmark::RegisterBenchmark(tname("Array::iterate_direct"), &iterate_direct<axom::Array<T>>)->Apply(CustomArgs);
+    benchmark::RegisterBenchmark(tname("vector::iterate_range"), &iterate_range<std::vector<T>>)->Apply(CustomArgs);
+    benchmark::RegisterBenchmark(tname("vector::iterate_direct"), &iterate_direct<std::vector<T>>)->Apply(CustomArgs);
+  }
   // clang-format on
 }
 
@@ -349,8 +440,88 @@ void RegisterBenchmarks()
 
 int main(int argc, char* argv[])
 {
+  // Parse command line args for array benchmarks
+  std::vector<int> local_test_sizes;
+  ArrayFeatureBenchmarks local_benchmark_features {ArrayFeatureBenchmarks::None};
+
+  axom::CLI::App app {"Axom array benchmarks"};
+  app.add_option("-s,--custom_sizes", local_test_sizes)
+    ->description(
+      "Adds custom array sizes to benchmark (positive numbers only)")
+    ->expected(-1)
+    ->default_val(std::vector<int> {1 << 16})
+    ->each([](const std::string& num_str) {
+      int num = std::stoi(num_str);
+      if(num < 0)
+      {
+        throw axom::CLI::ValidationError("Negative numbers are not allowed");
+      }
+    });
+  app
+    .add_flag_callback(
+      "--use_cache_related_sizes",
+      [&local_test_sizes]() {
+        local_test_sizes.push_back(1 << 3);   // small
+        local_test_sizes.push_back(1 << 16);  // larger than  32K L1 cache
+        local_test_sizes.push_back(1 << 19);  // larger than 256K L2 cache
+        local_test_sizes.push_back(1 << 25);  // larger than  25M L3 cache
+      })
+    ->description("Test array sizes related to typical cache sizes");
+
+  std::vector<std::string> feature_strings;
+  auto feature_opt =
+    app.add_option("-f,--features", feature_strings)
+      ->description(
+        "Features to benchmark (Constructors, Insertion, Iterators, All); "
+        "default is 'All'")
+      ->expected(-1)
+      ->each([&local_benchmark_features](const std::string& feature) {
+        static const std::map<std::string, ArrayFeatureBenchmarks> feature_map = {
+          {"constructors", ArrayFeatureBenchmarks::Constructors},
+          {"insertion", ArrayFeatureBenchmarks::Insertion},
+          {"iterators", ArrayFeatureBenchmarks::Iterators},
+          {"all", ArrayFeatureBenchmarks::All}};
+
+        std::string lower_feature = feature;
+        std::transform(lower_feature.begin(),
+                       lower_feature.end(),
+                       lower_feature.begin(),
+                       ::tolower);
+        auto it = feature_map.find(lower_feature);
+        if(it == feature_map.end())
+        {
+          throw axom::CLI::ValidationError("Invalid feature: " + feature);
+        }
+
+        local_benchmark_features |= it->second;
+      });
+
+  app.allow_extras();  // pass additional args to gbenchmark
+  CLI11_PARSE(app, argc, argv);
+
   ::benchmark::Initialize(&argc, argv);
   axom::slic::SimpleLogger logger;
+
+  // process input into global variables
+  {
+    // copy list of Features to test into global array; by default, test everything
+    ::args_benchmark_features = feature_opt->count() > 0
+      ? local_benchmark_features
+      : ArrayFeatureBenchmarks::All;
+
+    // sort and unique-ify the input sizes and copy into the global array variable
+    std::sort(local_test_sizes.begin(), local_test_sizes.end());
+    auto last = std::unique(local_test_sizes.begin(), local_test_sizes.end());
+    local_test_sizes.erase(last, local_test_sizes.end());
+    std::swap(::args_benchmark_sizes, local_test_sizes);
+
+    SLIC_INFO("Parsed and processed command line arguments:");
+    SLIC_INFO(axom::fmt::format("- Array sizes: {}",
+                                axom::fmt::join(::args_benchmark_sizes, ",")));
+    SLIC_INFO(axom::fmt::format("- Array features to test: {}",
+                                ::args_benchmark_features));
+  }
+
   RegisterBenchmarks<Types>();
   ::benchmark::RunSpecifiedBenchmarks();
   return 0;
