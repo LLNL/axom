e7fcc2b254862eb4f3cd22d6b8bd91b8aa7a2be7
Chris White <white238@llnl.gov>
2024-11-22 10:28:10 -0800

    add dim, swap for serac's tensor, unify filling and checking whole array

--- src/axom/sidre/tests/sidre_read_write_userdefined_data.cpp
+++ src/axom/sidre/tests/sidre_read_write_userdefined_data.cpp
@@ -13,6 +13,79 @@ using axom::sidre::View;
 
 //-----Mock Serac Structs------------------------------------------------------
 
+/**
+ * @brief Arbitrary-rank tensor class
+ * @tparam T The type stored at each index
+ * @tparam n The dimensions of the tensor
+ */
+template <typename T, int... n>
+struct tensor;
+
+template <typename T, int m, int... n>
+struct tensor<T, m, n...>
+{
+  template <typename i_type>
+  constexpr auto& operator()(i_type i)
+  {
+    return data[i];
+  }
+  template <typename i_type>
+  constexpr auto& operator()(i_type i) const
+  {
+    return data[i];
+  }
+  template <typename i_type, typename... jklm_type>
+  constexpr auto& operator()(i_type i, jklm_type... jklm)
+  {
+    return data[i](jklm...);
+  }
+  template <typename i_type, typename... jklm_type>
+  constexpr auto& operator()(i_type i, jklm_type... jklm) const
+  {
+    return data[i](jklm...);
+  }
+
+  constexpr auto& operator[](int i) { return data[i]; }
+  constexpr const auto& operator[](int i) const { return data[i]; }
+
+  tensor<T, n...> data[m];
+};
+
+template <typename T, int m>
+struct tensor<T, m>
+{
+  template <typename i_type>
+  constexpr auto& operator()(i_type i)
+  {
+    return data[i];
+  }
+  template <typename i_type>
+  constexpr auto& operator()(i_type i) const
+  {
+    return data[i];
+  }
+  constexpr auto& operator[](int i) { return data[i]; }
+  constexpr const auto& operator[](int i) const { return data[i]; }
+
+  template <int last_dimension = m,
+            typename = typename std::enable_if<last_dimension == 1>::type>
+  constexpr operator T()
+  {
+    return data[0];
+  }
+
+  template <int last_dimension = m,
+            typename = typename std::enable_if<last_dimension == 1>::type>
+  constexpr operator T() const
+  {
+    return data[0];
+  }
+
+  T data[m];
+};
+
+//---------------------
+
 template <typename T>
 bool check(const T&, double)
 {
@@ -25,65 +98,6 @@ void fill(T&, double)
 {
   SLIC_ERROR("You didn't implement a specialization for fill");
 }
-
-//---------------------
-
-template <typename T, std::size_t Rows, std::size_t Cols>
-class Tensor
-{
-public:
-  // Constructor to initialize all elements to a specific value
-  Tensor(const T& value = T())
-  {
-    for(auto& row : data)
-    {
-      row.fill(value);
-    }
-  }
-
-  // Constructor to initialize from an initializer list
-  Tensor(std::initializer_list<std::initializer_list<T>> init)
-  {
-    size_t row = 0;
-    for(auto& rowData : init)
-    {
-      size_t col = 0;
-      for(auto& element : rowData)
-      {
-        data[row][col++] = element;
-      }
-      row++;
-    }
-  }
-
-  // Accessor for elements (read/write)
-  T& at(size_t row, size_t col) { return data[row][col]; }
-
-  // Const accessor for elements (read-only)
-  const T& at(size_t row, size_t col) const { return data[row][col]; }
-
-  // Get tensor dimensions
-  std::pair<size_t, size_t> dimensions() const { return {Rows, Cols}; }
-
-  // Function to return a string representation of the tensor
-  std::string to_string() const
-  {
-    std::ostringstream oss;
-    for(const auto& row : data)
-    {
-      oss << "[ ";
-      for(const auto& elem : row)
-      {
-        oss << elem << " ";
-      }
-      oss << "]\n";
-    }
-    return oss.str();
-  }
-
-private:
-  std::array<std::array<T, Cols>, Rows> data;
-};
 
 //---------------------
 
@@ -167,45 +181,98 @@ void fill(StateThree& state, double value)
 
 struct StateTensor
 {
-  Tensor<double, 2, 2> t;
+  //tensor<double, 2, 2> t;
+  tensor<double, 2> t;
   double x;
 };
 
 template <>
 bool check(const StateTensor& state, double value)
 {
-  return (state.t.at(0, 0) == value) && (state.t.at(0, 1) == (value + 1)) &&
-    (state.t.at(1, 0) == (value + 2)) && (state.t.at(1, 1) == (value + 3)) &&
+  // SLIC_INFO(axom::fmt::format("check({}, {}, {}, {}), {}",
+  //                             state.t(0, 0),
+  //                             state.t(0, 1),
+  //                             state.t(1, 0),
+  //                             state.t(1, 1),
+  //                             state.x));
+
+  // return (state.t(0, 0) == value) && (state.t(0, 1) == (value + 1)) &&
+  //   (state.t(1, 0) == (value + 2)) && (state.t(1, 1) == (value + 3)) &&
+  //   (state.x == (value + 4));
+
+  SLIC_INFO(
+    axom::fmt::format("check({}, {}), {}", state.t(0), state.t(1), state.x));
+
+  return (state.t(0) == value) && (state.t(1) == (value + 1)) &&
     (state.x == (value + 4));
 }
 
 template <>
 void fill(StateTensor& state, double value)
 {
-  state.t = {{value, value + 1}, {value + 2, value + 3}};
+  // state.t(0,0) = value;
+  // state.t(0,1) = value + 1;
+  // state.t(1,0) = value + 2;
+  // state.t(1,1) = value + 3;
+  state.t(0) = value;
+  state.t(1) = value + 1;
   state.x = value + 4;
 }
 
 //---------------------
 
-template <typename T>
-using QuadratureData1D = axom::Array<T, 1>;
-template <typename T>
-using QuadratureData2D = axom::Array<T, 2>;
+template <typename T, int DIM>
+void fill_array(T, int, bool)
+{
+  SLIC_ERROR("You didn't implement the fill array of this type.");
+}
+
+template <typename T, int DIM>
+void check_array(T)
+{
+  SLIC_ERROR("You didn't implement the check array of this type.");
+}
+
+template <typename T, int DIM>
+void fill_array(axom::Array<T, DIM>& states, double fill_value, bool increment)
+{
+  int count = 0;
+  for(auto& state : states)
+  {
+    fill(state, fill_value);
+    if(increment)
+    {
+      fill_value++;
+    }
+    count++;
+  }
+  SLIC_INFO(
+    axom::fmt::format("filled {} states with end value {}", count, fill_value));
+}
+
+template <typename T, int DIM>
+void check_array(axom::Array<T, DIM> states)
+{
+  int count = 0;
+  double check_value = 0;
+  for(auto& state : states)
+  {
+    EXPECT_TRUE(check(state, check_value++));
+    count++;
+  }
+  SLIC_INFO(
+    axom::fmt::format("checked {} states with end value {}", count, check_value));
+}
 
 //------------------------------------------------------------------------------
 
-template <typename T>
+template <typename T, int DIM>
 void test_user_defined_data()
 {
   // populate data
   constexpr IndexType size = 10;
-  QuadratureData1D<T> states(size, size);
-  IndexType i = 0;
-  for(auto& state : states)
-  {
-    fill(state, i++);
-  }
+  axom::Array<T, DIM> states(size, size);
+  fill_array(states, 0, true);
 
   // Create datastore
   DataStore ds;
@@ -220,6 +287,12 @@ void test_user_defined_data()
   qd_group->createViewScalar("num_states", num_states);
   qd_group->createViewScalar("state_size", state_size);
   qd_group->createViewScalar("total_size", total_size);
+  SLIC_INFO(axom::fmt::format("Num of States={}", num_states));
+  SLIC_INFO(axom::fmt::format("State Size={}", state_size));
+  SLIC_INFO(axom::fmt::format("Total Size={}", total_size));
+  SLIC_INFO(axom::fmt::format("Double Size={}", sizeof(double)));
+  SLIC_INFO(axom::fmt::format("Total Size/Double Size={}",
+                              total_size / sizeof(double)));
 
   // write data to datastore as bytes
   View* data_view =
@@ -228,32 +301,22 @@ void test_user_defined_data()
   memcpy(sidre_state_data, states.data(), static_cast<std::size_t>(total_size));
 
   // mess with data before overriding it back to original in sidre
-  fill(states[0], 123);
-  fill(states[size / 2], 456);
-  fill(states[size - 1], 789);
+  fill_array(states, -1, false);
 
   // Copy original data back over local data
   memcpy(states.data(), sidre_state_data, static_cast<std::size_t>(total_size));
 
   // Test data is back to original
-  i = 0;
-  for(auto& state : states)
-  {
-    EXPECT_TRUE(check(state, i++));
-  }
+  check_array(states);
 }
 
-template <typename T>
+template <typename T, int DIM>
 void test_external_user_defined_data()
 {
   // populate data
   constexpr IndexType size = 10;
-  QuadratureData1D<T> states(size, size);
-  IndexType i = 0;
-  for(auto& state : states)
-  {
-    fill(state, i++);
-  }
+  axom::Array<T, DIM> states(size, size);
+  fill_array(states, 0, true);
 
   // Create datastore
   DataStore ds;
@@ -281,11 +344,8 @@ void test_external_user_defined_data()
   qd_group->save(filename);
 
   // Create new array to fill with saved data
-  QuadratureData1D<T> saved_states(size, size);
-  for(auto& state : saved_states)
-  {
-    fill(state, -1);
-  }
+  axom::Array<T, DIM> saved_states(size, size);
+  fill_array(saved_states, -1, false);
 
   // Load data into new array
   Group* new_qd_group = ds.getRoot()->createGroup("new_quadraturedata");
@@ -295,43 +355,48 @@ void test_external_user_defined_data()
   new_states_view->setExternalDataPtr(saved_states.data());
   new_qd_group->loadExternalData(filename);
 
-  // Test data is back to original
-  i = 0;
-  for(auto& state : saved_states)
-  {
-    EXPECT_TRUE(check(state, i++));
-  }
+  // Test data was read into the new location and overwrote the bad data
+  check_array(saved_states);
 }
 
 //------------------------------------------------------------------------------
 
-TEST(sidre, QD_double_readandwrite) { test_user_defined_data<double>(); }
+TEST(sidre, OneD_double_readandwrite) { test_user_defined_data<double, 1>(); }
 
-TEST(sidre, QD_StateOne_readandwrite) { test_user_defined_data<StateOne>(); }
+// TEST(sidre, OneD_StateOne_readandwrite) { test_user_defined_data<StateOne, 1>(); }
 
-TEST(sidre, QD_StateTwo_readandwrite) { test_user_defined_data<StateTwo>(); }
+// TEST(sidre, OneD_StateTwo_readandwrite) { test_user_defined_data<StateTwo, 1>(); }
 
-TEST(sidre, QD_StateThree_readandwrite)
+// TEST(sidre, OneD_StateThree_readandwrite) { test_user_defined_data<StateThree, 1>(); }
+
+// TEST(sidre, OneD_StateTensor_readandwrite) { test_user_defined_data<StateTensor, 1>(); }
+
+// //-------------------------
+
+// TEST(sidre, TwoD_double_readandwrite) { test_user_defined_data<double, 2>(); }
+
+// TEST(sidre, TwoD_StateOne_readandwrite) { test_user_defined_data<StateOne, 2>(); }
+
+// TEST(sidre, TwoD_StateTwo_readandwrite) { test_user_defined_data<StateTwo, 2>(); }
+
+// TEST(sidre, TwoD_StateThree_readandwrite) { test_user_defined_data<StateThree, 2>(); }
+
+TEST(sidre, TwoD_StateTensor_readandwrite)
 {
-  test_user_defined_data<StateThree>();
-}
-
-TEST(sidre, QD_StateTensor_readandwrite)
-{
-  test_user_defined_data<StateTensor>();
+  test_user_defined_data<StateTensor, 2>();
 }
 
 //------------------------------------------------------------------------------
 
-TEST(sidre, QD_double_external_readandwrite)
-{
-  test_external_user_defined_data<double>();
-}
+// TEST(sidre, OneD_double_external_readandwrite) { test_external_user_defined_data<double, 1>(); }
 
-TEST(sidre, QD_StateOne_external_readandwrite)
-{
-  test_external_user_defined_data<StateOne>();
-}
+// TEST(sidre, OneD_StateOne_external_readandwrite) { test_external_user_defined_data<StateOne, 1>(); }
+
+// TEST(sidre, OneD_StateTwo_external_readandwrite) { test_external_user_defined_data<StateTwo, 1>(); }
+
+// TEST(sidre, OneD_StateThree_external_readandwrite) { test_external_user_defined_data<StateThree, 1>(); }
+
+// TEST(sidre, OneD_StateTensor_external_readandwrite) { test_external_user_defined_data<StateTensor, 1>(); }
 
 //----------------------------------------------------------------------
 int main(int argc, char* argv[])
