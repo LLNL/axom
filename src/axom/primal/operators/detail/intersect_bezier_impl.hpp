d7311751cfdfd6a332cff0d68720478e64686253
Jacob Spainhour <Jacob.Spainhour@colorado.edu>
2024-12-09 09:27:31 -0700

    Fix tolerances on half-open curves

--- src/axom/primal/operators/detail/intersect_bezier_impl.hpp
+++ src/axom/primal/operators/detail/intersect_bezier_impl.hpp
@@ -298,9 +298,7 @@ bool intersect_ray_bezier(const Ray<T, 2> &r,
 
     // Need to check intersection with zero tolerance
     //  to handle cases where `intersect` treats the ray as collinear
-    bool isIntersect = intersect(r, seg, r0, s0, 0.0);
-    SLIC_INFO("isIntersect: " << isIntersect << " r0: " << r0 << " s0: " << s0);
-    if(intersect(r, seg, r0, s0, 0.0) && s0 < 1.0 - EPS)
+    if(intersect(r, seg, r0, s0, 0.0) && s0 > 0.0 - EPS && s0 < 1.0 - EPS)
     {
       rp.push_back(r0);
       cp.push_back(c_offset + c_scale * s0);
