bd45391146735fcf8c2321776169995072fcf8a6
Brad Whitlock <whitlock2@llnl.gov>
2025-01-27 16:54:51 -0800

    Added headers

--- src/axom/core/utilities/Annotations.cpp
+++ src/axom/core/utilities/Annotations.cpp
@@ -1,3 +1,9 @@
+// Copyright (c) 2017-2025, Lawrence Livermore National Security, LLC and
+// other Axom Project Developers. See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+
+
 #include "Annotations.hpp"
 
 #include "axom/core/utilities/About.hpp"
