33986e3634099636f59d7f23f0123d109923312e
Chris White <white238@llnl.gov>
2025-02-19 18:28:14 -0800

    fix missing link flags when in no fortran case

--- scripts/spack/packages/axom/package.py
+++ scripts/spack/packages/axom/package.py
@@ -330,7 +330,7 @@ class Axom(CachedCMakePackage, CudaPackage, ROCmPackage):
             rocm_root = os.path.dirname(spec["llvm-amdgpu"].prefix)
             entries.append(cmake_cache_path("ROCM_PATH", rocm_root))
 
-            hip_link_flags = ""
+            hip_link_flags = "-L{0}/lib -Wl,-rpath,{0}/lib ".format(rocm_root)
 
             # Recommended MPI flags
             hip_link_flags += "-lxpmem "
@@ -347,7 +347,6 @@ class Axom(CachedCMakePackage, CudaPackage, ROCmPackage):
                     hip_link_flags += "-L{0}/lib/llvm/lib -Wl,-rpath,{0}/lib/llvm/lib ".format(rocm_root)
                 else:
                     hip_link_flags += "-L{0}/llvm/lib -Wl,-rpath,{0}/llvm/lib ".format(rocm_root)
-                hip_link_flags += " -L{0}/lib -Wl,-rpath,{0}/lib ".format(rocm_root)
                 hip_link_flags += "-lpgmath -lflang -lflangrti -lompstub "
 
             # Remove extra link library for crayftn
