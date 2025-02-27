95b3710b093f1c9551d47fc8996d693743bd265e
Jacob Spainhour <Jacob.Spainhour@colorado.edu>
2025-02-27 14:18:19 -0700

    Add new test

--- src/axom/primal/tests/primal_trimming_curves.cpp
+++ src/axom/primal/tests/primal_trimming_curves.cpp
@@ -375,6 +375,62 @@ TEST_F(TrimmingCurveTest, knot_vector_manipulation)
   }
 }
 
+//------------------------------------------------------------------------------
+TEST_F(TrimmingCurveTest, trimming_disk_subdivision)
+{
+  SLIC_INFO(
+    "Testing disk subdivision of the curve");
+
+  NURBSPatchType nPatch(this->nPatch);
+  NURBSPatchType the_disk, the_rest;
+
+  // Apply the simplest subdivision strategy
+  nPatch.diskSplit(0.5, 0.5, 0.25, the_disk, the_rest, false);
+
+  constexpr int npts = 11;
+  double u_pts[npts], v_pts[npts];
+  axom::numerics::linspace(the_disk.getMinKnot_u(), the_disk.getMaxKnot_u(), u_pts, npts);
+  axom::numerics::linspace(the_disk.getMinKnot_v(), the_disk.getMaxKnot_v(), v_pts, npts);
+
+  for(auto u : u_pts)
+  {
+    for(auto v : v_pts)
+    {
+      bool isPointVisible = the_disk.isVisible(u, v);
+
+      if( ( u - 0.5 ) * ( u - 0.5 ) + ( v - 0.5 ) * ( v - 0.5 ) < 0.25 * 0.25 )
+      {
+        EXPECT_TRUE(isPointVisible);
+      }
+      else
+      {
+        EXPECT_FALSE(isPointVisible);
+      }
+    }
+  }
+
+  axom::numerics::linspace(the_rest.getMinKnot_u(), the_rest.getMaxKnot_u(), u_pts, npts);
+  axom::numerics::linspace(the_rest.getMinKnot_v(), the_rest.getMaxKnot_v(), v_pts, npts);
+
+  for(auto u : u_pts)
+  {
+    for(auto v : v_pts)
+    {
+      bool isPointVisible = the_rest.isVisible(u, v);
+      std::cout << u << " " << v << " " << isPointVisible << std::endl;
+
+      if( ( u - 0.5 ) * ( u - 0.5 ) + ( v - 0.5 ) * ( v - 0.5 ) < 0.25 * 0.25 )
+      {
+        EXPECT_FALSE(isPointVisible);
+      }
+      else
+      {
+        EXPECT_TRUE(isPointVisible);
+      }
+    }
+  }
+}
+
 int main(int argc, char* argv[])
 {
   int result = 0;
