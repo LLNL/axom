83a80c9648a79a7a78d9faa3750af2cbba044aee
Kenny Weiss <kenny@kennyweiss.com>
2025-03-12 19:02:20 -0700

    Fixes another `constexpr->static constexpr` for lambdas in msvc

--- src/axom/quest/util/mesh_helpers.cpp
+++ src/axom/quest/util/mesh_helpers.cpp
@@ -516,8 +516,8 @@ void convert_blueprint_structured_explicit_to_unstructured_impl_2d(
   const std::string& topoName)
 {
   AXOM_ANNOTATE_SCOPE("convert_to_unstructured");
-  constexpr int DIM = 2;
-  constexpr int NUM_VERTS_PER_QUAD = 4;
+  static constexpr int DIM = 2;
+  static constexpr int NUM_VERTS_PER_QUAD = 4;
 
   const std::string& coordsetName =
     meshGrp->getView("topologies/" + topoName + "/coordset")->getString();
