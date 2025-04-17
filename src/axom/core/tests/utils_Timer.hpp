51f3b4364dadde5220e7d16b3fee0ff281e1ebdb
Brian T.N. Gunney <gunney1@llnl.gov>
2025-04-17 12:48:42 -0700

    Autoformat.

--- src/axom/core/tests/utils_Timer.hpp
+++ src/axom/core/tests/utils_Timer.hpp
@@ -71,8 +71,10 @@ TEST(utils_Timer, timer_check_sum)
     t2.stop();
   }
 
-  std::cout << "t1 measured: " << t1.elapsed() << "s in " << t1.cycleCount() << " cycles" << std::endl;
-  std::cout << "t2 measured: " << t2.elapsed() << "s in " << t2.cycleCount() << " cycles" << std::endl;
+  std::cout << "t1 measured: " << t1.elapsed() << "s in " << t1.cycleCount() << " cycles"
+            << std::endl;
+  std::cout << "t2 measured: " << t2.elapsed() << "s in " << t2.cycleCount() << " cycles"
+            << std::endl;
 
   EXPECT_EQ(t1.cycleCount(), N);
   EXPECT_EQ(t2.cycleCount(), N);
